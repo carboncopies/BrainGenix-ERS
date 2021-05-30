2181009047f187002d6dd70edbb81f68fb32c486
Thomas Liao <tliao@carboncopies.org>
2021-05-30 16:14:45 -0700

    Create Main C++ File

---
+++ System/main.cpp
@@ -0,0 +1,26 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file is responsible for implementing the main server-side binary of the system.
+    Date Created: 2021-05-30
+*/
+
+
+#include <iostream>
+
+
+#include "Core/LocalWindowSystem/LocalWindowSystemModule.cpp"
+
+
+int main(){
+
+    std::cout << "Test\n";
+    
+
+    std::cout << test();
+    std::cout << "\n";
+
+    return 0;
+}
