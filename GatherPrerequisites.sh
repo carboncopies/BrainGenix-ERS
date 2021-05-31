5e5ca22eb5e29dda0135ff9d15807dfdaf0c44bf
Thomas Liao <tliao@carboncopies.org>
2021-05-31 01:29:00 -0700

    Add Configration Loading

---
+++ GatherPrerequisites.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+# Install G++ #
+echo "Installing G++"
+sudo apt install -y g++
+
+# Install Cmake #
+echo "Installing CMake"
+sudo apt install -y cmake
+
+# Install Other Libs #
+echo "Installing Other Libraries"
+sudo apt-get install -y libyaml-cpp-dev
