36b5ebf2fa3d7d10bb8837a231c415858b692e13
Thomas Liao <tliao@localhost.localdomain>
2021-06-01 19:05:13 -0400

    Create LocaLVisualRendering Config

--- System/Core/Rendering/Visual/VisualRenderingMainModule/VisualRenderingMainModule.cpp
+++ System/Core/Rendering/Visual/VisualRenderingMainModule/VisualRenderingMainModule.cpp
@@ -14,6 +14,9 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <string>
+#include <cstring>
+
 
 class VisualRenderingPipeline {
 
@@ -30,9 +33,35 @@ class VisualRenderingPipeline {
     // Define Externally Accessible Objects //
     public:
 
-        void InitializeConfiguration() {
+        void InitializeConfiguration(LoggerClass Logger, const YAML::Node VisualRenderingConfiguration) {
+
+            // TODO: CONFIGURATION DICTIONARY TELLING IT WHAT TO DO ABOUT LOCAL WINDOWS AND OTHER STUFF LIKE THAT.
+            // Initialization Message //
+            Logger.Log("Initializing Visual Rendering Pipeline");
+
+            // Decode Configuration Dictionary //
+            Logger.Log("Loading Configuration File For Visual Configuration Pipeline");
+
+            LocalWindowEnabled = VisualRenderingConfiguration["LocalWindowEnabled"].as<bool>();
+            WindowResolutionX = VisualRenderingConfiguration["WindowResolutionX"].as<int>();
+            WindowResolutionY = VisualRenderingConfiguration["WindowResolutionY"].as<int>();
+
+
+            // IS THERE A WAY TO FIX THIS???!?!?!?!? //
+            // Log Config Params //
+            //Logger.Log("Local Window Configuration:", 0);
+            //Logger.Log("CONFIG: LocalWindowEnabled" + std::string(LocalWindowEnabled).c_str(), 0);
+            //Logger.Log("CONFIG: LocalWindowResolutionX" + std::to_string(WindowResolutionX), 0);
+            //Logger.Log("CONFIG: LocalWindowResolutionY" + std::to_string(WindowResolutionY), 0);
+            
+
+            // If LocalWindow Is Enabled, Create A Local Window //
+            if (LocalWindowEnabled) {
+                
+            }
 
         }
+
 
         void InitVulkan() {
 
