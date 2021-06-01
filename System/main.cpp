0acd3ace8c6029097656d939de6f5dbeec840b7a
Thomas Liao <tliao@carboncopies.org>
2021-05-31 19:31:28 -0700

    Fix YAML Loading Issues

--- System/main.cpp
+++ System/main.cpp
@@ -22,16 +22,33 @@
 
 int main(){
 
+
     // Load Configration File For Logger //
+    std::cout << "Initializing System\n";
+    std::cout << "Loading Logger Configuration File\n";
     const YAML::Node LoggerConfigration = LoadConfig("Config/LoggerConfig.yaml");
 
-    // Instantiate
+
+    // Instantiate Logger //
+    std::cout << "Instantiating Logging System\n";
     Logger mLogger;
     mLogger.InitializeLogger();
 
-    mLogger.Log("Test", 0);
+    mLogger.Log("Logger Instantiation Successfull", 0);
+
+
+    // Load Remaining Configuration Files //
+    mLogger.Log("Loading Remaining Configuration Files");
+
+    mLogger.Log("Loading WindowSystem Configuration File");
+    const YAML::Node LocalWindowConfiguration = LoadConfig("Config/WindowSystemConfig.yaml");
+
+    mLogger.Log("Done Loading Configuration Files");
 
 
 
+
+
+    // Program Exit //
     return 0;
 }
