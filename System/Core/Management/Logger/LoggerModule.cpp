e2c4213927d4d54686fd4f5464be4de3ef5999a3
J-French1 <85071286+J-French1@users.noreply.github.com>
2021-06-20 15:34:49 -0500

    Update LoggerModule.cpp

--- System/Core/Management/Logger/LoggerModule.cpp
+++ System/Core/Management/Logger/LoggerModule.cpp
@@ -58,8 +58,8 @@ class LoggerClass {
         void InitializeLogger(YAML::Node LoggerConfiguration) { // ** NOTE: THIS SHOULD TAKE A CONFIG DICT FROM YAML IN THE FUTRE ** //
 
             // Update Local Config Parameters //
-            PrintLogOutput = (bool) (LoggerConfiguration["EnablePrintOutput"]);
-            MinimumLogLevel = (int) (LoggerConfiguration["SetMinimumLogLevel"]);
+            PrintLogOutput = LoggerConfiguration["EnablePrintOutput"].as<bool>();
+            MinimumLogLevel = LoggerConfiguration["SetMinimumLogLevel"].as<int>();
 
             // Print Log Key //
             if (PrintLogOutput) {
