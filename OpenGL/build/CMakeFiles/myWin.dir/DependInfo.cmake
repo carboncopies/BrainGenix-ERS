e555e64134466db5ece804876104f58639cf2c9a
datacrystals <tliao@carboncopies.org>
2021-09-21 19:00:44 -0700

    Testing OpenGL Cmake Build Config

---
+++ OpenGL/build/CMakeFiles/myWin.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/tliao/Documents/BrainGenix-ERS/OpenGL/src/glad.c" "/home/tliao/Documents/BrainGenix-ERS/OpenGL/build/CMakeFiles/myWin.dir/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  ".././include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tliao/Documents/BrainGenix-ERS/OpenGL/src/window.cpp" "/home/tliao/Documents/BrainGenix-ERS/OpenGL/build/CMakeFiles/myWin.dir/src/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
