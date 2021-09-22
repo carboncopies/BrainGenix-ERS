56e046cb78f480990c5b4592f5f00d6c7c2ac95c
datacrystals <tliao@carboncopies.org>
2021-09-21 22:51:35 -0700

    Add GLAD Subsystem

--- OpenGL/Source/Core/Initialization/GLADModule/GLADModule.cpp
+++ OpenGL/Source/Core/Initialization/GLADModule/GLADModule.cpp
@@ -3,7 +3,25 @@
 //======================================================================//
 
 /*
-    Description: This file is responsible for implementing GLFW features.
+    Description: This file is responsible for implementing GLAD Function Pointers.
     Additonal Notes: None
     Date Created: 2021-09-21
 */  
+
+#include <stdlib.h>
+
+void GLADInitialization(LoggerClass LoggingSystem) {
+
+    // Log GLAD Start //
+    LoggingSystem.Log("Initializing GLAD Subsystem", 4);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        LoggingSystem.Log("Failed to initialize GLAD, System Exiting", 9);
+        exit(-1);
+    }
+
+    // Log Initialization Successfull //
+    LoggingSystem.Log("GLAD Subsystem Initialization Successfull", 3)
+    return;
+}
