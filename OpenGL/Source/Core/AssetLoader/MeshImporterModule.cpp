90ce7f0b81b650642e1b7816caa87173dc35ae98
datacrystals <tliao@carboncopies.org>
2021-09-22 23:29:39 -0700

    Add Demo Square

---
+++ OpenGL/Source/Core/AssetLoader/MeshImporterModule.cpp
@@ -0,0 +1,18 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file is responsible for model importing.
+    Additonal Notes: None
+    Date Created: 2021-09-21
+*/  
+
+
+struct Vertex {
+
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+
+};
