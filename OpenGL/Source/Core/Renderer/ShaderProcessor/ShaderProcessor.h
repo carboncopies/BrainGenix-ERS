5c52ff4c4efa8e0a0e49d38a4751578b9116c9eb
datacrystals <tliao@carboncopies.org>
2021-09-23 10:38:01 -0700

    Add GLM To Processor

--- OpenGL/Source/Core/Renderer/ShaderProcessor/ShaderProcessor.h
+++ OpenGL/Source/Core/Renderer/ShaderProcessor/ShaderProcessor.h
@@ -13,7 +13,8 @@
 #define SHADER_H
 
 #include <glad/glad.h> // include glad to get all the required OpenGL headers
-  
+#include <glm/glm.hpp>
+
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -110,22 +111,69 @@ class Shader
         }
 
 
+        // activate the shader
+        // ------------------------------------------------------------------------
         void use() 
         { 
-            glUseProgram(ID);
-        }  
-
+            glUseProgram(ID); 
+        }
+        // utility uniform functions
+        // ------------------------------------------------------------------------
         void setBool(const std::string &name, bool value) const
         {         
             glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
         }
+        // ------------------------------------------------------------------------
         void setInt(const std::string &name, int value) const
         { 
             glUniform1i(glGetUniformLocation(ID, name.c_str()), value); 
         }
+        // ------------------------------------------------------------------------
         void setFloat(const std::string &name, float value) const
         { 
             glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
+        }
+        // ------------------------------------------------------------------------
+        void setVec2(const std::string &name, const glm::vec2 &value) const
+        { 
+            glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); 
+        }
+        void setVec2(const std::string &name, float x, float y) const
+        { 
+            glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y); 
+        }
+        // ------------------------------------------------------------------------
+        void setVec3(const std::string &name, const glm::vec3 &value) const
+        { 
+            glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); 
+        }
+        void setVec3(const std::string &name, float x, float y, float z) const
+        { 
+            glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z); 
+        }
+        // ------------------------------------------------------------------------
+        void setVec4(const std::string &name, const glm::vec4 &value) const
+        { 
+            glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); 
+        }
+        void setVec4(const std::string &name, float x, float y, float z, float w) 
+        { 
+            glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w); 
+        }
+        // ------------------------------------------------------------------------
+        void setMat2(const std::string &name, const glm::mat2 &mat) const
+        {
+            glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+        }
+        // ------------------------------------------------------------------------
+        void setMat3(const std::string &name, const glm::mat3 &mat) const
+        {
+            glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+        }
+        // ------------------------------------------------------------------------
+        void setMat4(const std::string &name, const glm::mat4 &mat) const
+        {
+            glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
         } 
 
 
