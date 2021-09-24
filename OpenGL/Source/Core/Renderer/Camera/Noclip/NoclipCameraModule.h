fff8257466b0799ec80e7da381d357339b55d9d3
datacrystals <tliao@carboncopies.org>
2021-09-23 23:15:58 -0700

    Fix Camera Movement Issues

--- OpenGL/Source/Core/Renderer/Camera/Noclip/NoclipCameraModule.h
+++ OpenGL/Source/Core/Renderer/Camera/Noclip/NoclipCameraModule.h
@@ -81,6 +81,7 @@ public:
     // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
     void ProcessKeyboard(Camera_Movement direction, float deltaTime)
     {
+        std::cout << "camera";
         float velocity = MovementSpeed * deltaTime;
         if (direction == FORWARD)
             Position += Front * velocity;
