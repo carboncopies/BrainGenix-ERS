e88b469d8d949d713a64c450711bb55ec46dae49
datacrystals <tliao@carboncopies.org>
2021-09-23 19:38:45 -0700

    Move System Input SystemInputModule

--- OpenGL/Source/Core/SystemInput/WindowInput/WindowInputModule.cpp
+++ OpenGL/Source/Core/SystemInput/WindowInput/WindowInputModule.cpp
@@ -11,24 +11,89 @@
 
 #include "Core/Renderer/Camera/Noclip/NoclipCameraModule.h"
 
-void ProcessWindowInput(GLFWwindow *Window, Camera Camera) {
 
-    float DeltaTime = 0.1;
+class WindowInputClass {
 
-    // Set Escape Key To Exit
-    if (glfwGetKey(Window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {        
-        glfwSetWindowShouldClose(Window, true);
-    }
+    // Declare Variables
+    float ScreenWidth;
+    float ScreenHeight;
+    GLFWwindow *Window;
+    Camera CameraInstance;
+    
+    float lastX;
+    float lastY;
+    
 
-    if (glfwGetKey(Window, GLFW_KEY_W) == GLFW_PRESS)
-        Camera.ProcessKeyboard(FORWARD, DeltaTime);
-    if (glfwGetKey(Window, GLFW_KEY_S) == GLFW_PRESS)
-        Camera.ProcessKeyboard(BACKWARD, DeltaTime);
-    if (glfwGetKey(Window, GLFW_KEY_A) == GLFW_PRESS)
-        Camera.ProcessKeyboard(LEFT, DeltaTime);
-    if (glfwGetKey(Window, GLFW_KEY_D) == GLFW_PRESS)
-        Camera.ProcessKeyboard(RIGHT, DeltaTime);
+    // Initialization Parameters
+    bool firstMouse = true;
 
 
+    public:
 
-}
+        void WindowInputClass(float ScreenWidthInput, float ScreenHeightInput, GLFWwindow *WindowInput, Camera CameraInput) {
+
+            // Define Variables
+            ScreenWidth = ScreenWidthInput;
+            ScreenHeight = ScreenHeightInput;
+
+            Window = WindowInput;
+            CameraInstance = CameraInput;
+
+            
+            // Set Mouse Window Definitions
+            lastX = ScreenWidth / 2.0f;
+            lastY = ScreenHeight / 2.0f;
+
+
+        }
+
+        void ProcessWindowInput(float DeltaTime) {
+
+            // Set Escape Key To Exit
+            if (glfwGetKey(Window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {        
+                glfwSetWindowShouldClose(Window, true);
+            }
+
+            if (glfwGetKey(Window, GLFW_KEY_W) == GLFW_PRESS)
+                CameraInstance.ProcessKeyboard(FORWARD, DeltaTime);
+            if (glfwGetKey(Window, GLFW_KEY_S) == GLFW_PRESS)
+                CameraInstance.ProcessKeyboard(BACKWARD, DeltaTime);
+            if (glfwGetKey(Window, GLFW_KEY_A) == GLFW_PRESS)
+                CameraInstance.ProcessKeyboard(LEFT, DeltaTime);
+            if (glfwGetKey(Window, GLFW_KEY_D) == GLFW_PRESS)
+                CameraInstance.ProcessKeyboard(RIGHT, DeltaTime);
+
+        }
+
+
+        //glfw: whenever the mouse moves, this callback is called
+        //-------------------------------------------------------
+        void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+        {
+            if (firstMouse)
+            {
+                lastX = xpos;
+                lastY = ypos;
+                firstMouse = false;
+            }
+
+            float xoffset = xpos - lastX;
+            float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+            lastX = xpos;
+            lastY = ypos;
+
+            CameraInstance.ProcessMouseMovement(xoffset, yoffset);
+        }
+
+
+
+        //glfw: whenever the mouse scroll wheel scrolls, this callback is called
+        //----------------------------------------------------------------------
+        void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+        {
+            CameraInstance.ProcessMouseScroll(yoffset);
+        }
+
+
+};
