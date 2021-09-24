97958f5b6f028375becf3039e1e47478318f36ca
datacrystals <tliao@carboncopies.org>
2021-09-24 13:49:45 -0700

    Add Specular Lighting To Shaders

--- OpenGL/Source/Core/Renderer/RenderingLoop/RenderLoopModule.cpp
+++ OpenGL/Source/Core/Renderer/RenderingLoop/RenderLoopModule.cpp
@@ -200,14 +200,29 @@ void MainRenderLoop(GLFWwindow* Window, LoggerClass Logger) {
 
         ////-------- RENDERING CODE HERE --------////
 
-        glm::vec4 LIGHTCOL = glm::vec4(1.0f, cos(glfwGetTime()), sin(glfwGetTime()), 0.0f);
+        glm::vec4 LIGHTCOL = glm::vec4(1.0f);//, cos(glfwGetTime()), sin(glfwGetTime()), 0.0f);
 
-        // be sure to activate shader when setting uniforms/drawing objects
+// be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
-        lightingShader.setVec3("objectColor", 1.0f, 1.0f, 1.0f);
-        lightingShader.setVec3("lightColor", glm::vec3(LIGHTCOL));
-        lightingShader.setVec3("lightPos", lightPos);
-        lightingShader.setVec3("viewPos", CameraInstance.Position); 
+        lightingShader.setVec3("light.position", lightPos);
+        lightingShader.setVec3("viewPos", CameraInstance.Position);
+
+        // light properties
+        glm::vec3 lightColor;
+        lightColor.x = sin(glfwGetTime() * 2.0f);
+        lightColor.y = sin(glfwGetTime() * 0.7f);
+        lightColor.z = sin(glfwGetTime() * 1.3f);
+        glm::vec3 diffuseColor = lightColor   * glm::vec3(0.5f); // decrease the influence
+        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); // low influence
+        lightingShader.setVec3("light.ambient", ambientColor);
+        lightingShader.setVec3("light.diffuse", diffuseColor);
+        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
+        // material properties
+        lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f); // specular lighting doesn't have full effect on this object's material
+        lightingShader.setFloat("material.shininess", 32.0f); 
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(CameraInstance.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -233,9 +248,9 @@ void MainRenderLoop(GLFWwindow* Window, LoggerClass Logger) {
         lightCubeShader.setVec4("LightColor", LIGHTCOL);
 
 
-        lightPos.x = sin(glfwGetTime()) * 2;
-        lightPos.z = cos(glfwGetTime()) * 2;
-        lightPos.y = cos(glfwGetTime()/3);
+        //lightPos.x = sin(glfwGetTime()) * 2;
+        //lightPos.z = cos(glfwGetTime()) * 2;
+        //lightPos.y = cos(glfwGetTime()/3);
 
         model = glm::mat4(1.0f);
         model = glm::translate(model, lightPos);
