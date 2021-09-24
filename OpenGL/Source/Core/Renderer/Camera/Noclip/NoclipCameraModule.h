81e5a958682ed429492ca3410a92b2dc4631bc4a
datacrystals <tliao@carboncopies.org>
2021-09-23 23:26:38 -0700

    Fix Camera Movement Issues

--- OpenGL/Source/Core/Renderer/Camera/Noclip/NoclipCameraModule.h
+++ OpenGL/Source/Core/Renderer/Camera/Noclip/NoclipCameraModule.h
@@ -18,14 +18,10 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 #include <vector>
+#include <iostream>
+#include <iomanip>
 
-// Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
-enum Camera_Movement {
-    FORWARD,
-    BACKWARD,
-    LEFT,
-    RIGHT
-};
+
 
 // Default camera values
 const float YAW         = -90.0f;
@@ -79,17 +75,20 @@ public:
     }
 
     // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
-    void ProcessKeyboard(Camera_Movement direction, float deltaTime)
+    void ProcessKeyboard(const char* direction, float deltaTime)
     {
-        std::cout << "camera\n";
+
+        std::cout << std::setprecision(2) << deltaTime;
+        std::cout << direction << "\n";
+
         float velocity = MovementSpeed * deltaTime;
-        if (direction == FORWARD)
+        if (direction == "FORWARD")
             Position += Front * velocity;
-        if (direction == BACKWARD)
+        if (direction == "BACKWARD")
             Position -= Front * velocity;
-        if (direction == LEFT)
+        if (direction == "LEFT")
             Position -= Right * velocity;
-        if (direction == RIGHT)
+        if (direction == "RIGHT")
             Position += Right * velocity;
     }
 
