45bdb2692f3f2cbc54aa42cde5fbff15c7fb1d07
datacrystals <tliao@carboncopies.org>
2021-09-24 13:40:30 -0700

    Add Phong Lighting

--- OpenGL/Source/Shaders/light_cube.fs
+++ OpenGL/Source/Shaders/light_cube.fs
@@ -1,7 +1,10 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec4 lightColor;
+
 void main()
 {
-    FragColor = vec4(1.0); // set alle 4 vector values to 1.0
+    FragColor = lightColor;//vec4(1.0, 0.5, 0.5, 1.0); // set all 4 vector values to 1.0
+
 } 
