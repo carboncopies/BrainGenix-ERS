268d86065dd67d5cfc0c5ced4c19e79d039131b0
datacrystals <tliao@carboncopies.org>
2021-09-23 18:14:41 -0700

    Created 3D Cube Test

--- OpenGL/Source/Core/SystemInput/WindowInput/WindowInputModule.cpp
+++ OpenGL/Source/Core/SystemInput/WindowInput/WindowInputModule.cpp
@@ -8,13 +8,27 @@
     Date Created: 2021-09-22
 */
 
-void ProcessWindowInput(GLFWwindow *Window) {
+
+#include "Core/Renderer/Camera/Noclip/NoclipCameraModule.h"
+
+void ProcessWindowInput(GLFWwindow *Window, Camera Camera) {
+
+    float DeltaTime = 0.1;
 
     // Set Escape Key To Exit
-    if (glfwGetKey(Window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        
+    if (glfwGetKey(Window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {        
         glfwSetWindowShouldClose(Window, true);
-
     }
+
+    if (glfwGetKey(Window, GLFW_KEY_W) == GLFW_PRESS)
+        Camera.ProcessKeyboard(FORWARD, DeltaTime);
+    if (glfwGetKey(Window, GLFW_KEY_S) == GLFW_PRESS)
+        Camera.ProcessKeyboard(BACKWARD, DeltaTime);
+    if (glfwGetKey(Window, GLFW_KEY_A) == GLFW_PRESS)
+        Camera.ProcessKeyboard(LEFT, DeltaTime);
+    if (glfwGetKey(Window, GLFW_KEY_D) == GLFW_PRESS)
+        Camera.ProcessKeyboard(RIGHT, DeltaTime);
+
+
 
 }
