cef82866606cd3131c229e39503a695a3c47e75a
datacrystals <tliao@carboncopies.org>
2021-09-24 23:04:22 -0700

    Add Model Loading

--- OpenGL/Source/Core/AssetLoader/TextureLoaderModule.cpp
+++ OpenGL/Source/Core/AssetLoader/TextureLoaderModule.cpp
@@ -11,44 +11,63 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+#include <string>
+#include <sstream>
 
 
-unsigned int LoadTexture (const char* ImagePath, LoggerClass Logger) {
+unsigned int LoadTexture (LoggerClass Logger, const char* ImagePath, std::string &Directory) {
 
+
+    // Update Filepath
+    const char* Filename = ImagePath;
+    const char* FilePath = std::string(std::string(Directory) + "/" + std::string(Filename)).c_str();
 
     // Initialize GL Textures
-    unsigned int Texture;
-    glGenTextures(1, &Texture);
-    glBindTexture(GL_TEXTURE_2D, Texture);
+    unsigned int TextureID;
+    glGenTextures(1, &TextureID);
+    glBindTexture(GL_TEXTURE_2D, TextureID);
 
-    // Set Texture Wrapping Params
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    // Set Texture Filter Params
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // Flip Texture
+    stbi_set_flip_vertically_on_load(true);
 
     // Load Image
     Logger.Log(std::string("Loading Texture Image '" + std::string(ImagePath) + "'").c_str(), 2);
     int width, height, nrChannels;
-    unsigned char *data = stbi_load(ImagePath, &width, &height, &nrChannels, 0);
+    unsigned char *Data = stbi_load(FilePath, &width, &height, &nrChannels, 0);
 
     // If Image Loaded
-    if (data) {
+    if (Data) {
 
-        // Push To OpenGL
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        // Check Image Format
+        GLenum format;
+        if (nrChannels == 1)
+            format = GL_RED;
+        else if (nrChannels == 3)
+            format = GL_RGB;
+        else if (nrChannels == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, TextureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, Data);
         glGenerateMipmap(GL_TEXTURE_2D);
+
+        // Set Texture Wrapping Params
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+        // Set Texture Filter Params
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
 
     } else {
         Logger.Log("Texture Image Load/Decode Failure", 8);
     }
 
     // Free Image Source Memory
-    stbi_image_free(data);
+    stbi_image_free(Data);
 
-    // Return Texture
-    return Texture;
+    // Return Texture ID
+    return TextureID;
 
 }
