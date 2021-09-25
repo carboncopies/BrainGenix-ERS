0f517dc84968e2e0bba2468671f816f7761e96d3
datacrystals <tliao@carboncopies.org>
2021-09-24 23:04:17 -0700

    Add Model Loading

--- OpenGL/Source/Core/AssetLoader/Model.h
+++ OpenGL/Source/Core/AssetLoader/Model.h
@@ -14,11 +14,12 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+
 #include <glad/glad.h> 
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <stb_image.h>
+
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
@@ -35,7 +36,9 @@
 #include <vector>
 using namespace std;
 
-unsigned int TextureFromFile(const char *path, const string &directory, bool gamma = false);
+
+
+
 
 class Model 
 {
@@ -45,15 +48,18 @@ public:
     vector<Mesh>    meshes;
     string directory;
     bool gammaCorrection;
+    LoggerClass Logger;
 
-    // constructor, expects a filepath to a 3D model.
-    Model(string const &path, bool gamma = false) : gammaCorrection(gamma)
+    // Model Loading Constructor
+    Model(LoggerClass Logger, string const &Path, bool gamma = false) : gammaCorrection(gamma)
     {
-        loadModel(path);
+        // Log Model Loading
+        Logger.Log(std::string("Loading Model" + std::string(Path)).c_str(), 3);
+        loadModel(Path);
     }
 
-    // draws the model, and thus all its meshes
-    void Draw(Shader &shader)
+    // Render Model
+    void Render(Shader &shader)
     {
         for(unsigned int i = 0; i < meshes.size(); i++)
             meshes[i].Draw(shader);
@@ -204,8 +210,11 @@ private:
             }
             if(!skip)
             {   // if texture hasn't been loaded already, load it
+
+                std::cout << "test\n";
+
                 Texture texture;
-                texture.id = TextureFromFile(str.C_Str(), this->directory);
+                texture.id = LoadTexture(Logger, str.C_Str(), this->directory);
                 texture.type = typeName;
                 texture.path = str.C_Str();
                 textures.push_back(texture);
@@ -216,45 +225,4 @@ private:
     }
 };
 
-
-unsigned int TextureFromFile(const char *path, const string &directory, bool gamma)
-{
-    string filename = string(path);
-    filename = directory + '/' + filename;
-
-    unsigned int textureID;
-    glGenTextures(1, &textureID);
-
-    int width, height, nrComponents;
-    unsigned char *data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
-    if (data)
-    {
-        GLenum format;
-        if (nrComponents == 1)
-            format = GL_RED;
-        else if (nrComponents == 3)
-            format = GL_RGB;
-        else if (nrComponents == 4)
-            format = GL_RGBA;
-
-        glBindTexture(GL_TEXTURE_2D, textureID);
-        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        stbi_image_free(data);
-    }
-    else
-    {
-        std::cout << "Texture failed to load at path: " << path << std::endl;
-        stbi_image_free(data);
-    }
-
-    return textureID;
-}
 #endif
-
