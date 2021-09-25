291cf9ec1aa08f6209b8713fe7a514d1a237596f
datacrystals <tliao@carboncopies.org>
2021-09-24 21:04:32 -0700

    Add Asset Importing Library

---
+++ OpenGL/Source/Core/AssetLoader/ModelLoader.cpp
@@ -0,0 +1,9 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file is responsible for model importing.
+    Additonal Notes: None
+    Date Created: 2021-09-24
+*/   
