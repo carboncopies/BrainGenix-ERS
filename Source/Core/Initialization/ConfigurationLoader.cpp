942c5ed5db780a3104b8c6ab8119ef92c5a376c5
datacrystals <tliao@carboncopies.org>
2021-09-28 19:56:20 -0700

    Create Config Loader

---
+++ Source/Core/Initialization/ConfigurationLoader.cpp
@@ -0,0 +1,11 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file loads the local system configuration file.
+    Additonal Notes: None
+    Date Created: 2021-09-28
+*/
+
+#include "yaml.h"
