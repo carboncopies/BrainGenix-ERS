94e63e7aea40623ea463c7eafd1eb7ae077a0819
datacrystals <tliao@carboncopies.org>
2021-09-29 14:36:40 -0700

    Create Graphcis Init Function

--- Source/Core/Initialization/GraphicsInitialization.cpp
+++ Source/Core/Initialization/GraphicsInitialization.cpp
@@ -20,6 +20,7 @@ class GraphicsSubsystem {
     // Declare Vars
     YAML::Node SystemConfiguration;
     LoggerClass Logger;
+    GLFWwindow* Window;
 
     public:
 
@@ -34,13 +35,23 @@ class GraphicsSubsystem {
             Logger.Log("Initializng Graphics Subsystem", 3);
 
             // Initialize Local Window
-            Logger.Log("Running Local Window Init", 3)
+            Logger.Log("Running Local Window Init", 3);
             Logger.Log("Checking Config File For: 'WindowEnabled' Parameter", 2);
 
             const bool LocalWindowEnabled = SystemConfiguration["WindowEnabled"].as<bool>();
 
-            Logger.Log(std::string("Found 'WindowEnabled' Parameter To Be: " + std::string(LocalWindowEnabled)).c_str(), 1);
+            if (LocalWindowEnabled == true) {
 
+                // Log Window Creation
+                Logger.Log("Local Window Enabled, Creating GLFW Window", 3);
+
+                // read glfw config for resolution, log it
+
+                // create window
+                glfwInit();
+                Window = glfwCreateWindow()
+
+            }
 
         }
 
