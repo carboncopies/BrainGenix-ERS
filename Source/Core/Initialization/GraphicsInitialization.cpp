2788d81ee726dc5d10a9b2f7d0ccacc77e81a41e
datacrystals <tliao@carboncopies.org>
2021-09-29 14:26:40 -0700

    Create Graphical Initialization System

--- Source/Core/Initialization/GraphicsInitialization.cpp
+++ Source/Core/Initialization/GraphicsInitialization.cpp
@@ -8,7 +8,9 @@
     Date Created: 2021-05-31
 */ 
 
-#include "IO/WindowManager.cpp"
+#include <string>
+
+#include "Core/IO/WindowManager.cpp"
 
 
 class GraphicsSubsystem {
@@ -25,10 +27,20 @@ class GraphicsSubsystem {
         void InitializeGraphics(YAML::Node SystemConfigurationDict, LoggerClass LoggerInstance) {
 
             // Create Pointers To SystemConfig and Logger
-            SystemConfiguration = SYstemConfigurationDict;
+            SystemConfiguration = SystemConfigurationDict;
             Logger = LoggerInstance;
 
             // Log Graphics Init
+            Logger.Log("Initializng Graphics Subsystem", 3);
+
+            // Initialize Local Window
+            Logger.Log("Running Local Window Init", 3)
+            Logger.Log("Checking Config File For: 'WindowEnabled' Parameter", 2);
+
+            const bool LocalWindowEnabled = SystemConfiguration["WindowEnabled"].as<bool>();
+
+            Logger.Log(std::string("Found 'WindowEnabled' Parameter To Be: " + std::string(LocalWindowEnabled)).c_str(), 1);
+
 
         }
 
