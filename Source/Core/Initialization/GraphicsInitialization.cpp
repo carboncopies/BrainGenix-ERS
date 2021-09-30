2827ee560adfb0d69ca5775c5debf3bae1f981a8
datacrystals <tliao@carboncopies.org>
2021-09-29 22:14:01 -0700

    Fix PlatformData Configuration

--- Source/Core/Initialization/GraphicsInitialization.cpp
+++ Source/Core/Initialization/GraphicsInitialization.cpp
@@ -13,14 +13,16 @@
 #include <bgfx/bgfx.h>
 
 
-
-#if BX_PLATFORM_LINUX || BX_PLATFORM_BSD 
-    #define GLFW_EXPOSE_NATIVE_X11
-#else
-    #define GLFW_EXPOSE_NATIVE_WIN32
-#endif
-
 #include <GLFW/glfw3.h>
+//#if BX_PLATFORM_LINUX || BX_PLATFORM_BSD 
+#define GLFW_EXPOSE_NATIVE_X11
+#define GLFW_EXPOSE_NATIVE_GLX
+//#else
+//    #define GLFW_EXPOSE_NATIVE_WIN32
+//#endif
+#include <GLFW/glfw3native.h>
+
+
 
 
 class GraphicsSubsystem {
@@ -91,35 +93,38 @@ class GraphicsSubsystem {
                 bgfx::PlatformData PlatformDataInstance;
 
 
-                // Magic Platform Instance Stuff
-                #if BX_PLATFORM_LINUX || BX_PLATFORM_BSD 
-                
-                    // Add Nice Linux Message :)
-                    Logger.Log("Detected Linux/BSD Platform - Far Better Than Windows/OSX", 3);
+                PlatformDataInstance.ndt      = glfwGetX11Display(); 
+                PlatformDataInstance.nwh      = (void*)glfwGetX11Window(Window);
 
-                    #if ENTRY_CONFIG_USE_WAYLAND
-                        PlatformDataInstance.ndt      = glfwGetWaylandDisplay(); 
-                    #else 
-                        PlatformDataInstance.ndt      = glfwGetX11Display(); 
-                        PlatformDataInstance.nwh      = (void*)glfwGetX11Window(Window);
-                    #endif 
+                // // Magic Platform Instance Stuff
+                // #if BX_PLATFORM_LINUX || BX_PLATFORM_BSD 
+                
+                //     // Add Nice Linux Message :)
+                //     Logger.Log("Detected Linux/BSD Platform - Far Better Than Windows/OSX", 3);
+
+                //     #if ENTRY_CONFIG_USE_WAYLAND
+                //         PlatformDataInstance.ndt      = glfwGetWaylandDisplay(); 
+                //     #else 
+                //         PlatformDataInstance.ndt      = glfwGetX11Display(); 
+                //         PlatformDataInstance.nwh      = (void*)glfwGetX11Window(Window);
+                //     #endif 
                     
-                #elif BX_PLATFORM_OSX
+                // #elif BX_PLATFORM_OSX
                 
-                        // Add Snarky OSX Message
-                        Logger.Log("Detected OSX - Yuck, You Should Use Linux/BSD", 3);
+                //         // Add Snarky OSX Message
+                //         Logger.Log("Detected OSX - Yuck, You Should Use Linux/BSD", 3);
 
-                        PlatformDataInstance.ndt      = NULL; 
+                //         PlatformDataInstance.ndt      = NULL; 
                 
-                #elif BX_PLATFORM_WINDOWS 
+                // #elif BX_PLATFORM_WINDOWS 
                 
-                        // Add Snarky Windows Message
-                        Logger.Log("Detected Windows - Not As Bad As OSX, Still Though, You Should Use Linux/BSD", 3);
+                //         // Add Snarky Windows Message
+                //         Logger.Log("Detected Windows - Not As Bad As OSX, Still Though, You Should Use Linux/BSD", 3);
 
-                        PlatformDataInstance.ndt      = NULL; 
-                        PlatformDataInstance.nwh      = glfwGetWin32Window(Window);
+                //         PlatformDataInstance.ndt      = NULL; 
+                //         PlatformDataInstance.nwh      = glfwGetWin32Window(Window);
                 
-                #endif
+                // #endif
 
                 // Log Setting Platform Data
                 Logger.Log("Setting Platform Data", 2);
