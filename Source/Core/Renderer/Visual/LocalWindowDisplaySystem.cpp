24e0faa16a4bc33a29b4875896658092829ea9ca
datacrystals <tliao@carboncopies.org>
2021-10-13 23:39:08 -0700

    Move LWDS

---
+++ Source/Core/Renderer/Visual/LocalWindowDisplaySystem.cpp
@@ -0,0 +1,23 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file is responsible for implementing the visual local display system.
+    Additonal Notes: None
+    Date Created: 2021-10-13
+*/ 
+
+
+// This Class Handles Local Machine Display Via A Local Window
+class LocalWindowDisplaySystem {
+
+    public:
+
+        // Window Initialization Function
+        void InitWindow() {
+            
+        }
+
+
+}
