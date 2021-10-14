aa245ec8fb0f2a553bc34a58860fb0434a0c9361
datacrystals <tliao@carboncopies.org>
2021-10-14 02:08:20 -0700

    Fix Variable Typos

--- Source/Core/Renderer/Visual/LocalWindowDisplaySystem.cpp
+++ Source/Core/Renderer/Visual/LocalWindowDisplaySystem.cpp
@@ -51,7 +51,7 @@ void LocalWindowDisplaySystem::InitWindow(LoggerClass sERSLogger, YAML::Node sER
 }
 
 // Define LocalWindowDisplaySystem::GetVulkanInitExtensions
-VkInstanceCreateInfo LocalWindowDisplaySystem::GetVulkanInitExtensions(VKInstanceCreateInfo VkCreateInfo) {
+VkInstanceCreateInfo LocalWindowDisplaySystem::GetVulkanInitExtensions(VkInstanceCreateInfo VkCreateInfo) {
 
     // Generate GLFW Required Vulkan Extensions Info
     Logger.Log("Getting GLFW Required Vulkan Extensions", 3);
--- Source/Core/Renderer/Visual/LocalWindowDisplaySystem.h
+++ Source/Core/Renderer/Visual/LocalWindowDisplaySystem.h
@@ -31,7 +31,7 @@ class LocalWindowDisplaySystem {
         void InitWindow(LoggerClass sERSLogger, YAML::Node sERSConfig);
 
         // Add GLFW Extensions To VkInstanceCreateInfo
-        VkInstanceCreateInfo GetVulkanInitExtensions(VKInstanceCreateInfo VkCreateInfo);
+        VkInstanceCreateInfo GetVulkanInitExtensions(VkInstanceCreateInfo VkCreateInfo);
 
         // Get Window Events
         void FetchEvents();
