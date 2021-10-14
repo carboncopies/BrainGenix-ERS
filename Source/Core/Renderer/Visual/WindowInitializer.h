6e81932bf83faad0297c44d45daba50bcba97c81
datacrystals <tliao@carboncopies.org>
2021-10-13 23:25:03 -0700

    Create Basic New Files

---
+++ Source/Core/Renderer/Visual/WindowInitializer.h
@@ -0,0 +1,11 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file is responsible for implementing the logger system used by the rest of the system.
+    Additonal Notes: None
+    Date Created: 2021-10-13
+*/ 
+
+
