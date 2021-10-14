f5d24e8d5ea0293709e10a8d8b907cde7a5ab9f3
datacrystals <tliao@carboncopies.org>
2021-10-13 23:25:29 -0700

    Create Info Header

--- Source/Core/Renderer/Visual/VisualRenderer.h
+++ Source/Core/Renderer/Visual/VisualRenderer.h
@@ -1,1 +1,12 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file is responsible for implementing the logger system used by the rest of the system.
+    Additonal Notes: None
+    Date Created: 2021-10-13
+*/ 
+
+
  
