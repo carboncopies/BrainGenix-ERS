19ebb6610e36b130dfd3f51b04dcdbeb4618d619
datacrystals <tliao@carboncopies.org>
2021-10-19 17:10:40 -0700

    Add ShaderLoader Template

---
+++ Source/Core/Loader/Shaders/ShaderLoader.h
@@ -0,0 +1,9 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is responsible for loading/compiling shaders.
+    Additonal Notes: None
+    Date Created: 2021-09-28
+*/
