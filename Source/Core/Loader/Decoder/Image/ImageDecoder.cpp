ae5f59e0566eb421bd8dd56be342f543757c33b8
datacrystals <tliao@carboncopies.org>
2021-10-28 20:48:15 -0700

    Add Image Decoding

---
+++ Source/Core/Loader/Decoder/Image/ImageDecoder.cpp
@@ -0,0 +1,11 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file is responsible for implementing image decoding functionality.
+    Additonal Notes: None
+    Date Created: 2021-10-28
+*/  
+
+
