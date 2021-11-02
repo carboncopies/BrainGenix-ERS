e3364fd9d44942aba70734c35a91db328b50f512
datacrystals <tliao@carboncopies.org>
2021-11-01 21:39:39 -0700

    Setup Sandbox

---
+++ Sandbox_Testing/Build.sh
@@ -0,0 +1,14 @@
+# Create Build Dir
+mkdir -p Build
+
+# Enter Build Dir
+cd Build
+
+# Make Only BrainGenix-ERS
+cmake ..
+cmake --build . --target BrainGenix-ERS -- -j 24
+
+# Run Program
+cd ../Binaries
+./BrainGenix-ERS
+cd ..
