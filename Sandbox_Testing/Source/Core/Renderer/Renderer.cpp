0882b47ab11b549c92436c2287c8714f2f0d9c3b
datacrystals <tliao@carboncopies.org>
2021-11-01 23:42:24 -0700

    Setup Sandbox

--- Sandbox_Testing/Source/Core/Renderer/Renderer.cpp
+++ Sandbox_Testing/Source/Core/Renderer/Renderer.cpp
@@ -38,7 +38,7 @@ void Renderer::InitializeGLFW() {
     // Read Out Width, Height
     WindowWidth_ = (*SystemConfiguration_)["WindowWidth"].as<int>();
     WindowHeight_ = (*SystemConfiguration_)["WindowHeight"].as<int>();
-    WindowTitle_ = (*SystemConfiguration_)["WindowTitle"].as<char*>();
+    WindowTitle_ = (*SystemConfiguration_)["WindowTitle"].as<std::string>().c_str();
 
 
     // Create Window Object
