8f1965c654494e0351952dd004baaae00bfdd1ee
datacrystals <tliao@carboncopies.org>
2021-11-03 02:02:00 -0700

    Fix Variable Names

--- Sandbox_Testing/Source/Core/Renderer/Renderer.cpp
+++ Sandbox_Testing/Source/Core/Renderer/Renderer.cpp
@@ -148,7 +148,7 @@ void Renderer::InitializeOpenGL() {
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    unsigned char *data = stbi_load(FileSystem::getPath("resources/textures/container.jpg").c_str(), &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("Assets/container.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
