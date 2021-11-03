59ec3fcb81bda16207ffa12b9a23a019718b6bc5
datacrystals <tliao@carboncopies.org>
2021-11-03 01:56:55 -0700

    Fix Variable Names

--- Sandbox_Testing/Source/Core/Renderer/Renderer.cpp
+++ Sandbox_Testing/Source/Core/Renderer/Renderer.cpp
@@ -153,12 +153,8 @@ bool Renderer::UpdateLoop() {
 
 
     Shader_.MakeActive();
-    // // draw our first triangle
-    // glUseProgram(shaderProgram);
-    glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-    //glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    // glBindVertexArray(0); // no need to unbind it every time 
 
 
     // Update Window Stuff
