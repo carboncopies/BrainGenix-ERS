9e39804dc4e608ac88306642ab6ab5ce180982b8
datacrystals <tliao@carboncopies.org>
2021-11-03 01:18:33 -0700

    Work On Texture Manager Struct

--- Sandbox_Testing/Source/Core/Renderer/VisualRenderer/TextureManager.h
+++ Sandbox_Testing/Source/Core/Renderer/VisualRenderer/TextureManager.h
@@ -8,4 +8,28 @@
     Date Created: 2021-11-03
 */
 
+#pragma once
 
+// Texture Struct (Could be modified later to be a material by adding multiple textures)
+struct ERSTexture{
+
+    // OpenGL References
+    unsigned int Texture;
+    
+    // Status Bools
+    bool _TextureInitialized = false;
+
+
+    void InitializeTexture(ERSImage *Image) {
+
+        // Generate Texture
+        glGenTextures(1, &Texture);
+        glBindTexture(GL_TEXTURE_2D, Texture);
+
+        
+
+
+    }
+
+
+};
