672c4a64e3e9b030923d92bad474af020b304b59
datacrystals <tliao@carboncopies.org>
2021-11-04 03:17:14 -0700

    Update TextureLoader

---
+++ Sandbox_Testing/Source/Core/Loader/Decoder/Texture/Texture.h
@@ -0,0 +1,15 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file Loads Textures.
+    Additonal Notes: None
+    Date Created: 2021-11-04
+*/  
+
+
+#pragma once
+
+
+unsigned int TextureFromFIle(const char *Path, const std::string &Directory, bool Gamma = false);
