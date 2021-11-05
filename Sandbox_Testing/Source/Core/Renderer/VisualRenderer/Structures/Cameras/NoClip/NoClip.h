32f9dace57fda83eba09242e107ac0736fbd3a17
datacrystals <tliao@carboncopies.org>
2021-11-04 23:02:20 -0700

    Add Camera Class

--- Sandbox_Testing/Source/Core/Renderer/VisualRenderer/Structures/Cameras/NoClip/NoClip.h
+++ Sandbox_Testing/Source/Core/Renderer/VisualRenderer/Structures/Cameras/NoClip/NoClip.h
@@ -9,3 +9,33 @@
 */
 
 #pragma once
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+
+
+// Define Camera Directions
+enum CameraMovement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+};
+
+
+// Setup Camera Options
+class Camera {
+
+    public:
+
+    private:
+
+
+};
+
+
+
+#include "Core/VisualRenderer/Structures/Cameras/NoClip.cpp"
