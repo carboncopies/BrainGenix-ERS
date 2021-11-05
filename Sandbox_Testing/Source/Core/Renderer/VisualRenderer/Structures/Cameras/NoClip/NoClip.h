7777254cd4950d264b472059422419ba88d55d8d
datacrystals <tliao@carboncopies.org>
2021-11-04 23:07:18 -0700

    Create Camera Constructor

--- Sandbox_Testing/Source/Core/Renderer/VisualRenderer/Structures/Cameras/NoClip/NoClip.h
+++ Sandbox_Testing/Source/Core/Renderer/VisualRenderer/Structures/Cameras/NoClip/NoClip.h
@@ -25,11 +25,39 @@ enum CameraMovement {
     RIGHT
 };
 
+// Set Default Attributes
+const float DefaultYaw = -90.0f;
+const float DefaultPitch = 0.0f;
+const float DefaultSpeed = 2.5f;
+const float DefaultSensitivity = 0.1f;
+const float DefaultZoom = 45.0f;
+
 
 // Setup Camera Options
-class Camera {
+class ERS_OBJECT_CAMERA_NOCLIP {
 
     public:
+
+        // Camera Attributes
+        glm::vec3 Position;
+        glm::vec3 Front;
+        glm::vec3 Up;
+        glm::vec3 Right;
+        glm::vec3 WorldUp;
+
+        // Euler Angles
+        float Yaw;
+        float Pitch;
+
+        // Camera Options
+        float MovementSpeed;
+        float MouseSensitivity;
+        float Zoom;
+
+        // Constructor
+        ERS_OBJECT_CAMERA_NOCLIP(glm::vec3 Position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f), float Yaw = DefaultYaw, float Pitch = DefaultPitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(DefaultSpeed), MouseSensitivity(DefaultSensitivity), Zoom(DefaultZoom)
+
+
 
     private:
 
