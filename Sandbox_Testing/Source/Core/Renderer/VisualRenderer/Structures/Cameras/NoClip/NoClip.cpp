dcfe698012d24e20cb330a0dfe618a391e04678b
datacrystals <tliao@carboncopies.org>
2021-11-04 23:00:22 -0700

    Add Camera Class

---
+++ Sandbox_Testing/Source/Core/Renderer/VisualRenderer/Structures/Cameras/NoClip/NoClip.cpp
@@ -0,0 +1,9 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is the Noclip Camera.
+    Additonal Notes: None
+    Date Created: 2021-11-05
+*/
---
+++ Sandbox_Testing/Source/Core/Renderer/VisualRenderer/Structures/Cameras/NoClip/NoClip.h
@@ -0,0 +1,11 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is the Noclip camera.
+    Additonal Notes: None
+    Date Created: 2021-11-05
+*/
+
+#pragma once
