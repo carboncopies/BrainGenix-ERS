904ca05066bc7ce01ff27b3fb2c101e936c9f30c
datacrystals <tliao@carboncopies.org>
2021-11-04 19:43:23 -0700

    Fix Compiler Errors

--- Sandbox_Testing/Source/Core/Renderer/VisualRenderer/Structures/Model/Model.cpp
+++ Sandbox_Testing/Source/Core/Renderer/VisualRenderer/Structures/Model/Model.cpp
@@ -179,7 +179,7 @@ std::vector<ERS_OBJECT_TEXTURE_2D> ERS_OBJECT_MODEL::LoadMaterialTextures(aiMate
             
             ERS_OBJECT_TEXTURE_2D Texture;
             Textures.ID = TextureFromFile(Str.C_Str(), this->Directory);
-            Texture.Type = TypeName;
+            Textures.Type = TypeName;
             Texture.Path = Str.C_Str();
             Textures.push_back(Texture);
             Textures_Loaded.push_back(Texture);
