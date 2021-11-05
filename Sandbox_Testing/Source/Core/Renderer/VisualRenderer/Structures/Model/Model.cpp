6613e3dc974c8823c6e4dd7c184a4ce93054060e
datacrystals <tliao@carboncopies.org>
2021-11-04 19:37:50 -0700

    Fix Compiler Errors

--- Sandbox_Testing/Source/Core/Renderer/VisualRenderer/Structures/Model/Model.cpp
+++ Sandbox_Testing/Source/Core/Renderer/VisualRenderer/Structures/Model/Model.cpp
@@ -142,7 +142,7 @@ ERS_OBJECT_MESH ERS_OBJECT_MODEL::ProcessMesh(aiMesh *Mesh, const aiScene *Scene
     std::vector<ERS_OBJECT_TEXTURE_2D> SpecularMaps = LoadMaterialTextures(Material, aiTextureType_SPECULAR, "texture_specular");
     Textures.insert(Textures.end(), SpecularMaps.begin(), SpecularMaps.end());
     // 3. normal maps
-    std::vector<ERS_OBJECT_TEXTURE_2D> NormalMaps = loadMaterialTextures(Material, aiTextureType_HEIGHT, "texture_normal");
+    std::vector<ERS_OBJECT_TEXTURE_2D> NormalMaps = LoadMaterialTextures(Material, aiTextureType_HEIGHT, "texture_normal");
     Textures.insert(Textures.end(), NormalMaps.begin(), NormalMaps.end());
     // 4. height maps
     std::vector<ERS_OBJECT_TEXTURE_2D> HeightMaps = loadMaterialTextures(Material, aiTextureType_AMBIENT, "texture_height");
