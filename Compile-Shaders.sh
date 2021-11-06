7407b3209f597c12d2bf636a84b184e6f99e476a
datacrystals <tliao@carboncopies.org>
2021-11-06 01:25:07 -0700

    Restructure Repo

--- Compile-Shaders.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p Source/Shaders/SPIR-V
-mkdir -p Source/Shaders/SPIR-V/Vertex
-mkdir -p Source/Shaders/SPIR-V/Fragment
-
-./Build-Tools/shaderc/bin/glslc Source/Shaders/Source/Vertex/main.vert -o Source/Shaders/SPIR-V/Vertex/main.spv
-./Build-Tools/shaderc/bin/glslc Source/Shaders/Source/Fragment/main.frag -o Source/Shaders/SPIR-V/Fragment/main.spv
