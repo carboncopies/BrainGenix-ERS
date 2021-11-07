ac028086d337bfc9e3880595c0bb99a2396cae7f
datacrystals <tliao@carboncopies.org>
2021-11-07 14:40:02 -0800

    Fix Cmake

---
+++ Tools/Install.sh
@@ -0,0 +1,6 @@
+# Install Packages
+sudo apt install libx11-dev -y
+sudo apt install libxrandr-dev -y
+sudo apt install libxinerama-dev -y
+sudo apt install libxcursor-dev -y
+sudo apt install libxi-dev -y
