3c2126ee4b97644d748c859bb7a19a8989af6c0c
datacrystals <tliao@carboncopies.org>
2021-11-06 17:13:11 -0700

    Update Build Script

--- Tools/Build.sh
+++ Tools/Build.sh
@@ -1,14 +1,22 @@
+# Exit Tools Dir
+echo "Exiting Tools Dir"
+cd ..
+
 # Create Build Dir
+echo "Creating Build Directory"
 mkdir -p Build
 
 # Enter Build Dir
-cd ../Build
+echo "Entering Build Directory"
+cd Build
 
 # Make Only BrainGenix-ERS
+echo "Running Cmake"
 cmake ..
 cmake --build . --target BrainGenix-ERS -- -j 24
 
 # Run Program
+echo "Running Program"
 cd ../Binaries
 ./BrainGenix-ERS
 cd ..
