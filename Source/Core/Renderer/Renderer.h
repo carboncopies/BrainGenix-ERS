288501ec91984e71788ff9cb10692a856e48d695
datacrystals <tliao@carboncopies.org>
2021-11-08 22:04:46 -0800

    Move Texture Loading Function To Texture Struct

--- Source/Core/Renderer/Renderer.h
+++ Source/Core/Renderer/Renderer.h
@@ -20,7 +20,6 @@
 
 #include "Core/Renderer/VisualRenderer/WindowInputProcessor.h"
 #include "Core/Renderer/VisualRenderer/ShaderManager.h"
-#include "Core/Renderer/VisualRenderer/TextureManager.h"
 #include "Core/Renderer/VisualRenderer/Structures/Model/Model.h"
 #include "Core/Renderer/VisualRenderer/Structures/Cameras/NoClip/NoClip.h"
 
