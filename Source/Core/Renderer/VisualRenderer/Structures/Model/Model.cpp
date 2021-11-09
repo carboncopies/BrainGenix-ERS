4f735b913503746eb0e1d4f916ae51647c0703c0
datacrystals <tliao@carboncopies.org>
2021-11-09 00:53:11 -0800

    Resolve Pointer Issues

--- Source/Core/Renderer/VisualRenderer/Structures/Model/Model.cpp
+++ Source/Core/Renderer/VisualRenderer/Structures/Model/Model.cpp
@@ -180,7 +180,6 @@ std::vector<ERS_OBJECT_TEXTURE_2D> ERS_OBJECT_MODEL::LoadMaterialTextures(aiMate
             // Load Texture From File
             std::cout<<std::string(std::string(this->Directory)  + std::string("/") + std::string(Str.C_Str())).c_str()<<std::endl;
             std::string FilePath = std::string(std::string(this->Directory)  + std::string("/") + std::string(Str.C_Str()));
-            std::cout<<FilePath.c_str()<<std::endl;
             std::ifstream File(FilePath.c_str(), std::ios::ate | std::ios::binary);
             // if (!File.is_open()) {
             //     Logger_->Log("Error Opening File", 10);
