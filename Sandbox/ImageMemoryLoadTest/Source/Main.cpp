f0407ee2d3e0a29b25d51e814f5b6ec09841566b
datacrystals <tliao@carboncopies.org>
2021-11-09 20:24:10 -0800

    Work On Sandbox Image Test

--- Sandbox/ImageMemoryLoadTest/Source/Main.cpp
+++ Sandbox/ImageMemoryLoadTest/Source/Main.cpp
@@ -1,1 +1,39 @@
- 
+// In-Memory Image Loading Test
+
+#include <iostream>
+#include <fstream>
+
+#include "FreeImage.h"
+
+
+char* LoadFile(const char* FilePath) {
+
+    // Open File
+    std::ifstream File(FilePath, std::ios::ate | std::ios::binary);
+
+    // Get File Size, Init Buffer
+    size_t FileSize = (size_t) File.tellg();
+    char* Buffer = NULL;
+
+    // Jump To File Beginning, Read
+    File.seekg(0);
+    File.read(Buffer, FileSize);
+    File.close();
+
+    // Return Buffer
+    return Buffer;
+}
+
+
+void main() {
+
+    // Init FreeImage
+    FreeImage_Initialise();
+
+    // Load Test.png
+    char* FileData = LoadFile("Assets/Test.png");
+
+    // De-Init FreeImage
+    FreeImage_DeInitialise();
+
+}
