a17bdeccf0d6fbb05e846c24889fa42172a5901e
datacrystals <tliao@carboncopies.org>
2021-11-09 20:28:51 -0800

    Work On Sandbox Image Test

--- Sandbox/ImageMemoryLoadTest/Source/Main.cpp
+++ Sandbox/ImageMemoryLoadTest/Source/Main.cpp
@@ -25,13 +25,20 @@ char* LoadFile(const char* FilePath) {
 }
 
 
+
+
+
 int main() {
 
     // Init FreeImage
     FreeImage_Initialise();
 
     // Load Test.png
+    std::cout<<"Loading Image: 'Assets/Test.png' From Disk\n";
     char* FileData = LoadFile("Assets/Test.png");
+    long ImageLength = std::size(*FileData);
+
+    FIMEMORY* InMemoryData = FreeImage_OpenMemory(reinterpret_cast<BYTE*>(FileData), ImageLength)
 
     // De-Init FreeImage
     FreeImage_DeInitialise();
