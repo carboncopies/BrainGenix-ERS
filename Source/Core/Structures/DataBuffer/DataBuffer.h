e79ff7b3dee16f190d88b24010200f7dcc99373f
datacrystals <tliao@carboncopies.org>
2021-11-12 02:02:11 -0800

    Update Internal Loading System

--- Source/Core/Structures/DataBuffer/DataBuffer.h
+++ Source/Core/Structures/DataBuffer/DataBuffer.h
@@ -22,15 +22,14 @@ struct ERS_STRUCTURE_DATA_BUFFER {
 
     // Create Buffers
     std::vector<std::vector<int>, std::vector<float>> Test;
-    std::map<long, std::map<long, long>> IndexMap; // Stores as follows: {AssetID: {Index Of Vector Stored In, Position In That Vector}}
+    std::map<long, std::vector<long, long>> IndexMap; // Stores as follows: {AssetID: {Index Of Vector Stored In, Position In That Vector}}
 
     // Add Type
     void AddIntToBuffer(int Input, long ID) {
 
         Test[0].push_back(Input);
 
-        std::map<long, long> InsertMap = {0, Test[0].size()};
-        IndexMap[ID] = InsertMap;
+        IndexMap[ID] = {0, Test[0].size()};
     
     }
 
