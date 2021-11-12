d719f4e6e9875b2022ad38cbe2d7e2dfb5b7ae78
datacrystals <tliao@carboncopies.org>
2021-11-12 03:39:04 -0800

    Add To-Do Messages To DataBuffer.h

--- Source/Core/Structures/DataBuffer/DataBuffer.h
+++ Source/Core/Structures/DataBuffer/DataBuffer.h
@@ -28,6 +28,10 @@ struct ERS_STRUCTURE_DATA_BUFFER {
     std::map<long, long> IndexMap; // Stores as follows: {AssetID: Index In Vector}
     std::vector<std::variant<ERS_STRUCTURE_SCENE, long>> StructureBuffer;
 
+    /*
+    TODO: ADD VECTOR HERE THAT HAS ALL THE TYPES AVAILABLE.
+    */
+
 
     // Initialize Buffer
     void Init( LoggerClass *Logger) {
@@ -48,6 +52,11 @@ struct ERS_STRUCTURE_DATA_BUFFER {
             Logger_->Log(std::string(std::string("Adding Asset To Data Buffer With Index: ") + std::to_string(ID)).c_str(), 7);
         }
 
+
+        /*
+        TODO: HAVE THE SYSTEM CHECK THE TYPE AND PICK THE INDEX OF THAT TYPES VECTOR, THEN APPEND TO VECTOR WITH THAT INDEX.
+        */
+
         StructureBuffer.push_back(Input);
         IndexMap.insert({ID, (long)StructureBuffer.size()-1});
 
@@ -55,6 +64,10 @@ struct ERS_STRUCTURE_DATA_BUFFER {
 
     // Get Data From Buffer
     auto* PullFromBuffer(long ID) {
+
+        /*
+        TODO: PULL FROM BUFFER WITH TYPE INDEX, USE std::get<TYPEINDEX> TO GET ANY TYPE.
+        */
 
         // Get Data From Buffer
         long VectorIndex = IndexMap[ID];
