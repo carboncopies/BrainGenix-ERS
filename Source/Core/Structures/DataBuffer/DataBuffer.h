f57439fe311859b3599dd8e18aa6ed35953044aa
datacrystals <tliao@carboncopies.org>
2021-11-12 14:35:41 -0800

    Refactor Header Files Co-authored-by: amukovozov <amukovozov@users.noreply.github.com>

--- Source/Core/Structures/DataBuffer/DataBuffer.h
+++ Source/Core/Structures/DataBuffer/DataBuffer.h
@@ -22,58 +22,58 @@
 struct ERS_STRUCTURE_DATA_BUFFER {
 
     // Internal Vars
-    LoggerClass *Logger_ = nullptr;
+    // LoggerClass *Logger_ = nullptr;
 
-    // Create Buffers
-    std::map<long, long> IndexMap; // Stores as follows: {AssetID: Index In Vector}
-    std::vector<std::variant<ERS_STRUCTURE_SCENE, long>> StructureBuffer;
+    // // Create Buffers
+    // std::map<long, long> IndexMap; // Stores as follows: {AssetID: Index In Vector}
+    // std::vector<std::variant<ERS_STRUCTURE_SCENE, long>> StructureBuffer;
 
-    /*
-    TODO: ADD VECTOR HERE THAT HAS ALL THE TYPES AVAILABLE.
-    */
-
-
-    // Initialize Buffer
-    void Init( LoggerClass *Logger) {
-
-        // Copy Pointer To The Logger
-        Logger_ = Logger;
-
-        // Log System Initialization
-        Logger_->Log("Initialized ERS Data Buffer", 6);
-
-    }
-
-    // Add To Data Buffer
-    void AddToBuffer(auto Input, long ID) {
-
-        // If Logger Enabled, Log Entry To Buffer
-        if (Logger_ != nullptr) {
-            Logger_->Log(std::string(std::string("Adding Asset To Data Buffer With Index: ") + std::to_string(ID)).c_str(), 7);
-        }
+    // /*
+    // TODO: ADD VECTOR HERE THAT HAS ALL THE TYPES AVAILABLE.
+    // */
 
 
-        /*
-        TODO: HAVE THE SYSTEM CHECK THE TYPE AND PICK THE INDEX OF THAT TYPES VECTOR, THEN APPEND TO VECTOR WITH THAT INDEX.
-        */
+    // // Initialize Buffer
+    // void Init( LoggerClass *Logger) {
 
-        StructureBuffer.push_back(Input);
-        IndexMap.insert({ID, (long)StructureBuffer.size()-1});
+    //     // Copy Pointer To The Logger
+    //     Logger_ = Logger;
 
-    }
+    //     // Log System Initialization
+    //     Logger_->Log("Initialized ERS Data Buffer", 6);
 
-    // Get Data From Buffer
-    auto* PullFromBuffer(long ID) {
+    // }
 
-        /*
-        TODO: PULL FROM BUFFER WITH TYPE INDEX, USE std::get<TYPEINDEX> TO GET ANY TYPE.
-        */
+    // // Add To Data Buffer
+    // void AddToBuffer(auto Input, long ID) {
 
-        // Get Data From Buffer
-        long VectorIndex = IndexMap[ID];
-        return &std::get<long>(StructureBuffer[VectorIndex]);
+    //     // If Logger Enabled, Log Entry To Buffer
+    //     if (Logger_ != nullptr) {
+    //         Logger_->Log(std::string(std::string("Adding Asset To Data Buffer With Index: ") + std::to_string(ID)).c_str(), 7);
+    //     }
 
-    }
+
+    //     /*
+    //     TODO: HAVE THE SYSTEM CHECK THE TYPE AND PICK THE INDEX OF THAT TYPES VECTOR, THEN APPEND TO VECTOR WITH THAT INDEX.
+    //     */
+
+    //     StructureBuffer.push_back(Input);
+    //     IndexMap.insert({ID, (long)StructureBuffer.size()-1});
+
+    // }
+
+    // // Get Data From Buffer
+    // auto* PullFromBuffer(long ID) {
+
+    //     /*
+    //     TODO: PULL FROM BUFFER WITH TYPE INDEX, USE std::get<TYPEINDEX> TO GET ANY TYPE.
+    //     */
+
+    //     // Get Data From Buffer
+    //     long VectorIndex = IndexMap[ID];
+    //     return &std::get<long>(StructureBuffer[VectorIndex]);
+
+    // }
 
 
 };
