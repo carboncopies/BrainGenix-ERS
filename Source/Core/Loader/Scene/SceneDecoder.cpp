0fc400c3735d81b2d4008537ae152edca8763d24
datacrystals <tliao@carboncopies.org>
2021-11-11 23:19:23 -0800

    Add Subnode To Struct

--- Source/Core/Loader/Scene/SceneDecoder.cpp
+++ Source/Core/Loader/Scene/SceneDecoder.cpp
@@ -50,11 +50,12 @@ ERS_STRUCTURE_SCENE  LoadScene(long SceneID, LoggerClass *Logger_, bool LogLoadi
     // Parse Scene Subnodes
     YAML::Node SubnodesMap = Scene.SceneData["Subnodes"];
     for (YAML::const_iterator it=SubnodesMap.begin(); it!=SubnodesMap.end(); ++it) {
-        Scene.Subnodes[it->first.as<long>()] = {
+        Scene.Subnodes[it->second["ID"].as<long>()] = {
             it->second
-
         };
     }
+
+    std::cout<<Scene.Subnodes[0]["Name"].as<std::string>().c_str()<<std::endl;
 
 
     // Return Scene
