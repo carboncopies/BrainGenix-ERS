11843043db92f96ce79beceeafb155fd6a13f390
datacrystals <tliao@carboncopies.org>
2021-11-13 01:48:44 -0800

    Debug Segfault

--- Source/Core/Loader/Scene/SceneDecoder.cpp
+++ Source/Core/Loader/Scene/SceneDecoder.cpp
@@ -53,23 +53,13 @@ ERS_OBJECT_SCENE  LoadScene(long SceneID, LoggerClass *Logger_, bool LogLoading)
     // Parse Scene Subnodes
     YAML::Node SubnodesMap = Scene.SceneData["Subnodes"];
     std::map<long, YAML::Node> Subnodes;
-
-
-    int i = 1;
     for (YAML::const_iterator it=SubnodesMap.begin(); it!=SubnodesMap.end(); ++it) {
-        long V1 = it->second["ID"].as<long>();
+        long V1 = it->first.as<long>();
         YAML::Node V2 = it->second;
         Subnodes.insert({V1, V2});
 
-        std::cout<<"\n Value Is Now: "<<Subnodes[i]<<std::endl<<std::endl;
-        i++;
     }
-
     Scene.Subnodes = Subnodes;
-
-    std::cout<<"*************************************\n";
-    std::cout<<Scene.Subnodes[1]<<std::endl;
-    std::cout<<"END\n";
 
 
     // Return Scene
