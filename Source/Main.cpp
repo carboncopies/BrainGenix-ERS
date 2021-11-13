844030c572e8523bc4fcc51f690441ed03e570e1
datacrystals <tliao@carboncopies.org>
2021-11-12 21:03:21 -0800

    Add Subsys Class

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -32,7 +32,7 @@ int main() {
     sERSLogger.Log("Initialized 'Management::Logger::LoggerClass'", 5);
 
     // Initialize System
-    sERSSubsystem sERSMainSubSystem(*sERSLogger);
+    sERSSubSystem sERSMainSubSystem(*sERSLogger);
 
 
     // Initialize Renderer
