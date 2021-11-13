877d40a03e2ade883a93901854fe341a54ddb050
datacrystals <tliao@carboncopies.org>
2021-11-12 20:56:13 -0800

    Add Subsys Class

---
+++ Source/Core/System.cpp
@@ -0,0 +1,11 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file handles initailization and loading of all other ERS systems.
+    Additonal Notes: None
+    Date Created: 2021-11-12
+*/
+
+
---
+++ Source/Core/System.h
@@ -0,0 +1,26 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file handles initailization and loading of all other ERS systems.
+    Additonal Notes: None
+    Date Created: 2021-11-12
+*/
+
+#include "Core/Management/LoggingSystem.h
+
+class sERSSubSystem {
+
+    private:
+
+        // Internal Vars
+        LoggerClass *Logger_;
+
+    public:
+
+        // Constructor
+        sERSSubSystem(LoggerClass *Logger);
+
+
+};
