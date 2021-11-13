1a18c4b76ba48f05091b02ed9d9e9a999e91f1d7
datacrystals <tliao@carboncopies.org>
2021-11-13 03:18:04 -0800

    Debug Hang

--- Source/Core/System.cpp
+++ Source/Core/System.cpp
@@ -42,7 +42,6 @@ sERSSubSystem::~sERSSubSystem() {
 // Main Update Loop
 void sERSSubSystem::UpdateRenderers() {
 
-    std::cout<<"test: "<<AssetLoader_->DataBuffer_->GetNumberObjectModels()<<std::endl;
 
     // Update Visual Renderer
     VisualRenderer_.UpdateLoop();
