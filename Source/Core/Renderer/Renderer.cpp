5771ce4b4d01f9ca01cbad2372e657cc0b835fb4
datacrystals <tliao@carboncopies.org>
2021-11-13 03:14:18 -0800

    Debug Hang

--- Source/Core/Renderer/Renderer.cpp
+++ Source/Core/Renderer/Renderer.cpp
@@ -139,7 +139,7 @@ void Renderer::UpdateLoop() {
 
 
     // render the loaded models
-    std::cout<<"test: "<<AssetLoader->DataBuffer_.GetNumberObjectModels()<<std::endl;
+    std::cout<<"test: "<<AssetLoader.DataBuffer_.GetNumberObjectModels()<<std::endl;
     std::vector<ERS_OBJECT_MODEL*> LoadedModelPointers = AssetLoader_->GetModelsToDraw();
 
     for (long i; i < size(LoadedModelPointers); i++) { 
