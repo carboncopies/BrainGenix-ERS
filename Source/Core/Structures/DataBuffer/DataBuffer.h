6a532699f45af3e2607a1c2f2c884083204a18a1
datacrystals <tliao@carboncopies.org>
2021-11-13 03:37:47 -0800

    Debug Hang

--- Source/Core/Structures/DataBuffer/DataBuffer.h
+++ Source/Core/Structures/DataBuffer/DataBuffer.h
@@ -38,7 +38,7 @@ struct ERS_STRUCTURE_DATA_BUFFER {
     std::map<long, VectorData> MetadataMap_;
     std::vector<ERS_OBJECT_MODEL> ERS_OBJECT_MODEL_Vector_; // THIS IS ERRONEOUSLY LARGE (WHEN USING SIZE FUNCTION, FIGRE OUT WHAT's GOING ON HERE!)
     std::vector<ERS_OBJECT_SCENE> ERS_OBJECT_SCENE_Vector_;
-
+    int TestVar = 5;
 
     // Return Number Of Elements In Model Vector
     long GetNumberObjectModels() {
