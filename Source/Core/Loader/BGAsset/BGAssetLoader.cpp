12932a5c418f3ebd435836fce8d95419522d6698
datacrystals <tliao@carboncopies.org>
2021-11-12 18:20:02 -0800

    Add Asset Loader Class

---
+++ Source/Core/Loader/BGAsset/BGAssetLoader.cpp
@@ -0,0 +1,11 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file is responsible for loading assets from the scene.
+    Additonal Notes: None
+    Date Created: 2021-10-13
+*/ 
+
+#include "BGAssetLoader.h"
---
+++ Source/Core/Loader/BGAsset/BGAssetLoader.h
@@ -0,0 +1,9 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file is responsible for implementing scene decoding.
+    Additonal Notes: None
+    Date Created: 2021-10-13
+*/ 
