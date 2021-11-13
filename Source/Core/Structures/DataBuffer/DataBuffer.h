fa025b6cf111cbde62fac4a5ba9b7f519d4931bb
datacrystals <tliao@carboncopies.org>
2021-11-12 16:40:15 -0800

    Refactor ERS Structure Data Buffer

--- Source/Core/Structures/DataBuffer/DataBuffer.h
+++ Source/Core/Structures/DataBuffer/DataBuffer.h
@@ -72,36 +72,6 @@ struct ERS_STRUCTURE_DATA_BUFFER {
 
     }
 
-    // // Add To Data Buffer
-    // void AddToBuffer(auto Input, long ID) {
-
-    //     // If Logger Enabled, Log Entry To Buffer
-    //     if (Logger_ != nullptr) {
-    //         Logger_->Log(std::string(std::string("Adding Asset To Data Buffer With Index: ") + std::to_string(ID)).c_str(), 7);
-    //     }
-
-
-    //     /*
-    //     TODO: HAVE THE SYSTEM CHECK THE TYPE AND PICK THE INDEX OF THAT TYPES VECTOR, THEN APPEND TO VECTOR WITH THAT INDEX.
-    //     */
-
-    //     StructureBuffer.push_back(Input);
-    //     IndexMap.insert({ID, (long)StructureBuffer.size()-1});
-
-    // }
-
-    // // Get Data From Buffer
-    // auto* PullFromBuffer(long ID) {
-
-    //     /*
-    //     TODO: PULL FROM BUFFER WITH TYPE INDEX, USE std::get<TYPEINDEX> TO GET ANY TYPE.
-    //     */
-
-    //     // Get Data From Buffer
-    //     long VectorIndex = IndexMap[ID];
-    //     return &std::get<long>(StructureBuffer[VectorIndex]);
-
-    // }
 
 
 };
