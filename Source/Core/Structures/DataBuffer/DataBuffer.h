466fdac74c2696eca54d8ec809ac30292d49f115
datacrystals <tliao@carboncopies.org>
2021-11-13 03:02:42 -0800

    Debug Hang

--- Source/Core/Structures/DataBuffer/DataBuffer.h
+++ Source/Core/Structures/DataBuffer/DataBuffer.h
@@ -39,7 +39,11 @@ struct ERS_STRUCTURE_DATA_BUFFER {
     std::vector<ERS_OBJECT_MODEL> ERS_OBJECT_MODEL_Vector_ = {}; // THIS IS ERRONEOUSLY LARGE (WHEN USING SIZE FUNCTION, FIGRE OUT WHAT's GOING ON HERE!)
     std::vector<ERS_OBJECT_SCENE> ERS_OBJECT_SCENE_Vector_ = {};
 
+    long GetNumberObjectModels() {
 
+        return ERS_OBJECT_MODEL_Vector_.size();
+
+    }
 
 
     // Initialize Buffer
