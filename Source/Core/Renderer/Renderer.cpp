a528f6b998282e6f562b742507be1f9710bc799c
datacrystals <tliao@carboncopies.org>
2021-11-13 18:34:02 -0800

    Fix Compiler Errors

--- Source/Core/Renderer/Renderer.cpp
+++ Source/Core/Renderer/Renderer.cpp
@@ -148,7 +148,7 @@ void Renderer::UpdateLoop() {
     model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
     model = glm::rotate(model, glm::radians(-0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
     Shader_.SetMat4("model", model);
-    Model_->Draw(Shader_);
+    Model_.Draw(Shader_);
 
 
 
