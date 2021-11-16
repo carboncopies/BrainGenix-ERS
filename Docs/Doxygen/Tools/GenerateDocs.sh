cd2b2ff0608ec4c325b27f0915af6d2d642c3429
datacrystals <tliao@carboncopies.org>
2021-11-15 22:40:22 -0800

    Restructure Docs

--- Docs/Doxygen/Tools/GenerateDocs.sh
+++ Docs/Doxygen/Tools/GenerateDocs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Make Docs Generated Folder
+echo "Making Generated Docs Folder"
+cd ..
+mkdir -p Generated
+
 # Generate Docs
 echo "Generating Documentation For BrainGenix-ERS"
-cd ..
 cd Source
 doxygen Doxyfile
