4d4d24a80e2351eb32f69dc1981770be9845f223
datacrystals <tliao@carboncopies.org>
2021-11-17 02:59:58 -0800

    Refactor ERS

---
+++ Source/Core/Structures/AssetLoaderQueue.h
@@ -0,0 +1,17 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is the asset loader queue struct.
+    Additional Notes: None
+    Date Created: 2021-11-17
+*/
+
+#pragma once
+
+struct ERS_STRUCT_ASSET_LOADER_QUEUE {
+
+
+
+};
