2a692192f001423abfae42954250c8e400e981f8
datacrystals <tliao@carboncopies.org>
2021-11-17 15:04:34 -0800

    Refactor ERS

---
+++ Source/Core/Structures/AssetLoaderQueue.cpp
@@ -0,0 +1,9 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is the asset loader queue struct.
+    Additional Notes: None
+    Date Created: 2021-11-17
+*/
--- Source/Core/Structures/AssetLoaderQueue.h
+++ Source/Core/Structures/AssetLoaderQueue.h
@@ -10,7 +10,7 @@
 
 #pragma once
 
-#include <map>
+#include <vector>
 
 /**
  * @brief Declare ERS Structure For Asset Loader Queue. Used as a queue of assets to be loaded. (FIXME: MAKE THREAD SAFE!)
@@ -18,6 +18,25 @@
  */
 struct ERS_STRUCT_ASSET_LOADER_QUEUE {
 
+    /**
+     * @brief queue used to store list of assets to load
+     * 
+     */
+    std::vector<long> AssetIDLoadingQueue;
 
+    
+    /**
+     * @brief Add Item To Queue
+     * 
+     * @param QueueID 
+     */
+    void EmplaceItem(long QueueID);
+
+    /**
+     * @brief Get Item From Queue
+     * 
+     * @return long 
+     */
+    long GetItem();
 
 };
