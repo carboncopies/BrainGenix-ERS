0ade5bf348cbe27f9a5880acfa4dbbe1ab1bbd50
datacrystals <tliao@carboncopies.org>
2021-11-16 18:16:33 -0800

    Restructure Build System

--- Source/Core/Renderer/VisualRenderer/Callbacks.cpp
+++ Source/Core/Renderer/VisualRenderer/Callbacks.cpp
@@ -7,3 +7,37 @@
     Additonal Notes: None
     Date Created: 2021-11-16
 */
+
+
+
+static void FramebufferSizeCallback(GLFWwindow* /*Window*/, int Width, int Height) {
+
+    // Update Viewport
+    glViewport(0,0, Width, Height);
+
+}
+
+static void MouseCallback(GLFWwindow* /*Window*/, double XPos, double YPos) {
+
+    // Update Positions
+    if (FirstMouse) {
+
+        LastX = XPos;
+        LastY = YPos;
+
+        FirstMouse = false;
+
+    }
+
+    // Calculate Offsets
+    float XOffset = XPos - LastX;
+    float YOffset = YPos - LastY;
+
+    // Update Last Positions
+    LastX = XPos;
+    LastY = YPos;
+
+    // Process Camera Movement
+    Camera_.ProcessMouseMovement(XOffset, YOffset);
+
+}
--- Source/Core/Renderer/VisualRenderer/Callbacks.h
+++ Source/Core/Renderer/VisualRenderer/Callbacks.h
@@ -15,42 +15,14 @@
  * @brief This callback function is used to update the size of the window when a resize event is called.
  * 
  */
-static void FramebufferSizeCallback(GLFWwindow* /*Window*/, int Width, int Height) {
-
-    // Update Viewport
-    glViewport(0,0, Width, Height);
-
-}
+static void FramebufferSizeCallback(GLFWwindow* /*Window*/, int Width, int Height);
 
 
 /**
  * @brief This function is called when a mouse input needs to be processed.
  * 
  */
-static void MouseCallback(GLFWwindow* /*Window*/, double XPos, double YPos) {
-
-    // Update Positions
-    if (FirstMouse) {
-
-        LastX = XPos;
-        LastY = YPos;
-
-        FirstMouse = false;
-
-    }
-
-    // Calculate Offsets
-    float XOffset = XPos - LastX;
-    float YOffset = YPos - LastY;
-
-    // Update Last Positions
-    LastX = XPos;
-    LastY = YPos;
-
-    // Process Camera Movement
-    Camera_.ProcessMouseMovement(XOffset, YOffset);
-
-}
+static void MouseCallback(GLFWwindow* /*Window*/, double XPos, double YPos);
 
 /**
  * @brief This callback processes scroll wheel input.
