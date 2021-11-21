a8cc701c640350b9ecefe256a2c0da43688d2b6a
datacrystals <tliao@carboncopies.org>
2021-11-20 16:51:20 -0800

    Fix Callbacks.h
    Co-authored-by: asmallwood1 <asmallwood1@users.noreply.github.com>
    Co-authored-by: rwhitney1 <rwhitney1@users.noreply.github.com>
    Co-authored-by: J-French1 <J-French1@users.noreply.github.com>

--- Source/Core/Renderer/RendererManager.h
+++ Source/Core/Renderer/RendererManager.h
@@ -10,8 +10,11 @@
 
 #pragma once
 
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
 
 // Internal Libraries (BG convention: use <> instead of "")
+#include <LoggingSystem.h>
 #include <VisualRendererManager.h>
 
 
