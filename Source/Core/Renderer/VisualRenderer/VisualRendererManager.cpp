5f37ee82720412ed0592714d5a4f24f3e92acb8e
datacrystals <tliao@carboncopies.org>
2021-11-20 16:32:26 -0800

    Fix Callbacks.h
    Co-authored-by: asmallwood1 <asmallwood1@users.noreply.github.com>
    Co-authored-by: rwhitney1 <rwhitney1@users.noreply.github.com>
    Co-authored-by: J-French1 <J-French1@users.noreply.github.com>

---
+++ Source/Core/Renderer/VisualRenderer/VisualRendererManager.cpp
@@ -0,0 +1,9 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file contains the visual renderer manager.
+    Additonal Notes: None
+    Date Created: 2021-11-20
+*/
