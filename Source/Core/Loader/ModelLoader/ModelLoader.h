e7c0d39378da321e0d48505b67435e383b2430fe
datacrystals <tliao@carboncopies.org>
2021-11-21 02:23:19 -0800

    Update ModelLoader

--- Source/Core/Loader/ModelLoader/ModelLoader.h
+++ Source/Core/Loader/ModelLoader/ModelLoader.h
@@ -8,3 +8,54 @@
     Date Created: 2021-11-13
 */
 
+#pragma once
+
+
+/**
+ * @brief Class for loading models (fbx, gltx, etc.) based on the ASSIMP library
+ * 
+ */
+class ModelLoader {
+
+
+    public:
+
+
+
+        // Logger
+        LoggerClass *Logger_;
+        TextureLoader *TextureLoader_;
+
+        /**
+         * @brief Construct a new Model Loader object
+         * 
+         * @param Logger 
+         * @param TextureLoader 
+         */
+        ModelLoader(LoggerClass* Logger, TextureLoader *TextureLoader);
+
+        /**
+         * @brief Destroy the Model Loader object
+         * 
+         */
+        ~ModelLoader();
+
+        // Load Model From File
+        void LoadModelFromFile(const char* AssetPath);
+
+        // Draw Model
+        void Draw(ERS_OBJECT_SHADER &Shader);
+
+
+    private:
+
+        void ProcessNode(aiNode *Node, const aiScene *Scene);
+
+        ERS_OBJECT_MESH ProcessMesh(aiMesh *Mesh, const aiScene *Scene);
+
+        std::vector<ERS_OBJECT_TEXTURE_2D> LoadMaterialTextures(aiMaterial *Mat, aiTextureType Type, std::string TypeName);
+
+
+
+};
+
--- Source/Core/Structures/Model/Model.h
+++ Source/Core/Structures/Model/Model.h
@@ -56,48 +56,4 @@ struct ERS_OBJECT_MODEL {
 };
 
 
-class ModelLoader {
-
-
-    public:
-
-
-
-        // Logger
-        LoggerClass *Logger_;
-        TextureLoader *TextureLoader_;
-
-        /**
-         * @brief Construct a new Model Loader object
-         * 
-         * @param Logger 
-         * @param TextureLoader 
-         */
-        ModelLoader(LoggerClass* Logger, TextureLoader *TextureLoader);
-
-        /**
-         * @brief Destroy the Model Loader object
-         * 
-         */
-        ~ModelLoader();
-
-        // Load Model From File
-        void LoadModelFromFile(const char* AssetPath);
-
-        // Draw Model
-        void Draw(ERS_OBJECT_SHADER &Shader);
-
-
-    private:
-
-        void ProcessNode(aiNode *Node, const aiScene *Scene);
-
-        ERS_OBJECT_MESH ProcessMesh(aiMesh *Mesh, const aiScene *Scene);
-
-        std::vector<ERS_OBJECT_TEXTURE_2D> LoadMaterialTextures(aiMaterial *Mat, aiTextureType Type, std::string TypeName);
-
-
-
-};
-
 
