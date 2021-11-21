cc9b5f03823f2df2ce320fc674f6b731cb7e1134
datacrystals <tliao@carboncopies.org>
2021-11-21 02:24:31 -0800

    Remove Extra Includes

--- Source/Core/Structures/Model/Model.h
+++ Source/Core/Structures/Model/Model.h
@@ -11,33 +11,11 @@
 #pragma once
 
 // Standard Libraries (BG convention: use <> instead of "")
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <iostream>
-#include <map>
 #include <vector>
-#include <stdio.h>
-#include <math.h>
-#include <assert.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-
-// Third-Party Libraries (BG convention: use <> instead of "")
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
 
 // Internal Libraries (BG convention: use <> instead of "")
 #include <Mesh.h>
 #include <2DTexture.h>
-#include <Vertex.h>
-#include <FileIO.h>
-#include <TextureLoader.h>
-
 
 
 struct ERS_OBJECT_MODEL {
