599487569644497ba2960f6e872afbbb530c4c3a
datacrystals <tliao@carboncopies.org>
2021-11-20 20:02:11 -0800

    Restructure Model Loader

--- Source/Core/Loader/TextureLoader/TextureLoader.cpp
+++ Source/Core/Loader/TextureLoader/TextureLoader.cpp
@@ -21,6 +21,11 @@ TextureLoader::TextureLoader(LoggerClass* Logger) {
     // Log Initialization
     Logger_->Log("Initializing Texture Loader Class", 5);
 
+    // Create Instance Of ImageDecoder
+    ImageDecoder_ = ImageDecoder;
+    ImageDecoder_.Initialize(Logger_);
+    
+
 }
 
 // Texture Loader Destructor
@@ -29,13 +34,16 @@ TextureLoader::~TextureLoader() {
     // Log Destructor Call
     Logger_->Log("TextureLoader Destructor Called", 6);
 
+    // Deinit ImageDecoder
+    ImageDecoder_.Cleanup();
+
 }
 
 
 // Load Texture Overloaded Functions
 ERS_OBJECT_TEXTURE_2D TextureLoader::LoadTexture(const char* Path) { // File Version
 
-    
+
 
 }
 
