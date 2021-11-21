bd008e2591956f335e30770552dbafc718037afc
datacrystals <tliao@carboncopies.org>
2021-11-20 19:17:33 -0800

    Restructure Model Loader

---
+++ Source/Core/Loader/TextureLoader/TextureLoader.h
@@ -0,0 +1,11 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is the 2d texture struct.
+    Additonal Notes: None
+    Date Created: 2021-11-21
+*/
+
+#pragma once
