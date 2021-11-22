32dda6daf34d9b97e79b52e4fd9c3950b70fd150
datacrystals <tliao@carboncopies.org>
2021-11-22 11:07:44 -0800

    Add SceneLoader Header

---
+++ Source/Core/Loader/SceneLoader/SceneLoader.h
@@ -0,0 +1,10 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file contains the scene loader class.
+    Documentation Status: Done
+    Additonal Notes: None
+    Date Created: 2021-11-22
+*/
