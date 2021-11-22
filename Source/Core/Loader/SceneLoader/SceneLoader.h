f0a0b7f5aebd1400cef72a1abf8f620662ac15b4
datacrystals <tliao@carboncopies.org>
2021-11-22 11:10:50 -0800

    Update SceneLoader

--- Source/Core/Loader/SceneLoader/SceneLoader.h
+++ Source/Core/Loader/SceneLoader/SceneLoader.h
@@ -8,3 +8,29 @@
     Additonal Notes: None
     Date Created: 2021-11-22
 */
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <LoggingSystem.h>
+
+
+/**
+ * @brief The Sceneloader Class Is responsible for loading scene files and associated models/textures.
+ * 
+ */
+class SceneLoader{
+
+    private:
+
+        LoggerClass *Logger_; 
+
+    public:
+    
+
+
+};
