ebf9c39636108fb1411e39dd37dd93c80dfbf3c8
datacrystals <tliao@carboncopies.org>
2021-11-21 22:19:29 -0800

    Debug Model LocRotScale System

--- Source/Core/Renderer/VisualRenderer/VisualRenderer.cpp
+++ Source/Core/Renderer/VisualRenderer/VisualRenderer.cpp
@@ -218,7 +218,7 @@ void VisualRenderer::UpdateLoop() {
     Shader_.SetMat4("model", Model_.ModelLocRotScale_);
     Model_.Draw(Shader_);
 
-    model = glm::mat4(1.0f);
+    glm::mat4 model = glm::mat4(1.0f);
     model = glm::translate(model, glm::vec3(-4.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
     model = glm::scale(model, glm::vec3(0.25f, 0.25f, 0.25f));	// it's a bit too big for our scene, so scale it down
     model = glm::rotate(model, glm::radians(-0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
