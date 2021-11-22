ca495d281402dfe03b273b9c20e5289a59622b86
datacrystals <tliao@carboncopies.org>
2021-11-21 17:52:43 -0800

    Add Model Manager Files

---
+++ Source/Core/Renderer/VisualRenderer/ModelManager.cpp
@@ -0,0 +1,10 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file contains the model manager classes.
+    Documentation Status: Done
+    Additonal Notes: None
+    Date Created: 2021-11-21
+*/
