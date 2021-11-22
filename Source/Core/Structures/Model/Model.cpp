865a55d1f1a767f14e5a04aa6a99d51b321ab28d
datacrystals <tliao@carboncopies.org>
2021-11-21 22:12:00 -0800

    Fix GLM Positions

--- Source/Core/Structures/Model/Model.cpp
+++ Source/Core/Structures/Model/Model.cpp
@@ -67,14 +67,14 @@ void ERS_OBJECT_MODEL::SetScale(double X, double Y, double Z) {
     glm::vec3 Scale = glm::vec3(X, Y, Z);
 
     // Set Scale to Pos
-    ModelLocRotScale_ = glm::rotate(ModelLocRotScale_, Scale);
+    ModelLocRotScale_ = glm::scale(ModelLocRotScale_, Scale);
 
 }
 
 void ERS_OBJECT_MODEL::SetScale(glm::vec3 Scale) {
 
     // Set Scale to Pos
-    ModelLocRotScale_ = glm::rotate(ModelLocRotScale_, Scale);
+    ModelLocRotScale_ = glm::scale(ModelLocRotScale_, Scale);
 
 }
 
@@ -89,8 +89,8 @@ void ERS_OBJECT_MODEL::SetLocRotScale(double LocX, double LocY, double LocZ, dou
 
     // Set Vecs
     ModelLocRotScale_ = glm::translate(ModelLocRotScale_, Position);
-    ModelLocRotScale_ = glm::translate(ModelLocRotScale_, Rotation);
-    ModelLocRotScale_ = glm::translate(ModelLocRotScale_, Scale);
+    ModelLocRotScale_ = glm::rotate(ModelLocRotScale_, glm::vec3(0.0f), Rotation);
+    ModelLocRotScale_ = glm::scale(ModelLocRotScale_, Scale);
 
 }
 
@@ -99,7 +99,7 @@ void ERS_OBJECT_MODEL::SetLocRotScale(glm::vec3 Position, glm::vec3 Rotation, gl
 
     // Set Vecs
     ModelLocRotScale_ = glm::translate(ModelLocRotScale_, Position);
-    ModelLocRotScale_ = glm::translate(ModelLocRotScale_, Rotation);
-    ModelLocRotScale_ = glm::translate(ModelLocRotScale_, Scale);
+    ModelLocRotScale_ = glm::rotate(ModelLocRotScale_, glm::vec3(0.0f), Rotation);
+    ModelLocRotScale_ = glm::scale(ModelLocRotScale_, Scale);
 
 }
