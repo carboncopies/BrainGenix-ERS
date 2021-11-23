5caa17b60344bf557929d643126a58a1a4675f61
datacrystals <tliao@carboncopies.org>
2021-11-23 02:34:52 -0800

    Cleanup Scene Docs

--- Source/Core/Structures/Scene/Scene.h
+++ Source/Core/Structures/Scene/Scene.h
@@ -20,6 +20,8 @@
 // Internal Libraries (BG convention: use <> instead of "")
 #include <Model.h>
 
+
+
 /**
  * @brief The scene structure contains the data for an entire "level". It references other models, scripts, audio, and more to construct the environment.
  * 
@@ -30,19 +32,13 @@ struct ERS_OBJECT_SCENE{
     YAML::Node SceneData; /**<YAML::Node Containing Copy Of Raw Scene Information*/
 
     // List Of Required Materials (For Now, Can Only Be Models)
-    std::vector<ERS_OBJECT_MODEL> Models;
+    std::vector<ERS_OBJECT_MODEL> Models; /**<Vector Containing Models That Are Parts Of The Scene*/
 
     // Scene Metadata
-    long SceneFormatVersion;
-    std::string SceneName;
-
+    long SceneFormatVersion; /**<Sets The Current Scene Version (from File/DB Entry), determines If System Can Load It*/
+    std::string SceneName; /**<Metadata Setting Scene Name From It's Scene File/DB Entry*/
 
     // Status Variables
-    bool IsSceneReadyToLoad = false;
-    bool IsSceneLoaded = false;
-
-
-
-
+    bool IsSceneLoaded = false; /**<Status Variable Indiciating If The Scene Can Be Rendered Yet, Or If It's Yet To Be Loaded/Still Loading*/
 
 };
