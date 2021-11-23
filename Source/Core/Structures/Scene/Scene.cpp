169ffabfa9bc169fffad70eabedda99eef0d2004
datacrystals <tliao@carboncopies.org>
2021-11-23 00:35:16 -0800

    Add Scene CPP File

---
+++ Source/Core/Structures/Scene/Scene.cpp
@@ -0,0 +1,13 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file creates the scene structure.
+    Documentation Status: Done
+    Additonal Notes: None
+    Date Created: 2021-11-22
+*/
+
+#include <Scene.h>
+
