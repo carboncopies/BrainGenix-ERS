772352849086504051e00a730de29f2f51611b0c
datacrystals <tliao@carboncopies.org>
2021-11-25 12:10:36 -0800

    Update VisualEditor

---
+++ Source/Core/Editor/VisualEditor/Grid.h
@@ -0,0 +1,10 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is responsible for providing a function to draw a grid when the editor is enabled.
+    Additonal Notes: None
+    Date Created: 2021-1-25
+*/
+
