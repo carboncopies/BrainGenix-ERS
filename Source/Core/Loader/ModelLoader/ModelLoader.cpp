4fc90d21c7ae5916b12da4fe8dd720fde6db1cc4
datacrystals <tliao@carboncopies.org>
2021-11-25 11:44:16 -0800

    Debug PBR Loading Issues

--- Source/Core/Loader/ModelLoader/ModelLoader.cpp
+++ Source/Core/Loader/ModelLoader/ModelLoader.cpp
@@ -168,15 +168,15 @@ ERS_OBJECT_MESH ModelLoader::ProcessMesh(aiMesh *Mesh, const aiScene *Scene) {
     // Diffuse Maps
     std::vector<ERS_OBJECT_TEXTURE_2D> DiffuseMaps = LoadMaterialTextures(Material, aiTextureType_DIFFUSE, "texture_diffuse");
     Textures.insert(Textures.end(), DiffuseMaps.begin(), DiffuseMaps.end());
-    // 2. specular maps
-    std::vector<ERS_OBJECT_TEXTURE_2D> SpecularMaps = LoadMaterialTextures(Material, aiTextureType_SPECULAR, "texture_specular");
-    Textures.insert(Textures.end(), SpecularMaps.begin(), SpecularMaps.end());
-    // 3. normal maps
-    std::vector<ERS_OBJECT_TEXTURE_2D> NormalMaps = LoadMaterialTextures(Material, aiTextureType_NORMALS, "texture_normal");
-    Textures.insert(Textures.end(), NormalMaps.begin(), NormalMaps.end());
-    // 4. height maps
-    std::vector<ERS_OBJECT_TEXTURE_2D> HeightMaps = LoadMaterialTextures(Material, aiTextureType_AMBIENT, "texture_height");
-    Textures.insert(Textures.end(), HeightMaps.begin(), HeightMaps.end());
+    // // 2. specular maps
+    // std::vector<ERS_OBJECT_TEXTURE_2D> SpecularMaps = LoadMaterialTextures(Material, aiTextureType_SPECULAR, "texture_specular");
+    // Textures.insert(Textures.end(), SpecularMaps.begin(), SpecularMaps.end());
+    // // 3. normal maps
+    // std::vector<ERS_OBJECT_TEXTURE_2D> NormalMaps = LoadMaterialTextures(Material, aiTextureType_NORMALS, "texture_normal");
+    // Textures.insert(Textures.end(), NormalMaps.begin(), NormalMaps.end());
+    // // 4. height maps
+    // std::vector<ERS_OBJECT_TEXTURE_2D> HeightMaps = LoadMaterialTextures(Material, aiTextureType_AMBIENT, "texture_height");
+    // Textures.insert(Textures.end(), HeightMaps.begin(), HeightMaps.end());
 
     // Mesh Object
     return ERS_OBJECT_MESH(Vertices, Indices, Textures);
