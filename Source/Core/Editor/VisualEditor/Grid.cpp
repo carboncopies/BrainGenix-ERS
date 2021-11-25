9585a0d0d6a0b7b459a98228f4b1dac06698d490
datacrystals <tliao@carboncopies.org>
2021-11-25 12:24:23 -0800

    Add Grid Function Declaration

--- Source/Core/Editor/VisualEditor/Grid.cpp
+++ Source/Core/Editor/VisualEditor/Grid.cpp
@@ -16,25 +16,25 @@ void DrawGrid(float UnitSize, int GridSize, float GridPosX, float GridPosY, floa
 
     // 
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glColor3f(1.0, 0.0, 0.0);
-    int xsize=0,ysize=0;
-    for(int j=0;j<10;j++)
-    {
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // //glColor3f(1.0, 0.0, 0.0);
+    // int xsize=0,ysize=0;
+    // for(int j=0;j<10;j++)
+    // {
 
-        xsize=0;
-        for(int i=0;i<10;i++)
-        {
-             glBegin(GL_POLYGON);
-               glVertex3f(-50.0+xsize, -50.0+ysize, 0.0);
-               glVertex3f(-40.0+xsize, -50.0+ysize, 0.0);
-               glVertex3f(-40.0+xsize, -40.0+ysize, 0.0);
-               glVertex3f(-50.0+xsize, -40.0+ysize, 0.0);
-            glEnd();
-         xsize+=10.0;
-     }
-     ysize+=10.0;
-     }
+    //     xsize=0;
+    //     for(int i=0;i<10;i++)
+    //     {
+    //          glBegin(GL_POLYGON);
+    //            glVertex3f(-50.0+xsize, -50.0+ysize, 0.0);
+    //            glVertex3f(-40.0+xsize, -50.0+ysize, 0.0);
+    //            glVertex3f(-40.0+xsize, -40.0+ysize, 0.0);
+    //            glVertex3f(-50.0+xsize, -40.0+ysize, 0.0);
+    //         glEnd();
+    //      xsize+=10.0;
+    //  }
+    //  ysize+=10.0;
+    //  }
 
 
 
