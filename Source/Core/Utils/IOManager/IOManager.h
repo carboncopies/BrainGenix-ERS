77e5eea1614ea8f071ad02641114411346e6be16
datacrystals <tliao@carboncopies.org>
2021-11-29 03:27:26 -0800

    Add IOManager Class

--- Source/Core/Utils/IOManager/IOManager.h
+++ Source/Core/Utils/IOManager/IOManager.h
@@ -14,10 +14,16 @@
 
 // Standard Libraries (BG convention: use <> instead of "")
 
+
 // Third-Party Libraries (BG convention: use <> instead of "")
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
 
 // Internal Libraries (BG convention: use <> instead of "")
 #include <LoggingSystem.h>
+
 
 
 
