15d46d385b584c6d8996c017d2b0581c3dd392c8
datacrystals <tliao@carboncopies.org>
2021-11-29 03:19:12 -0800

    Add IOManager Class

---
+++ Source/Core/Utils/IOManager/IOManager.h
@@ -0,0 +1,40 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file provides IOManager functionality.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-11-29
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <LoggingSystem.h>
+
+
+
+/**
+ * @brief The IOManager Class Gets All User Input And Provides It To The Rest Of The System
+ * 
+ */
+class IOManager {
+
+    private:
+
+
+        LoggerClass *Logger_ /**<Logging System Instance Pointer*/
+
+
+    public:
+
+
+
+};
