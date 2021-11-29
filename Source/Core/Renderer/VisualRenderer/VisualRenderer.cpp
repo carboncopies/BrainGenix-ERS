7c6746618f17797794b1f7caf50ac4367bfe7771
datacrystals <tliao@carboncopies.org>
2021-11-29 13:36:44 -0800

    Debug Segfault

--- Source/Core/Renderer/VisualRenderer/VisualRenderer.cpp
+++ Source/Core/Renderer/VisualRenderer/VisualRenderer.cpp
@@ -95,25 +95,16 @@ void VisualRenderer::UpdateLoop(float DeltaTime, ERS_OBJECT_CAMERA_NOCLIP *Camer
 
 
 
-
-    std::cout<< "1\n";
-
-
-
     // Rendering Commands Here
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    std::cout<< "1\n";
+
 
     // Use Shader
     Shader->MakeActive();
-    std::cout<< "1\n";
+
 
     // Update Camera
-    glfwGetFramebufferSize(Window_, &RenderWidth_, &RenderHeight_);
     float AspectRatio = (float)RenderWidth_ / (float)RenderHeight_;
-    std::cout<< "1\n";
-
-
     glm::mat4 projection = glm::perspective(glm::radians(Camera->Zoom), AspectRatio, 0.1f, 100.0f);
     glm::mat4 view = Camera->GetViewMatrix();
     Shader->SetMat4("projection", projection);
@@ -121,15 +112,7 @@ void VisualRenderer::UpdateLoop(float DeltaTime, ERS_OBJECT_CAMERA_NOCLIP *Camer
     std::cout<< "1\n";
 
 
-
     // Draw Models
     SceneManager_->Render(Shader);
-
-
-    
-    std::cout<< "1\n";
-
-
-
 
 }
