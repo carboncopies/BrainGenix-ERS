3444783a4477d1db1ba8b23faf35c7bbae922b5f
datacrystals <tliao@carboncopies.org>
2021-11-30 13:31:41 -0800

    Add Font Manager Source Files

---
+++ Source/Core/Editor/VisualEditor/FontManager/FontManager.cpp
@@ -0,0 +1,12 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file provides font loading/selecting functionality.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-11-30
+*/
+
+#include <FontManager.h>
---
+++ Source/Core/Editor/VisualEditor/FontManager/FontManager.h
@@ -0,0 +1,27 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file provides font loading/selecting functionality.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-11-30
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <vector>
+#include <iostream>
+#include <filesystem>
+#include <string>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
+
+#include <imgui.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <LoggingSystem.h>
