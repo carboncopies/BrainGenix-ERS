1273d00383af3f2bf2b7491b3c9f8bd4b1a3b96a
datacrystals <tliao@carboncopies.org>
2021-11-29 23:13:09 -0800

    Update Dark Theme

--- Source/Core/Editor/VisualEditor/Theme/Theme.cpp
+++ Source/Core/Editor/VisualEditor/Theme/Theme.cpp
@@ -153,10 +153,8 @@ void ThemeManager::ApplyThemes(int ThemeID) {
     Style.Colors[ImGuiCol_ScrollbarGrabHovered] = ReadColor("ScrollbarHoveredColor", ThemeNode);
     Style.Colors[ImGuiCol_ScrollbarGrabActive] = ReadColor("ScrollbarActiveColor", ThemeNode);
     Style.Colors[ImGuiCol_CheckMark] = ReadColor("CheckmarkColor", ThemeNode);
-
-
-    Style.Colors[ImGuiCol_SliderGrab]            = ImVec4(0.24f, 0.52f, 0.88f, 1.00f);
-    Style.Colors[ImGuiCol_SliderGrabActive]      = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+    Style.Colors[ImGuiCol_SliderGrab] = ReadColor("SliderGrabColor", ThemeNode);
+    Style.Colors[ImGuiCol_SliderGrabActive]      = ReadColor("SliderGrabActiveColor", ThemeNode);
     Style.Colors[ImGuiCol_Button]                = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
     Style.Colors[ImGuiCol_ButtonHovered]         = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
     Style.Colors[ImGuiCol_ButtonActive]          = ImVec4(0.06f, 0.53f, 0.98f, 1.00f);
