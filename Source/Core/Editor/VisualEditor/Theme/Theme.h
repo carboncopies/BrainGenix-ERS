5b3c925d6d9cbf68b8f283a11c4e9e6c97d4e33c
datacrystals <tliao@carboncopies.org>
2021-11-29 20:45:27 -0800

    Add Theme Configuration Class

---
+++ Source/Core/Editor/VisualEditor/Theme/Theme.h
@@ -0,0 +1,35 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file [XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX].
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: yyyy-mm-dd
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
+
+#include <imgui.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <LoggingSystem.h>
+
+
+class ThemeManager {
+
+
+    private:
+
+
+    public:
+
+
+};
