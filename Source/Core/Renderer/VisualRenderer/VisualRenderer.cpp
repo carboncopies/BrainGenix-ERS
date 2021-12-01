1275e21471e650bdf58fba09a33790482ba2459a
datacrystals <tliao@carboncopies.org>
2021-11-30 22:39:53 -0800

    Update Version

--- Source/Core/Renderer/VisualRenderer/VisualRenderer.cpp
+++ Source/Core/Renderer/VisualRenderer/VisualRenderer.cpp
@@ -106,6 +106,7 @@ void VisualRenderer::UpdateViewport(int Index, SceneManager *SceneManager, float
     InputProcessor->ProcessKeyboardInput(Logger_, DeltaTime, CaptureMouseCursor);
     InputProcessor->UpdateFramebuffer();
     InputProcessor->UpdateMouse(CaptureMouseCursor);
+    InputProcessor->
 
 
 
