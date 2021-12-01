76a0aa89af1eb76239272e5670a9248f69609280
datacrystals <tliao@carboncopies.org>
2021-11-30 23:19:52 -0800

    Add Project Loader Source File

---
+++ Source/Core/Loader/ProjectLoader/ProjectLoader.cpp
@@ -0,0 +1,35 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is the project loader.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-11-30
+*/
+
+#include <ProjectLoader.h>
+
+
+// Constructor
+ProjectLoader::ProjectLoader(LoggerClass *Logger, TextureLoader *TexLoader) {
+
+    // Create Local Pointer
+    Logger_ = Logger;
+    TextureLoader_ = TexLoader;
+
+    // Log Initialization
+    Logger_->Log("Initializing Project Loader", 5);
+
+}
+
+// Destructor
+ProjectLoader::~ProjectLoader() {
+
+    // Log Destructor Call
+    Logger_->Log("ProjectLoader Destructor Called", 6);
+
+}
+
+
---
+++ Source/Core/Loader/ProjectLoader/ProjectLoader.h
@@ -0,0 +1,64 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is the project loader.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-11-30
+*/
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <string>
+
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <LoggingSystem.h>
+
+
+
+/**
+ * @brief Class for loading project files
+ * 
+ */
+class ProjectLoader {
+
+
+    private:
+
+        // Logger
+        LoggerClass *Logger_; /**<Pointer to Logging System.*/
+
+
+
+    public:
+
+        /**
+         * @brief Construct a new Project Loader object
+         * 
+         * @param Logger 
+         */
+        ProjectLoader(LoggerClass* Logger);
+
+        /**
+         * @brief 
+         * 
+         */
+        ~ProjectLoader}();
+
+
+
+
+
+
+
+
+
+};
+
