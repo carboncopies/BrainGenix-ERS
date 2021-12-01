fb73ef96530978f99db2f5e715ed1dc83d490e5f
datacrystals <tliao@carboncopies.org>
2021-12-01 00:21:32 -0800

    Work On 3D Cursor

---
+++ Source/Core/Renderer/VisualRenderer/3DCursor/3DCursor.cpp
@@ -0,0 +1,13 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file is responsible for creating and managing 3d cursors.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-01
+*/
+
+#include <3DCursor.h>
+
