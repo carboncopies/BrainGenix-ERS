afec09a5637202659b908b13af74b41382f54260
datacrystals <tliao@carboncopies.org>
2021-12-02 04:28:57 -0800

    Add Framerate Plot

--- Source/Core/Editor/VisualEditor/Widgets/GUI_Widget_FrameratePlot.cpp
+++ Source/Core/Editor/VisualEditor/Widgets/GUI_Widget_FrameratePlot.cpp
@@ -42,7 +42,7 @@ void Widget_FrameratePlot::Draw() {
             ImVec2 GraphSize = ImVec2(WindowSize.x, WindowSize.y);
 
             // Plot
-            ImGui::PlotLine("Framerate", (const float*)FramerateHistory_.data(), FramerateHistory_.size(), -1, NULL, -1.0f, 100.0f, GraphSize);
+            ImGui::PlotLines("Framerate", (const float*)FramerateHistory_.data(), FramerateHistory_.size(), -1, NULL, -1.0f, 100.0f, GraphSize);
 
         // End System Info Window
         ImGui::End();
