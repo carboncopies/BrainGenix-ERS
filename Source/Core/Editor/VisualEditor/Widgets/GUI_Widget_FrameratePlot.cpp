382151c0191feece3cce13d4acd6f5d171c8e50b
datacrystals <tliao@carboncopies.org>
2021-12-02 04:27:08 -0800

    Add Framerate Plot

---
+++ Source/Core/Editor/VisualEditor/Widgets/GUI_Widget_FrameratePlot.cpp
@@ -0,0 +1,54 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This widget file is responsible for creating a system widget.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-02
+*/
+
+#include <GUI_Widget_FramerateHistogram.h>
+
+
+// Define Draw Function
+void Widget_FramerateHistogram::Draw() {
+
+    // Push Back Current Framerate To Vector
+    FramerateHistory_.push_back(ImGui::GetIO().Framerate);
+    
+
+
+    // Cap Array Length At Target Set
+    if (FramerateHistory_.size() > HistoryLength_) {
+        FramerateHistory_.erase(FramerateHistory_.begin());
+    } else {
+
+        // Fill With Zeros
+        int Delta = HistoryLength_ - FramerateHistory_.size();
+        for (int i = 0; i < Delta; i++) {
+            FramerateHistory_.push_back(0.0f);
+        }
+
+    }
+
+    // If Window Drawing Enabled
+    if (Enabled_) {
+        ImGui::Begin("Framerate Histogram", &Enabled_);
+
+            // Get Window Size (To Size Graph)
+            ImVec2 WindowSize = ImGui::GetContentRegionAvail();
+            ImVec2 GraphSize = ImVec2(WindowSize.x, WindowSize.y);
+
+            // Histogram
+            ImGui::PlotHistogram("Framerate", (const float*)FramerateHistory_.data(), FramerateHistory_.size(), -1, NULL, -1.0f, 100.0f, GraphSize);
+
+        // End System Info Window
+        ImGui::End();
+        
+
+    }
+
+
+}
