62ed10787c274317faf457dc1f85651e5baec1f3
datacrystals <tliao@carboncopies.org>
2021-12-02 03:10:19 -0800

    Add Framerate Histogram Widget

--- Source/Core/Editor/VisualEditor/Widgets/GUI_Widget_FramerateHistogram.cpp
+++ Source/Core/Editor/VisualEditor/Widgets/GUI_Widget_FramerateHistogram.cpp
@@ -26,7 +26,7 @@ void Widget_FramerateHistogram::Draw() {
         ImGui::Begin("Framerate Histogram", &Enabled_);
 
             // Histogram
-            ImGui::PlotHistogram("Framerate", (const float*)FramerateHistory_.data(), FramerateHistory_.size(), -1, NULL, -1.0f, 100.0f, ImVec2(400, 250));
+            ImGui::PlotHistogram("Framerate", (const float*)FramerateHistory_.data(), FramerateHistory_.size(), -1, NULL, -1.0f, 100.0f, ImGui::GetWindowSize());
 
         // End System Info Window
         ImGui::End();
