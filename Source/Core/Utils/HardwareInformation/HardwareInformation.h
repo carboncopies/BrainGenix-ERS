9d86d5bf123e9e27a8de74afcf27247cd8bb2305
datacrystals <tliao@carboncopies.org>
2021-12-02 16:16:41 -0800

    Add Hardware Info To CmakeLists

---
+++ Source/Core/Utils/HardwareInformation/HardwareInformation.h
@@ -0,0 +1,21 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is responsible for implementing hardware information collection.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-01-25
+*/
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <infoware/cpu.hpp>
+#include <infoware/version.hpp>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <LoggingSystem.h>
