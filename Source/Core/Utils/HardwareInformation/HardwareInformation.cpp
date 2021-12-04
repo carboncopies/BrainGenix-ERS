0072d3247a5311fa07fc8e0fc8330ba8813d8a92
datacrystals <tliao@carboncopies.org>
2021-12-03 22:17:43 -0800

    Implement HardwareInfo

--- Source/Core/Utils/HardwareInformation/HardwareInformation.cpp
+++ Source/Core/Utils/HardwareInformation/HardwareInformation.cpp
@@ -13,86 +13,6 @@
 
 
 
-
-
-
-
-const char* HardwareInformation::cache_type_name(iware::cpu::cache_type_t cache_type) noexcept {
-	switch(cache_type) {
-		case iware::cpu::cache_type_t::unified:
-			return "Unified";
-		case iware::cpu::cache_type_t::instruction:
-			return "Instruction";
-		case iware::cpu::cache_type_t::data:
-			return "Data";
-		case iware::cpu::cache_type_t::trace:
-			return "Trace";
-		default:
-			return "Unknown";
-	}
-}
-
-const char* HardwareInformation::architecture_name(iware::cpu::architecture_t architecture) noexcept {
-	switch(architecture) {
-		case iware::cpu::architecture_t::x64:
-			return "x64";
-		case iware::cpu::architecture_t::arm:
-			return "ARM";
-		case iware::cpu::architecture_t::itanium:
-			return "Itanium";
-		case iware::cpu::architecture_t::x86:
-			return "x86";
-		default:
-			return "Unknown";
-	}
-}
-
-const char* HardwareInformation::endianness_name(iware::cpu::endianness_t endianness) noexcept {
-	switch(endianness) {
-		case iware::cpu::endianness_t::little:
-			return "Little-Endian";
-		case iware::cpu::endianness_t::big:
-			return "Big-Endian";
-		default:
-			return "Unknown";
-	}
-}
-
-const char* HardwareInformation::kernel_variant_name(iware::system::kernel_t variant) noexcept {
-	switch(variant) {
-		case iware::system::kernel_t::windows_nt:
-			return "Windows NT";
-		case iware::system::kernel_t::linux:
-			return "Linux";
-		case iware::system::kernel_t::darwin:
-			return "Darwin";
-		default:
-			return "Unknown";
-	}
-}
-
-const char* HardwareInformation::gpu_vendor_name(iware::gpu::vendor_t vendor) noexcept {
-	switch(vendor) {
-		case iware::gpu::vendor_t::intel:
-			return "Intel";
-		case iware::gpu::vendor_t::amd:
-			return "AMD";
-		case iware::gpu::vendor_t::nvidia:
-			return "NVidia";
-		case iware::gpu::vendor_t::microsoft:
-			return "Microsoft";
-		case iware::gpu::vendor_t::qualcomm:
-			return "Qualcomm";
-		case iware::gpu::vendor_t::apple:
-			return "Apple";
-		default:
-			return "Unknown";
-	}
-}
-
-
-
-
 // HWinfo Constructor
 HardwareInformation::HardwareInformation(LoggerClass *Logger) {
 
@@ -204,4 +124,78 @@ void HardwareInformation::GetDynamicInformation() {
     HardwareInfo_.Dynamic_.SwapCapacity = MemoryInfo.virtual_total;
     HardwareInfo_.Dynamic_.SwapFree = MemoryInfo.virtual_available;
 
+}
+
+// Functions from https://github.com/ThePhD/infoware/blob/main/examples
+const char* HardwareInformation::cache_type_name(iware::cpu::cache_type_t cache_type) noexcept {
+	switch(cache_type) {
+		case iware::cpu::cache_type_t::unified:
+			return "Unified";
+		case iware::cpu::cache_type_t::instruction:
+			return "Instruction";
+		case iware::cpu::cache_type_t::data:
+			return "Data";
+		case iware::cpu::cache_type_t::trace:
+			return "Trace";
+		default:
+			return "Unknown";
+	}
+}
+
+const char* HardwareInformation::architecture_name(iware::cpu::architecture_t architecture) noexcept {
+	switch(architecture) {
+		case iware::cpu::architecture_t::x64:
+			return "x64";
+		case iware::cpu::architecture_t::arm:
+			return "ARM";
+		case iware::cpu::architecture_t::itanium:
+			return "Itanium";
+		case iware::cpu::architecture_t::x86:
+			return "x86";
+		default:
+			return "Unknown";
+	}
+}
+
+const char* HardwareInformation::endianness_name(iware::cpu::endianness_t endianness) noexcept {
+	switch(endianness) {
+		case iware::cpu::endianness_t::little:
+			return "Little-Endian";
+		case iware::cpu::endianness_t::big:
+			return "Big-Endian";
+		default:
+			return "Unknown";
+	}
+}
+
+const char* HardwareInformation::kernel_variant_name(iware::system::kernel_t variant) noexcept {
+	switch(variant) {
+		case iware::system::kernel_t::windows_nt:
+			return "Windows NT";
+		case iware::system::kernel_t::linux:
+			return "Linux";
+		case iware::system::kernel_t::darwin:
+			return "Darwin";
+		default:
+			return "Unknown";
+	}
+}
+
+const char* HardwareInformation::gpu_vendor_name(iware::gpu::vendor_t vendor) noexcept {
+	switch(vendor) {
+		case iware::gpu::vendor_t::intel:
+			return "Intel";
+		case iware::gpu::vendor_t::amd:
+			return "AMD";
+		case iware::gpu::vendor_t::nvidia:
+			return "NVidia";
+		case iware::gpu::vendor_t::microsoft:
+			return "Microsoft";
+		case iware::gpu::vendor_t::qualcomm:
+			return "Qualcomm";
+		case iware::gpu::vendor_t::apple:
+			return "Apple";
+		default:
+			return "Unknown";
+	}
 }
