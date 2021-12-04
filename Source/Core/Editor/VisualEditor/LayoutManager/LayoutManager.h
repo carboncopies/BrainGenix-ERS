eb4f67cbd585eae080070a3ed6174bddf67513ba
datacrystals <tliao@carboncopies.org>
2021-12-04 08:53:31 -0800

    Begin Implementing EditorLayout Saving/Loading

---
+++ Source/Core/Editor/VisualEditor/LayoutManager/LayoutManager.h
@@ -0,0 +1,6 @@
+
+
+// Loads layouts from EditorAssets/Configuration/Layouts (YAML + .ini pair)
+
+// Can Use: ImGui::LoadIniSettingsFromDisk(ini_to_load);
+// Probably also a save settings function?
