40777fb856f4ea65ab449fa775fb139e8779c178
datacrystals <tliao@carboncopies.org>
2021-12-03 19:11:14 -0800

    Implement HardwareInfo

--- Source/Core/Structures/ERS_HardwareInfo/ERS_STRUCT_HardwareInfo.h
+++ Source/Core/Structures/ERS_HardwareInfo/ERS_STRUCT_HardwareInfo.h
@@ -8,3 +8,39 @@
     Additonal Notes: None
     Date Created: 2021-12-03
 */
+
+
+
+/**
+ * @brief This structure is used to store static hardware information.
+ * 
+ */
+struct StaticHardwareInfo {
+
+
+};
+
+
+/**
+ * @brief This structure is used to store dynamically changing hardware information.
+ * 
+ */
+struct DynamicHardwareInfo {
+
+};
+
+
+
+/**
+ * @brief This structure is used as a container for static and dynamic hardware info structs.
+ * 
+ */
+struct ERS_STRUCT_HardwareInfo {
+
+    // Set Dynamic, Static Info
+    StaticHardwareInfo Static_; /**<Structure To Store Static Hardware Information*/
+    DynamicHardwareInfo Dynamic_; /**<Structure To Store Dynamic Hardware Information*/
+
+};
+
+
