3582aa2c68abc7d02498b5d1201501d3454b77d6
datacrystals <tliao@carboncopies.org>
2021-12-03 23:26:19 -0800

    Add User Profile Manager

--- Source/Core/Editor/VisualEditor/UserProfile/UserProfile.cpp
+++ Source/Core/Editor/VisualEditor/UserProfile/UserProfile.cpp
@@ -11,3 +11,23 @@
 
 #include <UserProfile.h>
 
+
+// Constructor
+UserProfileManager::UserProfileManager(LoggerClass *Logger) {
+
+    // Copy Pointer
+    Logger_ = Logger;
+
+    // Log Init
+    Logger_->Log("Initializing User Profile Manager", 5);
+
+}
+
+
+// Destructor
+UserProfileManager::~UserProfileManager() {
+
+    // Log Destructor Call
+    Logger_->Log("User Profile Destructo Called", 6);
+
+}
