e5ce28e8c3dc241ab52ed6e194b4cff1d067f73d
datacrystals <tliao@carboncopies.org>
2021-12-03 23:22:04 -0800

    Add User Profile Manager

---
+++ Source/Core/Editor/VisualEditor/UserProfile/UserProfile.h
@@ -0,0 +1,18 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is responsible for providing user profile management.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-04
+*/
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
