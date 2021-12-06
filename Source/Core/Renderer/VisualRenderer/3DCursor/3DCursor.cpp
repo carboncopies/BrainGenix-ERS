a729753d9f1760191b6cefd52cd20263a113b76c
datacrystals <tliao@carboncopies.org>
2021-12-06 01:46:50 -0800

    Update 3D Cursor Implementation

--- Source/Core/Renderer/VisualRenderer/3DCursor/3DCursor.cpp
+++ Source/Core/Renderer/VisualRenderer/3DCursor/3DCursor.cpp
@@ -36,74 +36,6 @@ void Cursors3D::BeginRenderpass(float* CameraView, float* CameraProjection) {
 
 // Test func
 void Cursors3D::EndRenderpass(float* cameraView, float* cameraProjection, float* matrix, bool editTransformDecomposition) {
-   static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::LOCAL);
-   static bool useSnap = false;
-   static float snap[3] = { 1.f, 1.f, 1.f };
-   static float bounds[] = { -0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.5f };
-   static float boundsSnap[] = { 0.1f, 0.1f, 0.1f };
-   static bool boundSizing = false;
-   static bool boundSizingSnap = false;
-
-{
-//    if (editTransformDecomposition)
-//    {
-//       if (ImGui::IsKeyPressed(90))
-//          mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
-//       if (ImGui::IsKeyPressed(69))
-//          mCurrentGizmoOperation = ImGuizmo::ROTATE;
-//       if (ImGui::IsKeyPressed(82)) // r Key
-//          mCurrentGizmoOperation = ImGuizmo::SCALE;
-//       if (ImGui::RadioButton("Translate", mCurrentGizmoOperation == ImGuizmo::TRANSLATE))
-//          mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
-//       ImGui::SameLine();
-//       if (ImGui::RadioButton("Rotate", mCurrentGizmoOperation == ImGuizmo::ROTATE))
-//          mCurrentGizmoOperation = ImGuizmo::ROTATE;
-//       ImGui::SameLine();
-//       if (ImGui::RadioButton("Scale", mCurrentGizmoOperation == ImGuizmo::SCALE))
-//          mCurrentGizmoOperation = ImGuizmo::SCALE;
-//       float matrixTranslation[3], matrixRotation[3], matrixScale[3];
-//       ImGuizmo::DecomposeMatrixToComponents(matrix, matrixTranslation, matrixRotation, matrixScale);
-//       ImGui::InputFloat3("Tr", matrixTranslation);
-//       ImGui::InputFloat3("Rt", matrixRotation);
-//       ImGui::InputFloat3("Sc", matrixScale);
-//       ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, matrix);
-
-//       if (mCurrentGizmoOperation != ImGuizmo::SCALE)
-//       {
-//          if (ImGui::RadioButton("Local", mCurrentGizmoMode == ImGuizmo::LOCAL))
-//             mCurrentGizmoMode = ImGuizmo::LOCAL;
-//          ImGui::SameLine();
-//          if (ImGui::RadioButton("World", mCurrentGizmoMode == ImGuizmo::WORLD))
-//             mCurrentGizmoMode = ImGuizmo::WORLD;
-//       }
-//       if (ImGui::IsKeyPressed(83))
-//          useSnap = !useSnap;
-//       ImGui::Checkbox("", &useSnap);
-//       ImGui::SameLine();
-
-//       switch (mCurrentGizmoOperation)
-//       {
-//       case ImGuizmo::TRANSLATE:
-//          ImGui::InputFloat3("Snap", &snap[0]);
-//          break;
-//       case ImGuizmo::ROTATE:
-//          ImGui::InputFloat("Angle Snap", &snap[0]);
-//          break;
-//       case ImGuizmo::SCALE:
-//          ImGui::InputFloat("Scale Snap", &snap[0]);
-//          break;
-//       }
-//       ImGui::Checkbox("Bound Sizing", &boundSizing);
-//       if (boundSizing)
-//       {
-//          ImGui::PushID(3);
-//          ImGui::Checkbox("", &boundSizingSnap);
-//          ImGui::SameLine();
-//          ImGui::InputFloat3("Snap", boundsSnap);
-//          ImGui::PopID();
-//       }
-//    }
-}
 
 
     float WindowWidth = (float)ImGui::GetWindowWidth();
@@ -113,7 +45,7 @@ void Cursors3D::EndRenderpass(float* cameraView, float* cameraProjection, float*
    
 
     //ImGuizmo::DrawCubes(cameraView, cameraProjection, &objectMatrix[0][0], gizmoCount);
-    ImGuizmo::Manipulate(cameraView, cameraProjection, mCurrentGizmoOperation, mCurrentGizmoMode, matrix, NULL, useSnap ? &snap[0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
+    ImGuizmo::Manipulate(cameraView, cameraProjection, mCurrentGizmoOperation, ImGuizmo::WORLD, matrix, NULL, NULL);
 
     ImGuizmo::ViewManipulate(cameraView, camDistance, ImVec2(WindowWidth + ImGui::GetWindowPos().x - 128, ImGui::GetWindowPos().y), ImVec2(128, 128), 0x10101010);
 
