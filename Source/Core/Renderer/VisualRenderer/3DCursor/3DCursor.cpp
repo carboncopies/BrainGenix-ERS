2aff5d84d21b2668dbd5457db376a8bf6b44d134
datacrystals <tliao@carboncopies.org>
2021-12-08 12:06:12 -0800

    Debug Cursor Movement Issues

--- Source/Core/Renderer/VisualRenderer/3DCursor/3DCursor.cpp
+++ Source/Core/Renderer/VisualRenderer/3DCursor/3DCursor.cpp
@@ -125,14 +125,11 @@ void Cursors3D::BeginRenderpass(ERS_OBJECT_CAMERA_NOCLIP *Camera, float* CameraV
         }
 
         // Check If Someone Else Is Setting Pos
-        std::cout<<ObjectTranslation_[0]<<"|"<<CurrentPos_.PosX<<std::endl;
-        bool PosEqual = ((ObjectTranslation_[0]== CurrentPos_.PosX) && (ObjectTranslation_[1] == CurrentPos_.PosY) && (ObjectTranslation_[2] == CurrentPos_.PosZ));
-        bool RotEqual = ((ObjectRotation_[0] == CurrentPos_.RotX) && (ObjectRotation_[1] == CurrentPos_.RotY) && (ObjectRotation_[2] == CurrentPos_.RotZ));
-        bool ScaleEqual = ((ObjectScale_[0] == CurrentPos_.ScaleX) && (ObjectScale_[1] == CurrentPos_.ScaleY) && (ObjectScale_[2] == CurrentPos_.ScaleZ));
+        bool PosEqual = ((ObjectTranslation_[0]== LastPos_.PosX) && (ObjectTranslation_[1] == LastPos_.PosY) && (ObjectTranslation_[2] == LastPos_.PosZ));
+        bool RotEqual = ((ObjectRotation_[0] == LastPos_.RotX) && (ObjectRotation_[1] == LastPos_.RotY) && (ObjectRotation_[2] == LastPos_.RotZ));
+        bool ScaleEqual = ((ObjectScale_[0] == LastPos_.ScaleX) && (ObjectScale_[1] == LastPos_.ScaleY) && (ObjectScale_[2] == LastPos_.ScaleZ));
         bool IsEqual = (PosEqual && RotEqual && ScaleEqual);
-
-        //std::cout<<PosEqual<<RotEqual<<ScaleEqual<<std::endl;
-        if (ObjectTranslation_[0] != LastPos_.PosX) {
+        if (IsEqual) {
 
             // Update Current LocRotScale
             CurrentPos_.PosX = ObjectTranslation_[0];
