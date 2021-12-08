b37dcbeea4629b997594749d274012ec241828e2
datacrystals <tliao@carboncopies.org>
2021-12-08 02:37:36 -0800

    Add Object Properties Widget

---
+++ Source/Core/Editor/VisualEditor/Widgets/GUI_Widget_ObjectProperties.cpp
@@ -0,0 +1,51 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This widget file is responsible for creating a system widget.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-02
+*/
+
+#include <GUI_Widget_RenderingSettings.h>
+
+
+// Define Draw Function
+void Widget_RenderingSettings::Draw() {
+
+    // If Window Drawing Enabled
+    if (Enabled_) {
+        ImGui::Begin("Rendering Settings", &Enabled_);
+
+            // Set Initial Window Size
+            ImGui::SetWindowSize(ImVec2(400,250), ImGuiCond_FirstUseEver);
+
+            // Wireframe Rendering Mode
+            ImGui::Checkbox("Wireframe Rendering Mode", &OpenGLDrawLines_);
+            ImGui::NewLine();
+
+
+            // Rendering Background Clear Color
+            ImGui::ColorEdit4("Background Clear Color", (float*)&ClearColor_);
+            ImGui::NewLine();
+
+
+
+        // End System Controls Window
+        ImGui::End();
+
+    }
+
+    // Update OpenGL Settings
+    if (OpenGLDrawLines_) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    } else {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+
+    glClearColor(ClearColor_.x, ClearColor_.y, ClearColor_.z, ClearColor_.w);
+
+
+}
---
+++ Source/Core/Editor/VisualEditor/Widgets/GUI_Widget_ObjectProperties.h
@@ -0,0 +1,50 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This widget file is responsible for creating a system widget.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-02
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+#include <imgui.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
+/**
+ * @brief Create GUI Widget "Rendering Settings"
+ * 
+ */
+class Widget_RenderingSettings {
+
+    private:
+
+        ImVec4 ClearColor_ = ImVec4(0.2f, 0.2f, 0.2f, 1.0f); /**<Background Clearing Color */
+        bool OpenGLDrawLines_ = false; /**<Default Rendering Mode */
+
+    public:
+
+        // Show/Hide Var
+        bool Enabled_ = true; /**<Show/Hide Widget*/
+
+
+        /**
+         * @brief Function that draws the window, should internally check if enable variable is set to true.
+         * 
+         */
+        void Draw();
+
+};
