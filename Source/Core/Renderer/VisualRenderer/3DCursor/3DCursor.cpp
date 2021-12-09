7a40ea3abb28a7c6df3dddb22830ef5050174817
datacrystals <tliao@carboncopies.org>
2021-12-09 01:38:23 -0800

    Fix Typo In Scene Struct

--- Source/Core/Renderer/VisualRenderer/3DCursor/3DCursor.cpp
+++ Source/Core/Renderer/VisualRenderer/3DCursor/3DCursor.cpp
@@ -173,19 +173,19 @@ void Cursors3D::BeginRenderpass(ERS_OBJECT_CAMERA_NOCLIP *Camera, float* CameraV
         
 
         // Force Scale To 1,1,1
-        if (FirstFrame_) {
-            ObjectScale_[0] = 1.0f;
-            ObjectScale_[1] = 1.0f;
-            ObjectScale_[2] = 1.0f;
-            ObjectRotation_[0] = 0.0f;
-            ObjectRotation_[1] = 0.0f;
-            ObjectRotation_[2] = 0.0f;
-            ObjectTranslation_[0] = 0.0f;
-            ObjectTranslation_[1] = 0.0f;
-            ObjectTranslation_[2] = 0.0f;
+        // if (FirstFrame_) {
+        //     ObjectScale_[0] = 1.0f;
+        //     ObjectScale_[1] = 1.0f;
+        //     ObjectScale_[2] = 1.0f;
+        //     ObjectRotation_[0] = 0.0f;
+        //     ObjectRotation_[1] = 0.0f;
+        //     ObjectRotation_[2] = 0.0f;
+        //     ObjectTranslation_[0] = 0.0f;
+        //     ObjectTranslation_[1] = 0.0f;
+        //     ObjectTranslation_[2] = 0.0f;
             
-            FirstFrame_ = false;
-        }
+        //     FirstFrame_ = false;
+        // }
 
 
 
