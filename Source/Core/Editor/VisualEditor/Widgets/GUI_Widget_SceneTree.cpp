b9d0287c617604cee607a1fdb1a5385c006df9e2
datacrystals <tliao@carboncopies.org>
2021-12-09 03:57:27 -0800

    Implement MultiScene Selection Tree

--- Source/Core/Editor/VisualEditor/Widgets/GUI_Widget_SceneTree.cpp
+++ Source/Core/Editor/VisualEditor/Widgets/GUI_Widget_SceneTree.cpp
@@ -71,7 +71,7 @@ void Widget_SceneTree::Draw() {
 
                 // If User Selected This Scene, Set Active Scene To This One
                 bool IsMouseOverArrow = (ImGui::GetMousePos().x < ImGui::GetTreeNodeToLabelSpacing() + ImGui::GetWindowPos().x);
-                std::cout<<!IsMouseOverArrow<<std::endl;
+                std::cout<<(ImGui::IsItemHovered()&&ImGui::IsMouseDown(0))<<!IsMouseOverArrow<<std::endl;
                 if (ImGui::IsItemClicked() && !IsMouseOverArrow) {
                     SceneManager_->ActiveScene_ = SceneIndex;
                     SceneManager_->Scenes_[SceneIndex].HasSelectionChanged = true;
