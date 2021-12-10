ca05f70a9c2ec536d181f0addc23337f76f01e16
datacrystals <tliao@carboncopies.org>
2021-12-09 19:07:31 -0800

    Add Save/Save All Buttons

--- Source/Core/Writers/SceneWriter/SceneWriter.cpp
+++ Source/Core/Writers/SceneWriter/SceneWriter.cpp
@@ -54,9 +54,15 @@ const char* SceneWriter::ProcessScene(ERS_OBJECT_SCENE InputScene) {
     // Create Emitter
     YAML::Emitter Output;
 
+    // Begin Writing
+    Output << YAML::BeginSeq;
+
     // Write Metadata
     Output << YAML::Key << "SceneFormatVersion" << YAML::Value << InputScene.SceneFormatVersion;
     Output << "Test";
+
+    // End Sequence
+    Output << YAML::EndSeq;
 
     // Return Scene
     return Output.c_str();
