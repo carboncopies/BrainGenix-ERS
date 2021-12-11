2e28c78f921ef07a402045be8317d4bf6ae27c60
datacrystals <tliao@carboncopies.org>
2021-12-11 03:14:27 -0800

    Implement Model Duplication Menu

---
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_SceneTree/Utils/GUI_Windowutil_DuplicateModel/GUI_Windowutil_DuplicateScene.h
@@ -0,0 +1,26 @@
+ 
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This Window file is responsible for creating a system Window.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-07
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <string>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <SceneManager.h>
+#include <Scene.h>
+
+
+void GUI_Windowutil_DuplicateScene(SceneManager* SceneManager, int SceneIndex);
