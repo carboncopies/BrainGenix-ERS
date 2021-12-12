b0b1ce2f02b0a3915e03ee18006c80ed18d82508
datacrystals <tliao@carboncopies.org>
2021-12-12 00:36:23 -0800

    Add Model Deletion Button

---
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_SceneTree/Popups/GUI_Subwindow_DeleteModel/GUI_Subwindow_DeleteScene.cpp
@@ -0,0 +1,123 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This Window file is responsible for creating a system Window.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-07
+*/
+
+#include <GUI_Subwindow_DeleteScene.h>
+
+
+Subwindow_DeleteScene::Subwindow_DeleteScene(SceneManager* SceneManager) {
+
+    // Copy Pointer
+    SceneManager_ = SceneManager;
+
+
+}
+
+Subwindow_DeleteScene::~Subwindow_DeleteScene() {
+
+    // Hide All Popups
+    bool ShowDeleteConfirm_ = false;
+    bool ShowOneSceneDeleteError_ = false;
+
+}
+
+void Subwindow_DeleteScene::DeleteScene(int SceneIndex) {
+
+    // Enable Firstframe
+    FirstFrame_ = true;
+
+    // Show Popup Windows
+    if (SceneManager_->Scenes_.size() != 1) {
+        SceneIndex_ = SceneIndex;
+        ShowDeleteConfirm_ = true;
+    } else {
+        ShowOneSceneDeleteError_ = true;
+    }
+
+    
+}
+
+void Subwindow_DeleteScene::Draw() {
+
+    // Update Windows
+    UpdateConfirmDeletePopup();
+    UpdateOneSceneDeleteErrorPopup();
+
+}
+
+void Subwindow_DeleteScene::UpdateConfirmDeletePopup() {
+
+    // Begin Window
+    if (ShowDeleteConfirm_) {
+    ImGui::Begin("Delete Scene?", &ShowDeleteConfirm_, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoCollapse);
+
+        // Grab Window Focus
+        ImGui::SetWindowFocus();
+        
+        // Set Default Keyboard Input
+        if (FirstFrame_) {
+            ImGui::SetKeyboardFocusHere(0);
+            FirstFrame_ = false;
+        }
+
+        // Add Confirm Message
+        ImGui::SetItemDefaultFocus();
+        ImGui::Text("This action will delete the selected scene.");
+        ImGui::Separator();
+
+        // Confirm And Abort Buttons
+        if (ImGui::Button("Confirm", ImVec2(120, 0)) || ImGui::IsKeyPressed(GLFW_KEY_ENTER)) { // If Button Pressed, Or Enter Key Pressed
+            if (SceneManager_->Scenes_.size() != 1) {
+                SceneManager_->Scenes_.erase(SceneManager_->Scenes_.begin() + SceneIndex_);
+            }
+            ShowDeleteConfirm_ = false;
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Abort", ImVec2(120, 0)) || ImGui::IsKeyPressed(GLFW_KEY_ESCAPE)) { // If Button Pressed, Or Escape Key Pressed
+            ShowDeleteConfirm_ = false;
+        }
+
+    ImGui::End();
+
+
+    }
+
+}
+
+
+void Subwindow_DeleteScene::UpdateOneSceneDeleteErrorPopup() {
+
+    // Begin Popup
+    if (ShowOneSceneDeleteError_) {
+    ImGui::Begin("Error", &ShowOneSceneDeleteError_, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoCollapse);
+
+        // Grab Window Focus
+        ImGui::SetWindowFocus();
+        
+        // Set Default Keyboard Input
+        if (FirstFrame_) {
+            ImGui::SetKeyboardFocusHere(0);
+            FirstFrame_ = false;
+        }
+
+        // Add Message
+        ImGui::SetItemDefaultFocus();
+        ImGui::Text("You must have at least one scene.");
+        ImGui::Separator();
+
+        // Close Button
+        if (ImGui::Button("Close", ImVec2(120, 0)) || ImGui::IsKeyPressed(GLFW_KEY_ESCAPE) || ImGui::IsKeyPressed(GLFW_KEY_ENTER)) { // If Button Pressed, Or Enter/Escape Key Pressed
+            ShowOneSceneDeleteError_ = false;
+        }
+
+    ImGui::End();
+    }
+
+}
---
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_SceneTree/Popups/GUI_Subwindow_DeleteModel/GUI_Subwindow_DeleteScene.h
@@ -0,0 +1,77 @@
+ 
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This Window file is responsible for creating a system Window.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-07
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <string>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <imgui.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <SceneManager.h>
+#include <Scene.h>
+
+
+/**
+ * @brief Implement Scene Deltion, Open Popup For Confirm, Error.
+ * 
+ */
+class Subwindow_DeleteScene {
+
+    // Private Vars
+    private:
+
+        SceneManager* SceneManager_; /**<SceneManager Instance*/
+        int SceneIndex_ = 0; /**<SceneIndex Used To Remember What Scene To Delete*/
+
+        bool FirstFrame_ = false; /**<Used to grab keyboard input on first frame window is open*/
+
+        bool ShowDeleteConfirm_ = false; /**<Confirm Deletion Of Scene*/
+        bool ShowOneSceneDeleteError_ = false; /**<Error popup that shows when one scene is remaining, (Can't have no scenes)*/
+
+    // Private Functions
+    private:
+
+        void UpdateConfirmDeletePopup(); /**<Function To Update Popup Deletion Window*/
+        void UpdateOneSceneDeleteErrorPopup(); /**<Last scene deletion error popup*/
+
+    public:
+
+        /**
+         * @brief Construct a new Subwindow_DeleteScene object
+         * 
+         */
+        Subwindow_DeleteScene(SceneManager* SceneManager);
+
+        /**
+         * @brief Destroy the Subwindow_DeleteScene object
+         * 
+         */
+        ~Subwindow_DeleteScene();
+
+        /**
+         * @brief Called by the GUI to delete a scene.
+         * 
+         * @param SceneIndex 
+         */
+        void DeleteScene(int SceneIndex);
+
+        /**
+         * @brief Update all windows created by this class. Call every frame.
+         * 
+         */
+        void Draw();
+
+};
