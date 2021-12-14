e27f59531bc7c07addc856962c13262f7c8596e3
datacrystals <tliao@carboncopies.org>
2021-12-14 08:57:40 -0800

    Implement Multithreaded Model Loading

--- Source/Core/Loader/ModelLoader/ModelLoader.cpp
+++ Source/Core/Loader/ModelLoader/ModelLoader.cpp
@@ -112,7 +112,6 @@ std::future<ERS_OBJECT_MODEL> ModelLoader::AsyncLoadModel(const char* AssetPath,
 
 // Load Model From File
 ERS_OBJECT_MODEL ModelLoader::LoadModelFromFile(std::string AssetPath, bool FlipTextures, bool DeIncrimentThreadCount) {
-    std::cout<<AssetPath<<std::endl;
 
     // Clear Model Instance
     ERS_OBJECT_MODEL Model;
