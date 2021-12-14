9b200cca9287e4cea359d1bce701f9b57c113271
datacrystals <tliao@carboncopies.org>
2021-12-14 12:09:40 -0800

    Implement Multithreaded Texture Preloading

--- Source/Core/Loader/ModelLoader/ModelLoader.cpp
+++ Source/Core/Loader/ModelLoader/ModelLoader.cpp
@@ -177,7 +177,11 @@ std::future<ERS_OBJECT_MODEL> ModelLoader::AsyncLoadModel(const char* AssetPath,
 ERS_OBJECT_MODEL ModelLoader::LoadModelFromFile(std::string AssetPath, bool FlipTextures, bool IsThread) {
 
     // Check If Metadata Exists
-    YAML::Node ModelMetadata = YAML::LoadFile(std::string(AssetPath.substr(0, AssetPath.find_last_of("/")) + std::string("Metadata.yaml")).c_str());
+    try {
+        YAML::Node ModelMetadata = YAML::LoadFile(std::string(AssetPath.substr(0, AssetPath.find_last_of("/")) + std::string("Metadata.yaml")).c_str());
+    } catch (YAML::BadFile) {
+        Logger_->Log(std::string(std::string("Metadata Is Missing For Model '") + std::string(AssetPath) + std::string("', Try Regenerating It")).c_str(), 7);
+    }
 
     // Clear Model Instance
     ERS_OBJECT_MODEL Model;
