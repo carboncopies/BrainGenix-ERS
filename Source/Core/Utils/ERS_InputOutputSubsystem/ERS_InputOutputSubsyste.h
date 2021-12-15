10309b55e410ea8331952c7f8c6158ce5d579a84
datacrystals <tliao@carboncopies.org>
2021-12-15 06:46:48 -0800

    Implement IO Operations Manager

---
+++ Source/Core/Utils/ERS_InputOutputSubsystem/ERS_InputOutputSubsyste.h
@@ -0,0 +1,53 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is responsible for providing a single I/O interface for asset loading/saving.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-19
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <LoggingSystem.h>
+
+
+/**
+ * @brief This class provides a single input/ouput file operation system. It is thread safe.
+ * 
+ */
+class ERS_CLASS_InputOutputSubsystem {
+
+    private:
+
+        std::shared_ptr<LoggerClass> Logger_; /**<Logging Class Pointer*/
+        bool UseDatabase; /**<Indicates If The Database Should Be Used For IO. Set By LocalSystemConfiguration Values In Constructor*/
+
+    public:
+
+        /**
+         * @brief Construct a new ers class inputoutputsubsystem object
+         * 
+         * @param Logger 
+         * @param LocalSystemConfiguration 
+         */
+        ERS_CLASS_InputOutputSubsystem(std::shared_ptr<LoggerClass> Logger, YAML::Node LocalSystemConfiguration);
+        
+        /**
+         * @brief Destroy the ers class inputoutputsubsystem object
+         * 
+         */
+        ~ERS_CLASS_InputOutputSubsystem();
+
+        
+
+};
