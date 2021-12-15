9b9dbb2e27a7b1a9c2944326ac0187103c91b5ae
datacrystals <tliao@carboncopies.org>
2021-12-14 18:25:03 -0800

    Implement Model Writer

---
+++ Source/Core/Writers/ModelWriter/ModelWriter.cpp
@@ -0,0 +1,14 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file contains the model writer class.
+    Documentation Status: Completed
+    Additonal Notes: None
+    Date Created: 2021-11-22
+*/
+
+#include <ModelWriter.h>
+
+
---
+++ Source/Core/Writers/ModelWriter/ModelWriter.h
@@ -0,0 +1,29 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file contains the model writer class.
+    Documentation Status: Completed
+    Additonal Notes: None
+    Date Created: 2021-11-22
+*/
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <string>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <LoggingSystem.h>
+#include <Model.h>
+#include <ModelLoader.h>
+#include <Scene.h>
+#include <Mesh.h>
+#include <2DTexture.h>
+
+
+#include <Shader.h>
