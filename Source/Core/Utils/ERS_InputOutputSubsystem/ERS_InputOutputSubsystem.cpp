639630806c0284a81b1f75ee20a2a6ffb4f5711c
datacrystals <tliao@carboncopies.org>
2021-12-16 10:53:57 -0800

    Update IOSubsystem To Index Used Asset IDs

--- Source/Core/Utils/ERS_InputOutputSubsystem/ERS_InputOutputSubsystem.cpp
+++ Source/Core/Utils/ERS_InputOutputSubsystem/ERS_InputOutputSubsystem.cpp
@@ -91,7 +91,7 @@ void ERS_CLASS_InputOutputSubsystem::IndexUsedAssetIDs() {
 
             // Get File Path        
             std::string FilePath{Entry.path().u8string()};
-            FilePath = FilePath.substr(0, FilePath.find_last_of(".")).substr(FilePath.find_last_of("/"), FilePath.length());
+            FilePath = FilePath.substr(0, FilePath.find_last_of(".")).substr(FilePath.find_last_of("/") + 1, FilePath.length());
 
             std::cout<<FilePath<<std::endl;
             // Convert To Long, Throw Log Message If Not Number
