0c04b82dce3c108175f9fbd3afe3c0590d59bbdd
datacrystals <tliao@carboncopies.org>
2021-12-16 14:44:40 -0800

    Add System Utils Struct To Simplify Argument Lens

--- Source/Core/Structures/ERS_STRUCT_SystemUtils/ERS_STRUCT_SystemUtils.h
+++ Source/Core/Structures/ERS_STRUCT_SystemUtils/ERS_STRUCT_SystemUtils.h
@@ -15,9 +15,12 @@
 #include <memory.h>
 
 // Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
 
 // Internal Libraries (BG convention: use <> instead of "")
 #include <LoggingSystem.h>
+#include <ERS_InputOutputSubsystem.h>
+#include <ERS_ModelWriter.h>
 
 /**
  * @brief The System utils struct contains most system utils prepopulated as shared pointers within.
@@ -26,6 +29,7 @@
 struct ERS_STRUCT_SystemUtils {
 
     std::shared_ptr<LoggerClass> Logger_; /**<Pointer To BG-ERS Logging System*/
-
+    std::shared_ptr<YAML::Node> LocalSystemConfiguration_; /**<Pointer To Config.yaml Data From Local Hard Drive*/
+    std::shared_ptr<
 
 };
