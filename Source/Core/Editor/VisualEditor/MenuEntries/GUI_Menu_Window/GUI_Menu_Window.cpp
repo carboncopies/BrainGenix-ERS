46cb7f6c47000bf680d5c8868080382273c164a0
datacrystals <tliao@carboncopies.org>
2021-12-17 08:30:08 -0800

    Move Window Menu To Seperate Class

---
+++ Source/Core/Editor/VisualEditor/MenuEntries/GUI_Menu_Window/GUI_Menu_Window.cpp
@@ -0,0 +1,64 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file provides a GUI menu entry.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-17
+*/
+
+#include <GUI_Menu_View.h>
+
+
+// Constructor
+GUI_Menu_View::GUI_Menu_View(ERS_STRUCT_SystemUtils SystemUtils, std::shared_ptr<ERS_CLASS_ThemeManager> ThemeManager, std::shared_ptr<ERS_CLASS_FontManager> FontManager) {
+
+    // Copy In Pointer Struct
+    SystemUtils_ = SystemUtils;
+    ThemeManager_ = ThemeManager;
+    FontManager_ = FontManager;
+
+    // Log Initialization
+    SystemUtils_.Logger_->Log("Editor Setting Up View Menu", 4);
+
+    // Create Class Instances
+    Window_ThemeSelector_ = std::make_unique<Window_ThemeSelector>(ThemeManager_); 
+
+
+}
+
+// Destructor
+GUI_Menu_View::~GUI_Menu_View() {
+
+    // Log Destructor
+    SystemUtils_.Logger_->Log("Editor Destroying View Menu", 4);
+
+}
+
+// Draw Function
+void GUI_Menu_View::Draw() {
+
+    // View Menu
+    if (ImGui::BeginMenu("View")) {
+
+
+        if (ImGui::MenuItem("Color Theme")) {
+            Window_ThemeSelector_->Enabled_ = true;
+        }
+
+        if (ImGui::MenuItem("System Font")) {
+            ShowFontPicker_ = true;
+        }
+
+
+    ImGui::EndMenu();
+    }
+
+
+    // Draw Windows
+    Window_ThemeSelector_->Draw();
+    FontManager_->FontSelectorWindow(&ShowFontPicker_);
+
+}
---
+++ Source/Core/Editor/VisualEditor/MenuEntries/GUI_Menu_Window/GUI_Menu_Window.h
@@ -0,0 +1,81 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file provides a GUI menu entry.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-17
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <vector>
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <memory>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <imgui.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_STRUCT_SystemUtils.h>
+
+#include <ERS_Editor_ThemeManager.h>
+#include <ERS_Editor_FontManager.h>
+
+#include <GUI_Window_ThemeSelector.h>
+
+
+
+
+/**
+ * @brief This class provides the "File" Menu In The Editor.
+ * 
+ */
+class GUI_Menu_View {
+
+
+    // Member Vars
+    private:
+
+        ERS_STRUCT_SystemUtils SystemUtils_; /**<SystemUtils Instance, Used To Get Systems From Other Classes*/
+        std::shared_ptr<ERS_CLASS_ThemeManager> ThemeManager_; /**<Theme Manager Instance Ptr*/
+        std::unique_ptr<Window_ThemeSelector> Window_ThemeSelector_; /**<Theme Selector Window*/
+        std::shared_ptr<ERS_CLASS_FontManager> FontManager_; /**<Pointer To FontManager Instance*/
+
+        // Window Show/Hide Vars
+        bool ShowFontPicker_ = false; /**<Control Varaible to show/hide font picker window*/
+
+
+    // Member Functions
+    public:
+
+
+        /**
+         * @brief Construct a new gui menu object
+         * 
+         * @param SystemUtils 
+         */
+        GUI_Menu_View(ERS_STRUCT_SystemUtils SystemUtils, std::shared_ptr<ERS_CLASS_ThemeManager> ThemeManager, std::shared_ptr<ERS_CLASS_FontManager> FontManager);
+
+
+        /**
+         * @brief Destroy the gui menu object
+         * 
+         */
+        ~GUI_Menu_View();
+
+
+        /**
+         * @brief This function Draws The View Menu Contents.
+         * 
+         */
+        void Draw();
+
+
+};
