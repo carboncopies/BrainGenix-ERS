82835b5eb9bf0fc344a060522ce1f205a71e9acb
datacrystals <tliao@carboncopies.org>
2021-12-17 07:15:12 -0800

    Implement Import Functionality

--- Source/Core/Editor/VisualEditor/MenuEntries/GUI_Menu_File/GUI_Menu_File.cpp
+++ Source/Core/Editor/VisualEditor/MenuEntries/GUI_Menu_File/GUI_Menu_File.cpp
@@ -43,20 +43,26 @@ void GUI_Menu_File::Draw() {
     // File Menu
     if (ImGui::BeginMenu("File")) {
 
-        if (ImGui::MenuItem("Save Active Scene")) {
-            SceneWriter_->ProcessScene(SceneManager_->Scenes_[SceneManager_->ActiveScene_], SceneManager_->Scenes_[SceneManager_->ActiveScene_].ScenePath.c_str());
-        }
-
-        if (ImGui::MenuItem("Save All")) {
-
-            for (int i = 0; i < SceneManager_->Scenes_.size(); i++) {
-                SceneWriter_->ProcessScene(SceneManager_->Scenes_[i], SceneManager_->Scenes_[i].ScenePath.c_str());
-            }
-
+        // Import Option
+        if (ImGui::MenuItem("Import Model")) {
+            
         }
 
         ImGui::Separator();
 
+        // Save Options
+        if (ImGui::MenuItem("Save Active Scene")) {
+            SceneWriter_->ProcessScene(SceneManager_->Scenes_[SceneManager_->ActiveScene_], SceneManager_->Scenes_[SceneManager_->ActiveScene_].ScenePath.c_str());
+        }
+        if (ImGui::MenuItem("Save All")) {
+            for (int i = 0; i < SceneManager_->Scenes_.size(); i++) {
+                SceneWriter_->ProcessScene(SceneManager_->Scenes_[i], SceneManager_->Scenes_[i].ScenePath.c_str());
+            }
+        }
+
+        ImGui::Separator();
+
+        // Exit Options
         if (ImGui::MenuItem("Exit")) {
             *SystemUtils_.SystemShouldRun_ = false;
         }
