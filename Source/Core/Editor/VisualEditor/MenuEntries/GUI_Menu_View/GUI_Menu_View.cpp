b2f45562f7d20387366e3fa2f9d10beda1ebcad0
datacrystals <tliao@carboncopies.org>
2021-12-17 07:45:38 -0800

    Move View Menu To Seperate Class

---
+++ Source/Core/Editor/VisualEditor/MenuEntries/GUI_Menu_View/GUI_Menu_View.cpp
@@ -0,0 +1,74 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file provides a GUI menu entry.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-17
+*/
+
+#include <GUI_Menu_View.h>
+
+
+// Constructor
+GUI_Menu_View::GUI_Menu_View(ERS_STRUCT_SystemUtils SystemUtils, std::shared_ptr<SceneManager> SceneManager) {
+
+    // Copy In Pointer Struct
+    SystemUtils_ = SystemUtils;
+    SceneManager_ = SceneManager;
+
+    // Log Initialization
+    SystemUtils_.Logger_->Log("Editor Setting Up File Menu", 4);
+
+    // Create Class Instances
+    SceneWriter_ = std::make_unique<SceneWriter>(SystemUtils_.Logger_);
+
+
+
+}
+
+// Destructor
+GUI_Menu_View::~GUI_Menu_View() {
+
+    // Log Destructor
+    SystemUtils_.Logger_->Log("Editor Destroying File Menu", 4);
+
+}
+
+// Draw Function
+void GUI_Menu_View::Draw() {
+
+    // File Menu
+    if (ImGui::BeginMenu("File")) {
+
+        // Import Option
+        if (ImGui::MenuItem("Import Model")) {
+            
+        }
+
+        ImGui::Separator();
+
+        // Save Options
+        if (ImGui::MenuItem("Save Active Scene")) {
+            SceneWriter_->ProcessScene(SceneManager_->Scenes_[SceneManager_->ActiveScene_], SceneManager_->Scenes_[SceneManager_->ActiveScene_].ScenePath.c_str());
+        }
+        if (ImGui::MenuItem("Save All")) {
+            for (int i = 0; i < SceneManager_->Scenes_.size(); i++) {
+                SceneWriter_->ProcessScene(SceneManager_->Scenes_[i], SceneManager_->Scenes_[i].ScenePath.c_str());
+            }
+        }
+
+        ImGui::Separator();
+
+        // Exit Options
+        if (ImGui::MenuItem("Exit")) {
+            *SystemUtils_.SystemShouldRun_ = false;
+        }
+
+        
+    ImGui::EndMenu();
+    }
+
+}
---
+++ Source/Core/Editor/VisualEditor/MenuEntries/GUI_Menu_View/GUI_Menu_View.h
@@ -0,0 +1,77 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file provides a GUI menu entry.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-17
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <vector>
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <memory>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
+
+#include <imgui.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <LoggingSystem.h>
+#include <ERS_STRUCT_SystemUtils.h>
+
+#include <ERS_SceneWriter.h>
+#include <SceneManager.h>
+
+
+
+
+/**
+ * @brief This class provides the "File" Menu In The Editor.
+ * 
+ */
+class GUI_Menu_View {
+
+    // Member Vars
+    private:
+
+        ERS_STRUCT_SystemUtils SystemUtils_; /**<SystemUtils Instance, Used To Get Systems From Other Classes*/
+        std::unique_ptr<SceneWriter> SceneWriter_; /**<Scene Writer Instance Pointer*/
+        std::shared_ptr<SceneManager> SceneManager_; /**<Scene Manager Instance Pointer*/
+
+
+    // Member Functions
+    public:
+
+
+        /**
+         * @brief Construct a new gui menu object
+         * 
+         * @param SystemUtils 
+         */
+        GUI_Menu_View(ERS_STRUCT_SystemUtils SystemUtils, std::shared_ptr<SceneManager> SceneManager);
+
+
+        /**
+         * @brief Destroy the gui menu object
+         * 
+         */
+        ~GUI_Menu_View();
+
+
+        /**
+         * @brief This function Draws The View Menu Contents.
+         * 
+         */
+        void Draw();
+
+
+};
