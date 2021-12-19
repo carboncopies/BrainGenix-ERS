c247f46699f141c370187c3008708176bee45dfc
datacrystals <tliao@carboncopies.org>
2021-12-18 18:00:54 -0800

    Add Asset Importer Util

---
+++ Source/Core/Editor/VisualEditor/MenuEntries/GUI_Menu_File/Utils/Util_Import_Asset/ERS_Editor_ImportAsset.cpp
@@ -0,0 +1,10 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file provides asset import functionality.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-18
+*/
---
+++ Source/Core/Editor/VisualEditor/MenuEntries/GUI_Menu_File/Utils/Util_Import_Asset/ERS_Editor_ImportAsset.h
@@ -0,0 +1,21 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file provides asset import functionality.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-18
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <memory>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
