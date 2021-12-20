9c14bfb3bf79fcd8fcbc89a4038b46c168f9e4dd
datacrystals <tliao@carboncopies.org>
2021-12-20 07:28:09 -0800

    Implement Import System

--- Source/Core/Editor/VisualEditor/MenuEntries/GUI_Menu_File/Utils/Import_Asset/GUI_ImportAsset.cpp
+++ Source/Core/Editor/VisualEditor/MenuEntries/GUI_Menu_File/Utils/Import_Asset/GUI_ImportAsset.cpp
@@ -65,12 +65,9 @@ void GUI_ImportAsset::Draw() {
 
     // Update Window Stats
     if (Window_ImportProgressBar_->Enabled_) {
-
-        Window_ImportProgressBar_->UpdateSubitems()
-
+        Window_ImportProgressBar_->UpdateSubitems(AssetImportBackend_->GetTotalSubitemsImported(), AssetImportBackend_->GetTotalSubitems());
+        Window_ImportProgressBar_->UpdateTotalItems(AssetImportBackend_->GetTotalItemsImported(), AssetImportBackend_->GetTotalItemsToImport());
     }
-
-
 
     Window_ImportProgressBar_->Draw();
 
