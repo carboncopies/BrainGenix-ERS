535680fa012f38c161ae6f3a8e6e42cc5edcd18b
datacrystals <tliao@carboncopies.org>
2021-12-20 14:33:09 -0800

    Fix Issue With Import Window

---
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_SystemLog/GUI_Window_SystemLog.cpp
@@ -0,0 +1,95 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This Window file is responsible for creating a system Window.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-17
+*/
+
+#include <GUI_Window_SystemLog.h>
+
+
+// Constructor
+Window_SystemLog::Window_SystemLog(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils) {
+
+    // Copy System Utils Pointer Struct
+    SystemUtils_ = SystemUtils;
+
+}
+
+// Destructor
+Window_SystemLog::~Window_SystemLog() {
+
+}
+
+
+
+// Update Total Model Stats
+void Window_SystemLog::UpdateTotalItems(long Current, long Total) {
+
+    // Update Totals
+    CurrentAssetNumber_ = Current;
+    TotalAssetsToImport_ = Total;
+
+}
+
+// Update Job State
+void Window_SystemLog::UpdateJobState(bool JobFinished) {
+
+    // Set Job State
+    if (JobFinished) {
+        ConsecFinished_ ++;
+        IsJobFinishing_ = true;
+    } else {
+        ConsecFinished_ = 0;
+        IsJobFinishing_ = false;
+    }
+
+    // Hide Window After Threshold Reached
+    if (ConsecFinished_ >= ConsecFinishedThreshold_) {
+        Enabled_ = false;
+        ConsecFinished_ = 0;
+    }
+
+}
+
+// Draw Window
+void Window_SystemLog::Draw() {
+
+
+    if (Enabled_) {
+    ImGuiWindowFlags WindowFlags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+    ImGui::Begin("Import Status", &Enabled_, WindowFlags);
+
+        // Set Window Size
+        ImGui::SetWindowSize(ImVec2(300,0));
+
+        // Set Window Position
+        ImVec2 WindowSize = ImGui::GetWindowSize();
+        ImVec2 WindowPos = ImVec2(SystemUtils_->RenderWidth_ - WindowSize.x, SystemUtils_->RenderHeight_ - WindowSize.y);
+        ImGui::SetWindowPos(WindowPos);
+
+        // Calculate Stats
+        if (!IsJobFinishing_) {
+            if (TotalAssetsToImport_ == 0) {
+                PercentDone_ = 0.0f;
+            } else {
+                PercentDone_ = (float)CurrentAssetNumber_ / (float)TotalAssetsToImport_;
+            }
+        } else {
+            PercentDone_ = 1.0f;
+        }
+
+        // Draw Total Progres Bar
+        ImGui::Text("Progress");
+        ImGui::ProgressBar(PercentDone_);
+
+
+    ImGui::End();
+    }
+
+
+}
---
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_SystemLog/GUI_Window_SystemLog.h
@@ -0,0 +1,78 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This Window file is responsible for creating a system Window.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-17
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <memory>
+#include <thread>
+#include <string>
+#include <mutex>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <imgui.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_STRUCT_SystemUtils.h>
+
+/**
+ * @brief Window for the Import progress bar.
+ * 
+ */
+class Window_SystemLog {
+
+    private:
+
+        std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils_; /**<Pointers To System Resources*/
+
+    public:
+
+        bool Enabled_ = false; /**<Is Popup Enabled*/
+
+
+    public:
+
+        /**
+         * @brief Construct a new Window_SystemLog object
+         * 
+         */
+        Window_SystemLog(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils);
+        
+        /**
+         * @brief Destroy the Window_SystemLog object
+         * 
+         */
+        ~Window_SystemLog();
+
+
+        /**
+         * @brief Update Total items being processed (progress bar)
+         * 
+         * @param Current 
+         * @param Total 
+         */
+        void UpdateTotalItems(long Current, long Total);
+
+        /**
+         * @brief close the window a few frames after the job finishes
+         * 
+         * @param JobState 
+         */
+        void UpdateJobState(bool JobFinished);
+
+        /**
+         * @brief Draw import bar if needed (call every frame)
+         * 
+         */
+        void Draw();
+
+};
