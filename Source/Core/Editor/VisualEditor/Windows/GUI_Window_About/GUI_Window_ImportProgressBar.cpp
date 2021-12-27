a9afb89dc1d161c3a1645876c21915edfc69bcea
datacrystals <tliao@carboncopies.org>
2021-12-27 10:19:42 -0800

    Add About Window To View Menu

---
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_About/GUI_Window_ImportProgressBar.cpp
@@ -0,0 +1,95 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This Window file is responsible for creating a system Window.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-17
+*/
+
+#include <GUI_Window_About.h>
+
+
+// Constructor
+Window_About::Window_About(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils) {
+
+    // Copy System Utils Pointer Struct
+    SystemUtils_ = SystemUtils;
+
+}
+
+// Destructor
+Window_About::~Window_About() {
+
+}
+
+
+
+// Update Total Model Stats
+void Window_About::UpdateTotalItems(long Current, long Total) {
+
+    // Update Totals
+    CurrentAssetNumber_ = Current;
+    TotalAssetsToImport_ = Total;
+
+}
+
+// Update Job State
+void Window_About::UpdateJobState(bool JobFinished) {
+
+    // Set Job State
+    if (JobFinished) {
+        ConsecFinished_ ++;
+        IsJobFinishing_ = true;
+    } else {
+        ConsecFinished_ = 0;
+        IsJobFinishing_ = false;
+    }
+
+    // Hide Window After Threshold Reached
+    if (ConsecFinished_ >= ConsecFinishedThreshold_) {
+        Enabled_ = false;
+        ConsecFinished_ = 0;
+    }
+
+}
+
+// Draw Window
+void Window_About::Draw() {
+
+
+    if (Enabled_) {
+    ImGuiWindowFlags WindowFlags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+    ImGui::Begin("Import Status", &Enabled_, WindowFlags);
+
+        // Set Window Size
+        ImGui::SetWindowSize(ImVec2(300,0));
+
+        // Set Window Position
+        ImVec2 WindowSize = ImGui::GetWindowSize();
+        ImVec2 WindowPos = ImVec2(SystemUtils_->RenderWidth_ - WindowSize.x, SystemUtils_->RenderHeight_ - WindowSize.y);
+        ImGui::SetWindowPos(WindowPos);
+
+        // Calculate Stats
+        if (!IsJobFinishing_) {
+            if (TotalAssetsToImport_ == 0) {
+                PercentDone_ = 0.0f;
+            } else {
+                PercentDone_ = (float)CurrentAssetNumber_ / (float)TotalAssetsToImport_;
+            }
+        } else {
+            PercentDone_ = 1.0f;
+        }
+
+        // Draw Total Progres Bar
+        ImGui::Text("Progress");
+        ImGui::ProgressBar(PercentDone_);
+
+
+    ImGui::End();
+    }
+
+
+}
