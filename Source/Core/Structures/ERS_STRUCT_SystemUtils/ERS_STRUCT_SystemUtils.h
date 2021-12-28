9451b07c23af1a91e6069aac2ca5eed43210245d
datacrystals <tliao@carboncopies.org>
2021-12-28 11:13:56 -0800

    Implement Framerate Cap

--- Source/Core/Structures/ERS_STRUCT_SystemUtils/ERS_STRUCT_SystemUtils.h
+++ Source/Core/Structures/ERS_STRUCT_SystemUtils/ERS_STRUCT_SystemUtils.h
@@ -35,8 +35,11 @@ struct ERS_STRUCT_SystemUtils {
     std::shared_ptr<ERS_CLASS_InputOutputSubsystem> ERS_IOSubsystem_; /**<Pointer To ERS Input Output Subsystem*/
     std::shared_ptr<ERS_CLASS_ModelWriter> ERS_ModelWriter_; /**<Pointer To ERS Model Writer Instance*/
     std::shared_ptr<bool> SystemShouldRun_; /**<Pointer To Variable Setting If System Should Run Or Not*/
+    std::shared_ptr<ERS_CLASS_FramerateManager> FramerateManager_; /**<Pointer to framerate subsystem*/
+
     int RenderWidth_ = 0; /**<Width of the display being rendered to (if using a window, this is the size of the window)*/
     int RenderHeight_ = 0; /**<Height of the display being rendered to (if using a window, this is the size of the window)*/
+    
     bool IsLinux_ = true; /**<Boolean Value Set To True When Binary Is Compiled For Linux, False Otherwise*/
 
 };
