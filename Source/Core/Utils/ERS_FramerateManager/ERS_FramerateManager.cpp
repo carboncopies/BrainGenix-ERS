0cff535e1171d25ac67b901ce4750fd9f7d46520
datacrystals <tliao@carboncopies.org>
2021-12-28 01:00:30 -0800

    Implement Framerate Cap

---
+++ Source/Core/Utils/ERS_FramerateManager/ERS_FramerateManager.cpp
@@ -0,0 +1,10 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is responsible for providing a system for managing the framerate, it also provides FR related metrics.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-28
+*/
