1e0bdbb1c5731c8bfe165b5591188928e552d9aa
datacrystals <tliao@carboncopies.org>
2021-12-28 12:18:01 -0800

    Implement Framerate Cap

--- Source/Core/Utils/ERS_FramerateManager/ERS_FramerateManager.cpp
+++ Source/Core/Utils/ERS_FramerateManager/ERS_FramerateManager.cpp
@@ -51,7 +51,7 @@ void ERS_CLASS_FramerateManager::DelayUntilNextFrame() {
 
     // Calculate Delta Time
     double FrameTime = (FrameEndTime_ - FrameStartTime_);
-    double FrameDelta = TargetFrameTime_ * 1000000000.0f;
+    double FrameDelta = TargetFrameTime_ * 1000.0f;
     double TargetTime = FrameEndTime_ + FrameDelta;
     std::cout<<FrameEndTime_ << "|"<< TargetTime<<std::endl;
 
