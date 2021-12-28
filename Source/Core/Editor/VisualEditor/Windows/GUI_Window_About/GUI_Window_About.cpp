c1623b680dd7f3b01d579195e98d68785a49dc2f
datacrystals <tliao@carboncopies.org>
2021-12-27 21:36:21 -0800

    Add About Window To View Menu

--- Source/Core/Editor/VisualEditor/Windows/GUI_Window_About/GUI_Window_About.cpp
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_About/GUI_Window_About.cpp
@@ -49,18 +49,18 @@ void Window_About::Draw() {
 
         // License Info
         ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), "License:");
-        ImGui::Text("This program is free software: you can redistribute it and/or modify "
+        ImGui::TextWrapped("This program is free software: you can redistribute it and/or modify "
                     "it under the terms of the GNU Affero General Public License as "
                     "published by the Free Software Foundation, either version 3 of the "
                     "License, or (at your option) any later version. "
 
                     "This program is distributed in the hope that it will be useful, "
                     "but WITHOUT ANY WARRANTY; without even the implied warranty of "
-                    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+                    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the "
                     "GNU Affero General Public License for more details. "
 
                     "You should have received a copy of the GNU Affero General Public License "
-                    "along with this program.  If not, see <https://www.gnu.org/licenses/>.");
+                    "along with this program. If not, see <https://www.gnu.org/licenses/>.");
 
 
 
