20e9418c52d3f71c2f31409698db4ccccfb43f33
datacrystals <tliao@carboncopies.org>
2021-12-28 11:14:24 -0800

    Implement Framerate Cap

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -71,6 +71,7 @@ int main() {
     // Setup Framerate Manager
     sERSLogger->Log("Initializing Framerate Manager Subsystem", 5);
     std::shared_ptr<ERS_CLASS_FramerateManager> sERSFrameRateManager = std::make_shared<ERS_CLASS_FramerateManager>(SystemUtils);
+    SystemUtils->FramerateManager_ = sERSFrameRateManager;
     sERSLogger->Log("Initialized Framerate Manager", 4);
 
     // Copy Config Params
