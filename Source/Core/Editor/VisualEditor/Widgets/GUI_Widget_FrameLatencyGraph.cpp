0a59c8ac409d7ea98cfbbfcc3c77e72be4e64762
datacrystals <tliao@carboncopies.org>
2021-12-28 22:31:43 -0800

    Add Frame Latency Graph

---
+++ Source/Core/Editor/VisualEditor/Widgets/GUI_Widget_FrameLatencyGraph.cpp
@@ -0,0 +1,60 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This widget file is responsible for creating a system widget.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-02
+*/
+
+#include <GUI_Widget_FramerateGraph.h>
+
+
+// Define Draw Function
+void Widget_FramerateGraph::Draw() {
+
+    // Push Back Current Framerate To Vector
+    FramerateHistory_.push_back(ImGui::GetIO().Framerate);
+    
+
+
+    // Cap Array Length At Target Set
+    if (FramerateHistory_.size() > HistoryLength_) {
+        FramerateHistory_.erase(FramerateHistory_.begin());
+    } else {
+
+        // Fill With Zeros
+        int Delta = HistoryLength_ - FramerateHistory_.size();
+        for (int i = 0; i < Delta; i++) {
+            FramerateHistory_.push_back(0.0f);
+        }
+
+    }
+
+    // If Window Drawing Enabled
+    if (Enabled_) {
+        ImGui::Begin("Framerate Graph", &Enabled_);
+
+            // Set Initial Window Size
+            ImGui::SetWindowSize(ImVec2(300,250), ImGuiCond_FirstUseEver);
+
+            // Get Window Size (To Size Graph)
+            ImVec2 WindowSize = ImGui::GetContentRegionAvail();
+            ImVec2 GraphSize = ImVec2(WindowSize.x, WindowSize.y);
+
+            // Graph
+            ImPlot::SetNextAxesToFit();
+            ImPlot::BeginPlot("Framerate Graph", GraphSize);
+            ImPlot::PlotLine("Framerate", (const float*)FramerateHistory_.data(), FramerateHistory_.size());
+            ImPlot::EndPlot();
+
+        // End System Info Window
+        ImGui::End();
+        
+
+    }
+
+
+}
---
+++ Source/Core/Editor/VisualEditor/Widgets/GUI_Widget_FrameLatencyGraph.h
@@ -0,0 +1,49 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This widget file is responsible for creating a system widget.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-02
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <vector>
+#include <iostream>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <imgui.h>
+#include <implot.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
+/**
+ * @brief Create GUI Widget "Framerate Graph"
+ * 
+ */
+class Widget_FramerateGraph {
+
+    private:
+
+        std::vector<float> FramerateHistory_; /**<Vector To Store Framerate histories*/ 
+        int HistoryLength_ = 1000; /**<Set Framerate History Length*/
+
+    public:
+
+        // Show/Hide Var
+        bool Enabled_ = true; /**<Show/Hide Widget*/
+
+
+        /**
+         * @brief Function that draws the window, should internally check if enable variable is set to true.
+         * 
+         */
+        void Draw();
+
+};
