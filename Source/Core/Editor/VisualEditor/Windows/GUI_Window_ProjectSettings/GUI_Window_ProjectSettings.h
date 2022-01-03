075223964bf4ef98d73678690e910bfaf5755b5c
datacrystals <tliao@carboncopies.org>
2022-01-03 15:17:43 -0800

    Add Window Draw Command

--- Source/Core/Editor/VisualEditor/Windows/GUI_Window_ProjectSettings/GUI_Window_ProjectSettings.h
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_ProjectSettings/GUI_Window_ProjectSettings.h
@@ -35,7 +35,7 @@ private:
     char ProjectModificationDateBuffer[64]; /** Project Modification Date*/
     char ProjectLicenseNameBuffer[128]; /**<Project License Name String*/
     char ProjectDescriptionBuffer[16384]; /**<Project Description*/
-    bool IsProjectPropreitary; /**<Presents A Warning If Propreitary*/
+    bool IsProjectProprietary; /**<Presents A Warning If Propreitary*/
     
 
     std::shared_ptr<ERS_STRUCT_ProjectUtils> ProjectUtils_; /**<Pointer To Project Utils Instance*/
