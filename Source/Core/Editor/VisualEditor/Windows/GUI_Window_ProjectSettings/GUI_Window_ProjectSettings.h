fe318b9a8a9b8dfd41ac570fefa78e5c027841f6
datacrystals <tliao@carboncopies.org>
2022-01-03 13:59:02 -0800

    Add Default Scene Param

---
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_ProjectSettings/GUI_Window_ProjectSettings.h
@@ -0,0 +1,38 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This Window file is responsible for creating a system Window.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2022-01-03
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <imgui.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_STRUCT_ProjectUtils.h>
+#include <ERS_STRUCT_SystemUtils.h>
+
+
+
+class Window_ProjectSettings {
+
+private:
+
+    
+
+public:
+
+    bool Enabled = true; /**<Is Window Shown Or Hidden*/
+
+public:
+
+};
