f2fb6dddd35c19d19bce01d024382fa26ea64eaf
datacrystals <tliao@carboncopies.org>
2022-01-03 12:58:10 -0800

    Remove Unused Code

---
+++ Source/Core/Manager/ERS_ProjectManager/ERS_ProjectManager.cpp
@@ -0,0 +1,10 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is responsible for providing a class to manage project loading/writing/storage functions.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2022-01-03
+*/
---
+++ Source/Core/Manager/ERS_ProjectManager/ERS_ProjectManager.h
@@ -0,0 +1,42 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is responsible for providing a class to manage project loading/writing/storage functions.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2022-01-03
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <memory>
+#include <thread>
+#include <vector>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
+/**
+ * @brief Project Manager Class
+ * 
+ */
+class ERS_CLASS_ProjectManager {
+
+
+private:
+
+
+public:
+
+
+    ERS_CLASS_ProjectManager();
+    ~ERS_CLASS_ProjectManager();
+
+
+};
