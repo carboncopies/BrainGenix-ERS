ec1f5e1a7fa1b701bec8e1f4d1f3644ddc8ed214
datacrystals <tliao@carboncopies.org>
2022-01-03 00:53:04 -0800

    Test Load ERS Project File

--- Source/Core/Structures/ERS_STRUCT_Project/ERS_STRUCT_Project.h
+++ Source/Core/Structures/ERS_STRUCT_Project/ERS_STRUCT_Project.h
@@ -36,7 +36,7 @@ struct ERS_STRUCT_Project {
     std::string ProjectModificationDate = ""; /**<Date when project was last modified*/
     std::string ProjectCreator = ""; /**<Name Of Organization/Team/Person That Created The Project*/
     
-    std::string ProjectLicenseName = ""; /**<Name Of License Used By Project*/
+    std::string ProjectLicense = ""; /**<Name Of License Used By Project*/
     bool IsLicenseProprietary = false; /**<Presents a user warning when loading to make them aware that the project is not free software*/
 
     std::vector<long> SceneIDs; /**<Vector Of Scene IDs Used In The Project*/
