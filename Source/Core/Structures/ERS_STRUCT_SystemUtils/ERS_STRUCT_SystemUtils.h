07073bb8c71ab4416693f303017c9a049bfff98b
datacrystals <tliao@carboncopies.org>
2022-01-02 22:45:51 -0800

    Setup Project Loader

--- Source/Core/Structures/ERS_STRUCT_SystemUtils/ERS_STRUCT_SystemUtils.h
+++ Source/Core/Structures/ERS_STRUCT_SystemUtils/ERS_STRUCT_SystemUtils.h
@@ -30,16 +30,23 @@
  */
 struct ERS_STRUCT_SystemUtils {
 
+
     std::shared_ptr<LoggerClass> Logger_; /**<Pointer To BG-ERS Logging System*/
     std::shared_ptr<YAML::Node> LocalSystemConfiguration_; /**<Pointer To Config.yaml Data From Local Hard Drive*/
     std::shared_ptr<ERS_CLASS_InputOutputSubsystem> ERS_IOSubsystem_; /**<Pointer To ERS Input Output Subsystem*/
     std::shared_ptr<ERS_CLASS_ModelWriter> ERS_ModelWriter_; /**<Pointer To ERS Model Writer Instance*/
     std::shared_ptr<bool> SystemShouldRun_; /**<Pointer To Variable Setting If System Should Run Or Not*/
     std::shared_ptr<ERS_CLASS_FramerateManager> FramerateManager_; /**<Pointer to framerate subsystem*/
+    std::shared_ptr<SceneManager> SceneManager_; /**<Shared Ptr To Scene Manager*/
+    std::shared_ptr<ERS_CLASS_ModelLoader> ModelLoader_; /**<Shared Ptr To Model Loader*/
+    std::shared_ptr<ERS_CLASS_SceneLoader> SceneLoader_; /**<Shared Ptr To Scene Loader*/
+
 
     int RenderWidth_ = 0; /**<Width of the display being rendered to (if using a window, this is the size of the window)*/
     int RenderHeight_ = 0; /**<Height of the display being rendered to (if using a window, this is the size of the window)*/
     
+
     bool IsLinux_ = true; /**<Boolean Value Set To True When Binary Is Compiled For Linux, False Otherwise*/
+
 
 };
