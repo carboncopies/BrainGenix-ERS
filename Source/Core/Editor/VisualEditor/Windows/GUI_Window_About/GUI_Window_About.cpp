35000a30b94999548b787698245b9c3171474125
datacrystals <tliao@carboncopies.org>
2022-01-04 11:31:04 -0800

    Fix ImGui Assertion Errors

--- Source/Core/Editor/VisualEditor/Windows/GUI_Window_About/GUI_Window_About.cpp
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_About/GUI_Window_About.cpp
@@ -32,51 +32,54 @@ void Window_About::Draw() {
 
 
     if (Enabled_) {
-    ImGui::Begin("About", &Enabled_);
+    bool Visible = ImGui::Begin("About", &Enabled_);
 
         // Set Window Size
         ImGui::SetWindowSize(ImVec2(600, 400), ImGuiCond_FirstUseEver);
 
-        
-        // ERS Description
-        ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), "About:");
-        ImGui::TextWrapped("BrainGenix Environment Rendering System (ERS) is a distributed multigpu "
-            "rendering system with realtime physics, cloth and more. [Note: This project"
-            " is currently in development].");
 
-        // Write Text
-        ImGui::Separator();
-        ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), "Branch:");
-        ImGui::Text("Unstable-Development");
+        if (Visible) {
+            
+            // ERS Description
+            ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), "About:");
+            ImGui::TextWrapped("BrainGenix Environment Rendering System (ERS) is a distributed multigpu "
+                "rendering system with realtime physics, cloth and more. [Note: This project"
+                " is currently in development].");
 
-        // Version Info?
+            // Write Text
+            ImGui::Separator();
+            ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), "Branch:");
+            ImGui::Text("Unstable-Development");
 
-        // Operating System Status
-        ImGui::Separator();
-        ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), "Feature Status:");
-        if (!SystemUtils_->IsLinux_) {
-            ImGui::TextColored(ImVec4(0.8f, 0.2f, 0.2f, 1.0f), "Some features are not available in Windows. To use all features, please use Linux/BSD.");
-        } else {
-            ImGui::TextColored(ImVec4(0.2f, 0.8f, 0.2f, 1.0f), "All features are enabled. Congrats on using Linux/BSD, you're better off without Windows/OSX.");
+            // Version Info?
+
+            // Operating System Status
+            ImGui::Separator();
+            ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), "Feature Status:");
+            if (!SystemUtils_->IsLinux_) {
+                ImGui::TextColored(ImVec4(0.8f, 0.2f, 0.2f, 1.0f), "Some features are not available in Windows. To use all features, please use Linux/BSD.");
+            } else {
+                ImGui::TextColored(ImVec4(0.2f, 0.8f, 0.2f, 1.0f), "All features are enabled. Congrats on using Linux/BSD, you're better off without Windows/OSX.");
+            }
+
+            // License Info
+            ImGui::Separator();
+            ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), "License:");
+            ImGui::TextWrapped("This program is free software: you can redistribute it and/or modify "
+                        "it under the terms of the GNU Affero General Public License as "
+                        "published by the Free Software Foundation, either version 3 of the "
+                        "License, or (at your option) any later version. "
+
+                        "This program is distributed in the hope that it will be useful, "
+                        "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+                        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the "
+                        "GNU Affero General Public License for more details. "
+
+                        "You should have received a copy of the GNU Affero General Public License "
+                        "along with this program. If not, see <https://www.gnu.org/licenses/>.");
+
+
         }
-
-        // License Info
-        ImGui::Separator();
-        ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), "License:");
-        ImGui::TextWrapped("This program is free software: you can redistribute it and/or modify "
-                    "it under the terms of the GNU Affero General Public License as "
-                    "published by the Free Software Foundation, either version 3 of the "
-                    "License, or (at your option) any later version. "
-
-                    "This program is distributed in the hope that it will be useful, "
-                    "but WITHOUT ANY WARRANTY; without even the implied warranty of "
-                    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the "
-                    "GNU Affero General Public License for more details. "
-
-                    "You should have received a copy of the GNU Affero General Public License "
-                    "along with this program. If not, see <https://www.gnu.org/licenses/>.");
-
-
 
     ImGui::End();
     }
