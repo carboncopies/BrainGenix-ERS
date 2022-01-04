0ee07588a93f32a4d0d2dc679e19e4d28602f6a7
datacrystals <tliao@carboncopies.org>
2022-01-04 00:24:58 -0800

    Implement Project Writer

---
+++ Source/Core/Writers/ERS_ProjectWriter/ERS_ProjectWriter.cpp
@@ -0,0 +1,12 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This class writes ers projects to asset ids.
+    Documentation Status: Completed
+    Additonal Notes: None
+    Date Created: 2022-01-04
+*/
+
+#include <ERS_ProjectWriter.h>
---
+++ Source/Core/Writers/ERS_ProjectWriter/ERS_ProjectWriter.h
@@ -0,0 +1,63 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This class writes ers projects to asset ids.
+    Documentation Status: Completed
+    Additonal Notes: None
+    Date Created: 2022-01-04
+*/
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <string>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_STRUCT_IOData.h>
+#include <ERS_STRUCT_Project.h>
+#include <ERS_STRUCT_SystemUtils.h>
+
+
+/**
+ * @brief This Class Implements Project Saving Functionality.
+ * 
+ */
+class ERS_CLASS_ProjectWriter {
+
+private:
+
+    std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils_; /**<System Utils Instance*/
+
+public:
+
+    /**
+     * @brief Construct a new ers class projectwriter object
+     * 
+     * @param SystemUtils 
+     */
+    ERS_CLASS_ProjectWriter(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils);
+
+    /**
+     * @brief Destroy the ers class projectwriter object
+     * 
+     */
+    ~ERS_CLASS_ProjectWriter();
+
+
+    /**
+     * @brief Save project to given asset id. Returns false on fail, true on success.
+     * 
+     * @param Project 
+     * @param AssetID 
+     * @return true 
+     * @return false 
+     */
+    bool SaveProject(std::shared_ptr<ERS_STRUCT_Project> Project, long AssetID);
+
+
+};
