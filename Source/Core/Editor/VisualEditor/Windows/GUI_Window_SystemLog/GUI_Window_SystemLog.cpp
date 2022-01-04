f731caafb017bdd1295f07f8c531f60fd4ad7bf3
datacrystals <tliao@carboncopies.org>
2022-01-04 11:33:41 -0800

    Fix ImGui Assertion Errors

--- Source/Core/Editor/VisualEditor/Windows/GUI_Window_SystemLog/GUI_Window_SystemLog.cpp
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_SystemLog/GUI_Window_SystemLog.cpp
@@ -37,60 +37,63 @@ Window_SystemLog::~Window_SystemLog() {
 void Window_SystemLog::Draw() {
 
     if (Enabled_) {
-    ImGui::Begin("System Log", &Enabled_);
+    bool Visible = ImGui::Begin("System Log", &Enabled_);
 
         // Set Default Window Size
         ImGui::SetWindowSize(ImVec2(300,0), ImGuiCond_FirstUseEver);
 
-        
-        // Autoscroll
-        ImGui::Checkbox("Lock To Bottom", &AutoScroll_);
-        ImGui::SameLine();
+
+        if (Visible) {
+
+            // Autoscroll
+            ImGui::Checkbox("Lock To Bottom", &AutoScroll_);
+            ImGui::SameLine();
 
 
-        // Clear Button
-        if (ImGui::Button("Clear")) {
-            StartingLogIndex_ = SystemUtils_->Logger_->LogLevels_.size();
-        }        
-        ImGui::SameLine();
+            // Clear Button
+            if (ImGui::Button("Clear")) {
+                StartingLogIndex_ = SystemUtils_->Logger_->LogLevels_.size();
+            }        
+            ImGui::SameLine();
 
-        // Restore Button
-        if (ImGui::Button("Restore")) {
-            StartingLogIndex_ = 0;
-        }
-        ImGui::SameLine();
+            // Restore Button
+            if (ImGui::Button("Restore")) {
+                StartingLogIndex_ = 0;
+            }
+            ImGui::SameLine();
 
-        // Min Log Level Slider
-        ImGui::SliderInt("Minimum Log Level", &MinLogLevel_, 0, 10);
+            // Min Log Level Slider
+            ImGui::SliderInt("Minimum Log Level", &MinLogLevel_, 0, 10);
 
-        ImGui::Separator();
-
-
-        // Draw Log Textbox
-        ImGui::BeginChild("Log Text", ImVec2(0,0), true, ImGuiWindowFlags_HorizontalScrollbar);
-        for (int i = StartingLogIndex_; i < SystemUtils_->Logger_->LogMessages_.size(); i++) {
-
-            // Check Log Level
-            if (SystemUtils_->Logger_->LogLevels_[i] > MinLogLevel_) {
-
-                float ColorRed = SystemUtils_->Logger_->LogColors_[i].Red / 255.0f;
-                float ColorGreen = SystemUtils_->Logger_->LogColors_[i].Green / 255.0f;
-                float ColorBlue = SystemUtils_->Logger_->LogColors_[i].Blue / 255.0f;
+            ImGui::Separator();
 
 
-                std::string LogText = SystemUtils_->Logger_->FullLogMessages_[i];
-                ImVec4 TextColor = ImVec4(ColorRed, ColorGreen, ColorBlue, 1.0f);
-                ImGui::TextColored(TextColor, "%s", LogText.c_str());
+            // Draw Log Textbox
+            ImGui::BeginChild("Log Text", ImVec2(0,0), true, ImGuiWindowFlags_HorizontalScrollbar);
+            for (int i = StartingLogIndex_; i < SystemUtils_->Logger_->LogMessages_.size(); i++) {
+
+                // Check Log Level
+                if (SystemUtils_->Logger_->LogLevels_[i] > MinLogLevel_) {
+
+                    float ColorRed = SystemUtils_->Logger_->LogColors_[i].Red / 255.0f;
+                    float ColorGreen = SystemUtils_->Logger_->LogColors_[i].Green / 255.0f;
+                    float ColorBlue = SystemUtils_->Logger_->LogColors_[i].Blue / 255.0f;
+
+
+                    std::string LogText = SystemUtils_->Logger_->FullLogMessages_[i];
+                    ImVec4 TextColor = ImVec4(ColorRed, ColorGreen, ColorBlue, 1.0f);
+                    ImGui::TextColored(TextColor, "%s", LogText.c_str());
+
+                }
 
             }
+            // Scroll To Bottom If Enabled
+            if (AutoScroll_) {
+                ImGui::SetScrollHereY(1.0f);
+            }
+            ImGui::EndChild();
 
         }
-        // Scroll To Bottom If Enabled
-        if (AutoScroll_) {
-            ImGui::SetScrollHereY(1.0f);
-        }
-        ImGui::EndChild();
-
 
     ImGui::End();
     }
