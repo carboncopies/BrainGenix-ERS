db565c61b6238f1d5d93af92572c9bc3df760163
datacrystals <tliao@carboncopies.org>
2022-01-04 18:25:26 -0800

    Implement Viewport Add/Remove Functionality

--- Source/Core/Renderer/VisualRenderer/VisualRenderer.h
+++ Source/Core/Renderer/VisualRenderer/VisualRenderer.h
@@ -43,52 +43,52 @@
 
 class VisualRenderer {
 
-    private:
+private:
 
-        // Member Variables
-        GLFWwindow *Window_; /**<GLFW Window Instance For Window Input To Viewports*/
-        std::shared_ptr<Cursors3D> Cursors3D_; /**<Setup 3D Cursor Class*/
-        std::shared_ptr<ERS_OBJECT_SHADER> Shader_; /**<Shader Object Ptr*/
+    // Member Variables
+    GLFWwindow *Window_; /**<GLFW Window Instance For Window Input To Viewports*/
+    std::shared_ptr<Cursors3D> Cursors3D_; /**<Setup 3D Cursor Class*/
+    std::shared_ptr<ERS_OBJECT_SHADER> Shader_; /**<Shader Object Ptr*/
 
-        std::vector<std::shared_ptr<ERS_OBJECT_CAMERA_NOCLIP>> Cameras_; /**<List Of Pointers To Camera Instances*/
-        std::vector<std::shared_ptr<ERS_OBJECT_SHADER>> Shaders_; /**<List Of Pointers To Shader Instances*/
-        std::vector<std::string> ViewportNames_; /**<List Of Names For Viewports*/
-        std::vector<std::shared_ptr<InputProcessor>> InputProcessors_; /**<Vector Of InputProcessors*/
-        std::vector<bool> WasSelected_; /**<List of true/false for was viewport selected last frame*/
-        std::vector<std::shared_ptr<bool>> ViewportEnabled_; /**<List of bool value pointers indicating if viewport should stay open*/
+    std::vector<std::shared_ptr<ERS_OBJECT_CAMERA_NOCLIP>> Cameras_; /**<List Of Pointers To Camera Instances*/
+    std::vector<std::shared_ptr<ERS_OBJECT_SHADER>> Shaders_; /**<List Of Pointers To Shader Instances*/
+    std::vector<std::string> ViewportNames_; /**<List Of Names For Viewports*/
+    std::vector<std::shared_ptr<InputProcessor>> InputProcessors_; /**<Vector Of InputProcessors*/
+    std::vector<bool> WasSelected_; /**<List of true/false for was viewport selected last frame*/
+    std::vector<std::shared_ptr<bool>> ViewportEnabled_; /**<List of bool value pointers indicating if viewport should stay open*/
 
-        std::vector<unsigned int> FramebufferObjects_; /**<Framebuffer Objects For Each Viewport*/
-        std::vector<unsigned int> FramebufferColorObjects_; /**<Render Texture For Displaying Color*/
-        std::vector<unsigned int> RenderbufferObjects_; /**<Render buffer Objects For Depth*/
+    std::vector<unsigned int> FramebufferObjects_; /**<Framebuffer Objects For Each Viewport*/
+    std::vector<unsigned int> FramebufferColorObjects_; /**<Render Texture For Displaying Color*/
+    std::vector<unsigned int> RenderbufferObjects_; /**<Render buffer Objects For Depth*/
 
-        std::vector<int> ViewportWidths_; /**<Vector Containing Viewport Widths In Pixels*/
-        std::vector<int> ViewportHeights_; /**<Vector Containing Viewport Heights In Pixels*/
+    std::vector<int> ViewportWidths_; /**<Vector Containing Viewport Widths In Pixels*/
+    std::vector<int> ViewportHeights_; /**<Vector Containing Viewport Heights In Pixels*/
 
-        std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils_; /**<System Utils Struct Containing Pointers To Important Info*/
-
-
-        bool CaptureCursor_ = false; /**<Indicate if cursor should be captured*/
-        int CaptureIndex_ = 0; /**<Index where cursor was captured*/
-
-        // Internal Functions
-        void InitializeOpenGL();
-        void ResizeViewport(int Index, int Width, int Height);
+    std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils_; /**<System Utils Struct Containing Pointers To Important Info*/
 
 
+    bool CaptureCursor_ = false; /**<Indicate if cursor should be captured*/
+    int CaptureIndex_ = 0; /**<Index where cursor was captured*/
 
-    public:
-
-        // Constructor
-        VisualRenderer(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils, GLFWwindow* Window, std::shared_ptr<Cursors3D> Cursors3D, std::shared_ptr<ERS_OBJECT_SHADER> Shader);
-
-        // Destructor
-        ~VisualRenderer();
+    // Internal Functions
+    void InitializeOpenGL();
+    void ResizeViewport(int Index, int Width, int Height);
 
 
-        void CreateViewport(std::string ViewportName);
-        void UpdateViewports(float DeltaTime, std::shared_ptr<ERS_CLASS_SceneManager> SceneManager);
-        void UpdateViewport(int Index, std::shared_ptr<ERS_CLASS_SceneManager> SceneManager, float DeltaTime);
-        void DeleteViewport(int Index);
+
+public:
+
+    // Constructor
+    VisualRenderer(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils, GLFWwindow* Window, std::shared_ptr<Cursors3D> Cursors3D, std::shared_ptr<ERS_OBJECT_SHADER> Shader);
+
+    // Destructor
+    ~VisualRenderer();
+
+
+    void CreateViewport(std::string ViewportName);
+    void UpdateViewports(float DeltaTime, std::shared_ptr<ERS_CLASS_SceneManager> SceneManager);
+    void UpdateViewport(int Index, std::shared_ptr<ERS_CLASS_SceneManager> SceneManager, float DeltaTime);
+    void DeleteViewport(int Index);
         
 
 };
