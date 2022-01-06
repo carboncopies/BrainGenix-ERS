ba5de8f7a4502e687f1bd21b177c21b0c907b16c
datacrystals <tliao@carboncopies.org>
2022-01-06 09:20:22 +0000

    Update Build Tools

--- Tools/Install.sh
+++ Tools/Install.sh
@@ -1,5 +1,8 @@
 # Install Compiler Tools
-sudo apt install binutils-dev git wget cmake ninja-build g++ -y
+sudo apt install git wget cmake ninja-build g++ -y
+
+# Backward Deps
+sudo apt install binutils-dev binutils-source -y
 
 # Install GLFW Deps
 sudo apt install libx11-dev libxrandr-dev ibxinerama-dev libxcursor-dev libxi-dev -y
