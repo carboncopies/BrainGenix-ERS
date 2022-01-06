e373becce8c3e4a154442cfe3a03fac7756bb916
datacrystals <tliao@carboncopies.org>
2022-01-06 21:56:06 +0000

    Add Eigen Lib

--- CMake/Build-Monado.cmake
+++ CMake/Build-Monado.cmake
@@ -5,3 +5,12 @@
 ## GLSLang tools needs to be here! (glslang-validator?)
 
 ## Ideally, Eigen would be built in the project too
+
+
+
+if (WIN32)
+  message("VR Support Disabled On Windows, Use Linux. The Monado Runtime Doesn't Support Windows")
+else()
+  message("[ERS BUILD SYSTEM] Building Monado OpenXR Runtime")
+  add_subdirectory(${LIB_DIR}/monado)
+endif()
