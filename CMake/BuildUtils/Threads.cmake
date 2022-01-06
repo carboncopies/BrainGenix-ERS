11d82051918d47dca7898369cddb98e3575bdd0f
datacrystals <tliao@carboncopies.org>
2022-01-06 23:50:32 +0000

    Restructure Build System

--- CMake/BuildUtils/Threads.cmake
+++ CMake/BuildUtils/Threads.cmake
@@ -2,10 +2,6 @@
 # Builds Library For ERS #
 ##########################
 
-if (WIN32)
-    ERSBuildLogger(${Yellow} "Database Support Disabled On Windows Due To SSL Errors, Please Use Linux/BSD Instead")
-else()
-    ERSBuildLogger(${Green} "Configuring MySQL Connector Library")
-    add_subdirectory(${LIB_DIR}/mysql-connector-linux)
-    ERSBuildLogger(${BoldGreen} "Finished Configuring MySQL Connector Library")
-endif()
+ERSBuildLogger(${Green} "Finding Threading Package")
+find_package(Threads REQUIRED)
+ERSBuildLogger(${BoldGreen} "Found Threading Package")
