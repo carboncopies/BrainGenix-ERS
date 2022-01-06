053ba82864ef9d56b062d61ad43cc9489f793e48
datacrystals <tliao@carboncopies.org>
2022-01-06 21:38:27 +0000

    Add Eigen Lib

---
+++ CMake/Build-Monado.cmake
@@ -0,0 +1,8 @@
+###################################################
+# Builds Monado's Prerequisites And Monado Itself #
+###################################################
+
+## GLSLang tools needs to be here! (glslang-validator?)
+
+# Add Eigen Library
+add_subdirectory(${LIB_DIR}/eigen)
