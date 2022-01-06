9c92a6a704ae1dc52266cd3340d0d4196fa1ea5f
datacrystals <tliao@carboncopies.org>
2022-01-06 08:32:34 +0000

    Update Install Script For Monado

--- Tools/Install.sh
+++ Tools/Install.sh
@@ -2,4 +2,7 @@
 sudo apt install binutils-dev git wget cmake ninja-build g++ -y
 
 # Install GLFW Deps
-libx11-dev libxrandr-dev ibxinerama-dev libxcursor-dev libxi-dev -y
+sudo apt install libx11-dev libxrandr-dev ibxinerama-dev libxcursor-dev libxi-dev -y
+
+# Install MySQL Connector Deps
+sudo apt install libssl-dev -y
