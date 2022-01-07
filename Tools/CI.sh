2a916d2efc91be98b0075f86046cb44eb945e2c2
datacrystals <tliao@carboncopies.org>
2022-01-07 21:58:22 +0000

    Fix CodeQL Analysis

---
+++ Tools/CI.sh
@@ -0,0 +1,9 @@
+#########################################################
+## THIS SCRIPT IS INTENDED TO BE RUN BY CI, NOT A USER ##
+#########################################################
+
+# Install Deps
+sudo bash Install.sh
+
+# Build System
+bash Install.sh
