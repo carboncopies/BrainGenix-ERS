1d22822680d1333a0732065130c4806711fbdc05
datacrystals <tliao@carboncopies.org>
2022-01-08 06:27:06 +0000

    Implement Settings Menu

---
+++ Source/Core/Editor/VisualEditor/MenuEntries/GUI_Menu_Settings/GUI_Menu_Settings.cpp
@@ -0,0 +1,97 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file provides a GUI menu entry.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-17
+*/
+
+#include <GUI_Menu_File.h>
+
+
+// Constructor
+GUI_Menu_File::GUI_Menu_File(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils, std::shared_ptr<ERS_CLASS_SceneManager> SceneManager, std::shared_ptr<ERS_STRUCT_ProjectUtils> ProjectUtils) {
+
+    // Copy In Pointer Struct
+    SystemUtils_ = SystemUtils;
+    SceneManager_ = SceneManager;
+    ProjectUtils_ = ProjectUtils;
+
+    // Log Initialization
+    SystemUtils_->Logger_->Log("Editor Setting Up File Menu", 4);
+
+    // Create Class Instances
+    SceneWriter_ = std::make_unique<SceneWriter>(SystemUtils_);
+    ImportAsset_ = std::make_unique<GUI_ImportAsset>(SystemUtils_);
+    Window_ProjectSettings_ = std::make_unique<Window_ProjectSettings>(ProjectUtils, SystemUtils);
+
+}
+
+// Destructor
+GUI_Menu_File::~GUI_Menu_File() {
+
+    // Log Destructor
+    SystemUtils_->Logger_->Log("Editor Destroying File Menu", 4);
+
+}
+
+// Draw Function
+void GUI_Menu_File::Draw() {
+
+    // File Menu
+    if (ImGui::BeginMenu("File")) {
+
+        // Project Options
+        if (ImGui::MenuItem("Save Project")) {
+            ProjectUtils_->ProjectManager_->WriteProject(0);
+        }
+        if (ImGui::MenuItem("Project Settings")) {
+            Window_ProjectSettings_->Enabled_ = !Window_ProjectSettings_->Enabled_;
+        }
+        ImGui::Separator();
+
+        // Import Option, Disabled On Windows Due To Filesystem Issues
+        if (SystemUtils_->IsLinux_) {
+            if (ImGui::MenuItem("Import Model")) {
+                ImportAsset_->OpenFileDialog();
+            }
+            ImGui::Separator();
+        }
+
+        // Save Options
+        if (ImGui::MenuItem("Save Active Scene")) {
+            SceneWriter_->ProcessScene(
+                std::make_shared<ERS_OBJECT_SCENE>(SceneManager_->Scenes_[SceneManager_->ActiveScene_]),
+                SceneManager_->Scenes_[SceneManager_->ActiveScene_].ScenePath
+                );
+        }
+        if (ImGui::MenuItem("Save All Scenes")) {
+            for (int i = 0; i < SceneManager_->Scenes_.size(); i++) {
+                SceneWriter_->ProcessScene(
+                    std::make_shared<ERS_OBJECT_SCENE>(SceneManager_->Scenes_[i]),
+                    SceneManager_->Scenes_[i].ScenePath
+                    );
+            }
+        }
+
+        ImGui::Separator();
+
+        // Exit Options
+        if (ImGui::MenuItem("Exit")) {
+            *SystemUtils_->SystemShouldRun_ = false;
+        }
+
+        
+    ImGui::EndMenu();
+    }
+
+
+    // Draw Subwindows
+    ImportAsset_->Draw();
+    Window_ProjectSettings_->Draw();
+
+
+}
---
+++ Source/Core/Editor/VisualEditor/MenuEntries/GUI_Menu_Settings/GUI_Menu_Settings.h
@@ -0,0 +1,86 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This file provides a GUI menu entry.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-17
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <vector>
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <memory>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
+
+#include <imgui.h>
+
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <LoggingSystem.h>
+#include <ERS_STRUCT_SystemUtils.h>
+#include <ERS_STRUCT_ProjectUtils.h>
+#include <ERS_ProjectLoader.h>
+
+#include <GUI_ImportAsset.h>
+
+#include <ERS_SceneWriter.h>
+#include <ERS_SceneManager.h>
+
+#include <GUI_Window_ProjectSettings.h>
+
+
+
+/**
+ * @brief This class provides the "File" Menu In The Editor.
+ * 
+ */
+class GUI_Menu_File {
+
+    // Member Vars
+    private:
+
+        std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils_; /**<SystemUtils Instance, Used To Get Systems From Other Classes*/
+        std::unique_ptr<SceneWriter> SceneWriter_; /**<Scene Writer Instance Pointer*/
+        std::shared_ptr<ERS_CLASS_SceneManager> SceneManager_; /**<Scene Manager Instance Pointer*/
+        std::unique_ptr<GUI_ImportAsset> ImportAsset_; /**<Pointer To Asset Importer System*/
+        std::unique_ptr<Window_ProjectSettings> Window_ProjectSettings_; /**<Project Settings Window*/
+        std::shared_ptr<ERS_STRUCT_ProjectUtils> ProjectUtils_; /**<Pointer To Project Utils Instance*/
+
+
+    // Member Functions
+    public:
+
+
+        /**
+         * @brief Construct a new gui menu file object
+         * 
+         * @param SystemUtils 
+         */
+        GUI_Menu_File(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils, std::shared_ptr<ERS_CLASS_SceneManager> SceneManager, std::shared_ptr<ERS_STRUCT_ProjectUtils> ProjectUtils);
+
+
+        /**
+         * @brief Destroy the gui menu file object
+         * 
+         */
+        ~GUI_Menu_File();
+
+
+        /**
+         * @brief This function Draws The File Menu Contents.
+         * 
+         */
+        void Draw();
+
+
+};
