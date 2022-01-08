f412b5457660818211cbf2c32f69ce945104119a
datacrystals <tliao@carboncopies.org>
2022-01-08 00:57:21 +0000

    Implement Joystick Control

---
+++ Source/Core/Manager/ERS_JoystickManager/ERS_JoystickManager.cpp
@@ -0,0 +1,13 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is responsible for providing a system for getting joystick input and adding it to the input struct.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2022-01-08
+*/
+
+#include <ERS_JoystickManager.h>
+
---
+++ Source/Core/Manager/ERS_JoystickManager/ERS_JoystickManager.h
@@ -0,0 +1,41 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is responsible for providing a system for getting joystick input and adding it to the input struct.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2022-01-08
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <vector>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+/**
+ * @brief Get and update all joystick input devices, as well as associated controller buttons
+ * 
+ */
+class ERS_CLASS_JoystickManager {
+
+private:
+
+
+public:
+
+
+
+};
