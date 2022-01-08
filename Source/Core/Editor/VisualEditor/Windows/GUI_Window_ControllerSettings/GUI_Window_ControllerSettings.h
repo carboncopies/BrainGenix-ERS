3dd277aa978e6fccf3a3df9b819c3e1e69d1990f
datacrystals <tliao@carboncopies.org>
2022-01-08 06:41:29 +0000

    Implement Controller Settings Window

---
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_ControllerSettings/GUI_Window_ControllerSettings.h
@@ -0,0 +1,63 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This Window file is responsible for creating a system Window.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-17
+*/
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <memory>
+#include <thread>
+#include <string>
+#include <mutex>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <imgui.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_STRUCT_SystemUtils.h>
+
+/**
+ * @brief Window for the Import progress bar.
+ * 
+ */
+class Window_About {
+
+    private:
+
+        std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils_; /**<Pointers To System Resources*/
+
+    public:
+
+        bool Enabled_ = false; /**<Is Popup Enabled*/
+
+
+    public:
+
+        /**
+         * @brief Construct a new Window_About object
+         * 
+         */
+        Window_About(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils);
+        
+        /**
+         * @brief Destroy the Window_About object
+         * 
+         */
+        ~Window_About();
+
+
+        /**
+         * @brief Draw import bar if needed (call every frame)
+         * 
+         */
+        void Draw();
+
+};
