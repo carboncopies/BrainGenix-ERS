a1b96a20aa8c8df70b5d8de9b1525a6db31f4af5
datacrystals <tliao@carboncopies.org>
2022-01-09 08:42:21 +0000

    Implement Controller Settings Loader

--- Source/Core/Loader/ERS_ControllerSettingsLoader/ERS_ControllerSettingsLoader.h
+++ Source/Core/Loader/ERS_ControllerSettingsLoader/ERS_ControllerSettingsLoader.h
@@ -20,3 +20,44 @@
 // Internal Libraries (BG convention: use <> instead of "")
 #include <ERS_STRUCT_SystemUtils.h>
 #include <ERS_STRUCT_ControllerSettings.h>
+
+
+class ERS_CLASS_ControllerSettingsLoader {
+
+private:
+
+    std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils_; /**<System Utils Info Struct*/
+
+private:
+
+public:
+
+public:
+
+    /**
+     * @brief Construct a new ers class controllersettingsloader object
+     * 
+     * @param SystemUtils 
+     */
+    ERS_CLASS_ControllerSettingsLoader(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils);
+
+
+    /**
+     * @brief Destroy the ers class controllersettingsloader object
+     * 
+     */
+    ~ERS_CLASS_ControllerSettingsLoader();
+
+
+    /**
+     * @brief Load controler settings from assetid
+     * 
+     * @param ControllerSettings 
+     * @param AssetID
+     * @return true 
+     * @return false 
+     */
+    bool LoadControllerSettings(std::shared_ptr<ERS_STRUCT_ControllerSettings> ControllerSettings, long AssetID);
+
+
+}
