a8adb2850e0e5453cff9d47e9507be4390680f30
datacrystals <tliao@carboncopies.org>
2022-01-09 01:05:49 +0000

    Implement Controller Configuration Settings

--- Source/Core/Editor/VisualEditor/Windows/GUI_Window_ControllerSettings/GUI_Window_ControllerSettings.cpp
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_ControllerSettings/GUI_Window_ControllerSettings.cpp
@@ -62,71 +62,109 @@ void Window_ControllerSettings::Draw() {
             ImGui::Separator();
 
 
-            // Info Child Window
-            ImGui::BeginChild("Controller Info");
-
-            // Check if there aren't any controllers, display no controllers message
-            if (NumberControllers == 0) {
-                ImGui::TextColored(ImVec4(1.0f, 0.2f, 0.2f, 1.0f), "No controllers detected. Try running 'Detect New Controllers'");
-            } else {
-
-                // Get Game Controller State
-                GLFWgamepadstate State = HIDUtils_->ControllerInputManager->ControllerStates_[SelectedController_];
-
-                // Joystick Info
-                ImGui::Separator();
-                ImGui::TextColored(ImVec4(0.2f, 1.0f, 0.2f, 1.0f), "Joysticks");
-                ImGui::Separator();
-
-                // Joystick Positions
-                ImGui::Text("Left Joystick Vertical Axis: %f", State.axes[GLFW_GAMEPAD_AXIS_LEFT_Y]);
-                ImGui::Text("Left Joystick Horizontal Axis: %f", State.axes[GLFW_GAMEPAD_AXIS_LEFT_X]);
-                
-                ImGui::Text("Right Joystick Vertical Axis: %f", State.axes[GLFW_GAMEPAD_AXIS_RIGHT_Y]);
-                ImGui::Text("Right Joystick Horizontal Axis: %f", State.axes[GLFW_GAMEPAD_AXIS_RIGHT_X]);
-
-                // Joysticks Pressed
-                ButtonText(std::string("Left Joystick"), State.buttons[GLFW_GAMEPAD_BUTTON_LEFT_THUMB]);
-                ButtonText(std::string("Right Joystick"), State.buttons[GLFW_GAMEPAD_BUTTON_RIGHT_THUMB]);
-
-                // Trigger Info
-                ImGui::Separator();
-                ImGui::TextColored(ImVec4(0.2f, 1.0f, 0.2f, 1.0f), "Triggers");
-                ImGui::Separator();
-
-                ImGui::Text("Left Trigger: %f", State.axes[GLFW_GAMEPAD_AXIS_LEFT_TRIGGER]);
-                ImGui::Text("Right Trigger: %f", State.axes[GLFW_GAMEPAD_AXIS_RIGHT_TRIGGER]);
-
-                ButtonText("Left Bumper", State.buttons[GLFW_GAMEPAD_BUTTON_LEFT_BUMPER]);
-                ButtonText("Right Bumper", State.buttons[GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER]);
+            // Begin Tabs For Info, Config
+            if (ImGui::BeginTabBar("Controller Settings Tab Bar")) {
 
 
-                // DPAD Info
-                ImGui::Separator();
-                ImGui::TextColored(ImVec4(0.2f, 1.0f, 0.2f, 1.0f), "DPAD");
-                ImGui::Separator();
+                // Info Tab
+                if (ImGui::BeginTabItem("Info")) {
 
-                ButtonText("DPAD Up", State.buttons[GLFW_GAMEPAD_BUTTON_DPAD_UP]);
-                ButtonText("DPAD Down", State.buttons[GLFW_GAMEPAD_BUTTON_DPAD_DOWN]);
-                ButtonText("DPAD Left", State.buttons[GLFW_GAMEPAD_BUTTON_DPAD_LEFT]);
-                ButtonText("DPAD Right", State.buttons[GLFW_GAMEPAD_BUTTON_DPAD_RIGHT]);
+                    // Info Child Window
+                    ImGui::BeginChild("Controller Info");
+
+                    // Check if there aren't any controllers, display no controllers message
+                    if (NumberControllers == 0) {
+                        ImGui::TextColored(ImVec4(1.0f, 0.2f, 0.2f, 1.0f), "No controllers detected. Try running 'Detect New Controllers'");
+                    } else {
+
+                        // Get Game Controller State
+                        GLFWgamepadstate State = HIDUtils_->ControllerInputManager->ControllerStates_[SelectedController_];
+
+                        // Joystick Info
+                        ImGui::Separator();
+                        ImGui::TextColored(ImVec4(0.2f, 1.0f, 0.2f, 1.0f), "Joysticks");
+                        ImGui::Separator();
+
+                        // Joystick Positions
+                        ImGui::Text("Left Joystick Vertical Axis: %f", State.axes[GLFW_GAMEPAD_AXIS_LEFT_Y]);
+                        ImGui::Text("Left Joystick Horizontal Axis: %f", State.axes[GLFW_GAMEPAD_AXIS_LEFT_X]);
+                        
+                        ImGui::Text("Right Joystick Vertical Axis: %f", State.axes[GLFW_GAMEPAD_AXIS_RIGHT_Y]);
+                        ImGui::Text("Right Joystick Horizontal Axis: %f", State.axes[GLFW_GAMEPAD_AXIS_RIGHT_X]);
+
+                        // Joysticks Pressed
+                        ButtonText(std::string("Left Joystick"), State.buttons[GLFW_GAMEPAD_BUTTON_LEFT_THUMB]);
+                        ButtonText(std::string("Right Joystick"), State.buttons[GLFW_GAMEPAD_BUTTON_RIGHT_THUMB]);
+
+                        // Trigger Info
+                        ImGui::Separator();
+                        ImGui::TextColored(ImVec4(0.2f, 1.0f, 0.2f, 1.0f), "Triggers");
+                        ImGui::Separator();
+
+                        ImGui::Text("Left Trigger: %f", State.axes[GLFW_GAMEPAD_AXIS_LEFT_TRIGGER]);
+                        ImGui::Text("Right Trigger: %f", State.axes[GLFW_GAMEPAD_AXIS_RIGHT_TRIGGER]);
+
+                        ButtonText("Left Bumper", State.buttons[GLFW_GAMEPAD_BUTTON_LEFT_BUMPER]);
+                        ButtonText("Right Bumper", State.buttons[GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER]);
 
 
-                // TRIANGLE/CIRCLE/SQUARE/X Buttons
-                ImGui::Separator();
-                ImGui::TextColored(ImVec4(0.2f, 1.0f, 0.2f, 1.0f), "Input Buttons");
-                ImGui::Separator();
+                        // DPAD Info
+                        ImGui::Separator();
+                        ImGui::TextColored(ImVec4(0.2f, 1.0f, 0.2f, 1.0f), "DPAD");
+                        ImGui::Separator();
 
-                ButtonText("Triangle", State.buttons[GLFW_GAMEPAD_BUTTON_TRIANGLE]);
-                ButtonText("Square", State.buttons[GLFW_GAMEPAD_BUTTON_SQUARE]);
-                ButtonText("Circle", State.buttons[GLFW_GAMEPAD_BUTTON_CIRCLE]);
-                ButtonText("Cross", State.buttons[GLFW_GAMEPAD_BUTTON_CROSS]);
-                
-                ButtonText("Back", State.buttons[GLFW_GAMEPAD_BUTTON_BACK]);
-                ButtonText("Menu", State.buttons[GLFW_GAMEPAD_BUTTON_GUIDE]);
+                        ButtonText("DPAD Up", State.buttons[GLFW_GAMEPAD_BUTTON_DPAD_UP]);
+                        ButtonText("DPAD Down", State.buttons[GLFW_GAMEPAD_BUTTON_DPAD_DOWN]);
+                        ButtonText("DPAD Left", State.buttons[GLFW_GAMEPAD_BUTTON_DPAD_LEFT]);
+                        ButtonText("DPAD Right", State.buttons[GLFW_GAMEPAD_BUTTON_DPAD_RIGHT]);
 
+
+                        // TRIANGLE/CIRCLE/SQUARE/X Buttons
+                        ImGui::Separator();
+                        ImGui::TextColored(ImVec4(0.2f, 1.0f, 0.2f, 1.0f), "Input Buttons");
+                        ImGui::Separator();
+
+                        ButtonText("Triangle", State.buttons[GLFW_GAMEPAD_BUTTON_TRIANGLE]);
+                        ButtonText("Square", State.buttons[GLFW_GAMEPAD_BUTTON_SQUARE]);
+                        ButtonText("Circle", State.buttons[GLFW_GAMEPAD_BUTTON_CIRCLE]);
+                        ButtonText("Cross", State.buttons[GLFW_GAMEPAD_BUTTON_CROSS]);
+                        
+                        ButtonText("Back", State.buttons[GLFW_GAMEPAD_BUTTON_BACK]);
+                        ButtonText("Menu", State.buttons[GLFW_GAMEPAD_BUTTON_GUIDE]);
+
+                    }
+                    ImGui::EndChild();
+
+                ImGui::EndTabItem();
+                }
+
+                // Settings Tab
+                if (ImGui::BeginTabItem("Settings")) {
+
+                    // Child Settings
+                    ImGui::BeginChild("Controller Setttings"); 
+
+
+                    // Check if there aren't any controllers, display no controllers message
+                    if (NumberControllers == 0) {
+                        ImGui::TextColored(ImVec4(1.0f, 0.2f, 0.2f, 1.0f), "No controllers detected. Try running 'Detect New Controllers'");
+                    } else {
+
+                        // Thresholds
+                        ImGui::Separator();
+                        ImGui::TextColored(ImVec4(0.2f, 1.0f, 0.2f, 1.0f), "Joystick Thresholds");
+                        ImGui::Separator();
+
+                    }
+
+                    ImGui::EndChild();
+
+                ImGui::EndTabItem();
+                }
+
+
+            ImGui::EndTabBar();
             }
-            ImGui::EndChild();
 
         }
 
--- Source/Core/Editor/VisualEditor/Windows/GUI_Window_ControllerSettings/GUI_Window_ControllerSettings.h
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_ControllerSettings/GUI_Window_ControllerSettings.h
@@ -27,6 +27,51 @@
 
 
 /**
+ * @brief Used to remap buttons, adjust gains, set thresholds, etc. User adjustable controller settings.
+ * 
+ */
+struct ControllerSettings {
+
+    // Joystick Settings
+    float MinThresholdJoystickLeftX; /**<Minimum value from joystick to be considered a value*/
+    float MinThresholdJoystickLeftY; /**<Minimum value from joystick to be considered a value*/
+    float MinThresholdJoystickRightX; /**<Minimum value from joystick to be considered a value*/
+    float MinThresholdJoystickRightY; /**<Minimum value from joystick to be considered a value*/
+
+    float MaxThresholdJoystickLeftX; /**<Maximum value from joystick before JS is fully on*/
+    float MaxThresholdJoystickLeftY; /**<Minimum value from joystick to be considered a value*/
+    float MaxThresholdJoystickRightX; /**<Minimum value from joystick to be considered a value*/
+    float MaxThresholdJoystickRightY; /**<Minimum value from joystick to be considered a value*/
+
+    float JoystickLeftXGain; /**<Joystick Gain*/
+    float JoystickLeftYGain; /**<Joystick Gain*/
+    float JoystickRightXGain; /**<Joystick Gain*/
+    float JoystickRightYGain; /**<Joystick Gain*/
+
+    // Trigger Settings
+    float MinThresholdTriggerLeft; /**<Minimum value from trigger to be considered valid, all values below this are discarded*/
+    float MinThresholdTriggerRight; /**<Minimum value from trigger to be considered valid, all values below this are discarded*/
+    
+    float MaxThresholdTriggerLeft; /**<Max value for trigger to be fully pressed*/
+    float MaxThresholdTriggerRight; /**<Max value for trigger to be fully pressed*/
+
+    // Button Remap
+    int TriangleButtonIndex; /**<Button Index*/
+    int SquareButtonIndex; /**<Button Index*/
+    int CrossButtonIndex; /**<Button Index*/
+    int CircleButtonIndex;/**<Button Index*/
+    int BackButtonIndex; /**<Button Index*/
+    int OptionsButtonIndex; /**<Button Index*/
+    int MenuButtonIndex; /**<Button Index*/
+    int JoystickRightButtonIndex; /**<Button Index*/
+    int JoystickLeftButtonIndex; /**<Button Index*/
+    int LeftBumperButtonIndex; /**<Button Index*/
+    int RightBumperButtonIndex; /**<Button Index*/
+
+};
+
+
+/**
  * @brief Window for the Import progress bar.
  * 
  */
