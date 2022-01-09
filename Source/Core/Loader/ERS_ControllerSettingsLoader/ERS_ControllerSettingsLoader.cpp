a82517df735345e0d9b04057124a3ec6a36ec688
datacrystals <tliao@carboncopies.org>
2022-01-09 08:36:42 +0000

    Implement Controller Settings Loader

---
+++ Source/Core/Loader/ERS_ControllerSettingsLoader/ERS_ControllerSettingsLoader.cpp
@@ -0,0 +1,13 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is the controller settings loader system.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2022-01-09
+*/
+
+#include <ERS_ControllerSettingsLoader.h>
+
---
+++ Source/Core/Loader/ERS_ControllerSettingsLoader/ERS_ControllerSettingsLoader.h
@@ -0,0 +1,22 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+/*
+    Description: This is the controller settings loader system.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2022-01-09
+*/
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <vector>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_STRUCT_SystemUtils.h>
+#include <ERS_STRUCT_ControllerSettings.h>
