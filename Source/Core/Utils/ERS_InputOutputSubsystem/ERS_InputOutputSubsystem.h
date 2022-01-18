92fac602e3265674d69e348e4180efb50b45c3bb
datacrystals <tliao@carboncopies.org>
2022-01-18 23:33:02 +0000

    Add Constructor/Destructor

--- Source/Core/Utils/ERS_InputOutputSubsystem/ERS_InputOutputSubsystem.h
+++ Source/Core/Utils/ERS_InputOutputSubsystem/ERS_InputOutputSubsystem.h
@@ -30,6 +30,8 @@
 // Internal Libraries (BG convention: use <> instead of "")
 #include <LoggingSystem.h>
 
+#include <ERS_CLASS_IOTypeIdentifier.h>
+
 #include <ERS_STRUCT_IOData.h>
 
 
@@ -42,6 +44,7 @@ class ERS_CLASS_InputOutputSubsystem {
     private:
 
         std::shared_ptr<LoggerClass> Logger_; /**<Logging Class Pointer*/
+        std::unique_ptr<ERS_CLASS_IOTypeIdentifier> IOTypeIdentifier_; /**<Pointer To IO Type Identifier Class Instance*/
 
     public:
 
