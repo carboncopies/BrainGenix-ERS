d3e72ececd0a82571c4f7f819095752519e811fc
datacrystals <tliao@carboncopies.org>
2022-01-18 23:06:24 +0000

    Add IOType Identifier System

--- Source/Core/Utils/ERS_IOTypeIdentifier/ERS_CLASS_IOTypeIdentifier.h
+++ Source/Core/Utils/ERS_IOTypeIdentifier/ERS_CLASS_IOTypeIdentifier.h
@@ -1,3 +1,26 @@
 //======================================================================//
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <chrono>
+#include <filesystem>
+#include <thread>
+#include <mutex>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <LoggingSystem.h>
+
+
