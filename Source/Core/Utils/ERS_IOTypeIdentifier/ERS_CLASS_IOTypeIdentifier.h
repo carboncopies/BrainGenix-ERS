1a984f0c5f27753c05c63ce27b1b5854d07d3e18
datacrystals <tliao@carboncopies.org>
2022-01-18 23:12:34 +0000

    Fix CMake Typos

--- Source/Core/Utils/ERS_IOTypeIdentifier/ERS_CLASS_IOTypeIdentifier.h
+++ Source/Core/Utils/ERS_IOTypeIdentifier/ERS_CLASS_IOTypeIdentifier.h
@@ -23,4 +23,27 @@
 // Internal Libraries (BG convention: use <> instead of "")
 #include <LoggingSystem.h>
 
+#include <ERS_STRUCT_IOData.h>
 
+
+class ERS_CLASS_IOTypeIdentifier {
+
+// Private Member Vars
+private:
+
+    std::shared_ptr<LoggerClass> Logger_; /**<Shared Pointer To Logger Instance*/
+
+// Private Methods
+private:
+
+
+// Public Member Vars
+public:
+
+// Public Methods
+public:
+
+    ERS_CLASS_IOTypeIdentifier(std::shared_ptr<LoggerClass> Logger);
+    ~ERS_CLASS_IOTypeIdentifier();
+
+};
