e9cff388449a174ec5a01304d87e99ae5effe401
datacrystals <tliao@carboncopies.org>
2022-01-19 09:38:47 +0000

    Revert "Debug Asset Read System"
    This reverts commit 476343fede254d05a2a1ad5f9c6b707c370e10f7.

--- Source/Core/Utils/ERS_InputOutputSubsystem/ERS_InputOutputSubsystem.cpp
+++ Source/Core/Utils/ERS_InputOutputSubsystem/ERS_InputOutputSubsystem.cpp
@@ -251,10 +251,10 @@ bool ERS_CLASS_InputOutputSubsystem::ReadAsset(long AssetID, std::shared_ptr<ERS
 
 
                     // If Using Metadata System, Remove Metadata Header (Skip first bytes)
-                    // if (MetadataEnabled_) {
-                    //     std::cout<<"offsetting file\n";
-                    //     fseek(Stream, 65535, 0);
-                    // }
+                    if (MetadataEnabled_) {
+                        std::cout<<"offsetting file\n";
+                        fseek(Stream, 65535, 0);
+                    }
 
                     // Read Data,
                     fread(OutputData->Data.get(), sizeof(unsigned char), Buffer.st_size, Stream);
@@ -262,10 +262,10 @@ bool ERS_CLASS_InputOutputSubsystem::ReadAsset(long AssetID, std::shared_ptr<ERS
                     fclose(Stream);
 
                     // If Using Metadata System, Remove Metadata Header
-                    if (MetadataEnabled_) {
-                        std::cout<<"offsetting\n";
-                        std::copy_backward(OutputData->Data.get() + 65535, OutputData->Data.get() + Buffer.st_size, OutputData->Data.get() + Buffer.st_size - 65535);
-                    }
+                    // if (MetadataEnabled_) {
+                    //     std::cout<<"offsetting\n";
+                    //     std::copy_backward(OutputData->Data.get() + 65535, OutputData->Data.get() + Buffer.st_size, OutputData->Data.get() + Buffer.st_size - 65535);
+                    // }
 
                     std::cout<<OutputData->Data.get()<<std::endl;
 
