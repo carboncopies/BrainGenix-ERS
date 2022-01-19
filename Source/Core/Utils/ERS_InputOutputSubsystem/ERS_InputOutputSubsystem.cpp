c6cee3839293f266c2ad3311c59756bf6e1e3437
datacrystals <tliao@carboncopies.org>
2022-01-19 09:38:48 +0000

    Revert "Debug Asset Read System"
    This reverts commit 67e6a62b2a5e128ea3f7ccde180b80634f9cb542.

--- Source/Core/Utils/ERS_InputOutputSubsystem/ERS_InputOutputSubsystem.cpp
+++ Source/Core/Utils/ERS_InputOutputSubsystem/ERS_InputOutputSubsystem.cpp
@@ -252,7 +252,6 @@ bool ERS_CLASS_InputOutputSubsystem::ReadAsset(long AssetID, std::shared_ptr<ERS
 
                     // If Using Metadata System, Remove Metadata Header (Skip first bytes)
                     if (MetadataEnabled_) {
-                        std::cout<<"offsetting file\n";
                         fseek(Stream, 65535, 0);
                     }
 
