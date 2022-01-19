4498e01f3913c1bc6a3c22c89177f5f5c6c6f530
datacrystals <tliao@carboncopies.org>
2022-01-19 03:40:23 +0000

    Implement YAML Decoding For Asset Metadata

--- Source/Core/Utils/ERS_IOTypeIdentifier/ERS_CLASS_IOTypeIdentifier.cpp
+++ Source/Core/Utils/ERS_IOTypeIdentifier/ERS_CLASS_IOTypeIdentifier.cpp
@@ -34,6 +34,8 @@ void ERS_CLASS_IOTypeIdentifier::IdentifyType(std::shared_ptr<ERS_STRUCT_IOData>
     memcpy(MetadataBytes, Data->Data.get(), 65535);
 
     // Convert To YAML::Node
+    YAML::Node Metadata = YAML::Load((const char*)MetadataBytes);
+
     
 
 }
