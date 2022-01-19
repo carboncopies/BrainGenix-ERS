e97b2fb22464592a7bdd3c10edfc28a619f17625
datacrystals <tliao@carboncopies.org>
2022-01-19 03:42:25 +0000

    Implement YAML Decoding For Asset Metadata

--- Source/Core/Utils/ERS_InputOutputSubsystem/ERS_InputOutputSubsystem.h
+++ Source/Core/Utils/ERS_InputOutputSubsystem/ERS_InputOutputSubsystem.h
@@ -41,113 +41,113 @@
  */
 class ERS_CLASS_InputOutputSubsystem {
 
-    private:
+private:
 
-        std::shared_ptr<LoggerClass> Logger_; /**<Logging Class Pointer*/
-        std::unique_ptr<ERS_CLASS_IOTypeIdentifier> IOTypeIdentifier_; /**<Pointer To IO Type Identifier Class Instance*/
+    std::shared_ptr<LoggerClass> Logger_; /**<Logging Class Pointer*/
+    std::unique_ptr<ERS_CLASS_IOTypeIdentifier> IOTypeIdentifier_; /**<Pointer To IO Type Identifier Class Instance*/
 
-    private:
-        
-        /**
-         * @brief Loads the first 65 kb for metadata identification
-         * 
-         * @param AssetID 
-         * @param AssetData 
-         */
-        void LoadFirst65KB(long AssetID, std::shared_ptr<ERS_STRUCT_IOData> AssetData);
+private:
+    
+    /**
+     * @brief Loads the first 65 kb for metadata identification
+     * 
+     * @param AssetID 
+     * @param AssetData 
+     */
+    void LoadFirst65KB(long AssetID, std::shared_ptr<ERS_STRUCT_IOData> AssetData);
 
-    public:
+public:
 
-        std::string AssetPath_; /**<Relative Path To Prepend To All IO Operations, Used By File Loading*/
-        std::vector<long> UsedAssetIDs_; /**<List Of Asset IDs That Are Already In Use*/
-        std::mutex LockAssetIDAllocation_; /**<Used When Allocating AssetIDs*/
-        bool UseDatabase_; /**<Indicates If The Database Should Be Used For IO. Set By LocalSystemConfiguration Values In Constructor*/
+    std::string AssetPath_; /**<Relative Path To Prepend To All IO Operations, Used By File Loading*/
+    std::vector<long> UsedAssetIDs_; /**<List Of Asset IDs That Are Already In Use*/
+    std::mutex LockAssetIDAllocation_; /**<Used When Allocating AssetIDs*/
+    bool UseDatabase_; /**<Indicates If The Database Should Be Used For IO. Set By LocalSystemConfiguration Values In Constructor*/
 
-    public:
+public:
 
-        /**
-         * @brief Construct a new ers class inputoutputsubsystem object
-         * 
-         * @param Logger 
-         * @param LocalSystemConfiguration 
-         */
-        ERS_CLASS_InputOutputSubsystem(std::shared_ptr<LoggerClass> Logger, YAML::Node LocalSystemConfiguration);
-        
-        /**
-         * @brief Destroy the ers class inputoutputsubsystem object
-         * 
-         */
-        ~ERS_CLASS_InputOutputSubsystem();
-
-
-        /**
-         * @brief Returns an unused assetid that is guarenteed to not already exist.
-         * 
-         * @return long 
-         */
-        long AllocateAssetID();
+    /**
+     * @brief Construct a new ers class inputoutputsubsystem object
+     * 
+     * @param Logger 
+     * @param LocalSystemConfiguration 
+     */
+    ERS_CLASS_InputOutputSubsystem(std::shared_ptr<LoggerClass> Logger, YAML::Node LocalSystemConfiguration);
+    
+    /**
+     * @brief Destroy the ers class inputoutputsubsystem object
+     * 
+     */
+    ~ERS_CLASS_InputOutputSubsystem();
 
 
-        /**
-         * @brief Allocate A Batch Of IDs For Usage, Returns Vector Of Allocated IDs
-         * 
-         * @param NumberIDs 
-         * @return std::vector<long> 
-         */
-        std::vector<long> BatchAllocateIDs(int NumberIDs);
+    /**
+     * @brief Returns an unused assetid that is guarenteed to not already exist.
+     * 
+     * @return long 
+     */
+    long AllocateAssetID();
 
 
-        /**
-         * @brief Write Asset Data To ID
-         * 
-         * @param AssetID 
-         * @param AssetData 
-         * @return true 
-         * @return false 
-         */
-        bool WriteAsset(long AssetID, std::shared_ptr<ERS_STRUCT_IOData> AssetData);
-
-        /**
-         * @brief Batch Write Data to IDs, Both Lists Should Be The Same Length. Returns List Of Status Variables (true=succes, false=fail)
-         * 
-         * @param AssetIDs 
-         * @param AssetDatas 
-         * @return std::vector<bool> 
-         */
-        std::vector<bool> BatchWriteAssets(std::vector<long> AssetIDs, std::vector<std::shared_ptr<ERS_STRUCT_IOData>> AssetDatas);
-
-        /**
-         * @brief Read Asset Data From ID
-         * 
-         * @param AssetID 
-         * @param AssetData 
-         * @return true 
-         * @return false 
-         */
-        bool ReadAsset(long AssetID, std::shared_ptr<ERS_STRUCT_IOData> AssetData);
-
-        /**
-         * @brief Read A List Of Assets From The Given IDs, Both Vectors Should Be Equal In Length.
-         * 
-         * @param AssetIDs 
-         * @param AssetDatas 
-         * @return std::vector<bool> 
-         */
-        std::vector<bool> BatchReadAssets(std::vector<long> AssetIDs, std::vector<std::shared_ptr<ERS_STRUCT_IOData>> AssetDatas);
+    /**
+     * @brief Allocate A Batch Of IDs For Usage, Returns Vector Of Allocated IDs
+     * 
+     * @param NumberIDs 
+     * @return std::vector<long> 
+     */
+    std::vector<long> BatchAllocateIDs(int NumberIDs);
 
 
-        /**
-         * @brief Updates the list of used asset IDs to prevent accidental data loss. Called during initialization.
-         * 
-         */
-        void IndexUsedAssetIDs();
+    /**
+     * @brief Write Asset Data To ID
+     * 
+     * @param AssetID 
+     * @param AssetData 
+     * @return true 
+     * @return false 
+     */
+    bool WriteAsset(long AssetID, std::shared_ptr<ERS_STRUCT_IOData> AssetData);
 
-        /**
-         * @brief Update asset path to string passed in. This allows you to change projects (IE: load another project, etc.) REMEMBER TO CALL ASSET ID INDEXING AFTER THIS, ELSE DATA LOSS CAN OCCUR.
-         * 
-         * @param AssetPath 
-         */
-        void UpdateAssetPath(std::string AssetPath);
+    /**
+     * @brief Batch Write Data to IDs, Both Lists Should Be The Same Length. Returns List Of Status Variables (true=succes, false=fail)
+     * 
+     * @param AssetIDs 
+     * @param AssetDatas 
+     * @return std::vector<bool> 
+     */
+    std::vector<bool> BatchWriteAssets(std::vector<long> AssetIDs, std::vector<std::shared_ptr<ERS_STRUCT_IOData>> AssetDatas);
+
+    /**
+     * @brief Read Asset Data From ID
+     * 
+     * @param AssetID 
+     * @param AssetData 
+     * @return true 
+     * @return false 
+     */
+    bool ReadAsset(long AssetID, std::shared_ptr<ERS_STRUCT_IOData> AssetData);
+
+    /**
+     * @brief Read A List Of Assets From The Given IDs, Both Vectors Should Be Equal In Length.
+     * 
+     * @param AssetIDs 
+     * @param AssetDatas 
+     * @return std::vector<bool> 
+     */
+    std::vector<bool> BatchReadAssets(std::vector<long> AssetIDs, std::vector<std::shared_ptr<ERS_STRUCT_IOData>> AssetDatas);
+
+
+    /**
+     * @brief Updates the list of used asset IDs to prevent accidental data loss. Called during initialization.
+     * 
+     */
+    void IndexUsedAssetIDs();
+
+    /**
+     * @brief Update asset path to string passed in. This allows you to change projects (IE: load another project, etc.) REMEMBER TO CALL ASSET ID INDEXING AFTER THIS, ELSE DATA LOSS CAN OCCUR.
+     * 
+     * @param AssetPath 
+     */
+    void UpdateAssetPath(std::string AssetPath);
 
 
 
