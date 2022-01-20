d848389a50f3897954329e8bc1991493bb450ed4
datacrystals <tliao@carboncopies.org>
2022-01-20 04:15:27 +0000

    Implement Asset Index Loader

--- Source/Core/Loader/ERS_AssetIndexLoader/ERS_CLASS_AssetIndexLoader.h
+++ Source/Core/Loader/ERS_AssetIndexLoader/ERS_CLASS_AssetIndexLoader.h
@@ -6,6 +6,7 @@
 #pragma once
 
 // Standard Libraries (BG convention: use <> instead of "")
+#include <memory>
 
 // Third-Party Libraries (BG convention: use <> instead of "")
 #include <yaml-cpp/yaml.h>
@@ -17,6 +18,32 @@
 
 class ERS_CLASS_AssetIndexLoader {
 
+private:
+
+    std::shared_ptr<LoggerClass> Logger_; /**<Pointer To Logger Instance*/
+    
+
+public:
+
+    /**
+     * @brief Construct a new ers class assetindexloader object
+     * 
+     * @param Logger 
+     */
+    ERS_CLASS_AssetIndexLoader(std::shared_ptr<LoggerClass> Logger);
+
+    /**
+     * @brief Destroy the ers class assetindexloader object
+     * 
+     */
+    ~ERS_CLASS_AssetIndexLoader();
+    
+    /**
+     * @brief Load The Asset Index Into Memory
+     * 
+     * @param Data 
+     */
+    void LoadAssetIndex(std::shared_ptr<ERS_STRUCT_IOData> Data);
 
 
 };
