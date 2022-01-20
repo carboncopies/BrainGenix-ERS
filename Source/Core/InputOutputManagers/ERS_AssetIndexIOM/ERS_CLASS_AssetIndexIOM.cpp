5601b9d7fb872ad6518ba96e5b50e7c5bde12304
datacrystals <tliao@carboncopies.org>
2022-01-20 04:20:25 +0000

    Implement Asset Index IO Manager

--- Source/Core/InputOutputManagers/ERS_AssetIndexIOM/ERS_CLASS_AssetIndexIOM.cpp
+++ Source/Core/InputOutputManagers/ERS_AssetIndexIOM/ERS_CLASS_AssetIndexIOM.cpp
@@ -1,1 +1,6 @@
- 
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#include <ERS_CLASS_AssetIndexIOM.h>
+
