af519c883b87557227a5ae107aad7b5e5695ea8e
datacrystals <tliao@carboncopies.org>
2022-01-20 04:09:03 +0000

    Implement Asset Index Loader

--- Source/Core/Loader/ERS_AssetIndex/ERS_CLASS_AssetIndex.h
+++ Source/Core/Loader/ERS_AssetIndex/ERS_CLASS_AssetIndex.h
@@ -1,1 +1,22 @@
- 
+ //======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <LoggingSystem.h>
+#include <ERS_STRUCT_IOData.h>
+
+
+class ERS_CLASS_AssetIndexLoader {
+
+
+
+};
