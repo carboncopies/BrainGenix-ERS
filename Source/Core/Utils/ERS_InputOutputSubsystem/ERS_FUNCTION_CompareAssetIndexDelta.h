97ab0df394a83fa43e97732cd66932fa9b43283d
datacrystals <tliao@carboncopies.org>
2022-01-20 05:25:53 +0000

    Implement Asset Index Comparison System

--- Source/Core/Utils/ERS_InputOutputSubsystem/ERS_FUNCTION_CompareAssetIndexDelta.h
+++ Source/Core/Utils/ERS_InputOutputSubsystem/ERS_FUNCTION_CompareAssetIndexDelta.h
@@ -24,4 +24,4 @@
  * @param UsedAssetIDIndex 
  * @param AssetIndexIOM 
  */
-void CompareIndexDelta(std::shared_ptr<LoggerClass> Logger, std::shared_ptr<std::vector<long>> UsedAssetIDIndex, std::shared_ptr<ERS_CLASS_AssetIndexIOM> AssetIndexIOM);
+void ERS_FUNCTION_CompareIndexDelta(std::shared_ptr<LoggerClass> Logger, std::shared_ptr<std::vector<long>> UsedAssetIDIndex, std::shared_ptr<ERS_CLASS_AssetIndexIOM> AssetIndexIOM);
