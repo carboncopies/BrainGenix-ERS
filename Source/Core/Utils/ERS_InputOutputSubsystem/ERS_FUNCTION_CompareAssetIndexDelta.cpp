a2a1a05ee177694f1bcb00836a47d29da0cc4147
datacrystals <tliao@carboncopies.org>
2022-01-20 05:44:19 +0000

    Implement Asset Index Comparison System

--- Source/Core/Utils/ERS_InputOutputSubsystem/ERS_FUNCTION_CompareAssetIndexDelta.cpp
+++ Source/Core/Utils/ERS_InputOutputSubsystem/ERS_FUNCTION_CompareAssetIndexDelta.cpp
@@ -15,13 +15,49 @@ void ERS_FUNCTION_CompareIndexDelta(std::shared_ptr<LoggerClass> Logger_, std::s
 
     // Make Copy Of Used Asset ID Index To Check Against
     std::vector<long> RemainingAssetIDs = *UsedAssetIDIndex;
+    std::vector<long> UsedAssetIDs = *UsedAssetIDIndex;
 
     // Iterate Through All Asset Index Metadata
     for (long i = 0; i < AssetIndexIOM->AssetIDsFound_.size(); i++) {
 
+        // Get Index Checking
+        long IndexChecking = AssetIndexIOM->AssetIDsFound_[i];
+        Logger_->Log(std::string(std::string("Checking Asset Index '") + std::to_string(IndexChecking) + std::string("'")).c_str(), 3);
+
+        // Check If Used asset ID Index Has Asset Found In Metadata
+        bool Found = false;
+        for (long i = 0; i < UsedAssetIDs.size(); i++) {
+            if (UsedAssetIDs[i] == IndexChecking) {
+                Found = true;
+                Logger_->Log(std::string(std::string("Matched Asset Pair With ID '") + std::to_string(IndexChecking) + std::string("'")).c_str(), 4);
+                break;
+            }
+        }
+
+        // If Found, Remove From IDs Of Remaining Indexes
+        if (Found) {
+            for (long i = 0; i < RemainingAssetIDs.size(); i++) {
+                if (RemainingAssetIDs[i] == IndexChecking) {
+                    RemainingAssetIDs.erase(RemainingAssetIDs.begin() + i);
+                    break;
+                }
+            }
+        } else {
+            Logger_->Log(std::string(std::string("Asset Metadata Index References Asset '") + std::to_string(IndexChecking) + std::string("', But This Asset Does Not Exist")).c_str(), 8);
+        }
 
 
     }
+
+    // Iterate Through Any Remaining Assets (Should Be Zero)
+    // Would Be Caused By An Asset Not Being In Metadata For Some Reason
+    for (long i = 0; i < RemainingAssetIDs.size(); i++) { 
+
+        long IndexChecking = RemainingAssetIDs[i];
+        Logger_->Log(std::string(std::string("No Asset Metadata Entry Exists For Asset '") + std::to_string(IndexChecking) + std::string("'")).c_str(), 8);
+
+    }
+
 
     // Log Comparison Finish
     Logger_->Log("Finished Comparing Index With Metadata", 5);
