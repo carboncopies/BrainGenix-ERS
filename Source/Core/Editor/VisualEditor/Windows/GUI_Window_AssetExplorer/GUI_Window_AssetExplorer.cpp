2ea36d849a50669ccefdef908aa06b4462baba9b
datacrystals <tliao@carboncopies.org>
2022-01-20 09:17:56 +0000

    Add Button To Re-Index Used Asset IDs

--- Source/Core/Editor/VisualEditor/Windows/GUI_Window_AssetExplorer/GUI_Window_AssetExplorer.cpp
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_AssetExplorer/GUI_Window_AssetExplorer.cpp
@@ -61,11 +61,6 @@ void Window_AssetExplorer::Draw() {
                     ImVec2 IDExplorerSize = ImVec2(ImGui::GetWindowWidth()-300, 0);
                     ImGui::BeginChild("ID Explorer Child Node", IDExplorerSize);
 
-                        // Refresh Button
-                        if (ImGui::Button("Refresh Asset Index")) {
-                            SystemUtils_->ERS_IOSubsystem_->IndexUsedAssetIDs();
-                        }
-
                         // Child Node Title
                         ImGui::Separator();
                         ImGui::TextColored(ImVec4(0.2f, 1.0f, 0.2f, 1.0f), "Asset ID");
@@ -117,6 +112,23 @@ void Window_AssetExplorer::Draw() {
                     ImGui::EndChild();
 
 
+
+                ImGui::EndTabItem();
+                }
+
+                // Tools bar
+                if (ImGui::BeginTabItem("Tools")) {
+
+                    // Child Window
+                    ImGui::BeginChild("Asset Explorer Tools");
+
+                    // Refresh Button
+                    if (ImGui::Button("Refresh Asset Index")) {
+                        SystemUtils_->ERS_IOSubsystem_->IndexUsedAssetIDs();
+                    }
+
+                    // End Child
+                    ImGui::EndChild();
 
                 ImGui::EndTabItem();
                 }
