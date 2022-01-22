a180088515547a8cb7628e2d101c6202ac56454d
datacrystals <tliao@carboncopies.org>
2022-01-22 09:01:37 +0000

    Fix Compiler Long Int Warning (#8)

--- Source/Core/Editor/VisualEditor/Windows/GUI_Window_AssetExplorer/GUI_Window_AssetExplorer.cpp
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_AssetExplorer/GUI_Window_AssetExplorer.cpp
@@ -78,7 +78,7 @@ void Window_AssetExplorer::Draw() {
 
                                 // Set Drag+Drop Payload
                                 ImGui::SetDragDropPayload("PAYLOAD_ASSET_MODEL_ID", &i, sizeof(long));
-                                ImGui::Text("ERS Model '%d'", i);
+                                ImGui::Text(std::string(std::string("ERS Model '") + std::to_string(i) + std::string("'")).c_str());
 
                             ImGui::EndDragDropSource();
                             }
