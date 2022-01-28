carboncopies/BrainGenix-ERS

commit 0b6bf9a8078ca1e52d41c0b7e4b32fb41f26dc95

Attempt To Convert AddImage To ImageButton (#10)

Source/Core/Renderer/VisualRenderer/VisualRenderer.cpp
@@ -220,15 +220,15 @@ void VisualRenderer::UpdateViewport(int Index, std::shared_ptr<ERS_CLASS_SceneMa
     SceneManager->Render(Shaders_[Index]);
 
 
-    // Render Framebuffer To Window
-    ImGui::GetWindowDrawList()->AddImage(
-        (void*)(intptr_t)FramebufferColorObjects_[Index],
-        ImVec2(ImGui::GetCursorScreenPos()),
-        ImVec2(ImGui::GetCursorScreenPos().x + ImGui::GetWindowSize().x,
-            ImGui::GetCursorScreenPos().y + ImGui::GetWindowSize().y),
-        ImVec2(0, 1),
-        ImVec2(1, 0)        
-    );
+    // // Render Framebuffer To Window
+    // ImGui::GetWindowDrawList()->AddImage(
+    //     (void*)(intptr_t)FramebufferColorObjects_[Index],
+    //     ImVec2(ImGui::GetCursorScreenPos()),
+    //     ImVec2(ImGui::GetCursorScreenPos().x + ImGui::GetWindowSize().x,
+    //         ImGui::GetCursorScreenPos().y + ImGui::GetWindowSize().y),
+    //     ImVec2(0, 1),
+    //     ImVec2(1, 0)        
+    // );
 
 
     // Finish 3D Cursor

commit f71f4339a70695693d7dbd1c13e69687cf10335f

Attempt to debug issue caused by #10 for #12

Source/Core/Renderer/VisualRenderer/VisualRenderer.cpp
@@ -135,15 +135,6 @@ void VisualRenderer::UpdateViewport(int Index, std::shared_ptr<ERS_CLASS_SceneMa
     int WindowTopLeftCornerY = ImGui::GetWindowPos().y;
     int WindowBottomRightCornerX = ImGui::GetWindowSize().x + WindowTopLeftCornerX;
     int WindowBottomRightCornerY = ImGui::GetWindowSize().y + WindowTopLeftCornerY;
-
-    // Get Mouse Pos
-    int MousePositionX = ImGui::GetMousePos().x;
-    int MousePositionY = ImGui::GetMousePos().y;
-
-    // Check If In Bounding Box
-    bool MouseXInRange = (MousePositionX >= WindowTopLeftCornerX) && (MousePositionX < WindowBottomRightCornerX);
-    bool MouseYInRange = (MousePositionY >= WindowTopLeftCornerY) && (MousePositionY < WindowBottomRightCornerY);
-    bool MouseInRange = MouseXInRange && MouseYInRange;
     
 
     // Check If Input Enabled
@@ -151,7 +142,7 @@ void VisualRenderer::UpdateViewport(int Index, std::shared_ptr<ERS_CLASS_SceneMa
     if (ImGui::IsKeyDown(341)) { // Bind to left control key
         EnableCameraMovement = true;
     }
-    if (EnableCameraMovement && ImGui::IsWindowFocused() && (MouseInRange | WasSelected_[Index]) && (glfwGetMouseButton(Window_, 0) == GLFW_PRESS)) {
+    if (EnableCameraMovement && ImGui::IsWindowFocused() && (ImGui::IsWindowHovered()) && (glfwGetMouseButton(Window_, 0) == GLFW_PRESS)) {
         CaptureCursor_ = true;
         CaptureIndex_ = Index;
         WasSelected_[Index] = true;