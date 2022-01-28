3d449de46926b4f27bd74a4aaefa65ccd5463f83
datacrystals <tliao@carboncopies.org>
2022-01-28 07:23:04 +0000

    Attempt To Convert AddImage To ImageButton (#10)

--- Source/Core/Renderer/VisualRenderer/VisualRenderer.cpp
+++ Source/Core/Renderer/VisualRenderer/VisualRenderer.cpp
@@ -229,7 +229,13 @@ void VisualRenderer::UpdateViewport(int Index, std::shared_ptr<ERS_CLASS_SceneMa
     //     ImVec2(0, 1),
     //     ImVec2(1, 0)        
     // );
-
+    ImTextureID TextureID = ImTextureID(FramebufferColorObjects_[Index]);
+    ImGui::ImageButton(TextureID,
+        ImGui::GetContentRegionAvail(),
+        ImVec2(0, 1),
+        ImVec2(1, 0),
+        0
+        );
 
     // Finish 3D Cursor
     Cursors3D_->EndRenderpass(Cameras_[Index], CaptureCursor_);
