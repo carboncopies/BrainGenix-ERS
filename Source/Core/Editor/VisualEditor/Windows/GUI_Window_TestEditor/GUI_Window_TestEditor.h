7afebd5c1134615e0eeb158de7c67b9d0f08b6ac
datacrystals <tliao@carboncopies.org>
2022-01-29 06:49:15 +0000

    Create TestEditor Base Files

---
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_TestEditor/GUI_Window_TestEditor.h
@@ -0,0 +1,19 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <memory>
+#include <thread>
+#include <string>
+#include <mutex>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <imgui.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_STRUCT_SystemUtils.h>
