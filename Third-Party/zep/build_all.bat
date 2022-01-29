affa2b49a4e1ba4e7513a056dcfc4fb458a7cd7b
datacrystals <tliao@carboncopies.org>
2022-01-29 07:26:33 +0000

    Switch To ImGuiColorTextEdit From Zep (due to docs)

--- Third-Party/zep/build_all.bat
+++
@@ -1,6 +0,0 @@
-call config_all.bat
-cd build
-cmake --build . --config Release --target Install
-cmake --build . --config Debug --target Install
-cmake --build . --config RelWithDebInfo --target Install
-cd ..
