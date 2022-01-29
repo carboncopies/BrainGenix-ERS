533c7b1a13a55db52c2a78f90c5c4f728bc5ae21
datacrystals <tliao@carboncopies.org>
2022-01-29 00:20:30 +0000

    Fix Darwin Build Errors

--- CMake/BuildUtils/MySQLConnector.cmake
+++ CMake/BuildUtils/MySQLConnector.cmake
@@ -3,7 +3,9 @@
 ##########################
 
 if (WIN32)
-    ERSBuildLogger(${Yellow} "Database Support Disabled On Windows Due To SSL Errors, Please Use Linux/BSD Instead")
+    ERSBuildLogger(${Yellow} "Database Support Disabled On Windows Due To SSL Errors, Please Use Linux Instead")
+elseif(APPLE)
+    ERSBuildLogger(${Yellow} "Database Support Disabled On Darwin Due To SSL Errors, Please Use Linux Instead")
 else()
     ERSBuildLogger(${Green} "Configuring MySQL Connector Library")
     add_subdirectory(${LIB_DIR}/mysql-connector-linux)
