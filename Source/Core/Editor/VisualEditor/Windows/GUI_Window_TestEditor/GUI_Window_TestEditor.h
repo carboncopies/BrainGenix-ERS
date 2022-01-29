ee65ce4af1a115303c20e4a00b9743a9186f4f4e
datacrystals <tliao@carboncopies.org>
2022-01-29 06:51:59 +0000

    Add Zep Includes

--- Source/Core/Editor/VisualEditor/Windows/GUI_Window_TestEditor/GUI_Window_TestEditor.h
+++ Source/Core/Editor/VisualEditor/Windows/GUI_Window_TestEditor/GUI_Window_TestEditor.h
@@ -15,5 +15,22 @@
 // Third-Party Libraries (BG convention: use <> instead of "")
 #include <imgui.h>
 
+#include "zep/mcommon/animation/timer.h"
+
+#include "zep/filesystem.h"
+#include "zep/imgui/display_imgui.h"
+#include "zep/imgui/editor_imgui.h"
+#include "zep/mode_standard.h"
+#include "zep/mode_vim.h"
+#include "zep/tab_window.h"
+#include "zep/theme.h"
+#include "zep/window.h"
+
+#include "orca/mode_orca.h"
+#include "repl/mode_repl.h"
+
+#include "zep/regress.h"
+
+
 // Internal Libraries (BG convention: use <> instead of "")
 #include <ERS_STRUCT_SystemUtils.h>
