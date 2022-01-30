f38965039d6fdcf89d4c7ed35569901454616a0e
datacrystals <tliao@carboncopies.org>
2022-01-30 06:27:14 +0000

    Refactor LocRotScale Struct

--- Source/Core/Structures/ERS_STRUCT_LocRotScale/ERS_STRUCT_LocRotScale.h
+++ Source/Core/Structures/ERS_STRUCT_LocRotScale/ERS_STRUCT_LocRotScale.h
@@ -2,13 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This is responsible for implementing the hardware information structure.
-    Documentation Status: Complete
-    Additonal Notes: None
-    Date Created: 2021-12-07
-*/
-
 #pragma once
 
 // Standard Libraries (BG convention: use <> instead of "")
