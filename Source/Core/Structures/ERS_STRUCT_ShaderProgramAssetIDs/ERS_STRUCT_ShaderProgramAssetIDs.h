0e1ef0611677abd7937cd626fb097eb82de02e6a
datacrystals <tliao@carboncopies.org>
2022-01-30 06:31:26 +0000

    Add Shader Program Asset ID Struct

--- Source/Core/Structures/ERS_STRUCT_ShaderProgramAssetIDs/ERS_STRUCT_ShaderProgramAssetIDs.h
+++ Source/Core/Structures/ERS_STRUCT_ShaderProgramAssetIDs/ERS_STRUCT_ShaderProgramAssetIDs.h
@@ -1,1 +1,28 @@
 // ToDO: Implement struct here to store shader program ids from proj file, then add to project struct, then update project loader/writer with this info. Then check trello board for other related tasks like live ediitng.
+
+
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
+
+/**
+ * @brief Stores Location, Rotation, Scale data in struct.
+ * 
+ */
+struct ERS_STRUCT_ShaderProgramAssetIDs {
+
+    // Shader Asset IDs
+    long VertexID; /**<Vertex Shader Asset ID*/
+    long FragmentID; /**<Fragment Shader Asset ID*/
+
+};
