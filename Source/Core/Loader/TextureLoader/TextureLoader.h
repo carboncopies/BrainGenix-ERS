e938c0ee917e24503ae503af66c055d89c4a4b6c
Thomas Liao <tliao@carboncopies.org>
2022-01-31 13:38:39 -0800

    Update Includes For Logging System Refactor

--- Source/Core/Loader/TextureLoader/TextureLoader.h
+++ Source/Core/Loader/TextureLoader/TextureLoader.h
@@ -17,7 +17,7 @@
 #include <GLFW/glfw3.h>
 
 // Internal Libraries (BG convention: use <> instead of "")
-#include <LoggingSystem.h>
+#include < ERS_CLASS_LoggingSystem.h>
 #include <ImageDecoder.h>
 #include <2DTexture.h>
 
