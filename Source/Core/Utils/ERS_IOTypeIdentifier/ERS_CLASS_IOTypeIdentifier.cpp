c8762fdb20738984f9ab55d047d642df9666e3f4
Thomas Liao <tliao@carboncopies.org>
2022-01-31 13:47:47 -0800

    Update Includes For Logging System Refactor

--- Source/Core/Utils/ERS_IOTypeIdentifier/ERS_CLASS_IOTypeIdentifier.cpp
+++ Source/Core/Utils/ERS_IOTypeIdentifier/ERS_CLASS_IOTypeIdentifier.cpp
@@ -6,7 +6,7 @@
 
 
 // Constructor
-ERS_CLASS_IOTypeIdentifier::ERS_CLASS_IOTypeIdentifier(std::shared_ptr<LoggerClass> Logger) {
+ERS_CLASS_IOTypeIdentifier::ERS_CLASS_IOTypeIdentifier(std::shared_ptr<ERS_CLASS_LoggingSystem> Logger) {
     
     // Copy Logger Pointer
     Logger_ = Logger;
