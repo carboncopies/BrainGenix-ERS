e433f5bdbe188fb75c051a7d89af811725746f84
Thomas Liao <tliao@carboncopies.org>
2022-01-31 13:24:58 -0800

    Refactor ERS Logging System

--- Source/Core/Utils/LoggingSystem/ERS_CLASS_LoggingSystem.cpp
+++ Source/Core/Utils/LoggingSystem/ERS_CLASS_LoggingSystem.cpp
@@ -2,15 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This file is responsible for implementing the logger system used by the rest of the system.
-    Additonal Notes: None
-    Date Created: 2021-05-31
-*/
-
-
-
-
 #include "ERS_CLASS_LoggingSystem.h"
 
 
