0ae1ce2e73dce0059f8b10031349b14716263e66
Thomas Liao <tliao@carboncopies.org>
2022-01-31 13:38:31 -0800

    Update Includes For Logging System Refactor

--- Source/Core/Loader/ERS_SceneLoader/ERS_SceneLoader.h
+++ Source/Core/Loader/ERS_SceneLoader/ERS_SceneLoader.h
@@ -18,7 +18,7 @@
 #include <yaml-cpp/yaml.h>
 
 // Internal Libraries (BG convention: use <> instead of "")
-#include <LoggingSystem.h>
+#include < ERS_CLASS_LoggingSystem.h>
 #include <Model.h>
 #include <ERS_ModelLoader.h>
 #include <Scene.h>
