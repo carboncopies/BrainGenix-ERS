e94e6b803ac7838516063698ec0ee960e67dbe07
Thomas Liao <tliao@carboncopies.org>
2022-01-31 13:37:24 -0800

    Refactor ERS Logging System

--- Docs/Templates/Template.h
+++ Docs/Templates/Template.h
@@ -17,4 +17,4 @@
 // Third-Party Libraries (BG convention: use <> instead of "")
 
 // Internal Libraries (BG convention: use <> instead of "")
-#include <LoggingSystem.h>
+#include < ERS_CLASS_LoggingSystem.h>
