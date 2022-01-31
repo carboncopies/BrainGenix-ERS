70c320078e927fb153cb5eb0dbb1f4fbe1cef711
Thomas Liao <tliao@carboncopies.org>
2022-01-31 14:00:15 -0800

    Fix LoggerClass Typo In Include Headers

--- Source/Core/Manager/ERS_SceneManager/ERS_SceneManager.h
+++ Source/Core/Manager/ERS_SceneManager/ERS_SceneManager.h
@@ -18,7 +18,7 @@
 #include <glm/glm.hpp>
 
 // Internal Libraries (BG convention: use <> instead of "")
-#include <ERS_CASS_LoggingSystem.h>
+#include <ERS_CLASS_LoggingSystem.h>
 #include <ERS_STRUCT_Shader.h>
 #include <Scene.h>
 #include <ERS_STRUCT_LocRotScale.h>
