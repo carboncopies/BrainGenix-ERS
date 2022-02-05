014f055f417f8659f11063631ec9ed6f843fa80f
Thomas Liao <tliao@carboncopies.org>
2022-02-05 00:52:00 -0800

    Refactor CMake Deps For ERS_STRUCT_Mesh  (#27)

--- Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.h
+++ Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.h
@@ -90,7 +90,7 @@ class ERS_CLASS_ModelLoader {
          * @param Scene 
          * @return ERS_STRUCT_Mesh 
          */
-        ERS_OBJECT_MESH ProcessMesh(ERS_OBJECT_MODEL* Model, aiMesh *Mesh, const aiScene *Scene, std::vector<std::string> TexturePaths);
+        ERS_STRUCT_Mesh ProcessMesh(ERS_OBJECT_MODEL* Model, aiMesh *Mesh, const aiScene *Scene, std::vector<std::string> TexturePaths);
 
         /**
          * @brief Load Textures From Model.
