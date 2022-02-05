670c1ebd54e0d1b224d6cf7e228ccdeff7fb9b0a
Thomas Liao <tliao@carboncopies.org>
2022-02-05 01:25:37 -0800

    Refactor ERS_STRUCT_Texture (#31)

--- Source/Core/Editor/MenuEntries/GUI_Menu_File/Utils/Import_Asset/ERS_Editor_ModelImporter.h
+++ Source/Core/Editor/MenuEntries/GUI_Menu_File/Utils/Import_Asset/ERS_Editor_ModelImporter.h
@@ -39,7 +39,7 @@
 
 // Internal Libraries (BG convention: use <> instead of "")
 #include <ERS_STRUCT_Mesh.h>
-#include <2DTexture.h>
+#include <ERS_STRUCT_Texture.h>
 #include <ERS_STRUCT_Vertex.h>
 #include <FileIO.h>
 #include <ERS_STRUCT_Model.h>
