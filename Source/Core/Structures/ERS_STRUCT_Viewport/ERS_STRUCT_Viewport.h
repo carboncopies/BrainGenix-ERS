83724e544771cc5c8943e81324275ebadb45a5e3
Thomas Liao <tliao@carboncopies.org>
2022-02-04 16:04:32 -0800

    Create Viewport Struct

---
+++ Source/Core/Structures/ERS_STRUCT_Viewport/ERS_STRUCT_Viewport.h
@@ -0,0 +1,24 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <memory.h>
+#include <string>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
+/**
+ * @brief The System utils struct contains most system utils prepopulated as shared pointers within.
+ * 
+ */
+struct ERS_STRUCT_SystemUtils {
+
+
+
+};
