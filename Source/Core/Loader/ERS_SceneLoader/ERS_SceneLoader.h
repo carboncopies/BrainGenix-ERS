3f41f580a651a917fb372ae5db84c7984f3ecc78
Thomas Liao <tliao@carboncopies.org>
2022-02-05 00:59:42 -0800

    Refactor Header Includes For Model Struct (#29)

--- Source/Core/Loader/ERS_SceneLoader/ERS_SceneLoader.h
+++ Source/Core/Loader/ERS_SceneLoader/ERS_SceneLoader.h
@@ -19,7 +19,7 @@
 
 // Internal Libraries (BG convention: use <> instead of "")
 #include <ERS_CLASS_LoggingSystem.h>
-#include <Model.h>
+#include <ERS_STRUCT_Model.h>
 #include <ERS_ModelLoader.h>
 #include <Scene.h>
 #include <ERS_STRUCT_Mesh.h>
