47b408f201b7857192a72a75c1572ef71985aca6
Thomas Liao <tliao@carboncopies.org>
2022-02-04 17:58:56 -0800

    Implement Preview Shader

--- Source/Core/Editor/Windows/GUI_Window_ShaderEditor/GUI_Window_ShaderEditor.cpp
+++ Source/Core/Editor/Windows/GUI_Window_ShaderEditor/GUI_Window_ShaderEditor.cpp
@@ -230,13 +230,14 @@ void Window_ShaderEditor::DrawToolsWindow() {
         LivePreviewShader_->CreateShaderProgram();
         bool ShaderCompiled = LivePreviewShader_->MakeActive();
         LivePreviewShader_->SetInt("texture_diffuse1", 0);
+        LivePreviewShader_->DisplayName = "Preview Shader";
+        LivePreviewShader_->InternalName = "Preview Shader";
 
 
 
         // If Autopreview, Update Shader
         if (LivePreview_ && ShaderCompiled) {
-            VisualRenderer_->SetShader(LivePreviewShader_, SelectedShaderProgramIndex_);
-            
+            VisualRenderer_->SetShader(LivePreviewShader_, VisualRenderer_->Shaders_.size());
         }
 
 
