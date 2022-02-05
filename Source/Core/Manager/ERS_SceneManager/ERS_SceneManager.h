c93e76687534ab5fc7d1e72e7aedd786d85448d1
Thomas Liao <tliao@carboncopies.org>
2022-02-05 01:21:06 -0800

    Update Includes For ERS_STRUCT_Scene (#30)

--- Source/Core/Manager/ERS_SceneManager/ERS_SceneManager.h
+++ Source/Core/Manager/ERS_SceneManager/ERS_SceneManager.h
@@ -20,7 +20,7 @@
 // Internal Libraries (BG convention: use <> instead of "")
 #include <ERS_CLASS_LoggingSystem.h>
 #include <ERS_STRUCT_Shader.h>
-#include <Scene.h>
+#include <ERS_STRUCT_Scene.h>
 #include <ERS_STRUCT_LocRotScale.h>
 
 
