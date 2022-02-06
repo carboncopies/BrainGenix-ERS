c65dbd088442956474132c38777b31cafbb525e1
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:44:41 -0800

    Remove Comment Headers

--- Source/Core/Structures/ERS_STRUCT_Texture/ERS_STRUCT_Texture.h
+++ Source/Core/Structures/ERS_STRUCT_Texture/ERS_STRUCT_Texture.h
@@ -2,12 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This is the 2d texture struct.
-    Additonal Notes: None
-    Date Created: 2021-11-04
-*/
-
 #pragma once
 
 
