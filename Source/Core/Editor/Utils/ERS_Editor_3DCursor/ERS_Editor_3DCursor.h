406ded474f69f3f02ec1ba46500034edc40539f4
Thomas Liao <tliao@carboncopies.org>
2022-02-06 11:05:39 -0800

    Remove Comment Headers

--- Source/Core/Editor/Utils/ERS_Editor_3DCursor/ERS_Editor_3DCursor.h
+++ Source/Core/Editor/Utils/ERS_Editor_3DCursor/ERS_Editor_3DCursor.h
@@ -2,14 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This file is responsible for creating and managing 3d cursors.
-    Documentation Status: Complete
-    Additonal Notes: None
-    Date Created: 2021-12-01
-*/
-
-
 #pragma once
 
 // Standard Libraries (BG convention: use <> instead of "")
