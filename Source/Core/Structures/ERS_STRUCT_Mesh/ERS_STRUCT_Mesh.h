14891e5e1a94992de21aa5faf50fbe44471b97fa
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:48:27 -0800

    Remove Comment Headers

--- Source/Core/Structures/ERS_STRUCT_Mesh/ERS_STRUCT_Mesh.h
+++ Source/Core/Structures/ERS_STRUCT_Mesh/ERS_STRUCT_Mesh.h
@@ -2,12 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This file creates the ERSMesh Struct/Functions.
-    Additonal Notes: None
-    Date Created: 2021-11-04
-*/ 
-
 #pragma once
 
 // Third-Party Libraries (BG convention: use <> instead of "")
