2c137c9845f6e387cade540f842be3b09ed66bda
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:43:19 -0800

    Remove Comment Headers

--- Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_LoggingSystem.h
+++ Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_LoggingSystem.h
@@ -2,12 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This file is responsible for implementing the logger system used by the rest of the system.
-    Additonal Notes: None
-    Date Created: 2021-05-31
-*/
-
 #pragma once
 
 // Standard Libraries (BG convention: use <> instead of "")
