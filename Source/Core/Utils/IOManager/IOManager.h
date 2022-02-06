bf411e0d629164b6e3e30a107ab83faf58a68b71
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:41:29 -0800

    Remove Comment Headers

--- Source/Core/Utils/IOManager/IOManager.h
+++ Source/Core/Utils/IOManager/IOManager.h
@@ -2,13 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This file provides IOManager functionality.
-    Documentation Status: Complete
-    Additonal Notes: None
-    Date Created: 2021-11-29
-*/
-
 #pragma once
 
 
