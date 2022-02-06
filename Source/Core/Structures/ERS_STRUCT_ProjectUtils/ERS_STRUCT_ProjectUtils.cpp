c76df1eadbd696c4b80e62fe2805a380ea6a7b8d
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:46:37 -0800

    Remove Comment Headers

--- Source/Core/Structures/ERS_STRUCT_ProjectUtils/ERS_STRUCT_ProjectUtils.cpp
+++ Source/Core/Structures/ERS_STRUCT_ProjectUtils/ERS_STRUCT_ProjectUtils.cpp
@@ -2,11 +2,4 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This is responsible for implementing a struct to simplify includes within classes..
-    Documentation Status: Complete
-    Additonal Notes: None
-    Date Created: 2021-12-16
-*/
-
 #include <ERS_STRUCT_ProjectUtils.h>
