5b167fa48e69e0477964566df1985106a2ae120e
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:48:13 -0800

    Remove Comment Headers

--- Source/Core/Structures/ERS_STRUCT_Model/ERS_STRUCT_Model.cpp
+++ Source/Core/Structures/ERS_STRUCT_Model/ERS_STRUCT_Model.cpp
@@ -2,12 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This file creates the ERSModel Struct/Functions.
-    Additonal Notes: None
-    Date Created: 2021-11-04
-*/ 
-
 
 #include "ERS_STRUCT_Model.h"
 
