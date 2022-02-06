0b37a7b43dcf36c885299fe865fb650a23114665
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:41:18 -0800

    Remove Comment Headers

--- Source/Core/Writers/ERS_ModelWriter/ERS_ModelWriter.cpp
+++ Source/Core/Writers/ERS_ModelWriter/ERS_ModelWriter.cpp
@@ -2,13 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This file contains the model writer class.
-    Documentation Status: Completed
-    Additonal Notes: None
-    Date Created: 2021-11-22
-*/
-
 #include <ERS_ModelWriter.h>
 
 
