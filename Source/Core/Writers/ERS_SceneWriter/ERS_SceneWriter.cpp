453d222d0b229900f74ad228776bbd162679f458
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:40:13 -0800

    Remove Comment Headers

--- Source/Core/Writers/ERS_SceneWriter/ERS_SceneWriter.cpp
+++ Source/Core/Writers/ERS_SceneWriter/ERS_SceneWriter.cpp
@@ -2,13 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This file contains the scene loader class.
-    Documentation Status: Completed
-    Additonal Notes: None
-    Date Created: 2021-11-22
-*/
-
 // FIXME: Make template textures added to their own map so they can be reused.
 // FIXME: Make dynamic/static models that can each be rotated/translated (dynamically or statically). Then make updateposition functions, etc.
 
