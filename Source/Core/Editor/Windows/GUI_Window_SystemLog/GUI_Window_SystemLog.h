c1b068b1fa49a2fbef1131056dce61dcd9818962
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:57:03 -0800

    Remove Comment Headers

--- Source/Core/Editor/Windows/GUI_Window_SystemLog/GUI_Window_SystemLog.h
+++ Source/Core/Editor/Windows/GUI_Window_SystemLog/GUI_Window_SystemLog.h
@@ -2,13 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This Window file is responsible for creating a system Window.
-    Documentation Status: Complete
-    Additonal Notes: None
-    Date Created: 2021-12-17
-*/
-
 #pragma once
 
 
