6a09866e34753e2eaf0522ad1c6445c3c13e3aa6
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:41:58 -0800

    Remove Comment Headers

--- Source/Core/Utils/InputProcessor/InputProcessor.cpp
+++ Source/Core/Utils/InputProcessor/InputProcessor.cpp
@@ -2,13 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This file contains the visual renderer callback functions.
-    Documentation Status: Complete
-    Additonal Notes: None
-    Date Created: 2021-11-16
-*/
-
 #include "InputProcessor.h"
 
 
