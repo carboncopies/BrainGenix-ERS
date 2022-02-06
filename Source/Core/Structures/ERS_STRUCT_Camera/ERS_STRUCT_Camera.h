58aa0a23dc9752f7feca7af84f18a25ff1198625
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:50:54 -0800

    Remove Comment Headers

--- Source/Core/Structures/ERS_STRUCT_Camera/ERS_STRUCT_Camera.h
+++ Source/Core/Structures/ERS_STRUCT_Camera/ERS_STRUCT_Camera.h
@@ -2,12 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This is the Noclip camera.
-    Additonal Notes: None
-    Date Created: 2021-11-05
-*/
-
 #pragma once
 
 // Standard Libraries (BG convention: use <> instead of "")
