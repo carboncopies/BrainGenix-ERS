a878fc638274a41eb1ec4d0c5538fc33d1d8353a
Thomas Liao <tliao@carboncopies.org>
2022-02-06 11:03:52 -0800

    Remove Comment Headers

--- Source/Core/Editor/Widgets/GUI_Widget_FrameLatencyGraph.h
+++ Source/Core/Editor/Widgets/GUI_Widget_FrameLatencyGraph.h
@@ -2,13 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This widget file is responsible for creating a system widget.
-    Documentation Status: Complete
-    Additonal Notes: None
-    Date Created: 2021-12-28
-*/
-
 #pragma once
 
 
