3f5073ab7dc6e66957793daa3a3709f17657a352
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:46:30 -0800

    Remove Comment Headers

--- Source/Core/Structures/ERS_STRUCT_ProjectUtils/ERS_STRUCT_ProjectUtils.h
+++ Source/Core/Structures/ERS_STRUCT_ProjectUtils/ERS_STRUCT_ProjectUtils.h
@@ -2,13 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This is responsible for implementing a struct to simplify includes within classes..
-    Documentation Status: Complete
-    Additonal Notes: None
-    Date Created: 2021-12-16
-*/
-
 #pragma once
 
 // Standard Libraries (BG convention: use <> instead of "")
