6cbd760808913498f3001c1b7bc55f79dab2ee66
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:53:06 -0800

    Remove Comment Headers

--- Source/Core/Manager/ERS_SceneManager/ERS_SceneManager.cpp
+++ Source/Core/Manager/ERS_SceneManager/ERS_SceneManager.cpp
@@ -2,13 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This file contains the scene manager classes.
-    Documentation Status: Done
-    Additonal Notes: None
-    Date Created: 2021-11-21
-*/
-
 #include <ERS_SceneManager.h>
 
 
