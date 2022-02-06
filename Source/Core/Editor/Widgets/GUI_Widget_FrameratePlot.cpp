1ea1ff0cf202c697876753e2fb97034951e2d785
Thomas Liao <tliao@carboncopies.org>
2022-02-06 11:02:54 -0800

    Remove Comment Headers

--- Source/Core/Editor/Widgets/GUI_Widget_FrameratePlot.cpp
+++ Source/Core/Editor/Widgets/GUI_Widget_FrameratePlot.cpp
@@ -2,13 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This widget file is responsible for creating a system widget.
-    Documentation Status: Complete
-    Additonal Notes: None
-    Date Created: 2021-12-02
-*/
-
 #include <GUI_Widget_FrameratePlot.h>
 
 
