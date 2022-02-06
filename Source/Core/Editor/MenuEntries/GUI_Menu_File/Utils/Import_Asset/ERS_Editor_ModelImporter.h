13694023272fcd2e33166a2fe5585ef542130ede
Thomas Liao <tliao@carboncopies.org>
2022-02-06 11:07:59 -0800

    Remove Comment Headers

--- Source/Core/Editor/MenuEntries/GUI_Menu_File/Utils/Import_Asset/ERS_Editor_ModelImporter.h
+++ Source/Core/Editor/MenuEntries/GUI_Menu_File/Utils/Import_Asset/ERS_Editor_ModelImporter.h
@@ -2,13 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This is the model loader.
-    Documentation Status: Complete
-    Additonal Notes: None
-    Date Created: 2021-11-13
-*/
-
 #pragma once
 
 // Standard Libraries (BG convention: use <> instead of "")
