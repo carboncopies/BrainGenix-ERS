9c4068cde89b2bbe9691903fb7d2985a7b8d66ca
Thomas Liao <tliao@carboncopies.org>
2022-02-06 10:53:26 -0800

    Remove Comment Headers

--- Source/Core/Manager/ERS_ProjectManager/ERS_ProjectManager.cpp
+++ Source/Core/Manager/ERS_ProjectManager/ERS_ProjectManager.cpp
@@ -2,13 +2,6 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-/*
-    Description: This is responsible for providing a class to manage project loading/writing/storage functions.
-    Documentation Status: Complete
-    Additonal Notes: None
-    Date Created: 2022-01-03
-*/
-
 #include <ERS_ProjectManager.h>
 
 
