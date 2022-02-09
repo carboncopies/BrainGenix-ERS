83a30b51d9b93cc87dee77c0224f1e7f4a74f6d4
datacrystals <tliao@carboncopies.org>
2022-02-09 23:47:57 +0000

    Refactor InputProcessor

--- Source/Core/Utils/IOManager/IOManager.h
+++ Source/Core/Utils/IOManager/IOManager.h
@@ -33,7 +33,7 @@ class IOManager {
 
         // Class Instances
         std::shared_ptr<ERS_CLASS_LoggingSystem> Logger_; /**<Logging System Instance Pointer*/
-        std::shared_ptr<InputProcessor> InputProcessor_; /**<Pointer to input processor instance*/
+        std::shared_ptr<ERS_CLASS_InputProcessor> InputProcessor_; /**<Pointer to input processor instance*/
         std::shared_ptr<ERS_STRUCT_Camera> Camera_; /**<Pointer To Camera Instance*/
         //FramebufferManager *FramebufferManager_; /**<Pointer To Framebuffer Manager Instance*/
 
