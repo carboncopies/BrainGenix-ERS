ce001c2d009bfd07b0337a6be325b0f35bd7d092
datacrystals <tliao@carboncopies.org>
2022-02-09 23:47:29 +0000

    Refactor InputProcessor

--- Source/Core/Structures/ERS_STRUCT_Viewport/ERS_STRUCT_Viewport.h
+++ Source/Core/Structures/ERS_STRUCT_Viewport/ERS_STRUCT_Viewport.h
@@ -31,7 +31,7 @@ struct ERS_STRUCT_Viewport {
     int ShaderIndex = 0; /**<Index of the shader to be used*/
 
     std::shared_ptr<bool> Enabled; /**<Indicates if the viewport is enabled*/
-    std::shared_ptr<InputProcessor> Processor; /**<Pointer to Input Processor*/
+    std::shared_ptr<ERS_CLASS_InputProcessor> Processor; /**<Pointer to Input Processor*/
     std::shared_ptr<ERS_STRUCT_Camera> Camera; /**<Pointer To Camera Instance*/
 
     unsigned int FramebufferObject; /**<FBO OpenGL ID*/
