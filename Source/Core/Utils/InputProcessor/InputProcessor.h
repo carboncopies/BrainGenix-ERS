4a9b33442143e2120ea31380e64c181c205037bb
datacrystals <tliao@carboncopies.org>
2022-02-09 23:40:43 +0000

    Refactor Framebuffer Code

--- Source/Core/Utils/InputProcessor/InputProcessor.h
+++ Source/Core/Utils/InputProcessor/InputProcessor.h
@@ -14,7 +14,7 @@
 // Internal Libraries (BG convention: use <> instead of "")
 #include <ERS_STRUCT_Camera.h>
 #include <ERS_CLASS_LoggingSystem.h>
-#include <Framebuffer.h>
+#include <ERS_CLASS_Framebuffer.h>
 
 
 /**
