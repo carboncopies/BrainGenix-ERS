6a2a7a8abacc19d734a1cfc14669dbddc117ad2a
datacrystals <tliao@carboncopies.org>
2022-02-13 18:36:04 +0000

    Remove Self-Explanitory Comments (#36)

--- Source/Core/Editor/MenuEntries/GUI_Menu_View/GUI_Menu_View.cpp
+++ Source/Core/Editor/MenuEntries/GUI_Menu_View/GUI_Menu_View.cpp
@@ -5,33 +5,25 @@
 #include <GUI_Menu_View.h>
 
 
-// Constructor
 GUI_Menu_View::GUI_Menu_View(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils, std::shared_ptr<ERS_CLASS_ThemeManager> ThemeManager, std::shared_ptr<ERS_CLASS_FontManager> FontManager) {
 
-    // Copy In Pointer Struct
     SystemUtils_ = SystemUtils;
     ThemeManager_ = ThemeManager;
     FontManager_ = FontManager;
-
-    // Log Initialization
     SystemUtils_->Logger_->Log("Editor Setting Up View Menu", 4);
 
-    // Create Class Instances
     Window_ThemeSelector_ = std::make_unique<Window_ThemeSelector>(ThemeManager_); 
     Window_About_ = std::make_unique<Window_About>(SystemUtils_);
 
 
 }
 
-// Destructor
 GUI_Menu_View::~GUI_Menu_View() {
 
-    // Log Destructor
     SystemUtils_->Logger_->Log("Editor Destroying View Menu", 4);
 
 }
 
-// Draw Function
 void GUI_Menu_View::Draw() {
 
     // View Menu
