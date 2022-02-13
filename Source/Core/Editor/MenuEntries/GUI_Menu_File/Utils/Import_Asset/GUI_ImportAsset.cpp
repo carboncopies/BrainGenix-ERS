4cccf583aad98774ca95dd9247f69d9eccbc4965
datacrystals <tliao@carboncopies.org>
2022-02-13 18:33:20 +0000

    Remove Self-Explanitory Comments (#36)

--- Source/Core/Editor/MenuEntries/GUI_Menu_File/Utils/Import_Asset/GUI_ImportAsset.cpp
+++ Source/Core/Editor/MenuEntries/GUI_Menu_File/Utils/Import_Asset/GUI_ImportAsset.cpp
@@ -5,32 +5,24 @@
 #include <GUI_ImportAsset.h>
 
 
-// Asset Importer Constructor
 GUI_ImportAsset::GUI_ImportAsset(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils) {
 
-    // Copy Shared Pointer
     SystemUtils_ = SystemUtils;
-
-    // Log Init
     SystemUtils_->Logger_->Log("Initializing Asset Importer GUI", 5);
 
-    // Instantiate
     Window_ImportProgressBar_ = std::make_unique<Window_ImportProgressBar>(SystemUtils_);
     AssetImportBackend_ = std::make_unique<ERS_CLASS_ImportAsset>(SystemUtils_);
 
 }
 
 
-// Asset Importer Destructor
 GUI_ImportAsset::~GUI_ImportAsset() {
 
-    // Log Destructor
     SystemUtils_->Logger_->Log("Asset Importer GUI Destructor Called", 6);
 
 }
 
 
-// Draw
 void GUI_ImportAsset::Draw() {
 
     // Draw File Dialog
@@ -50,7 +42,7 @@ void GUI_ImportAsset::Draw() {
             Window_ImportProgressBar_->Enabled_ = true;
 
         }
-    // close
+
     ImGuiFileDialog::Instance()->Close();
     }
 
@@ -66,11 +58,8 @@ void GUI_ImportAsset::Draw() {
 
 }
 
-// Open File Dialog
 void GUI_ImportAsset::OpenFileDialog() {
 
-    // Open Dialog
     ImGuiFileDialog::Instance()->OpenDialog("ImportModel", "Import", ".*", ".", "", 0);
-
 
 }
