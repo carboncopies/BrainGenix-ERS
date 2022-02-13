77a49ee0666f4d02bbb3a83504086127c050fb05
datacrystals <tliao@carboncopies.org>
2022-02-13 18:38:20 +0000

    Fix Header File Indentation (#37)

--- Source/Core/Editor/Utils/ERS_Editor_3DCursor/ERS_Editor_3DCursor.h
+++ Source/Core/Editor/Utils/ERS_Editor_3DCursor/ERS_Editor_3DCursor.h
@@ -43,99 +43,92 @@ enum class GizmoMode
  */
 class Cursors3D {
 
-    private:
+private:
+
+    float Matrix_[4][16] = { /**<Cube For Viewport Angle Identifier*/
+        { 1.f, 0.f, 0.f, 0.f,
+        0.f, 1.f, 0.f, 0.f,
+        0.f, 0.f, 1.f, 0.f,
+        0.f, 0.f, 0.f, 1.f },
+
+        { 1.f, 0.f, 0.f, 0.f,
+        0.f, 1.f, 0.f, 0.f,
+        0.f, 0.f, 1.f, 0.f,
+        2.f, 0.f, 0.f, 1.f },
+
+        { 1.f, 0.f, 0.f, 0.f,
+        0.f, 1.f, 0.f, 0.f,
+        0.f, 0.f, 1.f, 0.f,
+        2.f, 0.f, 2.f, 1.f },
+
+        { 1.f, 0.f, 0.f, 0.f,
+        0.f, 1.f, 0.f, 0.f,
+        0.f, 0.f, 1.f, 0.f,
+        0.f, 0.f, 2.f, 1.f }
+    };
 
 
-        float Matrix_[4][16] = { /**<Cube For Viewport Angle Identifier*/
-            { 1.f, 0.f, 0.f, 0.f,
-            0.f, 1.f, 0.f, 0.f,
-            0.f, 0.f, 1.f, 0.f,
-            0.f, 0.f, 0.f, 1.f },
+    bool IsCursorActive_; /**<Indicates If Cursor Active Or Disabled*/
+    float* CameraView_; /**<Camera View Matrix, Converted From GLM::Mat4*/
+    float* CameraProjection_; /**<Camera Projection Matrix, Converted From GLM::Mat4*/
 
-            { 1.f, 0.f, 0.f, 0.f,
-            0.f, 1.f, 0.f, 0.f,
-            0.f, 0.f, 1.f, 0.f,
-            2.f, 0.f, 0.f, 1.f },
+    std::shared_ptr<ERS_STRUCT_Camera> Camera_; /**<Camera Object Pointer*/
+    ImGuizmo::OPERATION CurrentGizmoOperation_ = ImGuizmo::TRANSLATE; /**<Set Current Cursor Operation*/
 
-            { 1.f, 0.f, 0.f, 0.f,
-            0.f, 1.f, 0.f, 0.f,
-            0.f, 0.f, 1.f, 0.f,
-            2.f, 0.f, 2.f, 1.f },
+    bool HasObjectChanged_ = false; /*<Indicate If Update Needed*/
 
-            { 1.f, 0.f, 0.f, 0.f,
-            0.f, 1.f, 0.f, 0.f,
-            0.f, 0.f, 1.f, 0.f,
-            0.f, 0.f, 2.f, 1.f }
-        };
+public:
 
+    glm::vec3 Pos_;
+    glm::vec3 Rot_;
+    glm::vec3 Scale_;
 
+public:
 
-        bool IsCursorActive_; /**<Indicates If Cursor Active Or Disabled*/
-        float* CameraView_; /**<Camera View Matrix, Converted From GLM::Mat4*/
-        float* CameraProjection_; /**<Camera Projection Matrix, Converted From GLM::Mat4*/
+    /**
+     * @brief Construct a new Cursors 3 D object
+     * 
+     */
+    Cursors3D();
 
-        std::shared_ptr<ERS_STRUCT_Camera> Camera_; /**<Camera Object Pointer*/
-        ImGuizmo::OPERATION CurrentGizmoOperation_ = ImGuizmo::TRANSLATE; /**<Set Current Cursor Operation*/
+    /**
+     * @brief Destroy the Cursors 3 D object
+     * 
+     */
+    ~Cursors3D();
 
-        bool HasObjectChanged_ = false; /*<Indicate If Update Needed*/
+    /**
+     * @brief Returns if the gizmo is being used
+     * 
+     * @return true 
+     * @return false 
+     */
+    bool IsUsing();
 
-    public:
+    /**
+     * @brief Returns if the gizmo is being hovered by the mouse
+     * 
+     * @return true 
+     * @return false 
+     */
+    bool IsHovered();
 
-        glm::vec3 Pos_;
-        glm::vec3 Rot_;
-        glm::vec3 Scale_;
+    /**
+     * @brief Draw the gizmo
+     * 
+     * @param Camera 
+     * @param IsCameraMoving 
+     */
+    void Draw(std::shared_ptr<ERS_STRUCT_Camera> Camera, bool IsCameraMoving, bool ShowCube, bool ShowCursor);
 
-    public:
-
-        /**
-         * @brief Construct a new Cursors 3 D object
-         * 
-         */
-        Cursors3D();
-
-        /**
-         * @brief Destroy the Cursors 3 D object
-         * 
-         */
-        ~Cursors3D();
-
-
-        /**
-         * @brief Returns if the gizmo is being used
-         * 
-         * @return true 
-         * @return false 
-         */
-        bool IsUsing();
-
-        /**
-         * @brief Returns if the gizmo is being hovered by the mouse
-         * 
-         * @return true 
-         * @return false 
-         */
-        bool IsHovered();
-
-
-        /**
-         * @brief Draw the gizmo
-         * 
-         * @param Camera 
-         * @param IsCameraMoving 
-         */
-        void Draw(std::shared_ptr<ERS_STRUCT_Camera> Camera, bool IsCameraMoving, bool ShowCube, bool ShowCursor);
-
-
-
-
-        /**
-         * @brief Set the Loc Rot Scale object, accepts three glm::vec3 arrays indicating position, rotation and scale (in that order).
-         * 
-         * @param Pos 
-         * @param Rot 
-         * @param Scale 
-         */
-        void SetLocRotScale(glm::vec3 Pos, glm::vec3 Rot, glm::vec3 Scale);
+    /**
+     * @brief Set the Loc Rot Scale object, accepts three glm::vec3 arrays indicating position, rotation and scale (in that order).
+     * 
+     * @param Pos 
+     * @param Rot 
+     * @param Scale 
+     */
+    void SetLocRotScale(glm::vec3 Pos, glm::vec3 Rot, glm::vec3 Scale);
 
 
 };
