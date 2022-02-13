c54dba2fe8de1eaa6ea8410aa104e6c39da334fc
datacrystals <tliao@carboncopies.org>
2022-02-13 18:46:24 +0000

    Remove Self-Explanitory Comments (#36)

--- Source/Core/Editor/Utils/ERS_Editor_UserProfileManager/ERS_Editor_UserProfileManager.cpp
+++ Source/Core/Editor/Utils/ERS_Editor_UserProfileManager/ERS_Editor_UserProfileManager.cpp
@@ -5,32 +5,24 @@
 #include <ERS_Editor_UserProfileManager.h>
 
 
-// Constructor
 ERS_CLASS_UserProfileManager::ERS_CLASS_UserProfileManager(std::shared_ptr<ERS_CLASS_LoggingSystem> Logger, const char* UserProfilePath) {
 
-    // Copy Pointer
     Logger_ = Logger;
     UserProfilePath_ = std::string(UserProfilePath);
-
-    // Log Init
     Logger_->Log("Initializing User Profile Manager", 5);
     LoadUserProfile();
 
 }
 
 
-// Destructor
 ERS_CLASS_UserProfileManager::~ERS_CLASS_UserProfileManager() {
 
-    // Log Destructor Call
     Logger_->Log("User Profile Destructo Called", 6);
 
 }
 
-// Load User Profile
 void ERS_CLASS_UserProfileManager::LoadUserProfile() {
 
-    // Log Loading
     Logger_->Log(std::string(std::string("Loading User Profile At Path: ") + UserProfilePath_).c_str(), 4);
 
     // Load File
@@ -43,7 +35,6 @@ void ERS_CLASS_UserProfileManager::LoadUserProfile() {
 
 }
 
-// Return Object Functions
 std::string ERS_CLASS_UserProfileManager::GetUserFont() {
     return UserFont_;
 }
