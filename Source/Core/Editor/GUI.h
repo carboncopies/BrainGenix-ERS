7ac82179a068b0eff0f560edd22b1e90fb702a40
datacrystals <tliao@carboncopies.org>
2022-02-13 19:49:56 +0000

    Fix Header File Indentation/Fix Comments (#36, #37)
    Co-authored-by: novembersvisions <novembersvisions@users.noreply.github.com>
    Co-authored-by: rwhitney1 <rwhitney1@users.noreply.github.com>

--- Source/Core/Editor/GUI.h
+++ Source/Core/Editor/GUI.h
@@ -53,67 +53,67 @@
 class GUISystem {
 
 
-    private:
+private:
 
-        GLFWwindow* Window_; /**<Pointer to window instance*/
-        std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils_; /**<Structure Containing Pointers To System Utilities*/
-        std::shared_ptr<ERS_STRUCT_ProjectUtils> ProjectUtils_; /**ProjectUtils Struct Pointer*/
+    GLFWwindow* Window_; /**<Pointer to window instance*/
+    std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils_; /**<Structure Containing Pointers To System Utilities*/
+    std::shared_ptr<ERS_STRUCT_ProjectUtils> ProjectUtils_; /**ProjectUtils Struct Pointer*/
 
-        std::shared_ptr<ERS_CLASS_FontManager> FontManager_; /**<Pointer To FontManager Instance*/
-        std::shared_ptr<ERS_CLASS_UserProfileManager> UserProfileManager_; /**<Pointer To User Profile Manager Instance*/
-        std::shared_ptr<Cursors3D> Cursors3D_; /**<Pointer to Cursors3D Instance*/
-        std::shared_ptr<ERS_CLASS_SceneManager> SceneManager_; /**<Scene Manager Instance Pointer*/
-        std::shared_ptr<ERS_CLASS_VisualRenderer> VisualRenderer_; /**<Pointer to visual renderer for viewport modification*/
-        std::shared_ptr<ERS_STRUCT_HumanInputDeviceUtils> HIDUtils_; /**<Pointer To Human Device Utils Struct*/
+    std::shared_ptr<ERS_CLASS_FontManager> FontManager_; /**<Pointer To FontManager Instance*/
+    std::shared_ptr<ERS_CLASS_UserProfileManager> UserProfileManager_; /**<Pointer To User Profile Manager Instance*/
+    std::shared_ptr<Cursors3D> Cursors3D_; /**<Pointer to Cursors3D Instance*/
+    std::shared_ptr<ERS_CLASS_SceneManager> SceneManager_; /**<Scene Manager Instance Pointer*/
+    std::shared_ptr<ERS_CLASS_VisualRenderer> VisualRenderer_; /**<Pointer to visual renderer for viewport modification*/
+    std::shared_ptr<ERS_STRUCT_HumanInputDeviceUtils> HIDUtils_; /**<Pointer To Human Device Utils Struct*/
 
-    public:
+public:
 
-        std::shared_ptr<ERS_CLASS_ThemeManager> ThemeManager_; /**<Pointer To ThemeManager Instance*/
+    std::shared_ptr<ERS_CLASS_ThemeManager> ThemeManager_; /**<Pointer To ThemeManager Instance*/
 
-        // Menu Items
-        std::unique_ptr<GUI_Menu_File> Menu_File_; /**<Editor Menu Entry*/
-        std::unique_ptr<GUI_Menu_View> Menu_View_; /**<Editor Menu Entry*/
-        std::unique_ptr<GUI_Menu_Window> Menu_Window_; /**<Editor Menu Entry*/
-        std::unique_ptr<GUI_Menu_Debug> Menu_Debug_; /**<Editor Menu Entry*/
-        std::unique_ptr<GUI_Menu_Settings> Menu_Settings_; /**<Editor Menu Entry*/
-
-
-        /**
-         * @brief Construct a new Gui System object
-         * 
-         * @param Logger 
-         */
-        GUISystem(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils,
-        GLFWwindow* Window,
-        std::shared_ptr<Cursors3D> Cursors3D,
-        std::shared_ptr<ERS_CLASS_SceneManager> SceneManager,
-        std::shared_ptr<ERS_STRUCT_ProjectUtils> ProjectUtils,
-        std::shared_ptr<ERS_CLASS_VisualRenderer> VisualRendererInstance,
-        std::shared_ptr<ERS_STRUCT_HumanInputDeviceUtils> HIDUtils
-        );
-
-        /**
-         * @brief Destroy the Gui System object
-         * 
-         */
-        ~GUISystem();
+    // Menu Items
+    std::unique_ptr<GUI_Menu_File> Menu_File_; /**<Editor Menu Entry*/
+    std::unique_ptr<GUI_Menu_View> Menu_View_; /**<Editor Menu Entry*/
+    std::unique_ptr<GUI_Menu_Window> Menu_Window_; /**<Editor Menu Entry*/
+    std::unique_ptr<GUI_Menu_Debug> Menu_Debug_; /**<Editor Menu Entry*/
+    std::unique_ptr<GUI_Menu_Settings> Menu_Settings_; /**<Editor Menu Entry*/
 
 
-        /**
-         * @brief Generate Frame REMEMBER TO CALL UPDATE FRAME AFTER THE CLEAR CALL.
-         * 
-         */
-        void UpdateGUI();
+    /**
+     * @brief Construct a new Gui System object
+     * 
+     * @param Logger 
+     */
+    GUISystem(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils,
+    GLFWwindow* Window,
+    std::shared_ptr<Cursors3D> Cursors3D,
+    std::shared_ptr<ERS_CLASS_SceneManager> SceneManager,
+    std::shared_ptr<ERS_STRUCT_ProjectUtils> ProjectUtils,
+    std::shared_ptr<ERS_CLASS_VisualRenderer> VisualRendererInstance,
+    std::shared_ptr<ERS_STRUCT_HumanInputDeviceUtils> HIDUtils
+    );
 
-        /**
-         * @brief Update Frame, make sure you've called updategui.
-         * 
-         */
-        void UpdateFrame();
-        
-        /**
-         * @brief Use this for functions/widgets/windows that need to be updated after the main rendering takes place.
-         * 
-         */
-        void DeferredFrameUpdate();
+    /**
+     * @brief Destroy the Gui System object
+     * 
+     */
+    ~GUISystem();
+
+
+    /**
+     * @brief Generate Frame REMEMBER TO CALL UPDATE FRAME AFTER THE CLEAR CALL.
+     * 
+     */
+    void UpdateGUI();
+
+    /**
+     * @brief Update Frame, make sure you've called updategui.
+     * 
+     */
+    void UpdateFrame();
+    
+    /**
+     * @brief Use this for functions/widgets/windows that need to be updated after the main rendering takes place.
+     * 
+     */
+    void DeferredFrameUpdate();
 };
