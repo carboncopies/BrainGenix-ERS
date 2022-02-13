93d92452c6e204297eab501e993cca8484be2efd
datacrystals <tliao@carboncopies.org>
2022-02-13 18:41:24 +0000

    Fix Header File Indentation (#37)

--- Source/Core/Editor/Utils/ERS_Editor_FontManager/ERS_Editor_FontManager.h
+++ Source/Core/Editor/Utils/ERS_Editor_FontManager/ERS_Editor_FontManager.h
@@ -28,30 +28,30 @@
  */
 class ERS_CLASS_FontManager {
 
-    private:
+private:
 
-        std::shared_ptr<ERS_CLASS_LoggingSystem> Logger_; /**<ERS_CLASS_LoggingSystem Instance Pointer*/
-        const char* FontsDirectoryPath_; /**<Path where fonts are to be searched for */
+    std::shared_ptr<ERS_CLASS_LoggingSystem> Logger_; /**<ERS_CLASS_LoggingSystem Instance Pointer*/
+    const char* FontsDirectoryPath_; /**<Path where fonts are to be searched for */
 
-        std::vector<std::string> FontPathList_; /**<List of fonts located at the given paths*/
-        std::vector<std::string> FontNameList_; /**<List of font names for gui rendering*/
-        
-        float FontSize_ = 14; /**<Set Default Font Size In Pixels*/
-        int FontSelector_ = 0; /**<Set Font Selector Int*/
-        bool UpdateFont_ = false; /**<Indicate If Font Needs To Be Updated*/
+    std::vector<std::string> FontPathList_; /**<List of fonts located at the given paths*/
+    std::vector<std::string> FontNameList_; /**<List of font names for gui rendering*/
+    
+    float FontSize_ = 14; /**<Set Default Font Size In Pixels*/
+    int FontSelector_ = 0; /**<Set Font Selector Int*/
+    bool UpdateFont_ = false; /**<Indicate If Font Needs To Be Updated*/
 
-        bool EndsWith(const std::string& Input, const std::string& Ending);
+    bool EndsWith(const std::string& Input, const std::string& Ending);
 
-    public:
+public:
 
-        ERS_CLASS_FontManager(std::shared_ptr<ERS_CLASS_LoggingSystem> Logger, const char* FontsDirPath = "EditorAssets/Fonts");
-        ~ERS_CLASS_FontManager();
+    ERS_CLASS_FontManager(std::shared_ptr<ERS_CLASS_LoggingSystem> Logger, const char* FontsDirPath = "EditorAssets/Fonts");
+    ~ERS_CLASS_FontManager();
 
-        void IndexFonts();
-        void UseFont(int FontIndex);
-        void UseFont(std::string FontName);
-        void SetFontSize(float FontSize);
-        void FontSelectorWindow(bool *WindowEnabled);
-        void CheckUpdateFont();
+    void IndexFonts();
+    void UseFont(int FontIndex);
+    void UseFont(std::string FontName);
+    void SetFontSize(float FontSize);
+    void FontSelectorWindow(bool *WindowEnabled);
+    void CheckUpdateFont();
 
 };
