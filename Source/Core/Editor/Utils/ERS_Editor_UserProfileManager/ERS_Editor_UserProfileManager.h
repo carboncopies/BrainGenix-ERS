ce3b0d3bba1b335a76ba2b3069330ba8553cbf6c
datacrystals <tliao@carboncopies.org>
2022-02-13 18:51:11 +0000

    Fix Header File Indentation (#37)

--- Source/Core/Editor/Utils/ERS_Editor_UserProfileManager/ERS_Editor_UserProfileManager.h
+++ Source/Core/Editor/Utils/ERS_Editor_UserProfileManager/ERS_Editor_UserProfileManager.h
@@ -22,36 +22,52 @@
 class ERS_CLASS_UserProfileManager {
 
 
-    private:
+private:
 
-        std::shared_ptr<ERS_CLASS_LoggingSystem> Logger_; /**<ERS_CLASS_LoggingSystem Instance Pointer*/
-        std::string UserProfilePath_; /**<Path To User Profile*/
-        YAML::Node UserProfile_; /**<YAML::Node Containing User Profile*/
+    std::shared_ptr<ERS_CLASS_LoggingSystem> Logger_; /**<ERS_CLASS_LoggingSystem Instance Pointer*/
+    std::string UserProfilePath_; /**<Path To User Profile*/
+    YAML::Node UserProfile_; /**<YAML::Node Containing User Profile*/
 
-        void LoadUserProfile(); /**<Load User Profile*/
+    void LoadUserProfile(); /**<Load User Profile*/
 
-        std::string UserFont_; /**<Name Of user fornt*/
-        int UserFontSize_; /**<Size of user font*/
-        std::string UserColorProfileName_; /**<User Color Profile Name*/
+    std::string UserFont_; /**<Name Of user fornt*/
+    int UserFontSize_; /**<Size of user font*/
+    std::string UserColorProfileName_; /**<User Color Profile Name*/
 
-    public:
+public:
 
-        /**
-         * @brief Construct a new User Profile Manager object
-         * 
-         * @param Logger 
-         */
-        ERS_CLASS_UserProfileManager(std::shared_ptr<ERS_CLASS_LoggingSystem> Logger, const char* UserProfilePath = "EditorAssets/Configuration/User/UserConfig.yaml");
+    /**
+     * @brief Construct a new User Profile Manager object
+     * 
+     * @param Logger 
+     */
+    ERS_CLASS_UserProfileManager(std::shared_ptr<ERS_CLASS_LoggingSystem> Logger, const char* UserProfilePath = "EditorAssets/Configuration/User/UserConfig.yaml");
 
-        /**
-         * @brief Destroy the User Profile Manager object
-         * 
-         */
-        ~ERS_CLASS_UserProfileManager();
+    /**
+     * @brief Destroy the User Profile Manager object
+     * 
+     */
+    ~ERS_CLASS_UserProfileManager();
 
+    /**
+     * @brief Get the User Font object
+     * 
+     * @return std::string 
+     */
+    std::string GetUserFont();
 
-        std::string GetUserFont();
-        int GetUserFontSize();
-        std::string GetUserColorProfile();
+    /**
+     * @brief Get the User Font Size object
+     * 
+     * @return int 
+     */
+    int GetUserFontSize();
+
+    /**
+     * @brief Get the User Color Profile object
+     * 
+     * @return std::string 
+     */
+    std::string GetUserColorProfile();
 
 };
