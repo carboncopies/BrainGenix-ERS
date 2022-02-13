84ef15868687db475768b0085da521efe344aadd
datacrystals <tliao@carboncopies.org>
2022-02-13 18:34:59 +0000

    Remove Self-Explanitory Comments (#36)

--- Source/Core/Editor/MenuEntries/GUI_Menu_Settings/GUI_Menu_Settings.cpp
+++ Source/Core/Editor/MenuEntries/GUI_Menu_Settings/GUI_Menu_Settings.cpp
@@ -5,31 +5,23 @@
 #include <GUI_Menu_Settings.h>
 
 
-// Constructor
 GUI_Menu_Settings::GUI_Menu_Settings(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils, std::shared_ptr<ERS_STRUCT_HumanInputDeviceUtils> HIDUtils, std::shared_ptr<ERS_STRUCT_ProjectUtils> ProjectUtils) {
 
-    // Copy In Pointer Struct
     SystemUtils_ = SystemUtils;
     HIDUtils_ = HIDUtils;
     ProjectUtils_ = ProjectUtils;
-
-    // Log Initialization
     SystemUtils_->Logger_->Log("Editor Setting Up Settings Menu", 4);
 
-    // Instantiate Window Classes
     Window_ControllerSettings_ = std::make_unique<Window_ControllerSettings>(SystemUtils_, HIDUtils_, ProjectUtils_);
 
 }
 
-// Destructor
 GUI_Menu_Settings::~GUI_Menu_Settings() {
 
-    // Log Destructor
     SystemUtils_->Logger_->Log("Editor Destroying Settings Menu", 4);
 
 }
 
-// Draw Function
 void GUI_Menu_Settings::Draw() {
 
     // File Menu
