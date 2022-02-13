016ed959eb8a651ee263f4c53b3ae7a68b078ad7
datacrystals <tliao@carboncopies.org>
2022-02-13 19:42:27 +0000

    Co-authored-by: novembersvisions <novembersvisions@users.noreply.github.com> Co-authored-by: rwhitney1 <rwhitney1@users.noreply.github.com>

--- Source/Core/Editor/Windows/GUI_Window_SystemLog/GUI_Window_SystemLog.cpp
+++ Source/Core/Editor/Windows/GUI_Window_SystemLog/GUI_Window_SystemLog.cpp
@@ -5,35 +5,26 @@
 #include <GUI_Window_SystemLog.h>
 
 
-// Constructor
 Window_SystemLog::Window_SystemLog(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUtils) {
 
-    // Copy System Utils Pointer Struct
     SystemUtils_ = SystemUtils;
-
-    // Log Init
     SystemUtils_->Logger_->Log("Initializing GUI_Window_SystemLog", 5);
 
 }
 
-// Destructor
 Window_SystemLog::~Window_SystemLog() {
 
-    // Log Destructor Call
     SystemUtils_->Logger_->Log("GUI_Window_SystemLog Destructor Called", 6);
-
 
 }
 
 
-// Draw Window
 void Window_SystemLog::Draw() {
 
     if (Enabled_) {
     bool Visible = ImGui::Begin("System Log", &Enabled_);
 
-        // Set Default Window Size
-        ImGui::SetWindowSize(ImVec2(300,0), ImGuiCond_FirstUseEver);
+ImGui::SetWindowSize(ImVec2(300,0), ImGuiCond_FirstUseEver);
 
 
         if (Visible) {
