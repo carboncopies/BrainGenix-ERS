a593086b30ae8f8fb1487ff6fee2a6cf54cb07d6
datacrystals <tliao@carboncopies.org>
2022-02-13 19:58:16 +0000

    Fix Header File Indentation/Fix Comments (#36, #37)
    Co-authored-by: novembersvisions <novembersvisions@users.noreply.github.com>
    Co-authored-by: rwhitney1 <rwhitney1@users.noreply.github.com>

--- Source/Core/Renderer/ERS_CLASS_InputProcessor/ERS_CLASS_InputProcessor.h
+++ Source/Core/Renderer/ERS_CLASS_InputProcessor/ERS_CLASS_InputProcessor.h
@@ -23,91 +23,88 @@
  */
 class ERS_CLASS_InputProcessor {
 
-    private:
+private:
 
-        // Internal Vars
-        bool FirstMouse = true; /**<Checks if the mouse is just being enabled.*/
-        float LastX = 400; /**<Location Of Last Mouse Position. (X coord) */
-        float LastY = 400; /**<Location Of Last Mouse Position. (Y Coord) */ 
-        std::shared_ptr<ERS_STRUCT_Camera> Camera_; /**<Pointer To Camera Instance */
-        //FramebufferManager *FramebufferManager_; /**<Pointer To Framebuffer Manager Instance For Resizing The Framebuffer*/
+    // Internal Vars
+    bool FirstMouse = true; /**<Checks if the mouse is just being enabled.*/
+    float LastX = 400; /**<Location Of Last Mouse Position. (X coord) */
+    float LastY = 400; /**<Location Of Last Mouse Position. (Y Coord) */ 
+    std::shared_ptr<ERS_STRUCT_Camera> Camera_; /**<Pointer To Camera Instance */
+    //FramebufferManager *FramebufferManager_; /**<Pointer To Framebuffer Manager Instance For Resizing The Framebuffer*/
 
-        GLFWwindow *Window_; /**<Pointer To Window Surface */
-
-
-        /**
-         * @brief Callback for the framebuffer size (window resize events)
-         * 
-         * @param Width 
-         * @param Height 
-         */
-        void FramebufferSizeCallback(int Width, int Height);
-
-        /**
-         * @brief Callback for mouse position (X,Y)
-         * 
-         * @param XPos 
-         * @param YPos 
-         */
-        void MouseCallback(double XPos, double YPos);
-
-        // FIXME: NO FUNCTION EXISTS TO GET SCROLL INPUT WITHOUT CALLBACK
-        /**
-         * @brief Callback for Mouse Scroll (!!!DOESN'T WORK RIGHT NOW, NEED TO BE FIXED!!!)
-         * 
-         * @param YOffset 
-         */
-        void ScrollCallback(double YOffset);
-
-    public:
-
-        /**
-         * @brief Construct a new Input Processor object
-         * 
-         * @param Camera 
-         */
-        ERS_CLASS_InputProcessor(std::shared_ptr<ERS_STRUCT_Camera> Camera, GLFWwindow* Window);
-
-        /**
-         * @brief Destroy the Input Processor object
-         * 
-         */
-        ~ERS_CLASS_InputProcessor();
+    GLFWwindow *Window_; /**<Pointer To Window Surface */
 
 
+    /**
+     * @brief Callback for the framebuffer size (window resize events)
+     * 
+     * @param Width 
+     * @param Height 
+     */
+    void FramebufferSizeCallback(int Width, int Height);
 
-        /**
-         * @brief Update the framebuffer size.
-         * 
-         * @param Window 
-         */
-        void UpdateFramebuffer();
+    /**
+     * @brief Callback for mouse position (X,Y)
+     * 
+     * @param XPos 
+     * @param YPos 
+     */
+    void MouseCallback(double XPos, double YPos);
 
-        /**
-         * @brief Update the mouse info.
-         * 
-         * @param Window 
-         * @param WindowMouseCaptureEnabled 
-         */
-        void UpdateMouse(bool WindowMouseCaptureEnabled);
+    // FIXME: NO FUNCTION EXISTS TO GET SCROLL INPUT WITHOUT CALLBACK
+    /**
+     * @brief Callback for Mouse Scroll (!!!DOESN'T WORK RIGHT NOW, NEED TO BE FIXED!!!)
+     * 
+     * @param YOffset 
+     */
+    void ScrollCallback(double YOffset);
 
-        /**
-         * @brief Processes mouse scroll wheel input.
-         * 
-         * @param CaptureEnabled 
-         */
-        void ProcessMouseScroll(bool CaptureEnabled);
+public:
 
+    /**
+     * @brief Construct a new Input Processor object
+     * 
+     * @param Camera 
+     */
+    ERS_CLASS_InputProcessor(std::shared_ptr<ERS_STRUCT_Camera> Camera, GLFWwindow* Window);
 
-        /**
-         * @brief Process keyboard input
-         * 
-         * @param Logger_ 
-         * @param DeltaTime 
-         */
-        void ProcessKeyboardInput(std::shared_ptr<ERS_CLASS_LoggingSystem> Logger_, float DeltaTime, bool WindowCaptureEnabled);
+    /**
+     * @brief Destroy the Input Processor object
+     * 
+     */
+    ~ERS_CLASS_InputProcessor();
 
 
 
+    /**
+     * @brief Update the framebuffer size.
+     * 
+     * @param Window 
+     */
+    void UpdateFramebuffer();
+
+    /**
+     * @brief Update the mouse info.
+     * 
+     * @param Window 
+     * @param WindowMouseCaptureEnabled 
+     */
+    void UpdateMouse(bool WindowMouseCaptureEnabled);
+
+    /**
+     * @brief Processes mouse scroll wheel input.
+     * 
+     * @param CaptureEnabled 
+     */
+    void ProcessMouseScroll(bool CaptureEnabled);
+
+
+    /**
+     * @brief Process keyboard input
+     * 
+     * @param Logger_ 
+     * @param DeltaTime 
+     */
+    void ProcessKeyboardInput(std::shared_ptr<ERS_CLASS_LoggingSystem> Logger_, float DeltaTime, bool WindowCaptureEnabled);
 
 };
