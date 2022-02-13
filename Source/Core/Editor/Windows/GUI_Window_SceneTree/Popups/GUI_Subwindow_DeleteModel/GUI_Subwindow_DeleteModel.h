6c16bbb845ebe4af4c7d479d749624648ed9e9a1
datacrystals <tliao@carboncopies.org>
2022-02-13 19:33:18 +0000

    Fix Header File Indentation/Fix Comments (#36, #37)
    Co-authored-by: novembersvisions <novembersvisions@users.noreply.github.com>

--- Source/Core/Editor/Windows/GUI_Window_SceneTree/Popups/GUI_Subwindow_DeleteModel/GUI_Subwindow_DeleteModel.h
+++ Source/Core/Editor/Windows/GUI_Window_SceneTree/Popups/GUI_Subwindow_DeleteModel/GUI_Subwindow_DeleteModel.h
@@ -22,49 +22,47 @@
  */
 class Subwindow_DeleteModel {
 
-    // Private Vars
-    private:
+private:
 
-        std::shared_ptr<ERS_CLASS_SceneManager> SceneManager_; /**<SceneManager Instance*/
-        int SceneIndex_ = 0; /**<Scene Index Used To Remember What Scene To Delete From*/
-        int ModelIndex_ = 0; /**<Use to identify what model to delete*/
+    std::shared_ptr<ERS_CLASS_SceneManager> SceneManager_; /**<SceneManager Instance*/
+    int SceneIndex_ = 0; /**<Scene Index Used To Remember What Scene To Delete From*/
+    int ModelIndex_ = 0; /**<Use to identify what model to delete*/
 
-        bool FirstFrame_ = false; /**<Used to grab keyboard input on first frame window is open*/
+    bool FirstFrame_ = false; /**<Used to grab keyboard input on first frame window is open*/
 
-        bool ShowDeleteConfirm_ = false; /**<Confirm Deletion Of Scene*/
+    bool ShowDeleteConfirm_ = false; /**<Confirm Deletion Of Scene*/
 
-    // Private Functions
-    private:
+private:
 
-        void UpdateConfirmDeletePopup(); /**<Function To Update Popup Deletion Window*/
+    void UpdateConfirmDeletePopup(); /**<Function To Update Popup Deletion Window*/
 
-    public:
+public:
 
-        /**
-         * @brief Construct a new Subwindow_DeleteModel object
-         * 
-         */
-        Subwindow_DeleteModel(std::shared_ptr<ERS_CLASS_SceneManager> SceneManager);
+    /**
+     * @brief Construct a new Subwindow_DeleteModel object
+     * 
+     */
+    Subwindow_DeleteModel(std::shared_ptr<ERS_CLASS_SceneManager> SceneManager);
 
-        /**
-         * @brief Destroy the Subwindow_DeleteModel object
-         * 
-         */
-        ~Subwindow_DeleteModel();
+    /**
+     * @brief Destroy the Subwindow_DeleteModel object
+     * 
+     */
+    ~Subwindow_DeleteModel();
 
-        /**
-         * @brief Called by the GUI to delete a scene.
-         * 
-         * @param SceneIndex 
-         * @param ModelIndex 
-         */
-        void DeleteModel(int SceneIndex, int ModelIndex);
+    /**
+     * @brief Called by the GUI to delete a scene.
+     * 
+     * @param SceneIndex 
+     * @param ModelIndex 
+     */
+    void DeleteModel(int SceneIndex, int ModelIndex);
 
-        /**
-         * @brief Update all windows created by this class. Call every frame.
-         * 
-         */
-        void Draw();
+    /**
+     * @brief Update all windows created by this class. Call every frame.
+     * 
+     */
+    void Draw();
 
 };
 
