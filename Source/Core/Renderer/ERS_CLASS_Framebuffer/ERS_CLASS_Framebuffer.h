326da872e7aec0c7ab98f42e593a219f57af6023
datacrystals <tliao@carboncopies.org>
2022-02-13 19:57:32 +0000

    Fix Header File Indentation/Fix Comments (#36, #37)
    Co-authored-by: novembersvisions <novembersvisions@users.noreply.github.com>
    Co-authored-by: rwhitney1 <rwhitney1@users.noreply.github.com>

--- Source/Core/Renderer/ERS_CLASS_Framebuffer/ERS_CLASS_Framebuffer.h
+++ Source/Core/Renderer/ERS_CLASS_Framebuffer/ERS_CLASS_Framebuffer.h
@@ -30,59 +30,59 @@
 class ERS_CLASS_Framebuffer {
 
 
-    private:
+private:
 
-        std::shared_ptr<ERS_CLASS_LoggingSystem> Logger_; /**<Pointer to instance of Logger Class*/
-        unsigned int FramebufferObject_; /**<Framebuffer OpenGL Handle (Used to Render Inside IMGUI Window Or On Screen Directly)*/
-        unsigned int RenderTexture_; /**<OpenGL Renderes To This Texture, Then It's Displayed. (OpenGL Handle)*/
-        unsigned int RenderBufferObject_; /**<RenderBuffer Object Handle.*/
+    std::shared_ptr<ERS_CLASS_LoggingSystem> Logger_; /**<Pointer to instance of Logger Class*/
+    unsigned int FramebufferObject_; /**<Framebuffer OpenGL Handle (Used to Render Inside IMGUI Window Or On Screen Directly)*/
+    unsigned int RenderTexture_; /**<OpenGL Renderes To This Texture, Then It's Displayed. (OpenGL Handle)*/
+    unsigned int RenderBufferObject_; /**<RenderBuffer Object Handle.*/
 
-        std::shared_ptr<ERS_CLASS_ShaderLoader> ShaderLoader_; /**<Pointer To Shader Loader Instance*/
-        ERS_STRUCT_Shader ScreenShader_; /**<Screen Shader Instance, Used To Display Framebuffer To Screen*/
-
-
-        unsigned int ScreenQuadVAO_; /**<Screen Quad VAO Reference*/
-        unsigned int ScreenQuadVBO_; /**<Screen Quad VBO Reference*/
+    std::shared_ptr<ERS_CLASS_ShaderLoader> ShaderLoader_; /**<Pointer To Shader Loader Instance*/
+    ERS_STRUCT_Shader ScreenShader_; /**<Screen Shader Instance, Used To Display Framebuffer To Screen*/
 
 
-    public:
+    unsigned int ScreenQuadVAO_; /**<Screen Quad VAO Reference*/
+    unsigned int ScreenQuadVBO_; /**<Screen Quad VBO Reference*/
 
 
-        /**
-         * @brief Construct a new Framebuffer Manager object.
-         * 
-         * @param Logger ERS_CLASS_LoggingSystem Instance Pointer
-         * @param Width Width In Pixels
-         * @param Height Height In Pixels
-         */
-        ERS_CLASS_Framebuffer(std::shared_ptr<ERS_CLASS_LoggingSystem> Logger, std::shared_ptr<ERS_CLASS_ShaderLoader> ShaderLoader, float Width = 800.0f, float Height = 600.0f);
-
-        /**
-         * @brief Destroy the Framebuffer Manager object.
-         * 
-         */
-        ~ERS_CLASS_Framebuffer();
+public:
 
 
+    /**
+     * @brief Construct a new Framebuffer Manager object.
+     * 
+     * @param Logger ERS_CLASS_LoggingSystem Instance Pointer
+     * @param Width Width In Pixels
+     * @param Height Height In Pixels
+     */
+    ERS_CLASS_Framebuffer(std::shared_ptr<ERS_CLASS_LoggingSystem> Logger, std::shared_ptr<ERS_CLASS_ShaderLoader> ShaderLoader, float Width = 800.0f, float Height = 600.0f);
 
-        /**
-         * @brief Start The renderpass On The Framebuffer Object.
-         * 
-         */
-        void StartFramebufferRenderPass();
+    /**
+     * @brief Destroy the Framebuffer Manager object.
+     * 
+     */
+    ~ERS_CLASS_Framebuffer();
 
-        /**
-         * @brief Start Renderpass To Screen (GUI Rendering Goes AFter This).
-         * 
-         */
-        void StartScreenRenderPass();
 
-        /**
-         * @brief Resizes the framebuffer to allow for proper scaling.
-         * 
-         * @param Width 
-         * @param Height 
-         */
-        void ResizeFramebuffer(int Width, int Height);
+
+    /**
+     * @brief Start The renderpass On The Framebuffer Object.
+     * 
+     */
+    void StartFramebufferRenderPass();
+
+    /**
+     * @brief Start Renderpass To Screen (GUI Rendering Goes AFter This).
+     * 
+     */
+    void StartScreenRenderPass();
+
+    /**
+     * @brief Resizes the framebuffer to allow for proper scaling.
+     * 
+     * @param Width 
+     * @param Height 
+     */
+    void ResizeFramebuffer(int Width, int Height);
 
 };
