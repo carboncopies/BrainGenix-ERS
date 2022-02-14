03d23f0865e0586112ebe3d9f3371c984840f43d
datacrystals <tliao@carboncopies.org>
2022-02-14 06:24:51 +0000

    Fix Header File Indentation/Fix Comments (#36, #37)

--- Source/Core/Structures/ERS_STRUCT_Model/ERS_STRUCT_Model.cpp
+++ Source/Core/Structures/ERS_STRUCT_Model/ERS_STRUCT_Model.cpp
@@ -6,7 +6,6 @@
 #include "ERS_STRUCT_Model.h"
 
 
-// Draw Model
 void ERS_STRUCT_Model::Draw(std::shared_ptr<ERS_STRUCT_Shader> Shader) {
 
     // Only Draw When Fully Ready
@@ -21,68 +20,52 @@ void ERS_STRUCT_Model::Draw(std::shared_ptr<ERS_STRUCT_Shader> Shader) {
 }
 
 
-// Set Position Functions
 void ERS_STRUCT_Model::SetPosition(double X, double Y, double Z) {
 
-    // Convert Doubles GLM Vec3
     glm::vec3 Position = glm::vec3(X, Y, Z);
-
-    // Set Position to Pos
     ModelPosition = Position;
 
 }
 
 void ERS_STRUCT_Model::SetPosition(glm::vec3 Position) {
 
-    // Set Position to Pos
     ModelPosition = Position;
 
 }
 
-// Set Rotation Functions
 void ERS_STRUCT_Model::SetRotation(double X, double Y, double Z) {
 
-    // Set Rotation to Pos
     ModelRotation = glm::vec3((float)X, (float)Y, (float)Z);
 
 }
 
 void ERS_STRUCT_Model::SetRotation(glm::vec3 Rotation) {
 
-    // Set Rotation to Pos
     ModelRotation = Rotation;
 
 }
 
 
-// Set Scale Functions
 void ERS_STRUCT_Model::SetScale(double X, double Y, double Z) {
 
-    // Convert Doubles GLM Vec3
     glm::vec3 Scale = glm::vec3(X, Y, Z);
-
-    // Set Scale to Pos
     ModelScale = Scale;
 
 }
 
 void ERS_STRUCT_Model::SetScale(glm::vec3 Scale) {
 
-    // Set Scale to Pos
     ModelScale = Scale;
 
 }
 
 
-// Set LocRotScale Functions
 void ERS_STRUCT_Model::SetLocRotScale(double LocX, double LocY, double LocZ, double RotX, double RotY, double RotZ, double ScaleX, double ScaleY, double ScaleZ) {
 
-    // Calculate Loc, Rot, Scale Vecs
     glm::vec3 Position = glm::vec3(LocX, LocY, LocZ);
     glm::vec3 Rotation = glm::vec3(RotX, RotY, RotZ);
     glm::vec3 Scale = glm::vec3(ScaleX, ScaleY, ScaleZ);
 
-    // Set Vecs
     ModelPosition = Position;
     ModelRotation = Rotation;
     ModelScale = Scale;
@@ -92,7 +75,6 @@ void ERS_STRUCT_Model::SetLocRotScale(double LocX, double LocY, double LocZ, dou
 
 void ERS_STRUCT_Model::SetLocRotScale(glm::vec3 Position, glm::vec3 Rotation, glm::vec3 Scale) {
 
-    // Set Vecs
     ModelPosition = Position;
     ModelRotation = Rotation;
     ModelScale = Scale;
@@ -100,40 +82,26 @@ void ERS_STRUCT_Model::SetLocRotScale(glm::vec3 Position, glm::vec3 Rotation, gl
 }
 
 
-// Apply Transformations
 bool ERS_STRUCT_Model::ApplyTransformations() {
 
-    // Check If Template Model
-    //if (!IsTemplateModel) {
-
-        // Reset ModelLocRotScale
-        ModelLocRotScale_ = SourceModelLocRotScale_;
+    // Reset ModelLocRotScale
+    ModelLocRotScale_ = SourceModelLocRotScale_;
 
 
-        // Apply Transforms
-        ModelLocRotScale_ = glm::translate(ModelLocRotScale_, ModelPosition);
-        ModelLocRotScale_ = glm::rotate(ModelLocRotScale_, glm::radians(ModelRotation[0]), glm::vec3(1, 0, 0));
-        ModelLocRotScale_ = glm::rotate(ModelLocRotScale_, glm::radians(ModelRotation[1]), glm::vec3(0, 1, 0));
-        ModelLocRotScale_ = glm::rotate(ModelLocRotScale_, glm::radians(ModelRotation[2]), glm::vec3(0, 0, 1));
-        ModelLocRotScale_ = glm::scale(ModelLocRotScale_, ModelScale);
+    // Apply Transforms
+    ModelLocRotScale_ = glm::translate(ModelLocRotScale_, ModelPosition);
+    ModelLocRotScale_ = glm::rotate(ModelLocRotScale_, glm::radians(ModelRotation[0]), glm::vec3(1, 0, 0));
+    ModelLocRotScale_ = glm::rotate(ModelLocRotScale_, glm::radians(ModelRotation[1]), glm::vec3(0, 1, 0));
+    ModelLocRotScale_ = glm::rotate(ModelLocRotScale_, glm::radians(ModelRotation[2]), glm::vec3(0, 0, 1));
+    ModelLocRotScale_ = glm::scale(ModelLocRotScale_, ModelScale);
 
-        
-        // Return Success
-        return true;
-
-    //} else {
-
-        // Return Failure
-        //return false;
-
-    //}
+    
+    return true;
 
 }
 
-// Get Mat4
 glm::mat4 ERS_STRUCT_Model::GetMat4() {
 
-    // Return Mat4 Projection Matrix
     return ModelLocRotScale_;
 
 }
