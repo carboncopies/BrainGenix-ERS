ac19d6086045e92d5a59c08254258c9c8a4f57a5
datacrystals <tliao@carboncopies.org>
2022-02-14 07:33:03 +0000

    Add Lighting Support To Scene Loader (#40)

--- Source/Core/Structures/ERS_STRUCT_Light/ERS_STRUCT_Light.h
+++ Source/Core/Structures/ERS_STRUCT_Light/ERS_STRUCT_Light.h
@@ -16,14 +16,27 @@ struct ERS_STRUCT_Light {
 
     std::string UserDefinedName; /**Name of the light assigned by user*/
     std::string UserAdditionalNotes; /**<Any user defined additional notes*/
-
+    
     std::string Type; /**<Type of light (point, area, etc.)*/
 
-    float Intensity; /**<Brightness of the light*/
 
+    float Intensity; /**<Brightness of the light*/
+    
     int Red; /**<Red Component*/
     int Green; /**<Green Component*/
     int Blue; /**<Blue Component*/
     
+
+    float PosX; /**<Position*/
+    float PosY; /**<Position*/
+    float PosZ; /**<Position*/
+
+    float RotX; /**<Rotation*/
+    float RotY; /**<Rotation*/
+    float RotZ; /**<Rotation*/
+
+    float ScaleX; /**<Scale*/
+    float ScaleY; /**<Scale*/
+    float ScaleZ; /**<Scale*/
 
 };
