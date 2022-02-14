472f6834defbd100d23a178927e7630241933a6a
datacrystals <tliao@carboncopies.org>
2022-02-14 06:28:41 +0000

    Fix Header File Indentation/Fix Comments (#36, #37)

--- Source/Core/Utils/ERS_InputOutputSubsystem/ERS_CLASS_AssetIndexIOM.cpp
+++ Source/Core/Utils/ERS_InputOutputSubsystem/ERS_CLASS_AssetIndexIOM.cpp
@@ -5,32 +5,22 @@
 #include <ERS_CLASS_AssetIndexIOM.h>
 
 
-// Constructor
 ERS_CLASS_AssetIndexIOM::ERS_CLASS_AssetIndexIOM(std::shared_ptr<ERS_CLASS_LoggingSystem> Logger) {
 
-    // Copy Logger Pointer
     Logger_ = Logger;
-
-    // Log Initialization Start
     Logger_->Log("Initializing ERS Asset Index Input/Output Manager", 5);
 
 }
 
-// Destructor
 ERS_CLASS_AssetIndexIOM::~ERS_CLASS_AssetIndexIOM() {
 
-    // Log Destructor Call
     Logger_->Log("ERS Asset Index Input/Output Manager Destructor Called", 6);
-
 }
 
 
-// Load Asset Index
 bool ERS_CLASS_AssetIndexIOM::LoadAssetIndex(std::shared_ptr<ERS_STRUCT_IOData> Data) {
 
-    // Log Asset Index Decoding
     Logger_->Log("Loading Asset Index", 4);
-
 
     // Decode Asset Index Into YAML::Node
     Logger_->Log("Decoding Asset Index Data From Bytes", 3);
@@ -58,7 +48,6 @@ bool ERS_CLASS_AssetIndexIOM::LoadAssetIndex(std::shared_ptr<ERS_STRUCT_IOData> 
     Logger_->Log("Populating Asset Index Metadata", 3);
     for (YAML::const_iterator it=AssetIndexMetadata.begin(); it!=AssetIndexMetadata.end(); ++it) {
 
-        // Log Reading Of Asset With ID
         Logger_->Log(std::string(std::string("Loading Metadata For Asset With ID: ") + std::to_string(it->first.as<long>())).c_str(), 3);
 
         // Get Asset Metadata Node
@@ -76,15 +65,12 @@ bool ERS_CLASS_AssetIndexIOM::LoadAssetIndex(std::shared_ptr<ERS_STRUCT_IOData> 
     Logger_->Log("Finished Populating Asset Index Metadata", 4);
 
 
-    // Return Success
     return true;
 
 }
 
-// Write Asset Index
 bool ERS_CLASS_AssetIndexIOM::WriteAssetIndex(std::shared_ptr<ERS_STRUCT_IOData> Data) {
 
-    // Log Serialization
     Logger_->Log("Serializing Asset Database Metadata", 4);
 
     // Create Yaml Emitter
@@ -92,7 +78,6 @@ bool ERS_CLASS_AssetIndexIOM::WriteAssetIndex(std::shared_ptr<ERS_STRUCT_IOData>
     Metadata<<YAML::BeginMap;
     for (long i = 0; i < AssetIDsFound_.size(); i++) {
         
-        // Log Asset ID Writing
         Logger_->Log(std::string(std::string("Serializing Metadata For Asset ") + std::to_string(AssetIDsFound_[i])).c_str(), 3);
 
         // Add To Metadata
@@ -108,38 +93,27 @@ bool ERS_CLASS_AssetIndexIOM::WriteAssetIndex(std::shared_ptr<ERS_STRUCT_IOData>
     }
     Metadata<<YAML::EndMap;
 
-    // Convert To String
     std::string ByteString = std::string(Metadata.c_str());
-
-    // Finish Serialization
     Logger_->Log("Finished Serializing Asset DB Metadata", 5);
 
 
     // Add To Data Struct
     Logger_->Log("Adding Encoded Metadata String To IOData Struct", 3);
-
-        
     Data->Data.reset(new unsigned char[ByteString.size()]);
     Data->Size_B = ByteString.size();
     memcpy(Data->Data.get(), ByteString.c_str(), ByteString.size());
-
-    // Finish Adding To Struct
     Logger_->Log("Finished Adding Encoded Metadata String To IOData Struct", 4);
 
-    // Return Success
     return true;
 
 }
 
-// Update Asset Metadata Info
 bool ERS_CLASS_AssetIndexIOM::UpdateAssetIndex(long AssetID, std::shared_ptr<ERS_STRUCT_IOData> Data) {
 
-    // Extract Relevant Params
     std::string AssetType = Data->AssetTypeName;
     std::string Modified = Data->AssetModificationDate;
     std::string Created = Data->AssetCreationDate;
 
-    // Add To Internal Maps
     AssetTypeName_[AssetID] = {AssetType};
     AssetCreationDate_[AssetID] = {Created};
     AssetModificationDate_[AssetID] = {Modified};
@@ -156,12 +130,10 @@ bool ERS_CLASS_AssetIndexIOM::UpdateAssetIndex(long AssetID, std::shared_ptr<ERS
         AssetIDsFound_.push_back(AssetID);
     }
 
-    // Return Success
     return true;
 
 }
 
-// Read Asset Metadata into Data Param
 bool ERS_CLASS_AssetIndexIOM::ReadAssetIndex(long AssetID, std::shared_ptr<ERS_STRUCT_IOData> Data) {
 
     // Lookup Asset Info (If ID Not Zero)
@@ -173,7 +145,6 @@ bool ERS_CLASS_AssetIndexIOM::ReadAssetIndex(long AssetID, std::shared_ptr<ERS_S
         Data->AssetTypeName = std::string("Asset Index Metadata");
     }
 
-    // Return Success
     return true;
 
 }
