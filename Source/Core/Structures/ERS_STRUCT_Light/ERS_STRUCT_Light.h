e9152091e0b12354e8994657d3b1f2d44f266409
datacrystals <tliao@carboncopies.org>
2022-02-14 07:13:11 +0000

    Add Light Struct (#39)

---
+++ Source/Core/Structures/ERS_STRUCT_Light/ERS_STRUCT_Light.h
@@ -0,0 +1,13 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <memory>
+#include <string>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
