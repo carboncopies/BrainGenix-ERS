57bd9743c145c4e3e4578ee593b3269c324326b4
datacrystals <tliao@carboncopies.org>
2022-02-14 08:29:53 +0000

    Debug Scene Writer Segmentation Fault (#41)

--- Source/Core/Writers/ERS_SceneWriter/ERS_SceneWriter.cpp
+++ Source/Core/Writers/ERS_SceneWriter/ERS_SceneWriter.cpp
@@ -99,6 +99,12 @@ std::string SceneWriter::ProcessScene(std::shared_ptr<ERS_STRUCT_Scene> InputSce
         Output << YAML::Key << "AssetType" << YAML::Value << "Light";
         Output << YAML::Key << "LightType" << YAML::Value << InputScene->Lights[i]->LightType;
 
+        Output << YAML::Key << "Intensity" << YAML::Value << InputScene->Lights[i]->Intensity;
+        Output << YAML::Key << "ColorX" << YAML::Value << InputScene->Lights[i]->ColorX;
+        Output << YAML::Key << "ColorY" << YAML::Value << InputScene->Lights[i]->ColorY;
+        Output << YAML::Key << "ColorZ" << YAML::Value << InputScene->Lights[i]->ColorZ;
+        
+
 
         Output << YAML::Key << "PosX" << YAML::Value << InputScene->Lights[i]->PosX;
         Output << YAML::Key << "PosY" << YAML::Value << InputScene->Lights[i]->PosY;
