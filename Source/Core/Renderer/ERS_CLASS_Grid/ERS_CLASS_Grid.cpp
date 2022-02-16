e9a7c4ad93ed04597f2f334603ec4c9f69f799cd
datacrystals <tliao@carboncopies.org>
2022-02-16 22:27:28 +0000

    Debug Grid Shaders (#45)

--- Source/Core/Renderer/ERS_CLASS_Grid/ERS_CLASS_Grid.cpp
+++ Source/Core/Renderer/ERS_CLASS_Grid/ERS_CLASS_Grid.cpp
@@ -32,11 +32,11 @@ ERS_CLASS_Grid::ERS_CLASS_Grid(ERS_STRUCT_SystemUtils* SystemUtils, std::shared_
     glGenVertexArrays(1, &GridVAO_);
     glBindVertexArray(GridVAO_);
 
-    int PositionIndex = 0;
+    int PositionIndex = 3;
     glVertexAttribPointer(PositionIndex, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(PositionIndex);
 
-    int TexCoordsIndex = 2;
+    int TexCoordsIndex = 4;
     glEnableVertexAttribArray(TexCoordsIndex);
     glVertexAttribPointer(TexCoordsIndex, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3*sizeof(float)));
 
