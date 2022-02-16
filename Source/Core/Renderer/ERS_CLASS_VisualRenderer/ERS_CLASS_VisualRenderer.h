73fcad2c9d1d19bd568bf959bd607fb18f72b2a6
datacrystals <tliao@carboncopies.org>
2022-02-16 07:14:55 +0000

    Create Grid Class For VisualRenderer (#45)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.h
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.h
@@ -22,20 +22,22 @@
 
 // Internal Libraries (BG convention: use <> instead of "")
 #include <ERS_CLASS_LoggingSystem.h>
-#include <ERS_STRUCT_Shader.h>
-#include <ERS_STRUCT_Model.h>
-#include <ERS_STRUCT_Camera.h>
-#include <ERS_SceneManager.h>
-#include <ERS_SceneLoader.h>
 #include <ERS_CLASS_ShaderLoader.h>
 #include <ERS_CLASS_InputProcessor.h>
 #include <ERS_CLASS_Framebuffer.h>
+#include <ERS_CLASS_Grid.h>
+
+#include <ERS_SceneManager.h>
+#include <ERS_SceneLoader.h>
+
 #include <ERS_Editor_3DCursor.h>
 
 #include <ERS_STRUCT_LocRotScale.h>
 #include <ERS_STRUCT_SystemUtils.h>
 #include <ERS_STRUCT_Viewport.h>
-
+#include <ERS_STRUCT_Shader.h>
+#include <ERS_STRUCT_Model.h>
+#include <ERS_STRUCT_Camera.h>
 
 /**
  * @brief Handles viewport creation/deletion/rendering/updating.
