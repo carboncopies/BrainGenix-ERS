8468c796f3cd22806b81f482e84bb66a1aed333a
datacrystals <tliao@carboncopies.org>
2022-02-16 19:58:12 +0000

    Debug Grid Issues (#45)

--- Source/Core/Renderer/ERS_CLASS_Grid/ERS_CLASS_Grid.cpp
+++ Source/Core/Renderer/ERS_CLASS_Grid/ERS_CLASS_Grid.cpp
@@ -16,8 +16,8 @@ ERS_CLASS_Grid::ERS_CLASS_Grid(std::shared_ptr<ERS_STRUCT_SystemUtils> SystemUti
     const float GridVertices[] = {
         -1.0,  1.0, 0.0, 1.0, 0.0,  // Top Left
         -1.0, -1.0, 0.0, 0.0, 0.0,  // Bottom Left
-        1.0, -1.0,  0.0, 1.0, 1.0,  // Bottom Right
-        1.0,  1.0,  0.0, 0.0, 1.0   // Top Right
+        1.0, -1.0,  0.0, 0.0, 1.0,  // Bottom Right
+        1.0,  1.0,  0.0, 1.0, 1.0   // Top Right
     };
 
     glGenBuffers(1, &GridVBO_);
