0d2d7ac68aa2465bd3d5a46202be4f114005b710
datacrystals <tliao@carboncopies.org>
2022-02-16 05:57:57 +0000

    Create Grid Class For VisualRenderer (#45)

---
+++ Source/Core/Renderer/ERS_CLASS_Grid/ERS_CLASS_Grid.h
@@ -0,0 +1,17 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+
+// Internal Libraries (BG convention: use <> instead of "")
