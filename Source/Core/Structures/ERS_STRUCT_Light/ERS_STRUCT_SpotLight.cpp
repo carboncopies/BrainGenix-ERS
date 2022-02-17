628e4bbe0a055f33cdd2686da61bb573e1cb2bd0
datacrystals <tliao@carboncopies.org>
2022-02-17 09:20:18 +0000

    Implement Spotlight Struct (#54)

--- Source/Core/Structures/ERS_STRUCT_Light/ERS_STRUCT_SpotLight.cpp
+++ Source/Core/Structures/ERS_STRUCT_Light/ERS_STRUCT_SpotLight.cpp
@@ -2,4 +2,4 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-#include <ERS_STRUCT_Light.h>
+#include <ERS_STRUCT_SpotLight.h>
