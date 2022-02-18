968dc7c42a6ce484a3fa53cd7f7906b34d8ecbe9
datacrystals <tliao@carboncopies.org>
2022-02-18 09:19:37 +0000

    Fix Model Importer Texture Loading

--- Source/Core/Editor/MenuEntries/GUI_Menu_File/Utils/Import_Asset/ERS_Editor_ModelImporter.cpp
+++ Source/Core/Editor/MenuEntries/GUI_Menu_File/Utils/Import_Asset/ERS_Editor_ModelImporter.cpp
@@ -147,11 +147,12 @@ void ERS_CLASS_ModelImporter::AddTexture(ERS_STRUCT_Model* Model, aiMaterial *Ma
         aiString Str;
         Mat->GetTexture(Type, i, &Str);
 
-        std::string Message = std::string("Found Texture '") + TypeName + std::string("'");
-        SystemUtils_->Logger_->Log(Message, 3);
+
 
         // Calculate Texture Path, Append If Not Already In List
         std::string FilePath = std::string(ModelDirectory + std::string(Model->Directory)  + std::string("/") + std::string(Str.C_Str()));
+        std::string Message = std::string("Found Texture Of Type '") + TypeName + std::string("' At Path '") + FilePath + std::string("'");
+        SystemUtils_->Logger_->Log(Message, 3);
         if (std::find(TextureList_.begin(), TextureList_.end(), FilePath) == TextureList_.end()) {
             TextureList_.push_back(FilePath);
         }   
