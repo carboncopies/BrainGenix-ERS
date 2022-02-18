f8dedd0972223ca1cc1c5e764ff8169e4f3245d4
datacrystals <tliao@carboncopies.org>
2022-02-18 23:12:26 +0000

    Switch Assimp Version To 5.1.0

--- Third-Party/assimp/test/unit/utVersion.cpp
+++ Third-Party/assimp/test/unit/utVersion.cpp
@@ -2,8 +2,7 @@
 Open Asset Import Library (assimp)
 ---------------------------------------------------------------------------
 
-Copyright (c) 2006-2017, assimp team
-
+Copyright (c) 2006-2021, assimp team
 
 All rights reserved.
 
@@ -45,20 +44,20 @@ class utVersion : public ::testing::Test {
 };
 
 TEST_F( utVersion, aiGetLegalStringTest ) {
-    const char *lv( aiGetLegalString() );
+    const char *lv = aiGetLegalString();
     EXPECT_NE( lv, nullptr );
     std::string text( lv );
 
-    size_t pos( text.find( std::string( "2017" ) ) );
+    size_t pos = text.find(std::string("2021"));
     EXPECT_NE( pos, std::string::npos );
 }
 
 TEST_F( utVersion, aiGetVersionMinorTest ) {
     EXPECT_EQ( aiGetVersionMinor(), 1U );
 }
-    
+
 TEST_F( utVersion, aiGetVersionMajorTest ) {
-    EXPECT_EQ( aiGetVersionMajor(), 4U );
+    EXPECT_EQ( aiGetVersionMajor(), 5U );
 }
 
 TEST_F( utVersion, aiGetCompileFlagsTest ) {
@@ -68,4 +67,9 @@ TEST_F( utVersion, aiGetCompileFlagsTest ) {
 TEST_F( utVersion, aiGetVersionRevisionTest ) {
     EXPECT_NE( aiGetVersionRevision(), 0U );
 }
+
+TEST_F( utVersion, aiGetBranchNameTest ) {
+    EXPECT_NE( nullptr, aiGetBranchName() );
+}
+
 
