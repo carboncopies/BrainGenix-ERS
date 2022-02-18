19a7dc37aa995c3a4669dccf5790c4bef8e78e97
datacrystals <tliao@carboncopies.org>
2022-02-18 23:08:59 +0000

    Downgrade Assimp To 4.1.0

--- CMake/BuildUtils/Assimp.cmake
+++ CMake/BuildUtils/Assimp.cmake
@@ -4,6 +4,6 @@
 
 ERSBuildLogger(${Green} "Configuring Assimp Library")
 add_subdirectory(${LIB_DIR}/assimp)
-target_include_directories(assimp ${LIB_DIR}/assimp/include)
+target_include_directories(assimp PUBLIC ${LIB_DIR}/assimp/include)
 disable_target_warnings(assimp)
 ERSBuildLogger(${BoldGreen} "Finished Configuring Assimp Library")
