2027fbe26a53f5bf11e26d1ee06c26a5276014df
datacrystals <tliao@carboncopies.org>
2022-02-19 08:40:26 +0000

    Implement Texture Path Autocorrection (#56)

--- Source/Core/Editor/MenuEntries/GUI_Menu_File/Utils/Import_Asset/ERS_Editor_ModelImporter.cpp
+++ Source/Core/Editor/MenuEntries/GUI_Menu_File/Utils/Import_Asset/ERS_Editor_ModelImporter.cpp
@@ -75,7 +75,7 @@ long ERS_CLASS_ModelImporter::ImportModel(std::string AssetPath) {
                 std::string FilePath{Entry.path().u8string()};
                 std::string FileName = FilePath.substr(FilePath.find_last_of("/") + 1, FilePath.size() - 1);
                 std::string FileNameWithoutExtension = FileName.substr(0, FileName.find_last_of("."));
-                std::cout<<FileNameWithoutExtension<<std::endl;
+
                 if (FileName == Path) {
                     Path = FilePath;
                     SystemUtils_->Logger_->Log(std::string("Found Potential Match '") + FilePath + std::string("', Attempting To Load"), 5);
