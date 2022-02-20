40e5a63393358bd5f932029983fe9b4b47248025
datacrystals <tliao@carboncopies.org>
2022-02-20 08:05:11 +0000

    Debug Stack Corruption/Double Free Error (#61)

--- Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
@@ -245,9 +245,9 @@ void ERS_CLASS_ModelLoader::LoadModel(long AssetID, std::shared_ptr<ERS_STRUCT_M
 
     // Read Metadata From Asset
     std::shared_ptr<ERS_STRUCT_IOData> ModelMetadata = std::make_shared<ERS_STRUCT_IOData>();
-    std::cout<<"Test Point 1\n";
+    std::cout<<"Test Point"<<AssetID<<std::endl;
     SystemUtils_->ERS_IOSubsystem_->ReadAsset(AssetID, ModelMetadata);
-    std::cout<<"End Test Point\n";
+    std::cout<<"End Test Point"<<AssetID<<std::endl;
     std::string ModelMetadataString = std::string((const char*)ModelMetadata->Data.get());
     YAML::Node Metadata = YAML::Load(ModelMetadataString);
 
