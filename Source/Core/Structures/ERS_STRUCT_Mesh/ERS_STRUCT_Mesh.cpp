856658529fc04588663c88b8627ac2383d092de6
datacrystals <tliao@carboncopies.org>
2022-02-22 00:03:00 +0000

    Create Default Texture (#58)

--- Source/Core/Structures/ERS_STRUCT_Mesh/ERS_STRUCT_Mesh.cpp
+++ Source/Core/Structures/ERS_STRUCT_Mesh/ERS_STRUCT_Mesh.cpp
@@ -169,8 +169,8 @@ void ERS_STRUCT_Mesh::Draw(std::shared_ptr<ERS_STRUCT_Shader> Shader) {
 
     }
 
-    glBindTexture(GL_TEXTURE_2D, 0);
-    glActiveTexture(0);
+    glBindTexture(GL_TEXTURE_2D, 1);
+    glActiveTexture(1);
 
     // Draw Mesh
     glBindVertexArray(VAO);
