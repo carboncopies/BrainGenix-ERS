29a7127ab8faf356f468f35d5c04929a6b704747
datacrystals <tliao@carboncopies.org>
2022-02-22 00:20:08 +0000

    Create Default Texture (#58)

---
+++ Source/Core/Structures/ERS_STRUCT_Defaults/ERS_STRUCT_Defaults.h
@@ -0,0 +1,25 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <string>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
+/**
+ * @brief struct to contain default opengl ids for textures, objects, etc.
+ * 
+ */
+struct ERS_STRUCT_Defaults {
+
+    unsigned int DefaultTexture_; /** Default Texture ID To Be Bound To*/
+    unsigned int DefaultObject_; /** Default Object To Be Bound To*/
+
+}
