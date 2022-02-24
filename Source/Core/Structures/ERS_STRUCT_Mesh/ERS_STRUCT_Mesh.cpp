87515916608fbe7719ba12fc1ecee5217c58e8ca
datacrystals <tliao@carboncopies.org>
2022-02-24 05:18:29 +0000

    Implement Texture Debugging Shaders

--- Source/Core/Structures/ERS_STRUCT_Mesh/ERS_STRUCT_Mesh.cpp
+++ Source/Core/Structures/ERS_STRUCT_Mesh/ERS_STRUCT_Mesh.cpp
@@ -164,6 +164,8 @@ void ERS_STRUCT_Mesh::Draw(ERS_STRUCT_OpenGLDefaults* OpenGLDefaults, std::share
 
         glUniform1i(glGetUniformLocation(Shader->ShaderProgram, (Name + Number).c_str()), i);
         
+        std::cout<<Name<<TextureIDs[i];
+
         // Bind Texture
         glBindTexture(GL_TEXTURE_2D, TextureIDs[i]);
 
