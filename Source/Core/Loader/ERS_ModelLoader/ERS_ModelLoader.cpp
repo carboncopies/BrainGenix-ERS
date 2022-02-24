884e0735c5c8771afcc415bbb975418c335a5873
datacrystals <tliao@carboncopies.org>
2022-02-24 19:58:32 +0000

    Debug Segfault With ModelLoader (#62)

--- Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
@@ -155,8 +155,12 @@ void ERS_CLASS_ModelLoader::ProcessGPU(std::shared_ptr<ERS_STRUCT_Model> Model) 
                 glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, Model->TexturesToPushToGPU_[i].Width, Model->TexturesToPushToGPU_[i].Height, 0, GL_BGRA, GL_UNSIGNED_BYTE, RawImageData);
             } else if (Model->TexturesToPushToGPU_[i].Channels == 3) {
                 glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, Model->TexturesToPushToGPU_[i].Width, Model->TexturesToPushToGPU_[i].Height, 0, GL_BGR, GL_UNSIGNED_BYTE, RawImageData);
+            } else if (Model->TexturesToPushToGPU_[i].Channels == 2) {
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RG, Model->TexturesToPushToGPU_[i].Width, Model->TexturesToPushToGPU_[i].Height, 0, GL_RG, GL_UNSIGNED_BYTE, RawImageData);
+            } else if (Model->TexturesToPushToGPU_[i].Channels == 1) {
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RG, Model->TexturesToPushToGPU_[i].Width, Model->TexturesToPushToGPU_[i].Height, 0, GL_RG, GL_UNSIGNED_BYTE, RawImageData);
             } else {
-                std::cout<<"Num Channels: "<<Model->TexturesToPushToGPU_[i].Channels<<std::endl;
+                SystemUtils_->Logger_->Log(std::string("Texture Has Unsupported Number Of Channels: ") + std::to_string(Model->TexturesToPushToGPU_[i].Channels), 8);
             }
             glGenerateMipmap(GL_TEXTURE_2D);
             
