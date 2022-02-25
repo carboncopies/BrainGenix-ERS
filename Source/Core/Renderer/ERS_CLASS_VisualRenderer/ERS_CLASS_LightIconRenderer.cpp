6edcc6c4c28e746e4d1f327ef6bd9d82ce653021
datacrystals <tliao@carboncopies.org>
2022-02-25 03:08:54 +0000

    Consolidate Renderer System (#63)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_LightIconRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_LightIconRenderer.cpp
@@ -9,6 +9,9 @@
 ERS_CLASS_LightIconRenderer::ERS_CLASS_LightIconRenderer(ERS_STRUCT_SystemUtils* SystemUtils, std::shared_ptr<ERS_STRUCT_Shader> LightIconRendererShader) {
 
 
+    SystemUtils_ = SystemUtils;
+    LightIconRendererShader_ = LightIconRendererShader;
+
     
 
 
