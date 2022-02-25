004c567db3bd97785ee1a0df8e6286e40e2d4537
datacrystals <tliao@carboncopies.org>
2022-02-25 22:32:27 +0000

    Implement Lighting Icons (#48)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_LightIconRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_LightIconRenderer.cpp
@@ -66,10 +66,10 @@ void ERS_CLASS_LightIconRenderer::Draw(ERS_STRUCT_Camera* Camera, ERS_CLASS_Scen
     // Draw All Point Lights
     for (int i = 0; i < SceneManager->Scenes_[SceneManager->ActiveScene_]->PointLights.size(); i++) {
 
-        glm::mat4 NewModelMatrix = glm::scale(LightIconRendererModelArray_, glm::vec3(LightIconRendererScale_));
-        NewModelMatrix = glm::translate(NewModelMatrix, SceneManager->Scenes_[SceneManager->ActiveScene_]->PointLights[i]->Pos);
+        //glm::mat4 NewModelMatrix = glm::scale(LightIconRendererModelArray_, glm::vec3(LightIconRendererScale_));
+        //NewModelMatrix = glm::translate(NewModelMatrix, SceneManager->Scenes_[SceneManager->ActiveScene_]->PointLights[i]->Pos);
 
-        LightIconRendererShader_->SetMat4("model", NewModelMatrix);
+        LightIconRendererShader_->SetMat4("model", LightIconRendererModelArray_);
         LightIconRendererShader_->SetMat4("view", View);
         LightIconRendererShader_->SetMat4("projection", Projection);
 
