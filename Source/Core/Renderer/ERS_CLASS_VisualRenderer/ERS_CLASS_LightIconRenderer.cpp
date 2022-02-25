bce9f2d8d00cc4c439c1e0b2656de0c93854868d
datacrystals <tliao@carboncopies.org>
2022-02-25 03:10:34 +0000

    Consolidate Renderer System (#63)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_LightIconRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_LightIconRenderer.cpp
@@ -12,7 +12,14 @@ ERS_CLASS_LightIconRenderer::ERS_CLASS_LightIconRenderer(ERS_STRUCT_SystemUtils*
     SystemUtils_ = SystemUtils;
     LightIconRendererShader_ = LightIconRendererShader;
 
-    
 
+    SystemUtils_->Logger_->Log("Setting Up VisualRenderer LightIconRenderer System", 5);
+
+
+}
+
+ERS_CLASS_LightIconRenderer::~ERS_CLASS_LightIconRenderer() {
+
+    SystemUtils_->Logger_->Log("LightIconRenderer Destructor Called", 6);
 
 }
