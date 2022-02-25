fe2f2fe9606c05c604b4fd28b0c767dad035da52
datacrystals <tliao@carboncopies.org>
2022-02-25 03:14:19 +0000

    Consolidate Renderer System (#63)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_LightIconRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_LightIconRenderer.cpp
@@ -17,7 +17,7 @@ ERS_CLASS_LightIconRenderer::ERS_CLASS_LightIconRenderer(ERS_STRUCT_SystemUtils*
 
 
 
-    const float GridVertices[] = {
+    const float LightIconRendererVertices[] = {
         -1.0,  1.0, 0.0, 0.0, 1.0,  // Top Left
         -1.0, -1.0, 0.0, 1.0, 1.0,  // Bottom Left
         1.0, -1.0,  0.0, 1.0, 0.0,  // Bottom Right
@@ -26,7 +26,7 @@ ERS_CLASS_LightIconRenderer::ERS_CLASS_LightIconRenderer(ERS_STRUCT_SystemUtils*
 
     glGenBuffers(1, &LightIconRendererVBO_);
     glBindBuffer(GL_ARRAY_BUFFER, LightIconRendererVBO_);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GridVertices), GridVertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(LightIconRendererVertices), LightIconRendererVertices, GL_STATIC_DRAW);
     
     glGenVertexArrays(1, &LightIconRendererVAO_);
     glBindVertexArray(LightIconRendererVAO_);
@@ -40,10 +40,10 @@ ERS_CLASS_LightIconRenderer::ERS_CLASS_LightIconRenderer(ERS_STRUCT_SystemUtils*
     glVertexAttribPointer(TexCoordsIndex, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3*sizeof(float)));
 
 
-    // Setup Grid Model Matrix
-    GridModelArray_ = glm::mat4();
-    GridModelArray_ = glm::scale(GridModelArray_, glm::vec3(GridScale_));
-    GridModelArray_ = glm::rotate(GridModelArray_, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    // Setup LightIconRenderer Model Matrix
+    LightIconRendererModelArray_ = glm::mat4();
+    LightIconRendererModelArray_ = glm::scale(LightIconRendererModelArray_, glm::vec3(LightIconRendererScale_));
+    LightIconRendererModelArray_ = glm::rotate(LightIconRendererModelArray_, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
 
 
