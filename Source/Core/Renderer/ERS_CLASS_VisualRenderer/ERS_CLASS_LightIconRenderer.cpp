ab6339e50814c4fee819791f24e311c9b62d73e3
datacrystals <tliao@carboncopies.org>
2022-02-26 07:37:25 +0000

    Debug Lighting Icons (#48)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_LightIconRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_LightIconRenderer.cpp
@@ -67,7 +67,7 @@ void ERS_CLASS_LightIconRenderer::Draw(ERS_STRUCT_Camera* Camera, ERS_CLASS_Scen
 
         glm::mat4 NewModelMatrix = glm::translate(LightIconRendererModelArray_, SceneManager->Scenes_[SceneManager->ActiveScene_]->PointLights[i]->Pos);
 
-        glm::vec3 RotateBillboard = Camera->Front;  
+        glm::vec3 RotateBillboard = Camera->Right;  
         std::cout<<glm::to_string(RotateBillboard)<<std::endl;
 
         NewModelMatrix = glm::rotate(NewModelMatrix, RotateBillboard.x, glm::vec3(1, 0, 0));
