2462c6445ba430a32a21603be7d2bd7db81dbee1
Thomas Liao <tliao@carboncopies.org>
2022-03-01 20:13:16 -0800

    Convert Shared Pointers To Unique/Raw Pointers (#49)

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -170,7 +170,7 @@ int main() {
     SystemUtils->Logger_->Log("", 5);
     SystemUtils->Logger_->Log("    +-----------------------------------------------------------------+", 4);
     SystemUtils->Logger_->Log("    |     BrainGenix-ERS Real-Time Environment Rendering System       |", 4);
-    sERSLogger->Log("    +-----------------------------------------------------------------+", 4);
+    SystemUtils->Logger_->Log("    +-----------------------------------------------------------------+", 4);
     sERSLogger->Log("", 4);
 
 
