7efc943529632bc7cee1bb9ba16a76214463cb2d
Thomas Liao <tliao@carboncopies.org>
2022-03-02 13:13:33 -0800

    Convert Shared Pointers To Unique/Standard Pointers (#49)

--- Source/Core/Structures/ERS_STRUCT_ProjectUtils/ERS_STRUCT_ProjectUtils.h
+++ Source/Core/Structures/ERS_STRUCT_ProjectUtils/ERS_STRUCT_ProjectUtils.h
@@ -32,6 +32,6 @@ struct ERS_STRUCT_ProjectUtils {
     std::unique_ptr<ERS_CLASS_SceneLoader> SceneLoader_; /**<Shared Ptr To Scene Loader*/
     std::unique_ptr<ERS_CLASS_ProjectLoader> ProjectLoader_; /**<Shared Pointer To Project Loader*/
     std::unique_ptr<ERS_CLASS_ProjectManager> ProjectManager_; /**<Shared Pointer To Projet Manager Instance*/
-    std::shared_ptr<ERS_CLASS_ProjectWriter> ProjectWriter_; /**<Shared Pointer to project writer instance*/
+    std::unique_ptr<ERS_CLASS_ProjectWriter> ProjectWriter_; /**<Shared Pointer to project writer instance*/
 
 };
