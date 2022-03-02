9428ba5157afbc8c25e25679a8ae193f0accc4cb
Thomas Liao <tliao@carboncopies.org>
2022-03-01 20:13:22 -0800

    Convert Shared Pointers To Unique/Raw Pointers (#49)

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -88,7 +88,7 @@ int main() {
     // Startup IO Subsystem And Other Related Systems
     std::shared_ptr<ERS_CLASS_InputOutputSubsystem> sERSIOSubSystem = std::make_shared<ERS_CLASS_InputOutputSubsystem>(SystemUtils->Logger_, sERSLocalSystemConfiguration);
     SystemUtils->ERS_IOSubsystem_ = sERSIOSubSystem;
-    std::shared_ptr<ERS_CLASS_ModelWriter> sERSModelWriter = std::make_shared<ERS_CLASS_ModelWriter>(sERSLogger, sERSIOSubSystem);
+    std::shared_ptr<ERS_CLASS_ModelWriter> sERSModelWriter = std::make_shared<ERS_CLASS_ModelWriter>(SystemUtils->Logger_, sERSIOSubSystem);
     SystemUtils->ERS_ModelWriter_ = sERSModelWriter;
 
 
