0134b3ddfbb939fddf3efee3a9ab2cc8d859b5c5
Thomas Liao <tliao@carboncopies.org>
2022-03-02 13:20:30 -0800

    Convert Shared Pointers To Unique/Standard Pointers (#49)

--- Source/Core/Editor/GUI.cpp
+++ Source/Core/Editor/GUI.cpp
@@ -41,7 +41,7 @@ GUISystem::GUISystem(ERS_STRUCT_SystemUtils* SystemUtils, GLFWwindow* Window, st
     // Initialize Windows
     SystemUtils_->Logger_->Log("Initializing Editor Menu", 5);
     Menu_File_ = std::make_unique<GUI_Menu_File>(SystemUtils_, SceneManager_, ProjectUtils_, VisualRenderer_);
-    Menu_View_ = std::make_unique<GUI_Menu_View>(SystemUtils_, ThemeManager_, FontManager_);
+    Menu_View_ = std::make_unique<GUI_Menu_View>(SystemUtils_, ThemeManager_.get(), FontManager_);
     Menu_Window_ = std::make_unique<GUI_Menu_Window>(SystemUtils_, ProjectUtils_, Cursors3D_, SceneManager_, VisualRenderer_);
     Menu_Debug_ = std::make_unique<GUI_Menu_Debug>(SystemUtils_);
     Menu_Settings_ = std::make_unique<GUI_Menu_Settings>(SystemUtils_, HIDUtils_, ProjectUtils_);
