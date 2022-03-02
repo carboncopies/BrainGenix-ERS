230e206270ff3bd08be0078e6b104bc7c339b295
Thomas Liao <tliao@carboncopies.org>
2022-03-01 20:08:50 -0800

    Convert Shared Pointers To Unique/Raw Pointers (#49)

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -126,7 +126,7 @@ int main() {
     SystemUtils->Logger_->Log("Instantiating Project Writer Shared Pointer", 4);
     ProjectUtils->ProjectWriter_ = sERSProjectWriter;
 
-    sERSLogger->Log("Instantiating ERS Project Manager Pointer", 4);
+    SystemUtils->Logger_->Log("Instantiating ERS Project Manager Pointer", 4);
     std::shared_ptr<ERS_CLASS_ProjectManager> sERSProjectManager = std::make_shared<ERS_CLASS_ProjectManager>(SystemUtils.get(), sERSProjectLoader, sERSProjectWriter, sERSSceneManager, sERSSceneLoader);
     sERSLogger->Log("Copying Shared Pointer To Project Utils Struct", 3);
     ProjectUtils->ProjectManager_ = sERSProjectManager;
