fc94b67e5b389941e1be74ad4e7064a5936564a4
Thomas Liao <tliao@carboncopies.org>
2022-03-02 11:40:20 -0800

    Convert Shared Pointers To Unique/Standard Pointers (#49)

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -107,8 +107,7 @@ int main() {
     ProjectUtils->SceneManager_ = std::make_unique<ERS_CLASS_SceneManager>(SystemUtils->Logger_.get());
 
     SystemUtils->Logger_->Log("Instantiating Model Loader Shared Pointer", 4);
-    ProjectUtils->ModelLoader_ = std::make_shared<ERS_CLASS_ModelLoader>(SystemUtils.get());
-    SystemUtils->Logger_->Log("Copying Shared Pointer To Project Utils Struct", 3);
+    ProjectUtils->ModelLoader_ = std::make_unique<ERS_CLASS_ModelLoader>(SystemUtils.get());
 
     SystemUtils->Logger_->Log("Instantiating Scene Loader Shared Pointer", 4);
     std::shared_ptr<ERS_CLASS_SceneLoader> sERSSceneLoader = std::make_shared<ERS_CLASS_SceneLoader>(SystemUtils.get(), sERSModelLoader);
