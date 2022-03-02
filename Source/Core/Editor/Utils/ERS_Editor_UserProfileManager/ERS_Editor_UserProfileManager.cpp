503b7f47f31d6f35954dfd93793e4b975aeab8d5
Thomas Liao <tliao@carboncopies.org>
2022-03-01 22:20:26 -0800

    Convert Shared Pointers To Unique/Raw Pointers (#49)

--- Source/Core/Editor/Utils/ERS_Editor_UserProfileManager/ERS_Editor_UserProfileManager.cpp
+++ Source/Core/Editor/Utils/ERS_Editor_UserProfileManager/ERS_Editor_UserProfileManager.cpp
@@ -5,7 +5,7 @@
 #include <ERS_Editor_UserProfileManager.h>
 
 
-ERS_CLASS_UserProfileManager::ERS_CLASS_UserProfileManager(std::shared_ptr<ERS_CLASS_LoggingSystem> Logger, const char* UserProfilePath) {
+ERS_CLASS_UserProfileManager::ERS_CLASS_UserProfileManager(ERS_CLASS_LoggingSystem* Logger, const char* UserProfilePath) {
 
     Logger_ = Logger;
     UserProfilePath_ = std::string(UserProfilePath);
