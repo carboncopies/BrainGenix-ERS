f16a7c86f4a67c43d8e81b01bc3748d5bca1f1e1
Thomas Liao <tliao@carboncopies.org>
2022-03-02 13:26:17 -0800

    Convert Shared Pointers To Unique/Standard Pointers (#49)

--- Source/Core/Utils/ERS_InputOutputSubsystem/ERS_InputOutputSubsystem.cpp
+++ Source/Core/Utils/ERS_InputOutputSubsystem/ERS_InputOutputSubsystem.cpp
@@ -196,7 +196,7 @@ void ERS_CLASS_InputOutputSubsystem::IndexUsedAssetIDs() {
   // Load Asset Metadata
   Logger_->Log("Attempting To Load Asset Metadata Index", 3);
   std::shared_ptr<ERS_STRUCT_IOData> Data =
-      std::make_shared<ERS_STRUCT_IOData>();
+      std::make_unique<ERS_STRUCT_IOData>();
   ReadAsset(0, Data);
   AssetIndexIOManager_->LoadAssetIndex(Data);
   Logger_->Log("Finished Loading Asset Metadata Index", 4);
