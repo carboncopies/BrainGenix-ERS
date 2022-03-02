bc89e502c925b95c39cb114479a015cf04071a54
Thomas Liao <tliao@carboncopies.org>
2022-03-02 13:38:24 -0800

    Convert Shared Pointers To Unique/Standard Pointers (#49)

--- Source/Core/Loader/ERS_ProjectLoader/ERS_ProjectLoader.cpp
+++ Source/Core/Loader/ERS_ProjectLoader/ERS_ProjectLoader.cpp
@@ -24,7 +24,7 @@ ERS_STRUCT_Project ERS_CLASS_ProjectLoader::LoadProject(long AssetID) {
     // Log Project Loading
     SystemUtils_->Logger_->Log(std::string(std::string("Loading Project With Asset ID ") + std::to_string(AssetID)).c_str(), 4);
     std::unique_ptr<ERS_STRUCT_IOData> ProjectData = std::make_unique<ERS_STRUCT_IOData>();
-    SystemUtils_->ERS_IOSubsystem_->ReadAsset(AssetID, ProjectData);
+    SystemUtils_->ERS_IOSubsystem_->ReadAsset(AssetID, ProjectData.get());
 
     // Decode YAML
     std::string DataString = std::string((const char*)ProjectData->Data.get());
