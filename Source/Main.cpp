278aec3dc7f5d5b6eabac4b58cf9ddd15de09e5c
Thomas Liao <tliao@carboncopies.org>
2022-03-02 11:20:25 -0800

    Convert Shared Pointers To Unique/Standard Pointers (#49)

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -127,7 +127,7 @@ int main() {
     ProjectUtils->ProjectWriter_ = sERSProjectWriter;
 
     SystemUtils->Logger_->Log("Instantiating ERS Project Manager Pointer", 4);
-    std::shared_ptr<ERS_CLASS_ProjectManager> sERSProjectManager = std::make_shared<ERS_CLASS_ProjectManager>(SystemUtils.get(), sERSProjectLoader, sERSProjectWriter, sERSSceneManager, sERSSceneLoader);
+    std::shared_ptr<ERS_CLASS_ProjectManager> sERSProjectManager = std::make_shared<ERS_CLASS_ProjectManager>(SystemUtils.get(), sERSProjectLoader, sERSProjectWriter, ProjectUtils->SceneManager_.get(), sERSSceneLoader);
     SystemUtils->Logger_->Log("Copying Shared Pointer To Project Utils Struct", 3);
     ProjectUtils->ProjectManager_ = sERSProjectManager;
 
