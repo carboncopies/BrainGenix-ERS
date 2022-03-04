89d9adc94d1079afbd952c2a7dac2ccd5f242249
Thomas Liao <tliao@carboncopies.org>
2022-03-03 17:19:36 -0800

    Resolve Compiler Warnings (#64)

--- Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
@@ -339,7 +339,7 @@ void ERS_CLASS_ModelLoader::ProcessNode(ERS_STRUCT_Model* Model, aiNode *Node, c
 
 }
 
-ERS_STRUCT_Mesh ERS_CLASS_ModelLoader::ProcessMesh(ERS_STRUCT_Model* Model, aiMesh *Mesh, const aiScene *Scene, std::vector<std::string> TexturePaths) {
+ERS_STRUCT_Mesh ERS_CLASS_ModelLoader::ProcessMesh(aiMesh *Mesh, const aiScene *Scene, std::vector<std::string> TexturePaths) {
 
     // Create Data Holders
     ERS_STRUCT_Mesh OutputMesh;
