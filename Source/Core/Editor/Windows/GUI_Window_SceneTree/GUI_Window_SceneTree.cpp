c7d27c7bd00940a606d2ce7ed4fbc2577c5fc655
Thomas Liao <tliao@carboncopies.org>
2022-03-04 00:03:03 -0800

    Convert Shared Pointers To Unique/Raw Pointers (#49)

--- Source/Core/Editor/Windows/GUI_Window_SceneTree/GUI_Window_SceneTree.cpp
+++ Source/Core/Editor/Windows/GUI_Window_SceneTree/GUI_Window_SceneTree.cpp
@@ -139,7 +139,7 @@ void Window_SceneTree::Draw() {
             if (const ImGuiPayload* Payload = ImGui::AcceptDragDropPayload("PAYLOAD_ASSET_MODEL_ID")) {
                 memcpy(&PayloadID, Payload->Data, sizeof(long));
                 SystemUtils_->Logger_->Log(std::string(std::string("Window_SceneTree Recieved Drag Drop Payload 'PAYLOAD_ASSET_MODEL_ID' With Value '") + std::to_string(PayloadID) + std::string("'")).c_str(), 0);
-                std::shared_ptr<ERS_STRUCT_Scene> Scene = ProjectUtils_->SceneManager_->Scenes_[ProjectUtils_->SceneManager_->ActiveScene_];
+                ERS_STRUCT_Scene* Scene = ProjectUtils_->SceneManager_->Scenes_[ProjectUtils_->SceneManager_->ActiveScene_];
                 ProjectUtils_->SceneLoader_->AddModel(Scene, PayloadID);
             }
 
