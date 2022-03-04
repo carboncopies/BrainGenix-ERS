6ea6de7aabc06d96838661ad0746dc99c2f4e828
Thomas Liao <tliao@carboncopies.org>
2022-03-03 16:43:04 -0800

    Resolve Compiler Warnings (#64)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.cpp
@@ -108,7 +108,7 @@ void ERS_CLASS_VisualRenderer::UpdateViewports(float DeltaTime, ERS_CLASS_SceneM
         }
 
         // Update Viewport Camera/Position/Etc.
-        InputProcessorInstance->ProcessKeyboardInput(SystemUtils_->Logger_.get(), DeltaTime, CaptureEnabled);
+        InputProcessorInstance->ProcessKeyboardInput(DeltaTime, CaptureEnabled);
         InputProcessorInstance->UpdateFramebuffer();
         InputProcessorInstance->UpdateMouse(CaptureEnabled);
         InputProcessorInstance->ProcessMouseScroll(CaptureEnabled);
