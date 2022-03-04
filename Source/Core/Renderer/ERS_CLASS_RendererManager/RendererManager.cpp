d8613b38598ad5a3e1a597197f37ebbf197bc758
Thomas Liao <tliao@carboncopies.org>
2022-03-04 10:24:33 -0800

    Convert Shared Pointers To Unique/Raw Pointers (#49)

--- Source/Core/Renderer/ERS_CLASS_RendererManager/RendererManager.cpp
+++ Source/Core/Renderer/ERS_CLASS_RendererManager/RendererManager.cpp
@@ -31,7 +31,7 @@ RendererManager::RendererManager(ERS_STRUCT_SystemUtils* SystemUtils, ERS_STRUCT
     VisualRenderer_->SetOpenGLDefaults(OpenGLDefaults_.get());
 
     // Setup Shaders
-    ShaderLoader_ = std::make_shared<ERS_CLASS_ShaderLoader>(SystemUtils_);
+    ShaderLoader_ = std::make_unique<ERS_CLASS_ShaderLoader>(SystemUtils_);
     for (int i = 0; (long)i < (long)ProjectUtils_->ProjectManager_->Project_.ShaderPrograms.size(); i++) {
         long VertexShaderID = ProjectUtils_->ProjectManager_->Project_.ShaderPrograms[i].VertexID;
         long FragmentShaderID = ProjectUtils_->ProjectManager_->Project_.ShaderPrograms[i].FragmentID;
