ef0d6fbdffa045a09dab043d5405f59b6c1c22c7
Thomas Liao <tliao@carboncopies.org>
2022-03-04 00:09:12 -0800

    Resolve Compiler Warnings (#64)

--- Source/Core/Editor/Windows/GUI_Window_SceneTree/ERS_GUI_FUNCTION_DuplicateScene.cpp
+++ Source/Core/Editor/Windows/GUI_Window_SceneTree/ERS_GUI_FUNCTION_DuplicateScene.cpp
@@ -8,12 +8,12 @@
 void GUI_Windowutil_DuplicateScene(ERS_CLASS_SceneManager* SceneManager, int SceneIndex) {
 
     // Get Current Scene
-    ERS_STRUCT_Scene* NewScene = SceneManager->Scenes_[SceneIndex].get();
+    ERS_STRUCT_Scene NewScene = *SceneManager->Scenes_[SceneIndex].get();
 
     // Update Name
-    std::string CurrentName = NewScene->SceneName;
+    std::string CurrentName = NewScene.SceneName;
     std::string NewName = CurrentName + std::string(" - Copy");
-    NewScene->SceneName = NewName;
+    NewScene.SceneName = NewName;
 
     // Add To SceneManager
     SceneManager->Scenes_.push_back(NewScene);
