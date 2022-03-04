2743709f77e487a502c4f8eee4070db36e11cfba
Thomas Liao <tliao@carboncopies.org>
2022-03-03 16:25:06 -0800

    Resolve Compiler Warnings (#64)

--- Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
@@ -181,7 +181,7 @@ void ERS_CLASS_ModelLoader::ProcessGPU(std::shared_ptr<ERS_STRUCT_Model> Model) 
 
     // Process Texture References, Setup Meshes
     for (int i = 0; (long)i < (long)Model->Meshes.size(); i++) {
-        for (int Index = 0; Index < Model->Meshes[i].TextureReferences_.size(); Index++) { // IF TEXTURES DONT WORK, CHECK HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        for (int Index = 0; (long)Index < (long)Model->Meshes[i].TextureReferences_.size(); Index++) { // IF TEXTURES DONT WORK, CHECK HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
             int TextureIndex = Model->Meshes[i].TextureReferences_[Index];
         
