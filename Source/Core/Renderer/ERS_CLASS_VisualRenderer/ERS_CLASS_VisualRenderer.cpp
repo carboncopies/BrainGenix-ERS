24672d0dbd2cdd39d0937e27b127f516a7458e23
datacrystals <tliao@carboncopies.org>
2022-03-06 07:06:54 +0000

    Debug Gizmo Issues With Multiple Viewports (#7)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.cpp
@@ -291,6 +291,9 @@ void ERS_CLASS_VisualRenderer::UpdateViewport(int Index, ERS_CLASS_SceneManager*
 
         if (DrawCursor) {
             Cursors3D_->Draw(Viewports_[Index]->Camera.get(), CaptureCursor_, Viewports_[Index]->ShowCube, Viewports_[Index]->GizmoEnabled);
+        } else {
+            Cursors3D_->Draw(Viewports_[Index]->Camera.get(), false, Viewports_[Index]->ShowCube, false);
+
         }
 
     }
