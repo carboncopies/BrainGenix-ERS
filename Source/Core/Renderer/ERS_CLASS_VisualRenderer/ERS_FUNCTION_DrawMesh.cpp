15f99eb1da8ef6202a5b08cabeb3daaacb7b5085
datacrystals <tliao@carboncopies.org>
2022-03-11 23:15:11 +0000

    Refactor Rendering System  (#111)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_DrawMesh.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_DrawMesh.cpp
@@ -3,3 +3,10 @@
 //======================================================================//
 
 #include <ERS_FUNCTION_DrawMesh.h>
+
+
+void ERS_FUNCTION_DrawMesh(ERS_STRUCT_Mesh* Mesh, ERS_STRUCT_OpenGLDefaults* OpenGLDefaults, std::shared_ptr<ERS_STRUCT_Shader> Shader) {
+
+    
+
+}
--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_DrawMesh.h
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_DrawMesh.h
@@ -26,4 +26,4 @@
  * @param OpenGLDefaults 
  * @param Shader 
  */
-void Draw(ERS_STRUCT_Mesh* Mesh, ERS_STRUCT_OpenGLDefaults* OpenGLDefaults, std::shared_ptr<ERS_STRUCT_Shader> Shader);
+void ERS_FUNCTION_DrawMesh(ERS_STRUCT_Mesh* Mesh, ERS_STRUCT_OpenGLDefaults* OpenGLDefaults, std::shared_ptr<ERS_STRUCT_Shader> Shader);
