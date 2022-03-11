472112a8117703afd781637fa20ed3cad447b0bd
datacrystals <tliao@carboncopies.org>
2022-03-11 22:07:04 +0000

    Implement Transparency (#57)

---
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_MeshRenderer.h
@@ -0,0 +1,63 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <iostream>
+#include <string>
+#include <chrono>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_CLASS_LoggingSystem.h>
+#include <ERS_CLASS_ShaderLoader.h>
+#include <ERS_CLASS_InputProcessor.h>
+#include <ERS_CLASS_Framebuffer.h>
+#include <ERS_CLASS_Grid.h>
+#include <ERS_CLASS_LightIconRenderer.h>
+
+#include <ERS_SceneManager.h>
+#include <ERS_SceneLoader.h>
+
+#include <ERS_Editor_3DCursor.h>
+
+#include <ERS_STRUCT_LocRotScale.h>
+#include <ERS_STRUCT_SystemUtils.h>
+#include <ERS_STRUCT_Viewport.h>
+#include <ERS_STRUCT_Shader.h>
+#include <ERS_STRUCT_Model.h>
+#include <ERS_STRUCT_Camera.h>
+#include <ERS_STRUCT_OpenGLDefaults.h>
+
+
+
+/**
+ * @brief This class handles extracting, sorting, and rendering all meshes in a scene.
+ * 
+ */
+class ERS_CLASS_MeshRenderer {
+
+private:
+
+    ERS_STRUCT_SystemUtils* SystemUtils_; /**<Pointer to system utils struct*/
+    
+
+public:
+
+    ERS_CLASS_MeshRenderer(ERS_STRUCT_SystemUtils* SystemUtils);
+    ~ERS_CLASS_MeshRenderer();
+
+
+};
