2fb67695e9b9f85df9627d8b12f62b69d47dd245
datacrystals <tliao@carboncopies.org>
2022-03-11 22:59:10 +0000

    Implement Transparency (#57)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_MeshTransparencySort.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_MeshTransparencySort.cpp
@@ -2,7 +2,7 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-#include <ERS_FUCNTION_MeshTransparencySort.h>
+#include <ERS_FUNCTION_MeshTransparencySort.h>
 
 
 void ERS_FUNCTION_MeshTransparencySort(std::vector<ERS_STRUCT_Mesh*>* OpaqueMeshes, std::vector<ERS_STRUCT_Mesh*>* TransparentMeshes, ERS_STRUCT_Scene* Scene) {
