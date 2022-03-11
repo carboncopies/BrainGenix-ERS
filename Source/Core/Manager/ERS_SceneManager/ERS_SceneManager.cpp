d96fa91fdba7a46b4ee6aa1db2e7f5b0035736d7
datacrystals <tliao@carboncopies.org>
2022-03-11 22:52:52 +0000

    Implement Transparency (#57)

--- Source/Core/Manager/ERS_SceneManager/ERS_SceneManager.cpp
+++ Source/Core/Manager/ERS_SceneManager/ERS_SceneManager.cpp
@@ -42,14 +42,6 @@ bool ERS_CLASS_SceneManager::AddScene(ERS_STRUCT_Scene Scene) {
 
 }
 
-void ERS_CLASS_SceneManager::Render(ERS_STRUCT_OpenGLDefaults* OpenGLDefaults, std::shared_ptr<ERS_STRUCT_Shader> Shader) {
-    
-
-
-
-
-}
-
 bool ERS_CLASS_SceneManager::SetActiveScene(int SceneIndex) {
 
     // Check SceneIndex Validity
