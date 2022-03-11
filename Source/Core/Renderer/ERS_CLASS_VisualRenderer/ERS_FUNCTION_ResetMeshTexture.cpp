a176d948893c2641e54d1eda124dd2235c3ffd98
datacrystals <tliao@carboncopies.org>
2022-03-11 23:18:28 +0000

    Refactor Rendering System  (#111)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_ResetMeshTexture.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_ResetMeshTexture.cpp
@@ -3,3 +3,12 @@
 //======================================================================//
 
 #include <ERS_FUNCTION_ResetMeshTexture.h>
+
+
+void ERS_FUNCTION_ResetMeshTexture(const char* Name, int Offset, unsigned int ShaderProgram, unsigned int TextureID) {
+
+    glUniform1i(glGetUniformLocation(ShaderProgram, Name), Offset);
+    glActiveTexture(GL_TEXTURE0 + Offset);
+    glBindTexture(GL_TEXTURE_2D, TextureID);
+
+}
--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_ResetMeshTexture.h
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_ResetMeshTexture.h
@@ -16,3 +16,14 @@
 
 #include <ERS_STRUCT_Shader.h>
 #include <ERS_STRUCT_Model.h>
+
+
+/**
+ * @brief Binds to the given texture using the given shader program.
+ * 
+ * @param Name 
+ * @param Offset 
+ * @param ShaderProgram 
+ * @param TextureID 
+ */
+void ERS_FUNCTION_ResetMeshTexture(const char* Name, int Offset, unsigned int ShaderProgram, unsigned int TextureID);
