083a71f84042dadaf9c5f8dea9b65a72ad04fb6f
datacrystals <tliao@carboncopies.org>
2022-03-11 06:15:31 +0000

    Implement Transparency (#57)

--- Source/Core/Structures/ERS_STRUCT_Mesh/ERS_STRUCT_Mesh.h
+++ Source/Core/Structures/ERS_STRUCT_Mesh/ERS_STRUCT_Mesh.h
@@ -60,7 +60,7 @@ private:
 public:
 
     std::vector<ERS_STRUCT_Vertex> Vertices; /**<Array of Vertices*/
-    std::vector<unsigned int> Indices; /**<Array of Indices*/
+    std::vector<unsigned int> Indices; /**<Array of Vertex Indices*/
     long NumberIndices; /**<Size of Indices Array*/
     unsigned int VAO; /**<Vertex Array Object OpenGL Handle*/
 
