4cdab3eb085e1b27305c5df14f752d0e3e5bb2b9
datacrystals <tliao@carboncopies.org>
2022-03-12 02:15:14 +0000

    Refactor Rendering System  (#111)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_MeshRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_MeshRenderer.cpp
@@ -32,8 +32,6 @@ void ERS_CLASS_MeshRenderer::RenderScene(ERS_STRUCT_Scene* Scene, ERS_STRUCT_Ope
     for (unsigned long i = 0; i < OpaqueMeshes.size(); i++) {
         glBindTexture(GL_TEXTURE_2D, OpenGLDefaults->DefaultTexture_);
         glActiveTexture(OpenGLDefaults->DefaultTexture_);
-        Shader->SetMat4("model", OpaqueMeshes[i]->ModelMatrix);
-        //OpaqueMeshes[i]->Draw(OpenGLDefaults, Shader);
         ERS_FUNCTION_DrawMesh(OpaqueMeshes[i], OpenGLDefaults, Shader);
     }
 
