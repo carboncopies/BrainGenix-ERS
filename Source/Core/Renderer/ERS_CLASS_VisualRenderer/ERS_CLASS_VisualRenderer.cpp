066c2f996919c25fe3312258f8686433e31f93d6
datacrystals <tliao@carboncopies.org>
2022-03-31 23:41:31 +0000

    Add Ram Usage Graph (#114)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.cpp
@@ -355,6 +355,20 @@ void ERS_CLASS_VisualRenderer::CreateViewport() {
 
 }
 
+
+long ERS_CLASS_VisualRenderer::FindShaderIndex(std::string Name) {
+
+    long Index = 0;
+    for (unsigned long i = 0; i < Shaders_.size(); i++) {
+        if (Shaders_[i]->InternalName == Name) {
+            Index = i;
+            break;
+        }
+    }
+
+}
+
+
 void ERS_CLASS_VisualRenderer::CreateViewport(std::string ViewportName) {
 
 
