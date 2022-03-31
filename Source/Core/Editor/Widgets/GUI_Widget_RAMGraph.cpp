880fb8e88851aa9deac1bf4d058c0622e1278416
datacrystals <tliao@carboncopies.org>
2022-03-31 23:54:12 +0000

    Add Ram Usage Graph (#114)

--- Source/Core/Editor/Widgets/GUI_Widget_RAMGraph.cpp
+++ Source/Core/Editor/Widgets/GUI_Widget_RAMGraph.cpp
@@ -21,7 +21,7 @@ void Widget_RAMGraph::Draw() {
     // Update Data In Vectors
     ERS_STRUCT_HardwareInfo HWInfo = SystemUtils_->HardwareInformation_->GetHWInfo();
     TotalRAM_.push_back(HWInfo.Dynamic_.PhysicalMemoryCapacity / 1073741824.0);
-    FreeRAM_.push_back(WInfo.Dynamic_.PhysicalMemoryFree / 1073741824.0);
+    FreeRAM_.push_back(HWInfo.Dynamic_.PhysicalMemoryFree / 1073741824.0);
     TotalSwap_.push_back(HWInfo.Dynamic_.SwapCapacity / 1073741824.0);
     FreeSwap_.push_back(HWInfo.Dynamic_.SwapFree / 1073741824.0);
 
