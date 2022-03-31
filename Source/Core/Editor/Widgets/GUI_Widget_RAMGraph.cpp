8f04cfcbec1895b11bb4e8cbcb620cdaeba422cd
datacrystals <tliao@carboncopies.org>
2022-03-31 23:23:26 +0000

    Add Ram Usage Graph (#114)

--- Source/Core/Editor/Widgets/GUI_Widget_RAMGraph.cpp
+++ Source/Core/Editor/Widgets/GUI_Widget_RAMGraph.cpp
@@ -49,10 +49,10 @@ void Widget_RAMGraph::Draw() {
 
 
                     if (PlotVisible) {
-                        ImPlot::PlotLine("Total RAM", (const float*)TotalRAM_.data(), TotalRAM_.size());
-                        ImPlot::PlotLine("Used RAM", (const float*)UsedRAM_.data(), UsedRAM_.size());
-                        ImPlot::PlotLine("Total Swap", (const float*)TotalSwap_.data(), TotalSwap_.size());
-                        ImPlot::PlotLine("Used Swap", (const float*)UsedSwap_.data(), UsedSwap_.size());
+                        ImPlot::PlotLine("Total RAM (GB)", (const float*)TotalRAM_.data(), TotalRAM_.size());
+                        ImPlot::PlotLine("Used RAM (GB)", (const float*)UsedRAM_.data(), UsedRAM_.size());
+                        ImPlot::PlotLine("Total Swap (GB)", (const float*)TotalSwap_.data(), TotalSwap_.size());
+                        ImPlot::PlotLine("Used Swap (GB)", (const float*)UsedSwap_.data(), UsedSwap_.size());
 
 
                         ImPlot::EndPlot();
