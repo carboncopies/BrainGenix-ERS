b633fafac7997dead33b9bdeb6a0055243bcc9b4
datacrystals <tliao@carboncopies.org>
2022-04-02 21:48:45 +0000

    Debug Texture Reference Mapping (#116)

--- Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
@@ -485,7 +485,6 @@ void ERS_CLASS_ModelLoader::LoadMaterialTextures(std::vector<int>* IDs, std::vec
         // Search Texture List For Index Of Same Match, Add -1 If Not Found
         int Index = -1;
         for (unsigned long x = 0; x < TextureList.size(); x++) {
-            std::cout<<TextureList[x]<<" | "<<TextureIdentifier<<std::endl;
             if (TextureList[x] == TextureIdentifier) {
                 Index = x;
                 break;
@@ -497,6 +496,8 @@ void ERS_CLASS_ModelLoader::LoadMaterialTextures(std::vector<int>* IDs, std::vec
 
             for (unsigned long x = 0; x < TextureList.size(); x++) {
                 if (TextureList[x] == TextureIdentifier.substr(TextureIdentifier.find_last_of("/"), TextureIdentifier.length()-1)) {
+                std::cout<<TextureList[x]<<" | "<<TextureIdentifier.substr(TextureIdentifier.find_last_of("/"), TextureIdentifier.length()-1)<<std::endl;
+
                     Index = x;
                     break;
                 }
