2edb95183c721afb023419418a0df5fc051a9997
datacrystals <tliao@carboncopies.org>
2022-04-03 00:48:20 +0000

    Add PointLight SceneTree Context Menu  (#117)

---
+++ Source/Core/Editor/Windows/GUI_Window_SceneTree/ERS_GUI_CLASS_DeletePointLight.cpp
@@ -0,0 +1,71 @@
+
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#include <ERS_GUI_CLASS_DeletePointLight.h>
+
+
+Subwindow_DeletePointLight::Subwindow_DeletePointLight(ERS_CLASS_SceneManager* SceneManager) {
+
+    SceneManager_ = SceneManager;
+
+
+}
+
+Subwindow_DeletePointLight::~Subwindow_DeletePointLight() {
+
+}
+
+void Subwindow_DeletePointLight::DeletePointLight(int SceneIndex, int PointLightIndex) {
+
+    FirstFrame_ = true;
+
+    // Show Popup Windows
+    SceneIndex_ = SceneIndex;
+    PointLightIndex_ = PointLightIndex;
+    ShowDeleteConfirm_ = true;
+    
+}
+
+void Subwindow_DeletePointLight::Draw() {
+
+    UpdateConfirmDeletePopup();
+
+}
+
+void Subwindow_DeletePointLight::UpdateConfirmDeletePopup() {
+
+    if (ShowDeleteConfirm_) {
+    ImGui::Begin("Delete PointLight?", &ShowDeleteConfirm_, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoCollapse);
+
+        // Grab Window Focus
+        ImGui::SetWindowFocus();
+        
+        // Set Default Keyboard Input
+        if (FirstFrame_) {
+            ImGui::SetKeyboardFocusHere(0);
+            FirstFrame_ = false;
+        }
+
+        // Add Confirm Message
+        ImGui::SetItemDefaultFocus();
+        ImGui::Text("This action will delete the selected PointLight.");
+        ImGui::Separator();
+
+        // Confirm And Abort Buttons
+        if (ImGui::Button("Confirm", ImVec2(120, 0)) || ImGui::IsKeyPressed(GLFW_KEY_ENTER)) { // If Button Pressed, Or Enter Key Pressed
+            SceneManager_->Scenes_[SceneIndex_]->PointLights.erase(SceneManager_->Scenes_[SceneIndex_]->PointLights.begin() + PointLightIndex_);
+            ShowDeleteConfirm_ = false;
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Abort", ImVec2(120, 0)) || ImGui::IsKeyPressed(GLFW_KEY_ESCAPE)) { // If Button Pressed, Or Escape Key Pressed
+            ShowDeleteConfirm_ = false;
+        }
+
+    ImGui::End();
+
+
+    }
+
+}
