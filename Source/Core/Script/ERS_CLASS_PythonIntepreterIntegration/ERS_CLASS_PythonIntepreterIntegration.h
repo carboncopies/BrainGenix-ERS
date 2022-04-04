ee7486dcbe3cb82baa90a33270c59e5df479848e
datacrystals <tliao@carboncopies.org>
2022-04-04 03:32:15 +0000

    Integrate Python Scripting Language (#87)

---
+++ Source/Core/Script/ERS_CLASS_PythonIntepreterIntegration/ERS_CLASS_PythonIntepreterIntegration.h
@@ -0,0 +1,13 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <vector>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
