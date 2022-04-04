91dff53b210479013c71cf656f44a483d2e89691
datacrystals <tliao@carboncopies.org>
2022-04-04 03:55:55 +0000

    Integrate Python Scripting Language (#87)

--- Source/Core/Script/ERS_CLASS_PythonInterpreterIntegration/ERS_CLASS_PythonInterpreterIntegration.cpp
+++ Source/Core/Script/ERS_CLASS_PythonInterpreterIntegration/ERS_CLASS_PythonInterpreterIntegration.cpp
@@ -15,7 +15,7 @@ ERS_CLASS_PythonInterpreterIntegration::ERS_CLASS_PythonInterpreterIntegration(E
 
     // Start Python Interpreter
     Logger_->Log("Starting Python Interpreter", 5);
-     guard{};
+    Guard_ = std::make_unique<pybind11::scoped_interpreter>();
     Logger_->Log("Finished Starting Python Interpreter", 4);
     
 
