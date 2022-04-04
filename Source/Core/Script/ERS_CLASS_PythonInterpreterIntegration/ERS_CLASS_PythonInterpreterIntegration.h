76c38f32590550cabcd981c88917d33f5b242b7b
datacrystals <tliao@carboncopies.org>
2022-04-04 03:58:49 +0000

    Integrate Python Scripting Language (#87)

--- Source/Core/Script/ERS_CLASS_PythonInterpreterIntegration/ERS_CLASS_PythonInterpreterIntegration.h
+++ Source/Core/Script/ERS_CLASS_PythonInterpreterIntegration/ERS_CLASS_PythonInterpreterIntegration.h
@@ -16,6 +16,9 @@
 #include <ERS_CLASS_LoggingSystem.h>
 
 
+// Clear warnings about visibility issues
+#pragma GCC visibility push(hidden)
+
 /**
  * @brief This class hosts the python Interpreter, all other python systems use this class to run their python code.
  * 
@@ -45,3 +48,7 @@ public:
 
 
 };
+
+
+// Re-enable warnings about visibility
+#pragma GCC visibility pop
