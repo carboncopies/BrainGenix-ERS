2c8eccf7d6e66db3d32741c5b74b41314455b74a
datacrystals <tliao@carboncopies.org>
2022-04-07 02:28:16 +0000

    Add Script Asset Type (#124)

--- Source/Core/Writers/ERS_SceneWriter/ERS_SceneWriter.cpp
+++ Source/Core/Writers/ERS_SceneWriter/ERS_SceneWriter.cpp
@@ -87,9 +87,8 @@ std::string SceneWriter::ProcessScene(ERS_STRUCT_Scene* InputScene) {
 
         Output<<YAML::Key<<"AttachedScripts";
         Output<<YAML::Key<<YAML::BeginMap;
-        std::cout<<i<<"|"<<InputScene->Models[i]->AttachedScriptIndexes_.size()<<std::endl;
-        for (unsigned long i = 0; i < InputScene->Models[i]->AttachedScriptIndexes_.size(); i++) {
-            Output<<YAML::Key<<i<<YAML::Value<<InputScene->Models[i]->AttachedScriptIndexes_[i];
+        for (unsigned long x = 0; x < InputScene->Models[i]->AttachedScriptIndexes_.size(); x++) {
+            Output<<YAML::Key<<x<<YAML::Value<<InputScene->Models[i]->AttachedScriptIndexes_[x];
         }
         Output<<YAML::EndMap;
 
