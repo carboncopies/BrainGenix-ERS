d29a39ba3b7f46ab019308a3faf3a3650c3370ef
datacrystals <tliao@carboncopies.org>
2022-04-07 01:16:39 +0000

    Add Script Asset Type (#124)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.cpp
@@ -848,7 +848,7 @@ void ERS_CLASS_VisualRenderer::DrawViewportMenu(int Index, ERS_CLASS_SceneManage
                 NewScript.AssetID = SystemUtils_->ERS_IOSubsystem_->AllocateAssetID();
                 NewScript.Code_ = "";
                 NewScript.Name_ = "Untitled Script";
-
+                std::cout<<NewScript.AssetID<<std::endl;
                 ProjectUtils_->ProjectManager_->Project_.Scripts.push_back(NewScript);
             
             }
