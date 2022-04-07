8b5ded749f7ff0c74286481511dc3dcb3af7ed4d
datacrystals <tliao@carboncopies.org>
2022-04-07 02:15:36 +0000

    Add Script Asset Type (#124)

--- Source/Core/Loader/ERS_SceneLoader/ERS_SceneLoader.cpp
+++ Source/Core/Loader/ERS_SceneLoader/ERS_SceneLoader.cpp
@@ -103,6 +103,16 @@ ERS_STRUCT_Scene ERS_CLASS_SceneLoader::ProcessScene(YAML::Node RawSceneData, lo
             Scene.Models[CurrentSize-1]->Name = AssetName;
             Scene.Models[CurrentSize-1]->FlipTextures = FlipTextures;
 
+
+            // Load Attached Scripts
+            if (SceneDataNode[i]["AttachedScripts"]) {
+                YAML::Node Scripts = SceneDataNode[i]["AttachedScripts"];
+                for (YAML::const_iterator it=Scripts.begin(); it!=Scripts.end(); ++it) {
+                    Scene.Models[CurrentSize-1]->AttachedScriptIndexes_.push_back(it->second.as<long>());
+                }
+            }
+
+
         } else if (AssetType == std::string("DirectionalLight")) {
 
             // Setup Model Pointer In Scene To Work On
