61105b97b5bdc6a409b6a3e1e419e19ff66cccd1
datacrystals <tliao@carboncopies.org>
2022-04-07 03:45:39 +0000

    Implement System To Load Scripts (#125)

--- Source/Core/Loader/ERS_ProjectLoader/ERS_ProjectLoader.cpp
+++ Source/Core/Loader/ERS_ProjectLoader/ERS_ProjectLoader.cpp
@@ -122,7 +122,6 @@ ERS_STRUCT_Project ERS_CLASS_ProjectLoader::LoadProject(long AssetID) {
         SystemUtils_->ERS_IOSubsystem_->ReadAsset(Script->AssetID, ScriptData.get());
         Script->Code_ = std::string((const char*)ScriptData->Data.get());
 
-
         LogStr = std::string("Loaded Script '") + Script->Name_ + std::string("'");
         SystemUtils_->Logger_->Log(LogStr, 3);
     }
