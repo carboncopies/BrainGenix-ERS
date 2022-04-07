b10471095d72b890dfdbf777b2dd8a399b4d54bd
datacrystals <tliao@carboncopies.org>
2022-04-07 02:18:49 +0000

    Add Script Asset Type (#124)

--- Source/Core/Writers/ERS_SceneWriter/ERS_SceneWriter.cpp
+++ Source/Core/Writers/ERS_SceneWriter/ERS_SceneWriter.cpp
@@ -85,6 +85,14 @@ std::string SceneWriter::ProcessScene(ERS_STRUCT_Scene* InputScene) {
         Output << YAML::Key << "FlipTextures" << YAML::Value << InputScene->Models[i]->FlipTextures;
 
 
+        Output<<YAML::Key<<"AttachedScripts";
+        Output<<YAML::Key<<YAML::BeginMap;
+        for (unsigned long i = 0; i < InputScene->Models[i]->AttachedScriptIndexes_.size(); i++) {
+            Output<<YAML::Key<<i<<YAML::Value<<InputScene->Models[i]->AttachedScriptIndexes_[i];
+        }
+        Output<<YAML::EndMap;
+
+
         Output << YAML::EndMap;
         AssetIndex++;
     }
