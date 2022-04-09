2f4bb3442696780c7fe6f4d8e5e6a00500f47741
datacrystals <tliao@carboncopies.org>
2022-04-09 21:33:51 +0000

    Finalize Scripting Integration (#136)

--- Source/Core/Editor/Widgets/GUI_Widget_ObjectProperties.cpp
+++ Source/Core/Editor/Widgets/GUI_Widget_ObjectProperties.cpp
@@ -28,17 +28,11 @@ void Widget_ObjectProperties::Draw() {
 
 
                 // LocRotScale Properties
-                ImGui::Spacing();
-                ImGui::Spacing();
-                ImGui::Separator();
-                ImGui::TextColored(ImVec4(0.2f, 1.0f, 0.2f, 1.0f), "Physical Parameters");
-                ImGui::Separator();
-                ImGui::Spacing();
-                ImGui::Spacing();
-                ImGui::DragFloat3("Location", (float*)glm::value_ptr(Cursors3D_->Pos_), 0.05f);
-                ImGui::DragFloat3("Rotation", (float*)glm::value_ptr(Cursors3D_->Rot_), 0.05f);// FIXME: MAKE ROLL OVER TO 180 Degrees?
-                ImGui::DragFloat3("Scale", (float*)glm::value_ptr(Cursors3D_->Scale_), 0.05f, 0.0f, 65535.0f);
-
+                if (ImGui::CollapsingHeader("Physical Parameters")) {
+                    ImGui::DragFloat3("Location", (float*)glm::value_ptr(Cursors3D_->Pos_), 0.05f);
+                    ImGui::DragFloat3("Rotation", (float*)glm::value_ptr(Cursors3D_->Rot_), 0.05f);// FIXME: MAKE ROLL OVER TO 180 Degrees?
+                    ImGui::DragFloat3("Scale", (float*)glm::value_ptr(Cursors3D_->Scale_), 0.05f, 0.0f, 65535.0f);
+                }
             
                 // Handle Extra Options For Lights
                 unsigned long SelectedSceneObject = SceneManager_->Scenes_[SceneManager_->ActiveScene_]->SelectedObject;
