9b26aab9abff97a56877417afb786e3495461bd4
datacrystals <tliao@carboncopies.org>
2022-04-10 01:12:58 +0000

    Resolve ImGuiFileDialog Issues On Windows (#140)

--- CMake/BuildUtils/ImGuiFileDialog.cmake
+++ CMake/BuildUtils/ImGuiFileDialog.cmake
@@ -3,6 +3,6 @@
 ##########################
 
 ERSBuildLogger(${Green} "Configuring ImGuiFileDialog Library")
-add_subdirectory(${LIB_DIR}/ImGuiFileDialog/ImGuiFileDialog)
+add_subdirectory(${LIB_DIR}/ImGuiFileDialog)
 disable_target_warnings(ImGuiFileDialog)
 ERSBuildLogger(${BoldGreen} "Finished Configuring ImGuiFileDialog Library")
