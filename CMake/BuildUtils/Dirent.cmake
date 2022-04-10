d473c38dd7045b1362b1a48c002dfdd56130dd00
datacrystals <tliao@carboncopies.org>
2022-04-10 00:32:07 +0000

    Resolve ImGuiFileDialog Issues On Windows (#140)

--- CMake/BuildUtils/Dirent.cmake
+++ CMake/BuildUtils/Dirent.cmake
@@ -4,5 +4,4 @@
 
 ERSBuildLogger(${Green} "Configuring Dirent Library")
 add_subdirectory(${LIB_DIR}/dirent)
-disable_target_warnings(Dirent)
 ERSBuildLogger(${BoldGreen} "Finished Configuring Dirent Library")
