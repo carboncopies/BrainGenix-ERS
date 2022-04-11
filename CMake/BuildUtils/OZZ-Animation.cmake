1a8e0020f48975a455fa0e677ef9f9bb3748bc28
datacrystals <tliao@carboncopies.org>
2022-04-11 23:02:54 +0000

    Add Ozz-Animation Library (#89)

--- CMake/BuildUtils/OZZ-Animation.cmake
+++ CMake/BuildUtils/OZZ-Animation.cmake
@@ -4,5 +4,5 @@
 
 ERSBuildLogger(${Green} "Configuring OZZ-Animation Library")
 add_subdirectory(${LIB_DIR}/ozz-animation)
-disable_target_warnings(ozz-animation)
+disable_target_warnings(ozz_animation)
 ERSBuildLogger(${BoldGreen} "Finished Configuring OZZ-Animation Library")
