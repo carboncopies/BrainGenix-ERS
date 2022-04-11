9ed0906796f3c90d07beb8d82589a2edb3d646f1
datacrystals <tliao@carboncopies.org>
2022-04-11 21:36:31 +0000

    Fix Windows Linker Errors (#150)

--- Source/Core/Structures/ERS_STRUCT_Texture/ERS_STRUCT_Texture.cpp
+++ Source/Core/Structures/ERS_STRUCT_Texture/ERS_STRUCT_Texture.cpp
@@ -2,34 +2,4 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-#pragma once
-
-
-
-// Standard Libraries (BG convention: use <> instead of "")
-#include <string>
-
-// Third-Party Libraries (BG convention: use <> instead of "")
-#include <FreeImage.h>
-
-// Internal Libraries (BG convention: use <> instead of "")
-
-
-// Structure To Hold 2D ERS Texture
-struct ERS_STRUCT_Texture {
-
-    // Set OpenGL Handles
-    unsigned int ID;
-    
-    // Set ImageData For Deferred OpenGL Loading
-    float Channels;
-    float Width;
-    float Height;
-    FIBITMAP* ImageData;
-    bool HasImageData = false;
-
-    // Set Metadata
-    std::string Type;
-    std::string Path;
-
-};
+#include <ERS_STRUCT_Texture.h>
