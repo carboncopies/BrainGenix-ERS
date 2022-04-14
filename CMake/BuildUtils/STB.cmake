b0d46972395c3c96d1074a965337f3c4cba4f0ad
datacrystals <tliao@carboncopies.org>
2022-04-14 22:44:30 +0000

    Fix DevIL CMake Warnings  (#172)

--- CMake/BuildUtils/STB.cmake
+++ CMake/BuildUtils/STB.cmake
@@ -2,6 +2,6 @@
 # Builds Library For ERS #
 ##########################
 
-ERSBuildLogger(${Green} "Configuring DevIL Library")
-add_subdirectory(${LIB_DIR}/DevIL/DevIL)
-ERSBuildLogger(${BoldGreen} "Finished Configuring DevIL Library")
+ERSBuildLogger(${Green} "Configuring STB Library")
+add_subdirectory(${LIB_DIR}/STB)
+ERSBuildLogger(${BoldGreen} "Finished Configuring STB Library")
