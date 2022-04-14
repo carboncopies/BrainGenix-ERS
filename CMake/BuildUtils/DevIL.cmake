14acd1af0a6e3ea4497e12bcc6a757d954143488
datacrystals <tliao@carboncopies.org>
2022-04-14 22:17:55 +0000

    Add DevIL Image Lib For Fallback Loading (#172)

--- CMake/BuildUtils/DevIL.cmake
+++ CMake/BuildUtils/DevIL.cmake
@@ -4,5 +4,4 @@
 
 ERSBuildLogger(${Green} "Configuring DevIL Library")
 add_subdirectory(${LIB_DIR}/DevIL/DevIL)
-disable_target_warnings(DevIL)
 ERSBuildLogger(${BoldGreen} "Finished Configuring DevIL Library")
