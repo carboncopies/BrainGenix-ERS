1cb7adcbccc2b436947178014e7909bf05ca7f30
datacrystals <tliao@carboncopies.org>
2022-04-16 01:00:03 +0000

    Implement PBR Shaders (#171)

--- Source/Core/Structures/ERS_STRUCT_Light/ERS_STRUCT_PointLight.h
+++ Source/Core/Structures/ERS_STRUCT_Light/ERS_STRUCT_PointLight.h
@@ -19,15 +19,11 @@ struct ERS_STRUCT_PointLight {
 
     std::string UserDefinedName; /**Name of the light assigned by user*/
     
-    float RolloffConstant; /**<Rolloff constant*/
-    float RolloffLinear; /**<Rolloff constant*/
-    float RolloffQuadratic; /**<Rolloff constant*/
+    float Intensity; /**<Intensity of the light*/
 
     std::vector<long> AttachedScriptIndexes_; /**<Indexes of attached scripts (index in the project struct's list of scripts)*/
 
-    glm::vec3 Ambient; /**<Ambient color of the light*/
-    glm::vec3 Diffuse; /**<Ambient color of the light*/
-    glm::vec3 Specular; /**<Ambient color of the light*/
+    glm::vec3 Color; /**<Color of the light*/
 
     glm::vec3 Pos; /**<Position*/
 
