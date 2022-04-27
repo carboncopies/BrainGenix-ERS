a9011845a4689a3bcbaddadb5b23cc4fbf158a88
datacrystals <tliao@carboncopies.org>
2022-04-27 09:31:43 +0000

    Debug Rotation Gizmo (#6)

--- Source/Core/Editor/Utils/ERS_Editor_3DCursor/ERS_Editor_3DCursor.cpp
+++ Source/Core/Editor/Utils/ERS_Editor_3DCursor/ERS_Editor_3DCursor.cpp
@@ -78,7 +78,8 @@ void Cursors3D::Draw(ERS_STRUCT_Camera* Camera, bool IsCameraMoving, bool ShowCu
 
     // Set Gizmo LocRotScale To Object LocRotScale
     float TmpMatrix[16];
-    ImGuizmo::RecomposeMatrixFromComponents((float*)glm::value_ptr(Pos_), (float*)glm::value_ptr(Rot_), (float*)glm::value_ptr(Scale_), TmpMatrix);
+    glm::vec3 GizmoInputRot = glm::vec3(0);
+    ImGuizmo::RecomposeMatrixFromComponents((float*)glm::value_ptr(Pos_), (float*)glm::value_ptr(GizmoInputRot), (float*)glm::value_ptr(Scale_), TmpMatrix);
 
 
     // Only Draw When ShowCursor Is True, Otherwise Don't Draw
@@ -98,8 +99,7 @@ void Cursors3D::Draw(ERS_STRUCT_Camera* Camera, bool IsCameraMoving, bool ShowCu
         if (CurrentGizmoOperation_ == ImGuizmo::OPERATION::TRANSLATE) {
             Pos_ = glm::vec3(TmpTranslation[0], TmpTranslation[1], TmpTranslation[2]);
         } else if (CurrentGizmoOperation_ == ImGuizmo::OPERATION::ROTATE) {
-            glm::vec3 Delta = Rot_ - glm::vec3(TmpRotation[0], TmpRotation[1], TmpRotation[2]);
-            Rot_ += Delta;
+            Rot_ += glm::vec3(TmpRotation[0], TmpRotation[1], TmpRotation[2]);
         } else if (CurrentGizmoOperation_ == ImGuizmo::OPERATION::SCALE) {
             Scale_ = glm::vec3(TmpScale[0], TmpScale[1], TmpScale[2]);
         }
