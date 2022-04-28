b4448a7c01fd96c8db66c5bd7637152307971ebe
datacrystals <tliao@carboncopies.org>
2022-04-28 00:12:51 +0000

    Improve Loading Performance (#183)

--- Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
@@ -139,7 +139,7 @@ void ERS_CLASS_ModelLoader::ProcessGPU(std::shared_ptr<ERS_STRUCT_Model> Model) 
         glBindTexture(GL_TEXTURE_2D, TextureID);
 
         // Set Texture Properties
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
