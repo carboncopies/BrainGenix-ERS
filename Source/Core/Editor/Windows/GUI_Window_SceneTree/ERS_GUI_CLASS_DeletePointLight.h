f3d1ef11ef4f523268f097f885604996bc91c31c
datacrystals <tliao@carboncopies.org>
2022-05-09 21:27:32 +0000

    Deleting Model Changes Position (#161)

--- Source/Core/Editor/Windows/GUI_Window_SceneTree/ERS_GUI_CLASS_DeletePointLight.h
+++ Source/Core/Editor/Windows/GUI_Window_SceneTree/ERS_GUI_CLASS_DeletePointLight.h
@@ -27,6 +27,9 @@ class Subwindow_DeletePointLight {
 private:
 
     ERS_CLASS_SceneManager* SceneManager_; /**<SceneManager Instance*/
+    Cursors3D* Cursor_; /**<Instance of the 3d cursor object*/
+
+    
     int SceneIndex_ = 0; /**<Scene Index Used To Remember What Scene To Delete From*/
     int PointLightIndex_ = 0; /**<Use to identify what PointLight to delete*/
 
