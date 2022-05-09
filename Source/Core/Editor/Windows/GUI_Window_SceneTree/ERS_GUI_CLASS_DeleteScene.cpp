2c4672bfa52494e34b0c3f9ed05b02598f4e680d
datacrystals <tliao@carboncopies.org>
2022-05-09 21:26:33 +0000

    Deleting Model Changes Position (#161)

--- Source/Core/Editor/Windows/GUI_Window_SceneTree/ERS_GUI_CLASS_DeleteScene.cpp
+++ Source/Core/Editor/Windows/GUI_Window_SceneTree/ERS_GUI_CLASS_DeleteScene.cpp
@@ -5,7 +5,7 @@
 #include <ERS_GUI_CLASS_DeleteScene.h>
 
 
-Subwindow_DeleteScene::Subwindow_DeleteScene(ERS_CLASS_SceneManager* SceneManager) {
+Subwindow_DeleteScene::Subwindow_DeleteScene(ERS_CLASS_SceneManager* SceneManager, Cursors3D* Cursor) {
 
     SceneManager_ = SceneManager;
 
