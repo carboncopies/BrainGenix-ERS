6afcfc18013befaf7ff4442ecc4bafc3bb0367f9
datacrystals <tliao@carboncopies.org>
2022-05-10 02:45:13 +0000

    Add Vert/Tex Count Displays (#162)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.cpp
@@ -1022,7 +1022,9 @@ void ERS_CLASS_VisualRenderer::DrawViewportOverlay(int Index, ERS_CLASS_SceneMan
             }
             
         }
-        std::string VertexMessage = std::string("Scene: ") + std::to_string(NumVerts) + std::string(" Verts, ") + std::to_string(NumIndices) + std::string(" Indices, ") + ;
+        std::string SceneMessage = std::string("Scene: ") + std::to_string(NumVerts) + std::string(" Verts, ") + std::to_string(NumIndices) + std::string(" Indices, ") + std::to_string(NumTextures) + std::string(" Textures"); // add info about number of instanced models vs real ones
+        std::string ResourcesMessage; // Contains info about actual system load (gpu ram, cpu ram, num loaded verts/indexes and num real textures on hardware)
+        std::string LoadingTimeMessage = std::string("Longest Asset Loading Time: ") + std::to_string(LongestLoadingTime) + std::string(" Seconds");
 
         ImGui::Text("A Test Message!");
 
