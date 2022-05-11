74d50e7c8805d386792f3e93c33a291eac283160
datacrystals <tliao@carboncopies.org>
2022-05-11 04:09:23 +0000

    Implement Grid Snapping For LRS (#200)

--- Source/Core/Editor/Utils/ERS_Editor_3DCursor/ERS_Editor_3DCursor.cpp
+++ Source/Core/Editor/Utils/ERS_Editor_3DCursor/ERS_Editor_3DCursor.cpp
@@ -96,7 +96,6 @@ void Cursors3D::Draw(ERS_STRUCT_Camera* Camera, bool IsCameraMoving, bool ShowCu
             GridSnapArray[0] = GridSnapAmountTranslate_;
             GridSnapArray[1] = GridSnapAmountTranslate_;
             GridSnapArray[2] = GridSnapAmountTranslate_;
-            std::cout<<GridSnapArray[0]<<std::endl;
         } else if (CurrentGizmoOperation_ == ImGuizmo::ROTATE) {
             GridSnapArray[0] = GridSnapAmountRotate_;
             GridSnapArray[1] = GridSnapAmountRotate_;
