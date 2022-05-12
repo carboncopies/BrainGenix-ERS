94afd35d90ba63434bcc24fb2069b420b4292321
datacrystals <tliao@carboncopies.org>
2022-05-12 02:23:51 +0000

    Add Option To Sort Scene Alphaetically (#206)

--- Source/Core/Editor/Windows/GUI_Window_SceneTree/GUI_Window_SceneTree.cpp
+++ Source/Core/Editor/Windows/GUI_Window_SceneTree/GUI_Window_SceneTree.cpp
@@ -294,15 +294,15 @@ void Window_SceneTree::DrawScene(ERS_STRUCT_Scene* Scene, int SceneIndex) {
             if (ImGui::BeginPopupContextItem()) {
 
                 if (ImGui::MenuItem("Rename")) {
-                    Subwindow_ModelRenameModal_->Activate(SceneIndex, Scene->SceneObjects_[i].Index_);
+                    Subwindow_ModelRenameModal_->Activate(SceneIndex, Scene->SceneObjects_[IndexInSceneObjects].Index_);
                 } if (ImGui::MenuItem("Duplicate")) {
-                    GUI_Windowutil_DuplicateModel(SceneManager_, SceneIndex, Scene->SceneObjects_[i].Index_);
+                    GUI_Windowutil_DuplicateModel(SceneManager_, SceneIndex, Scene->SceneObjects_[IndexInSceneObjects].Index_);
                 } if (ImGui::MenuItem("Replace All Instances")) {
-                    Subwindow_ModelReplaceModal_->Activate(SceneIndex, Scene->SceneObjects_[i].Index_);
+                    Subwindow_ModelReplaceModal_->Activate(SceneIndex, Scene->SceneObjects_[IndexInSceneObjects].Index_);
                 }
                 ImGui::Separator();
                 if (ImGui::MenuItem("Delete")) {
-                    Subwindow_DeleteModel_->DeleteModel(SceneIndex, Scene->SceneObjects_[i].Index_);
+                    Subwindow_DeleteModel_->DeleteModel(SceneIndex, Scene->SceneObjects_[IndexInSceneObjects].Index_);
                 }
 
             ImGui::EndPopup();
