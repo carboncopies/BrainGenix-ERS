ccea5108983fc30da5b34dd9c8d8fe866e890500
datacrystals <tliao@carboncopies.org>
2022-05-12 04:16:06 +0000

    Add Option To Sort Scene Alphabetically (#206)

--- Source/Core/Editor/Windows/GUI_Window_SceneTree/GUI_Window_SceneTree.cpp
+++ Source/Core/Editor/Windows/GUI_Window_SceneTree/GUI_Window_SceneTree.cpp
@@ -439,7 +439,7 @@ void Window_SceneTree::DrawScene(ERS_STRUCT_Scene* Scene, int SceneIndex) {
 
 
             // Context Menu
-            if (ImGui::BeginPopupContextItem()) {
+            if (ImGui::BeginPopupContextItem(std::string(std::string("DirectionalLight")+std::to_string(i)).c_str()))) {
 
                 if (ImGui::MenuItem("Rename")) {
                     Subwindow_DirectionalLightRenameModal_->Activate(SceneIndex, i);
