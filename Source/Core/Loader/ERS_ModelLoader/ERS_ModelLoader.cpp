67c221326bd2772c6633403da65b5b0d0c49bab9
datacrystals <tliao@carboncopies.org>
2022-05-13 14:27:13 +0000

    Fix Flickering Texture Issue

--- Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_ModelLoader.cpp
@@ -161,7 +161,7 @@ void ERS_CLASS_ModelLoader::ProcessGPU(std::shared_ptr<ERS_STRUCT_Model> Model) 
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         // Convert FIBITMAP* To Raw Image Bytes
         unsigned char* RawImageData = Model->TexturesToPushToGPU_[i].ImageBytes;
