a8594734e694045e0336d8044e791ec367e71035
datacrystals <tliao@carboncopies.org>
2022-05-17 20:37:51 +0000

    Cleanup VisualRenderer Class (#210)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ViewportOverlay.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ViewportOverlay.cpp
@@ -3,3 +3,26 @@
 //======================================================================//
 
 #include <ERS_CLASS_ViewportOverlay.h>
+
+
+ERS_CLASS_ViewportOverlay::ERS_CLASS_ViewportOverlay(ERS_STRUCT_SystemUtils* SystemUtils) {
+
+    SystemUtils_ = SystemUtils;
+
+    SystemUtils_->Logger_->Log("Initializing Viewport Overlay Subsystem", 5);
+
+}
+
+
+ERS_CLASS_ViewportOverlay::~ERS_CLASS_ViewportOverlay() {
+
+    SystemUtils_->Logger_->Log("Viewport Overlay Subsystem Destructor Invoked", 6);
+
+}
+
+
+void ERS_CLASS_ViewportOverlay::DrawOverlay(ERS_STRUCT_Viewport* Viewport) {
+
+    // 
+
+}
