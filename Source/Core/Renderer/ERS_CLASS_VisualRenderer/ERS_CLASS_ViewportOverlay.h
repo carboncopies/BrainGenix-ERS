9dce4d3e7308b4b3873db6aa1d75cf4e9f566a55
datacrystals <tliao@carboncopies.org>
2022-05-17 20:31:16 +0000

    Cleanup VisualRenderer Class (#210)

---
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ViewportOverlay.h
@@ -0,0 +1,20 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <iostream>
+#include <string>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_SceneManager.h>
+
+#include <ERS_FUNCTION_ResetMeshTexture.h>
+
+#include <ERS_STRUCT_Shader.h>
+#include <ERS_STRUCT_Model.h>
