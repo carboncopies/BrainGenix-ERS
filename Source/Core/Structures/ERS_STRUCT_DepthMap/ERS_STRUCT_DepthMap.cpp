d382ec099aed04942a85eb60cc8b96ad6696fbbc
datacrystals <tliao@carboncopies.org>
2022-05-17 22:48:11 +0000

    Implement Support For Shadow Maps (#154)

--- Source/Core/Structures/ERS_STRUCT_DepthMap/ERS_STRUCT_DepthMap.cpp
+++ Source/Core/Structures/ERS_STRUCT_DepthMap/ERS_STRUCT_DepthMap.cpp
@@ -2,4 +2,4 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-#include <ERS_STRUCT_RGBColor.h>
+#include <ERS_STRUCT_DepthMap.h>
