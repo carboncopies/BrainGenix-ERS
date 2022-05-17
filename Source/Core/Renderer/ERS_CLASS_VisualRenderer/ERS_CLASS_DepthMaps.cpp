f59891ac0d178934402ce74f7ec00df0aba31ff1
datacrystals <tliao@carboncopies.org>
2022-05-17 22:34:36 +0000

    Implement Support For Shadow Maps (#154)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
@@ -2,10 +2,10 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-#include <ERS_CLASS_DepthMap.h>
+#include <ERS_CLASS_DepthMaps.h>
 
 
-ERS_CLASS_DepthMap::ERS_CLASS_DepthMap(ERS_STRUCT_SystemUtils* SystemUtils, ERS_STRUCT_ProjectUtils* ProjectUtils) {
+ERS_CLASS_DepthMaps::ERS_CLASS_DepthMaps(ERS_STRUCT_SystemUtils* SystemUtils, ERS_STRUCT_ProjectUtils* ProjectUtils) {
 
     SystemUtils_ = SystemUtils;
     ProjectUtils_ = ProjectUtils;
@@ -15,7 +15,7 @@ ERS_CLASS_DepthMap::ERS_CLASS_DepthMap(ERS_STRUCT_SystemUtils* SystemUtils, ERS_
 }
 
 
-ERS_CLASS_DepthMap::~ERS_CLASS_DepthMap() {
+ERS_CLASS_DepthMaps::~ERS_CLASS_DepthMaps() {
 
     SystemUtils_->Logger_->Log("Viewport Overlay Subsystem Destructor Invoked", 6);
 
