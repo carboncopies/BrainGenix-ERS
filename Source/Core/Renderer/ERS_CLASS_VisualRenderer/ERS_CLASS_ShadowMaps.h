2425b2448ed88a1e42ff227aab539435088e6e44
datacrystals <tliao@carboncopies.org>
2022-05-17 20:20:36 +0000

    Implement Support For Shadow Maps (#154)

---
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ShadowMaps.h
@@ -0,0 +1,25 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <iostream>
+#include <string>
+#include <chrono>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
