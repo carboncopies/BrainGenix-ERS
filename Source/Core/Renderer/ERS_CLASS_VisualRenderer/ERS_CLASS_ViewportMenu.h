ff6bc6526a798d33e4e41234b8e75a4df50da3c9
datacrystals <tliao@carboncopies.org>
2022-05-17 21:28:00 +0000

    Cleanup VisualRenderer Class (#210)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ViewportMenu.h
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ViewportMenu.h
@@ -42,6 +42,8 @@ private:
     ERS_STRUCT_SystemUtils* SystemUtils_; /**<Pointer to systemtuils resource*/
     ERS_STRUCT_ProjectUtils* ProjectUtils_; /**<Pointer to project utils instance*/
 
+    std::map<int, std::shared_ptr<ERS_STRUCT_Shader>>* Shaders_; /**<Pointer to map containing shader information*/
+
     bool* IsEditorMode_; /**<Pointer to bool setting editor mode to enabled or disablewd*/
     float* GamestartTime_; /**<Pointer to float setting the glfw time when the game clock started*/
 
@@ -53,7 +55,7 @@ public:
      * 
      * @param SystemUtils 
      */
-    ERS_CLASS_ViewportMenu(ERS_STRUCT_SystemUtils* SystemUtils, ERS_STRUCT_ProjectUtils* ProjectUtils, float* GameStartTime, bool* IsEditorMode);
+    ERS_CLASS_ViewportMenu(ERS_STRUCT_SystemUtils* SystemUtils, ERS_STRUCT_ProjectUtils* ProjectUtils, float* GameStartTime, bool* IsEditorMode, std::map<int, std::shared_ptr<ERS_STRUCT_Shader>>* Shaders);
 
     /**
      * @brief Destroy the ers class viewportMenu object
