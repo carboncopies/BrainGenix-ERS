f7c753ba2e2d24931d427366b3b2f74960589dc8
datacrystals <tliao@carboncopies.org>
2022-05-17 21:23:34 +0000

    Cleanup VisualRenderer Class (#210)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ViewportMenu.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ViewportMenu.cpp
@@ -33,21 +33,21 @@ void ERS_CLASS_ViewportMenu::DrawMenu(ERS_STRUCT_Viewport* Viewport) {
         if (ImGui::BeginMenu("Controls")) {
 
             // Draw Controls
-            ImGui::MenuItem("Scene Info Overlay", nullptr, &Viewports_[Index]->ShowSceneInfo_);
-            ImGui::MenuItem("System Resources Overlay", nullptr, &Viewports_[Index]->ShowResourceInfo_);
-            ImGui::MenuItem("Loading Time Overlay", nullptr, &Viewports_[Index]->ShowLoadingTimeInfo_);
+            ImGui::MenuItem("Scene Info Overlay", nullptr, &Viewport->ShowSceneInfo_);
+            ImGui::MenuItem("System Resources Overlay", nullptr, &Viewport->ShowResourceInfo_);
+            ImGui::MenuItem("Loading Time Overlay", nullptr, &Viewport->ShowLoadingTimeInfo_);
 
             ImGui::Separator();
 
-            ImGui::MenuItem("Rotation Indicator", nullptr, &Viewports_[Index]->ShowCube);
-            ImGui::MenuItem("Gizmo", nullptr, &Viewports_[Index]->GizmoEnabled);
-            ImGui::MenuItem("Grid", nullptr, &Viewports_[Index]->GridEnabled);
-            ImGui::MenuItem("Light Icons", nullptr, &Viewports_[Index]->LightIcons);
+            ImGui::MenuItem("Rotation Indicator", nullptr, &Viewport->ShowCube);
+            ImGui::MenuItem("Gizmo", nullptr, &Viewport->GizmoEnabled);
+            ImGui::MenuItem("Grid", nullptr, &Viewport->GridEnabled);
+            ImGui::MenuItem("Light Icons", nullptr, &Viewport->LightIcons);
 
             ImGui::Separator();
 
-            ImGui::MenuItem("Gamma Correction", nullptr, &Viewports_[Index]->GammaCorrection);
-            ImGui::MenuItem("HDR", nullptr, &Viewports_[Index]->HDREnabled_);
+            ImGui::MenuItem("Gamma Correction", nullptr, &Viewport->GammaCorrection);
+            ImGui::MenuItem("HDR", nullptr, &Viewport->HDREnabled_);
 
 
         ImGui::EndMenu();
@@ -61,8 +61,8 @@ void ERS_CLASS_ViewportMenu::DrawMenu(ERS_STRUCT_Viewport* Viewport) {
             for (int i = 0; (long)i < (long)Shaders_.size(); i++) {
 
                 if (strcmp(Shaders_[i]->DisplayName.substr(0, 1).c_str(), "_")) {
-                    if (ImGui::Selectable(Shaders_[i]->DisplayName.c_str(), i == Viewports_[Index]->ShaderIndex)) {
-                        Viewports_[Index]->ShaderIndex = i;
+                    if (ImGui::Selectable(Shaders_[i]->DisplayName.c_str(), i == Viewport->ShaderIndex)) {
+                        Viewport->ShaderIndex = i;
                     }
                 }
 
@@ -77,32 +77,32 @@ void ERS_CLASS_ViewportMenu::DrawMenu(ERS_STRUCT_Viewport* Viewport) {
             // Grid Scale Submenu
             if (ImGui::BeginMenu("Scale")) {
 
-                if (ImGui::MenuItem("0.0625 Units", nullptr, (Viewports_[Index]->Grid->GridSize_ == 0.00125f))) {
-                    Viewports_[Index]->Grid->GridSize_ = 0.00125f;
+                if (ImGui::MenuItem("0.0625 Units", nullptr, (Viewport->Grid->GridSize_ == 0.00125f))) {
+                    Viewport->Grid->GridSize_ = 0.00125f;
                 }
 
-                if (ImGui::MenuItem("0.125 Units", nullptr, (Viewports_[Index]->Grid->GridSize_ == 0.0025f))) {
-                    Viewports_[Index]->Grid->GridSize_ = 0.0025f;
+                if (ImGui::MenuItem("0.125 Units", nullptr, (Viewport->Grid->GridSize_ == 0.0025f))) {
+                    Viewport->Grid->GridSize_ = 0.0025f;
                 }
 
-                if (ImGui::MenuItem("0.25 Units", nullptr, (Viewports_[Index]->Grid->GridSize_ == 0.005f))) {
-                    Viewports_[Index]->Grid->GridSize_ = 0.005f;
+                if (ImGui::MenuItem("0.25 Units", nullptr, (Viewport->Grid->GridSize_ == 0.005f))) {
+                    Viewport->Grid->GridSize_ = 0.005f;
                 }
 
-                if (ImGui::MenuItem("0.5 Units", nullptr, (Viewports_[Index]->Grid->GridSize_ == 0.01f))) {
-                    Viewports_[Index]->Grid->GridSize_ = 0.01f;
+                if (ImGui::MenuItem("0.5 Units", nullptr, (Viewport->Grid->GridSize_ == 0.01f))) {
+                    Viewport->Grid->GridSize_ = 0.01f;
                 }
 
-                if (ImGui::MenuItem("1 Unit", nullptr, (Viewports_[Index]->Grid->GridSize_ == 0.02f))) {
-                    Viewports_[Index]->Grid->GridSize_ = 0.02f;
+                if (ImGui::MenuItem("1 Unit", nullptr, (Viewport->Grid->GridSize_ == 0.02f))) {
+                    Viewport->Grid->GridSize_ = 0.02f;
                 }
 
-                if (ImGui::MenuItem("2.5 Units", nullptr, (Viewports_[Index]->Grid->GridSize_ == 0.05f))) {
-                    Viewports_[Index]->Grid->GridSize_ = 0.05f;
+                if (ImGui::MenuItem("2.5 Units", nullptr, (Viewport->Grid->GridSize_ == 0.05f))) {
+                    Viewport->Grid->GridSize_ = 0.05f;
                 }
 
-                if (ImGui::MenuItem("5 Units", nullptr, (Viewports_[Index]->Grid->GridSize_ == 0.1f))) {
-                    Viewports_[Index]->Grid->GridSize_ = 0.1f;
+                if (ImGui::MenuItem("5 Units", nullptr, (Viewport->Grid->GridSize_ == 0.1f))) {
+                    Viewport->Grid->GridSize_ = 0.1f;
                 }
                 
             ImGui::EndMenu();
@@ -111,24 +111,24 @@ void ERS_CLASS_ViewportMenu::DrawMenu(ERS_STRUCT_Viewport* Viewport) {
             // Line Thickness Submenu
             if (ImGui::BeginMenu("Line Thickness")) {
 
-                if (ImGui::MenuItem("0.5%", nullptr, (Viewports_[Index]->Grid->GridLineThickness_ == 0.005f))) {
-                    Viewports_[Index]->Grid->GridLineThickness_ = 0.005f;
+                if (ImGui::MenuItem("0.5%", nullptr, (Viewport->Grid->GridLineThickness_ == 0.005f))) {
+                    Viewport->Grid->GridLineThickness_ = 0.005f;
                 }
 
-                if (ImGui::MenuItem("1%", nullptr, (Viewports_[Index]->Grid->GridLineThickness_ == 0.01f))) {
-                    Viewports_[Index]->Grid->GridLineThickness_ = 0.01f;
+                if (ImGui::MenuItem("1%", nullptr, (Viewport->Grid->GridLineThickness_ == 0.01f))) {
+                    Viewport->Grid->GridLineThickness_ = 0.01f;
                 }
 
-                if (ImGui::MenuItem("2%", nullptr, (Viewports_[Index]->Grid->GridLineThickness_ == 0.02f))) {
-                    Viewports_[Index]->Grid->GridLineThickness_ = 0.02f;
+                if (ImGui::MenuItem("2%", nullptr, (Viewport->Grid->GridLineThickness_ == 0.02f))) {
+                    Viewport->Grid->GridLineThickness_ = 0.02f;
                 }
 
-                if (ImGui::MenuItem("3%", nullptr, (Viewports_[Index]->Grid->GridLineThickness_ == 0.03f))) {
-                    Viewports_[Index]->Grid->GridLineThickness_ = 0.03f;
+                if (ImGui::MenuItem("3%", nullptr, (Viewport->Grid->GridLineThickness_ == 0.03f))) {
+                    Viewport->Grid->GridLineThickness_ = 0.03f;
                 }
 
-                if (ImGui::MenuItem("4%", nullptr, (Viewports_[Index]->Grid->GridLineThickness_ == 0.04f))) {
-                    Viewports_[Index]->Grid->GridLineThickness_ = 0.04f;
+                if (ImGui::MenuItem("4%", nullptr, (Viewport->Grid->GridLineThickness_ == 0.04f))) {
+                    Viewport->Grid->GridLineThickness_ = 0.04f;
                 }
                 
             ImGui::EndMenu();
@@ -142,27 +142,27 @@ void ERS_CLASS_ViewportMenu::DrawMenu(ERS_STRUCT_Viewport* Viewport) {
 
 
                     if (ImGui::MenuItem("White")) {
-                        Viewports_[Index]->Grid->GridColor_ = glm::vec3(1.0f);
+                        Viewport->Grid->GridColor_ = glm::vec3(1.0f);
                     }
 
                     if (ImGui::MenuItem("Light Grey")) {
-                        Viewports_[Index]->Grid->GridColor_ = glm::vec3(0.75f);
+                        Viewport->Grid->GridColor_ = glm::vec3(0.75f);
                     }
 
                     if (ImGui::MenuItem("Grey")) {
-                        Viewports_[Index]->Grid->GridColor_ = glm::vec3(0.5f);
+                        Viewport->Grid->GridColor_ = glm::vec3(0.5f);
                     }
 
                     if (ImGui::MenuItem("Dark Grey")) {
-                        Viewports_[Index]->Grid->GridColor_ = glm::vec3(0.25f);
+                        Viewport->Grid->GridColor_ = glm::vec3(0.25f);
                     }
 
                     if (ImGui::MenuItem("Very Dark Grey")) {
-                        Viewports_[Index]->Grid->GridColor_ = glm::vec3(0.1f);
+                        Viewport->Grid->GridColor_ = glm::vec3(0.1f);
                     }
 
                     if (ImGui::MenuItem("Black")) {
-                        Viewports_[Index]->Grid->GridColor_ = glm::vec3(0.0f);
+                        Viewport->Grid->GridColor_ = glm::vec3(0.0f);
                     }
 
                 ImGui::EndMenu();
@@ -173,27 +173,27 @@ void ERS_CLASS_ViewportMenu::DrawMenu(ERS_STRUCT_Viewport* Viewport) {
 
 
                     if (ImGui::MenuItem("Red")) {
-                        Viewports_[Index]->Grid->GridColorX_ = glm::vec3(1.0f, 0.0f, 0.0f);
+                        Viewport->Grid->GridColorX_ = glm::vec3(1.0f, 0.0f, 0.0f);
                     }
 
                     if (ImGui::MenuItem("Green")) {
-                        Viewports_[Index]->Grid->GridColorX_ = glm::vec3(0.0f, 1.0f, 0.0f);
+                        Viewport->Grid->GridColorX_ = glm::vec3(0.0f, 1.0f, 0.0f);
                     }
 
                     if (ImGui::MenuItem("Blue")) {
-                        Viewports_[Index]->Grid->GridColorX_ = glm::vec3(0.0f, 0.0f, 1.0f);
+                        Viewport->Grid->GridColorX_ = glm::vec3(0.0f, 0.0f, 1.0f);
                     }
 
                     if (ImGui::MenuItem("Dark Grey")) {
-                        Viewports_[Index]->Grid->GridColorX_ = glm::vec3(0.25f);
+                        Viewport->Grid->GridColorX_ = glm::vec3(0.25f);
                     }
 
                     if (ImGui::MenuItem("Very Dark Grey")) {
-                        Viewports_[Index]->Grid->GridColorX_ = glm::vec3(0.1f);
+                        Viewport->Grid->GridColorX_ = glm::vec3(0.1f);
                     }
 
                     if (ImGui::MenuItem("Black")) {
-                        Viewports_[Index]->Grid->GridColorX_ = glm::vec3(0.0f);
+                        Viewport->Grid->GridColorX_ = glm::vec3(0.0f);
                     }
 
                 ImGui::EndMenu();
@@ -205,27 +205,27 @@ void ERS_CLASS_ViewportMenu::DrawMenu(ERS_STRUCT_Viewport* Viewport) {
 
 
                     if (ImGui::MenuItem("Red")) {
-                        Viewports_[Index]->Grid->GridColorZ_ = glm::vec3(1.0f, 0.0f, 0.0f);
+                        Viewport->Grid->GridColorZ_ = glm::vec3(1.0f, 0.0f, 0.0f);
                     }
 
                     if (ImGui::MenuItem("Green")) {
-                        Viewports_[Index]->Grid->GridColorZ_ = glm::vec3(0.0f, 1.0f, 0.0f);
+                        Viewport->Grid->GridColorZ_ = glm::vec3(0.0f, 1.0f, 0.0f);
                     }
 
                     if (ImGui::MenuItem("Blue")) {
-                        Viewports_[Index]->Grid->GridColorZ_ = glm::vec3(0.0f, 0.0f, 1.0f);
+                        Viewport->Grid->GridColorZ_ = glm::vec3(0.0f, 0.0f, 1.0f);
                     }
 
                     if (ImGui::MenuItem("Dark Grey")) {
-                        Viewports_[Index]->Grid->GridColorZ_ = glm::vec3(0.25f);
+                        Viewport->Grid->GridColorZ_ = glm::vec3(0.25f);
                     }
 
                     if (ImGui::MenuItem("Very Dark Grey")) {
-                        Viewports_[Index]->Grid->GridColorZ_ = glm::vec3(0.1f);
+                        Viewport->Grid->GridColorZ_ = glm::vec3(0.1f);
                     }
 
                     if (ImGui::MenuItem("Black")) {
-                        Viewports_[Index]->Grid->GridColorZ_ = glm::vec3(0.0f);
+                        Viewport->Grid->GridColorZ_ = glm::vec3(0.0f);
                     }
 
                 ImGui::EndMenu();
@@ -280,26 +280,26 @@ void ERS_CLASS_ViewportMenu::DrawMenu(ERS_STRUCT_Viewport* Viewport) {
 
             if (ImGui::BeginMenu("Translation")) {
 
-                if (ImGui::MenuItem("Disabled", nullptr, (Viewports_[Index]->GridSnapAmountTranslate_ == 0.0f))) {
-                    Viewports_[Index]->GridSnapAmountTranslate_ = 0.0f;
+                if (ImGui::MenuItem("Disabled", nullptr, (Viewport->GridSnapAmountTranslate_ == 0.0f))) {
+                    Viewport->GridSnapAmountTranslate_ = 0.0f;
                 }
 
                 ImGui::Separator();
                 
-                if (ImGui::MenuItem("0.1", nullptr, (Viewports_[Index]->GridSnapAmountTranslate_ == 0.1f))) {
-                    Viewports_[Index]->GridSnapAmountTranslate_ = 0.1f;
+                if (ImGui::MenuItem("0.1", nullptr, (Viewport->GridSnapAmountTranslate_ == 0.1f))) {
+                    Viewport->GridSnapAmountTranslate_ = 0.1f;
                 }
-                if (ImGui::MenuItem("0.25", nullptr, (Viewports_[Index]->GridSnapAmountTranslate_ == 0.25f))) {
-                    Viewports_[Index]->GridSnapAmountTranslate_ = 0.25f;
+                if (ImGui::MenuItem("0.25", nullptr, (Viewport->GridSnapAmountTranslate_ == 0.25f))) {
+                    Viewport->GridSnapAmountTranslate_ = 0.25f;
                 }
-                if (ImGui::MenuItem("0.5", nullptr, (Viewports_[Index]->GridSnapAmountTranslate_ == 0.5f))) {
-                    Viewports_[Index]->GridSnapAmountTranslate_ = 0.5f;
+                if (ImGui::MenuItem("0.5", nullptr, (Viewport->GridSnapAmountTranslate_ == 0.5f))) {
+                    Viewport->GridSnapAmountTranslate_ = 0.5f;
                 }
-                if (ImGui::MenuItem("0.75", nullptr, (Viewports_[Index]->GridSnapAmountTranslate_ == 0.75f))) {
-                    Viewports_[Index]->GridSnapAmountTranslate_ = 0.75f;
+                if (ImGui::MenuItem("0.75", nullptr, (Viewport->GridSnapAmountTranslate_ == 0.75f))) {
+                    Viewport->GridSnapAmountTranslate_ = 0.75f;
                 }
-                if (ImGui::MenuItem("1.0", nullptr, (Viewports_[Index]->GridSnapAmountTranslate_ == 1.0f))) {
-                    Viewports_[Index]->GridSnapAmountTranslate_ = 1.0f;
+                if (ImGui::MenuItem("1.0", nullptr, (Viewport->GridSnapAmountTranslate_ == 1.0f))) {
+                    Viewport->GridSnapAmountTranslate_ = 1.0f;
                 }
 
             ImGui::EndMenu();
@@ -308,29 +308,29 @@ void ERS_CLASS_ViewportMenu::DrawMenu(ERS_STRUCT_Viewport* Viewport) {
 
             if (ImGui::BeginMenu("Rotate")) {
 
-                if (ImGui::MenuItem("Disabled", nullptr, (Viewports_[Index]->GridSnapAmountRotate_ == 0.0f))) {
-                    Viewports_[Index]->GridSnapAmountRotate_ = 0.0f;
+                if (ImGui::MenuItem("Disabled", nullptr, (Viewport->GridSnapAmountRotate_ == 0.0f))) {
+                    Viewport->GridSnapAmountRotate_ = 0.0f;
                 }
 
                 ImGui::Separator();
                 
-                if (ImGui::MenuItem("1", nullptr, (Viewports_[Index]->GridSnapAmountRotate_ == 1.0f))) {
-                    Viewports_[Index]->GridSnapAmountRotate_ = 1.0f;
+                if (ImGui::MenuItem("1", nullptr, (Viewport->GridSnapAmountRotate_ == 1.0f))) {
+                    Viewport->GridSnapAmountRotate_ = 1.0f;
                 }
-                if (ImGui::MenuItem("5", nullptr, (Viewports_[Index]->GridSnapAmountRotate_ == 5.0f))) {
-                    Viewports_[Index]->GridSnapAmountRotate_ = 5.0f;
+                if (ImGui::MenuItem("5", nullptr, (Viewport->GridSnapAmountRotate_ == 5.0f))) {
+                    Viewport->GridSnapAmountRotate_ = 5.0f;
                 }
-                if (ImGui::MenuItem("10", nullptr, (Viewports_[Index]->GridSnapAmountRotate_ == 10.0f))) {
-                    Viewports_[Index]->GridSnapAmountRotate_ = 10.0f;
+                if (ImGui::MenuItem("10", nullptr, (Viewport->GridSnapAmountRotate_ == 10.0f))) {
+                    Viewport->GridSnapAmountRotate_ = 10.0f;
                 }
-                if (ImGui::MenuItem("25", nullptr, (Viewports_[Index]->GridSnapAmountRotate_ == 25.0f))) {
-                    Viewports_[Index]->GridSnapAmountRotate_ = 25.0f;
+                if (ImGui::MenuItem("25", nullptr, (Viewport->GridSnapAmountRotate_ == 25.0f))) {
+                    Viewport->GridSnapAmountRotate_ = 25.0f;
                 }
-                if (ImGui::MenuItem("45", nullptr, (Viewports_[Index]->GridSnapAmountRotate_ == 45.0f))) {
-                    Viewports_[Index]->GridSnapAmountRotate_ = 45.0f;
+                if (ImGui::MenuItem("45", nullptr, (Viewport->GridSnapAmountRotate_ == 45.0f))) {
+                    Viewport->GridSnapAmountRotate_ = 45.0f;
                 }
-                if (ImGui::MenuItem("90", nullptr, (Viewports_[Index]->GridSnapAmountRotate_ == 90.0f))) {
-                    Viewports_[Index]->GridSnapAmountRotate_ = 90.0f;
+                if (ImGui::MenuItem("90", nullptr, (Viewport->GridSnapAmountRotate_ == 90.0f))) {
+                    Viewport->GridSnapAmountRotate_ = 90.0f;
                 }
 
             ImGui::EndMenu();
@@ -340,26 +340,26 @@ void ERS_CLASS_ViewportMenu::DrawMenu(ERS_STRUCT_Viewport* Viewport) {
 
             if (ImGui::BeginMenu("Scale")) {
 
-                if (ImGui::MenuItem("Disabled", nullptr, (Viewports_[Index]->GridSnapAmountScale_ == 0.0f))) {
-                    Viewports_[Index]->GridSnapAmountScale_ = 0.0f;
+                if (ImGui::MenuItem("Disabled", nullptr, (Viewport->GridSnapAmountScale_ == 0.0f))) {
+                    Viewport->GridSnapAmountScale_ = 0.0f;
                 }
 
                 ImGui::Separator();
                 
-                if (ImGui::MenuItem("0.1", nullptr, (Viewports_[Index]->GridSnapAmountScale_ == 0.1f))) {
-                    Viewports_[Index]->GridSnapAmountScale_ = 0.1f;
+                if (ImGui::MenuItem("0.1", nullptr, (Viewport->GridSnapAmountScale_ == 0.1f))) {
+                    Viewport->GridSnapAmountScale_ = 0.1f;
                 }
-                if (ImGui::MenuItem("0.25", nullptr, (Viewports_[Index]->GridSnapAmountScale_ == 0.25f))) {
-                    Viewports_[Index]->GridSnapAmountScale_ = 0.25f;
+                if (ImGui::MenuItem("0.25", nullptr, (Viewport->GridSnapAmountScale_ == 0.25f))) {
+                    Viewport->GridSnapAmountScale_ = 0.25f;
                 }
-                if (ImGui::MenuItem("0.5", nullptr, (Viewports_[Index]->GridSnapAmountScale_ == 0.5f))) {
-                    Viewports_[Index]->GridSnapAmountScale_ = 0.5f;
+                if (ImGui::MenuItem("0.5", nullptr, (Viewport->GridSnapAmountScale_ == 0.5f))) {
+                    Viewport->GridSnapAmountScale_ = 0.5f;
                 }
-                if (ImGui::MenuItem("0.75", nullptr, (Viewports_[Index]->GridSnapAmountScale_ == 0.75f))) {
-                    Viewports_[Index]->GridSnapAmountScale_ = 0.75f;
+                if (ImGui::MenuItem("0.75", nullptr, (Viewport->GridSnapAmountScale_ == 0.75f))) {
+                    Viewport->GridSnapAmountScale_ = 0.75f;
                 }
-                if (ImGui::MenuItem("1.0", nullptr, (Viewports_[Index]->GridSnapAmountScale_ == 1.0f))) {
-                    Viewports_[Index]->GridSnapAmountScale_ = 1.0f;
+                if (ImGui::MenuItem("1.0", nullptr, (Viewport->GridSnapAmountScale_ == 1.0f))) {
+                    Viewport->GridSnapAmountScale_ = 1.0f;
                 }
 
             ImGui::EndMenu();
