1122c45735744dd6c9b1336b798611e820554d34
datacrystals <tliao@carboncopies.org>
2022-05-17 20:33:49 +0000

    Implement Support For Shadow Maps (#154)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ViewportOverlay.h
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ViewportOverlay.h
@@ -8,13 +8,38 @@
 // Standard Libraries (BG convention: use <> instead of "")
 #include <iostream>
 #include <string>
+#include <chrono>
 
 // Third-Party Libraries (BG convention: use <> instead of "")
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 
 // Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_CLASS_LoggingSystem.h>
+
 #include <ERS_SceneManager.h>
 
-#include <ERS_FUNCTION_ResetMeshTexture.h>
+#include <ERS_STRUCT_ProjectUtils.h>
+#include <ERS_STRUCT_SystemUtils.h>
+#include <ERS_STRUCT_Viewport.h>
 
-#include <ERS_STRUCT_Shader.h>
-#include <ERS_STRUCT_Model.h>
+
+
+/**
+ * @brief This class renders any ui/overlay info onto the viewport as requested by the viewport struct.
+ * 
+ */
+class ERS_CLASS_ViewportOverlay {
+
+
+    ERS_CLASS_ViewportOverlay();
+    ~ERS_CLASS_ViewportOverlay();
+
+
+};
