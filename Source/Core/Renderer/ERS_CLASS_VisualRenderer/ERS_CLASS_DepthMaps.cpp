094d2b7b7021609951d2a8f01a4d34fedace8588
datacrystals <tliao@carboncopies.org>
2022-05-17 23:05:34 +0000

    Implement Support For Shadow Maps (#154)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
@@ -28,12 +28,22 @@ ERS_STRUCT_DepthMap ERS_CLASS_DepthMaps::GenerateDepthMap(int ResolutionX, int R
 
     SystemUtils_->Logger_->Log(std::string("Creating Depth Map With Resolution Of ") + std::to_string(ResolutionX) + std::string("x") + std::to_string(ResolutionY), 5, LogEnable);
 
-
+    // Setup Struct
     ERS_STRUCT_DepthMap Output;
+    Output.ResolutionX = ResolutionX;
+    Output.ResolutionY = ResolutionY;
 
     // Generate FBO
-    
+    SystemUtils_->Logger_->Log("Generating Framebuffer Object", 4, LogEnable);
     glGenFramebuffers(1, &Output.FrameBufferObjectID);
+    SystemUtils_->Logger_->Log("Generated Framebuffer Object", 3, LogEnable);
+
+    // Generate Depth Map Texture (What We Render The Depth Map To)
+    SystemUtils_->Logger_->Log("Creating Depth Map Texture", 4, LogEnable);
+    glGenTextures(1, &Output.DepthMapTextureID);
+    glBindTexture(GL_TEXTURE_2D, &Output.DepthMapTextureID);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, )
 
 
 
