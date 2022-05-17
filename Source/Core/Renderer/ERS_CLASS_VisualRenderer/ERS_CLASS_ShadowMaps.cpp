64bcb414b4d01805669b4ef8cf17b085023c951b
datacrystals <tliao@carboncopies.org>
2022-05-17 23:36:50 +0000

    Implement Support For Shadow Maps (#154)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ShadowMaps.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ShadowMaps.cpp
@@ -2,5 +2,21 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-#include <ERS_CLASS_ShadowMaps.h>
+#include <ERS_CLASS_DepthMaps.h>
+
+
+ERS_CLASS_DepthMaps::ERS_CLASS_DepthMaps(ERS_STRUCT_SystemUtils* SystemUtils, ERS_STRUCT_ProjectUtils* ProjectUtils) {
+
+    SystemUtils_ = SystemUtils;
+    ProjectUtils_ = ProjectUtils;
+
+    SystemUtils_->Logger_->Log("Initializing Viewport Overlay Subsystem", 5);
+
+}
+
+ERS_CLASS_DepthMaps::~ERS_CLASS_DepthMaps() {
+
+    SystemUtils_->Logger_->Log("Viewport Overlay Subsystem Destructor Invoked", 6);
+
+}
 
--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ShadowMaps.h
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ShadowMaps.h
@@ -21,5 +21,78 @@
 
 
 // Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_CLASS_LoggingSystem.h>
+
+#include <ERS_SceneManager.h>
+
+#include <ERS_STRUCT_ProjectUtils.h>
+#include <ERS_STRUCT_SystemUtils.h>
+#include <ERS_STRUCT_Viewport.h>
+#include <ERS_STRUCT_DepthMap.h>
 
 
+
+/**
+ * @brief This class renders any ui/overlay info onto the viewport as requested by the viewport struct.
+ * 
+ */
+class ERS_CLASS_DepthMaps {
+
+private:
+
+    ERS_STRUCT_SystemUtils* SystemUtils_; /**<Pointer to systemtuils resource*/
+    ERS_STRUCT_ProjectUtils* ProjectUtils_; /**<Pointer to project utils instance*/
+
+
+    /**
+     * @brief Generates a depth map with the given resolution.
+     * This is usually used for shadows later on in the rendering process.
+     * Will also default to a 2048x2048 depth map by default. 
+     * 
+     * @param ResolutionX Width in pixels of the depth map
+     * @param ResolutionY Height in pixels of the depth map
+     * @param LogOutput Enable/disable logging output
+     * @return ERS_STRUCT_DepthMap Struct containing the relevant opengl ids for this depth map
+     */
+    ERS_STRUCT_DepthMap GenerateDepthMap(int ResolutionX = 2048, int ResolutionY = 2048, bool LogOutput = true);
+
+    /**
+     * @brief Setup The Depth Map For Rendering.
+     * This will bind to the depth map's framebuffer, clear the screen, and get map ready to be rendered to.
+     * 
+     * @param Input 
+     */
+    void BindToDepthMap(ERS_STRUCT_DepthMap* Input);
+
+
+public:
+
+    /**
+     * @brief Construct a new ers class DepthMaps object
+     * 
+     * @param SystemUtils 
+     */
+    ERS_CLASS_DepthMaps(ERS_STRUCT_SystemUtils* SystemUtils, ERS_STRUCT_ProjectUtils* ProjectUtils);
+
+    /**
+     * @brief Destroy the ers class DepthMaps object
+     * 
+     */
+    ~ERS_CLASS_DepthMaps();
+
+
+    /**
+     * @brief Checks the number of depth maps vs the number of lights and makes sure that the number of depth maps is equal to the number of lights.
+     * Will generate more depth maps and remove unused ones by calling internal functions in this class.
+     * 
+     */
+    void UpdateNumberOfDepthMaps();
+
+    /**
+     * @brief Iterates over all lights in the scene and renders all depth maps.
+     * 
+     */
+    void UpdateDepthMaps();
+
+
+};
