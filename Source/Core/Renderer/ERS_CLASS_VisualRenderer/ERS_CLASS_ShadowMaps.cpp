3cdc3055cbdb8138b3eeeea5a68e35929fd39acd
datacrystals <tliao@carboncopies.org>
2022-05-18 01:11:37 +0000

    Implement Support For Shadow Maps (#154)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ShadowMaps.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ShadowMaps.cpp
@@ -12,7 +12,7 @@ ERS_CLASS_ShadowMaps::ERS_CLASS_ShadowMaps(ERS_STRUCT_SystemUtils* SystemUtils, 
     DepthMapShader_ = DepthMapShader;
 
     SystemUtils_->Logger_->Log("Initializing Shadow Map Subsystem", 5);
-    ERS_CLASS_DepthMaps_ = std::make_shared<ERS_CLASS_DepthMaps>(SystemUtils_, ProjectUtils_); 
+    ERS_CLASS_DepthMaps_ = std::make_unique<ERS_CLASS_DepthMaps>(SystemUtils_, ProjectUtils_); 
 
     DeleteMe = ERS_CLASS_DepthMaps_->GenerateDepthMap();
 
