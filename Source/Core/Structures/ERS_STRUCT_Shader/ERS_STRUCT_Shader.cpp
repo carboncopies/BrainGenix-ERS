d4cad67df49d77bfc8d2626bb0b6071adcc391d6
datacrystals <tliao@carboncopies.org>
2022-05-21 00:15:48 +0000

    Implement Support For Shadow Maps (#154)

--- Source/Core/Structures/ERS_STRUCT_Shader/ERS_STRUCT_Shader.cpp
+++ Source/Core/Structures/ERS_STRUCT_Shader/ERS_STRUCT_Shader.cpp
@@ -120,56 +120,56 @@ bool ERS_STRUCT_Shader::MakeActive(ERS_CLASS_LoggingSystem* Logger) {
 // ------------------------------------------------------------------------
 void ERS_STRUCT_Shader::SetBool(const std::string &name, bool value) const
 {         
-    glUniform1i(glGetUniformLocation(ShaderProgram, name.c_str()), (int)value); 
+    glUniform1i(glGetUniformLocation(ShaderProgram_, name.c_str()), (int)value); 
 }
 // ------------------------------------------------------------------------
 void ERS_STRUCT_Shader::SetInt(const std::string &name, int value) const
 { 
-    glUniform1i(glGetUniformLocation(ShaderProgram, name.c_str()), value); 
+    glUniform1i(glGetUniformLocation(ShaderProgram_, name.c_str()), value); 
 }
 void ERS_STRUCT_Shader::SetFloat(const std::string &name, float value) const
 { 
-    glUniform1f(glGetUniformLocation(ShaderProgram, name.c_str()), value); 
+    glUniform1f(glGetUniformLocation(ShaderProgram_, name.c_str()), value); 
 }
 // ------------------------------------------------------------------------
 void ERS_STRUCT_Shader::SetVec2(const std::string &name, const glm::vec2 &value) const
 { 
-    glUniform2fv(glGetUniformLocation(ShaderProgram, name.c_str()), 1, &value[0]); 
+    glUniform2fv(glGetUniformLocation(ShaderProgram_, name.c_str()), 1, &value[0]); 
 }
 void ERS_STRUCT_Shader::SetVec2(const std::string &name, float x, float y) const
 { 
-    glUniform2f(glGetUniformLocation(ShaderProgram, name.c_str()), x, y); 
+    glUniform2f(glGetUniformLocation(ShaderProgram_, name.c_str()), x, y); 
 }
 // ------------------------------------------------------------------------
 void ERS_STRUCT_Shader::SetVec3(const std::string &name, const glm::vec3 &value) const
 { 
-    glUniform3fv(glGetUniformLocation(ShaderProgram, name.c_str()), 1, &value[0]); 
+    glUniform3fv(glGetUniformLocation(ShaderProgram_, name.c_str()), 1, &value[0]); 
 }
 void ERS_STRUCT_Shader::SetVec3(const std::string &name, float x, float y, float z) const
 { 
-    glUniform3f(glGetUniformLocation(ShaderProgram, name.c_str()), x, y, z); 
+    glUniform3f(glGetUniformLocation(ShaderProgram_, name.c_str()), x, y, z); 
 }
 // ------------------------------------------------------------------------
 void ERS_STRUCT_Shader::SetVec4(const std::string &name, const glm::vec4 &value) const
 { 
-    glUniform4fv(glGetUniformLocation(ShaderProgram, name.c_str()), 1, &value[0]); 
+    glUniform4fv(glGetUniformLocation(ShaderProgram_, name.c_str()), 1, &value[0]); 
 }
 void ERS_STRUCT_Shader::SetVec4(const std::string &name, float x, float y, float z, float w) 
 { 
-    glUniform4f(glGetUniformLocation(ShaderProgram, name.c_str()), x, y, z, w); 
+    glUniform4f(glGetUniformLocation(ShaderProgram_, name.c_str()), x, y, z, w); 
 }
 // ------------------------------------------------------------------------
 void ERS_STRUCT_Shader::SetMat2(const std::string &name, const glm::mat2 &mat) const
 {
-    glUniformMatrix2fv(glGetUniformLocation(ShaderProgram, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    glUniformMatrix2fv(glGetUniformLocation(ShaderProgram_, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 }
 // ------------------------------------------------------------------------
 void ERS_STRUCT_Shader::SetMat3(const std::string &name, const glm::mat3 &mat) const
 {
-    glUniformMatrix3fv(glGetUniformLocation(ShaderProgram, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    glUniformMatrix3fv(glGetUniformLocation(ShaderProgram_, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 }
 // ------------------------------------------------------------------------
 void ERS_STRUCT_Shader::SetMat4(const std::string &name, const glm::mat4 &mat) const
 {
-    glUniformMatrix4fv(glGetUniformLocation(ShaderProgram, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    glUniformMatrix4fv(glGetUniformLocation(ShaderProgram_, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 }
