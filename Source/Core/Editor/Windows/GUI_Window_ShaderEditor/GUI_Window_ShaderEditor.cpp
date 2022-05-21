475b79c0c82a0f3bd42ecff1548bc3f1efa4c3ad
datacrystals <tliao@carboncopies.org>
2022-05-21 20:45:47 +0000

    Implement Support For Shadow Maps (#154)

--- Source/Core/Editor/Windows/GUI_Window_ShaderEditor/GUI_Window_ShaderEditor.cpp
+++ Source/Core/Editor/Windows/GUI_Window_ShaderEditor/GUI_Window_ShaderEditor.cpp
@@ -282,7 +282,7 @@ void Window_ShaderEditor::DrawToolsWindow() {
         std::cout<<"IsShader2: "<<State<<std::endl;
         std::cout<<LivePreviewShader_->ShaderProgram_<<std::endl;
         //VisualRenderer_->Shaders_[LivePreviewShaderIndex_] = LivePreviewShader_;
-        std::shared_ptr<ERS_STRUCT_Shader> Test = LivePreviewShader_;
+        std::shared_ptr<ERS_STRUCT_Shader> Test;
         VisualRenderer_->Shaders_[LivePreviewShaderIndex_] = Test;
         State = glIsProgram(LivePreviewShader_->ShaderProgram_);
         std::cout<<"IsShader3: "<<State<<std::endl;
