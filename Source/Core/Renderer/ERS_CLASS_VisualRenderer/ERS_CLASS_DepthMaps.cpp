9d6a57fb5b0bb34c34799b7467e67ade97022435
datacrystals <tliao@carboncopies.org>
2022-05-23 23:34:14 +0000

    Implement Support For Shadow Maps (#154)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
@@ -160,8 +160,8 @@ void ERS_CLASS_DepthMaps::UpdateDepthMaps(ERS_STRUCT_Shader* DepthShader) {
 
         // Render To Depth Map
         glm::mat4* LightSpaceMatrix = new glm::mat4();
-
         UpdateDepthMap(&Light->DepthMap, DepthShader, Light->Pos, false); // set this to false later, debugging
+        Light->LightSpaceMatrix = *LightSpaceMatrix;
 
     } 
 
