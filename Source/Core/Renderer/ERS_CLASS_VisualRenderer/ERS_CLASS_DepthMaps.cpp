017b41c1a71b20eb2ec126c05d7694609dab01ff
datacrystals <tliao@carboncopies.org>
2022-06-07 05:53:10 +0000

    Implement Support For Shadow Maps (#154)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
@@ -54,7 +54,7 @@ bool ERS_CLASS_DepthMaps::RegenerateDepthMapTextureArray(int NumberOfTextures, i
     glBindTexture(GL_TEXTURE_2D_ARRAY, DepthTextureArrayID_);
     
     glTexStorage3D(GL_TEXTURE_2D_ARRAY,
-        1, // Number OF Mipmaps
+        1, // Number OF Mipmaps, Note that we're not using mipmaps so this is set to 1
         GL_DEPTH_COMPONENT, // Storage Format, Using Depth Format Here As We're Setting Up A Depth Map
         Width, Height, // Width and Height, Pretty Self Explanitory
         NumberOfTextures // Total Number Of Textures In The Array
