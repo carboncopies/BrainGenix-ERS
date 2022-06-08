bd5c9ff665a3e48cbf8bf586e66368187310bf5a
datacrystals <tliao@carboncopies.org>
2022-06-08 23:12:19 +0000

    Implement Support For Shadow Maps (#154)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_VisualRenderer.cpp
@@ -685,10 +685,6 @@ void ERS_CLASS_VisualRenderer::UpdateShader(int ShaderIndex, float DeltaTime, in
 
         ActiveShader->SetInt((UniformName + std::string(".DepthMapIndex")).c_str(), ActiveScene->DirectionalLights[i]->DepthMap.DepthMapTextureIndex);
         ActiveShader->SetMat4((UniformName + std::string(".LightSpaceMatrix")).c_str(), ActiveScene->DirectionalLights[i]->LightSpaceMatrix);
-        
-        // std::cout<<"Index: "<<ActiveScene->DirectionalLights[i]->DepthMap.DepthMapTextureIndex
-        // <<(UniformName + std::string(".LightSpaceMatrix")).c_str()<<glm::to_string(ActiveScene->DirectionalLights[i]->LightSpaceMatrix)
-        // <<std::endl;
     
     }
 
