bdc8d497390a9bb6b228b798802fa980113a3f9c
datacrystals <tliao@carboncopies.org>
2022-06-12 03:48:18 +0000

    Implement Spot Light Shadows (#217)

--- Source/Core/Editor/Widgets/GUI_Widget_ObjectProperties.cpp
+++ Source/Core/Editor/Widgets/GUI_Widget_ObjectProperties.cpp
@@ -167,10 +167,10 @@ void Widget_ObjectProperties::Draw() {
                         ImGui::HelpMarker("Sets the distance after which the scene is no longer affected by this light source.");
 
 
-                        ImGui::DragFloat("Cutoff Angle", &SceneManager_->Scenes_[SceneManager_->ActiveScene_]->SpotLights[Index]->CutOff, 1.0f);
+                        ImGui::DragFloat("Cutoff Angle", &SceneManager_->Scenes_[SceneManager_->ActiveScene_]->SpotLights[Index]->CutOff, 1.0f, 0.0f, 120.0f);
                         ImGui::SameLine();
                         ImGui::HelpMarker("Specifies the angle of the spot light's outer cone.");
-                        ImGui::DragFloat("Rolloff Angle", &SceneManager_->Scenes_[SceneManager_->ActiveScene_]->SpotLights[Index]->Rolloff, 0.5f);
+                        ImGui::DragFloat("Rolloff Angle", &SceneManager_->Scenes_[SceneManager_->ActiveScene_]->SpotLights[Index]->Rolloff, 0.5f, 0.0f, SceneManager_->Scenes_[SceneManager_->ActiveScene_]->SpotLights[Index]->CutOff);
                         ImGui::SameLine();
                         ImGui::HelpMarker("Sets the angle at which the outer cone begins to roll off. This angle sets the inner cone which is unaffected by rolloff. Rolloff occurs in the area between the outer and inner cone (this angle).");
 
