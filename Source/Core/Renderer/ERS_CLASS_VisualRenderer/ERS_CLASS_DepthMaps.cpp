30718293a8fb1df808ac4e9b2e9eb70b54c3d98f
datacrystals <tliao@carboncopies.org>
2022-06-14 23:55:04 +0000

    Implement Point Light Shadows (#215)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
@@ -499,14 +499,18 @@ void ERS_CLASS_DepthMaps::UpdateDepthMap(ERS_STRUCT_PointLight* Light, ERS_STRUC
     ShadowTransforms.push_back(ObjectProjection * glm::lookAt(Light->Pos, Light->Pos + glm::vec3( 0.0, 0.0, 1.0), glm::vec3(0.0,-1.0, 0.0)));
     ShadowTransforms.push_back(ObjectProjection * glm::lookAt(Light->Pos, Light->Pos + glm::vec3( 0.0, 0.0,-1.0), glm::vec3(0.0,-1.0, 0.0)));
 
-    std::cout<<"-------------------------------\n";
-    std::cout<<glGetError()<<std::endl;
-    std::cout<<"----\n";
 
 
     // Render All Sides
     glViewport(0, 0, DepthTextureArrayWidth_, DepthTextureArrayHeight_);
     glBindFramebuffer(GL_FRAMEBUFFER, Light->DepthMap.FrameBufferObjectID);
+
+
+    std::cout<<"-------------------------------\n";
+    std::cout<<glGetError()<<std::endl;
+    std::cout<<"----\n";
+
+
     for (unsigned int i = 0; i < ShadowTransforms.size(); i++) {
 
         // Render With Depth Shader
