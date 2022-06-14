a47c6be0afd98c14290a68f59bea3252949806be
datacrystals <tliao@carboncopies.org>
2022-06-14 01:20:51 +0000

    Implement Spot Light Shadows (#217)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
@@ -14,7 +14,7 @@ ERS_CLASS_DepthMaps::ERS_CLASS_DepthMaps(ERS_STRUCT_SystemUtils* SystemUtils, ER
     SystemUtils_->Logger_->Log("Initializing Viewport Overlay Subsystem", 5);
 
     // Create Array Texture For Depth Maps
-    RegenerateDepthMapTextureArray(64, 2048, 2048);
+    RegenerateDepthMapTextureArray(64, SystemUtils_->RendererSettings_->ShadowMapX_, SystemUtils_->RendererSettings_->ShadowMapY_);
 
 
 }
