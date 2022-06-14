15f770b5c65bb895dd9a423549599bb38fd9c301
datacrystals <tliao@carboncopies.org>
2022-06-14 09:04:14 +0000

    Implement Point Light Shadows (#215)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
@@ -374,9 +374,7 @@ void ERS_CLASS_DepthMaps::UpdateDepthMaps(ERS_STRUCT_Shader* DepthShader) {
         }
 
         // Render To Depth Map
-        glm::mat4* LightSpaceMatrix = new glm::mat4();
-        UpdateDepthMap(Light, DepthShader, LightSpaceMatrix);
-        Light->LightSpaceMatrix = *LightSpaceMatrix;
+        UpdateDepthMap(Light, DepthShader);
 
     } 
 
