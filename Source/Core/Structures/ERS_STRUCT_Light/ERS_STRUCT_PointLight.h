89209232ebbcc9d31674c08fb2a74b92ed017946
datacrystals <tliao@carboncopies.org>
2022-06-14 09:05:52 +0000

    Implement Point Light Shadows (#215)

--- Source/Core/Structures/ERS_STRUCT_Light/ERS_STRUCT_PointLight.h
+++ Source/Core/Structures/ERS_STRUCT_Light/ERS_STRUCT_PointLight.h
@@ -31,7 +31,4 @@ struct ERS_STRUCT_PointLight {
 
     ERS_STRUCT_DepthMap DepthMap; /**<Contains OpenGL IDS Used To Calculate Shadows*/
 
-    glm::mat4 LightSpaceMatrix; /**<Matrix From Light's Perspective*/
-
-
 };
