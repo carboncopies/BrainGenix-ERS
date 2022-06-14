647a89145b61462883de89b4b6643fc9fe1d3e79
datacrystals <tliao@carboncopies.org>
2022-06-14 23:47:39 +0000

    Implement Point Light Shadows (#215)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
@@ -508,12 +508,10 @@ void ERS_CLASS_DepthMaps::UpdateDepthMap(ERS_STRUCT_PointLight* Light, ERS_STRUC
     for (unsigned int i = 0; i < ShadowTransforms.size(); i++) {
 
         // Render With Depth Shader
-
-        GLenum face = GL_TEXTURE_CUBE_MAP_POSITIVE_X + i;
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, face, depthCubemap, 0);
-        glFramebufferTexture2D()
-
-        glFramebufferTextureLayer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, DepthTextureArrayID_, 0, Light->DepthMap.DepthMapTextureIndex*6 + i);
+        GLenum Face = GL_TEXTURE_CUBE_MAP_POSITIVE_X + i;
+        //glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, Face, depthCubemap, 0);
+        glFramebufferTexture3D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, Face, DepthTextureCubemapArrayID_, 0, Light->DepthMap.DepthMapTextureIndex*6 + i);
+        //glFramebufferTextureLayer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, DepthTextureArrayID_, 0, Light->DepthMap.DepthMapTextureIndex*6 + i);
 
         DepthShader->MakeActive();
         DepthShader->SetMat4("LightSpaceMatrix", ShadowTransforms[i]);
