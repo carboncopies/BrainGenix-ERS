abec459526e31e67aaf14381f7af415d684eb7b0
datacrystals <tliao@carboncopies.org>
2022-06-18 02:51:39 +0000

    Implement Point Light Shadows (#215)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_DepthMaps.cpp
@@ -532,13 +532,6 @@ void ERS_CLASS_DepthMaps::UpdateDepthMap(ERS_STRUCT_PointLight* Light, ERS_STRUC
     DepthShader->MakeActive();
 
     // Render With Depth Shader
-
-
-
-
-
-
-
     //glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, DepthTextureCubemapArrayID_, Light->DepthMap.DepthMapTextureIndex*6, 0);
     for (unsigned int i = 0; i < ShadowTransforms.size(); i++) {
         DepthShader->SetMat4(std::string("shadowMatricies[") + std::to_string(i) + std::string("]"), ShadowTransforms[i]);
