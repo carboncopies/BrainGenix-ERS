194c4206e9b0868b46eed241f755e7ea532316a2
datacrystals <tliao@carboncopies.org>
2022-06-19 07:28:07 +0000

    Implement Debug Callbacks (#222)

--- Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_OpenGLLoggingSystem.h
+++ Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_OpenGLLoggingSystem.h
@@ -18,6 +18,9 @@
 
 std::vector<std::string> OpenGLLogs_; /**List of messages as collected by the opengl logging system*/
 
+void APIENTRY GLDebugMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *msg, const void *data) {
+    
+}
 
 
 /**
