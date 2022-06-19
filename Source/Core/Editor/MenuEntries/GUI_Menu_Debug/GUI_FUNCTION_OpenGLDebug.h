44cceb35cece5c04e7340a04ca245a5f750f3b14
datacrystals <tliao@carboncopies.org>
2022-06-19 06:25:23 +0000

    Implement Point Light Shadows (#215)

---
+++ Source/Core/Editor/MenuEntries/GUI_Menu_Debug/GUI_FUNCTION_OpenGLDebug.h
@@ -0,0 +1,22 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+
+#pragma once
+
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <vector>
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <memory>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <imgui.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_STRUCT_SystemUtils.h>
+
+#include <GUI_Window_TestEditor.h>
