1b72b6c5408bb450cbd2c68e338878841d9b11fe
datacrystals <tliao@carboncopies.org>
2022-06-19 07:06:29 +0000

    Implement Debug Callbacks (#222)

--- Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_OpenGLLoggingSystem.h
+++ Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_OpenGLLoggingSystem.h
@@ -13,3 +13,36 @@
 #include <glad/glad.h>
 
 // Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_CLASS_LoggingSystem.h>
+
+
+std::vector<std::string> OpenGLLogs_; /**List of messages as collected by the opengl logging system*/
+
+
+
+/**
+ * @brief This Class Provides A System To Get OpenGL Context Issues as well as debug other things related to opengl.
+ * 
+ */
+class ERS_CLASS_OpenGLLoggingSystem() {
+
+private:
+
+    ERS_CLASS_LoggingSystem* Logger_; /**<Class to prettyify the opengl logs once they're extracted*/
+
+public:
+
+    /**
+     * @brief Construct a new ers class openglloggingsystem object
+     * 
+     * @param Logger 
+     */
+    ERS_CLASS_OpenGLLoggingSystem(ERS_CLASS_LoggingSystem* Logger);
+
+    /**
+     * @brief Destroy the ers class openglloggingsystem object
+     * 
+     */
+    ~ERS_CLASS_OpenGLLoggingSystem();
+
+};
