5443f020bb2fdb33f6f520e4b74eabc56818f477
datacrystals <tliao@carboncopies.org>
2022-06-19 06:31:49 +0000

    Implement Debug Callbacks (#222)

--- Source/Core/Editor/MenuEntries/GUI_Menu_Debug/ERS_CLASS_OpenGLDebug.cpp
+++ Source/Core/Editor/MenuEntries/GUI_Menu_Debug/ERS_CLASS_OpenGLDebug.cpp
@@ -5,3 +5,14 @@
 #include <ERS_CLASS_OpenGLDebug.h>
 
 
+ERS_CLASS_OpenGLDebug::ERS_CLASS_OpenGLDebug(ERS_STRUCT_SystemUtils* SystemUtils) {
+
+    SystemUtils_ = SystemUtils;
+
+
+    SystemUtils_->Logger->Log("Initializing OpenGL Debug Menu GUI Subsystem", 5);
+
+    // Set Default Settings
+    glDisable(GL_DEBUG_OUTPUT);
+
+}
