2f5c2866cec2a2edad9d949646bb1ec246245e77
datacrystals <tliao@carboncopies.org>
2022-06-19 06:58:45 +0000

    Implement Debug Callbacks (#222)

---
+++ Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_OpenGLLoggingSystem.h
@@ -0,0 +1,11 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Interna; Libraries (BG convention: use <> instead of "")
