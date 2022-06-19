4e35406cdc607cf4b4b69f0ac359ee6c77e6e8ca
datacrystals <tliao@carboncopies.org>
2022-06-19 20:38:41 +0000

    Implement Debug Callbacks (#222)

--- Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_OpenGLLoggingSystem.cpp
+++ Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_OpenGLLoggingSystem.cpp
@@ -6,7 +6,11 @@
 
 
 // Callback function for printing debug statements
-void APIENTRY ERS_MessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *msg, const void *Logger) {
+void APIENTRY ERS_MessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *msg, const void *data) {
+
+
+    ERS_CLASS_LoggingSystem* Logger = (ERS_CLASS_LoggingSystem*)data;
+
     char* _source;
     char* _type;
     char* _severity;
