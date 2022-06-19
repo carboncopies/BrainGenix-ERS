d35367311e9c85828375d9abb21704051bedff39
datacrystals <tliao@carboncopies.org>
2022-06-19 06:37:11 +0000

    Implement Debug Callbacks (#222)

--- Source/Core/Editor/MenuEntries/GUI_Menu_Debug/GUI_Menu_Debug.h
+++ Source/Core/Editor/MenuEntries/GUI_Menu_Debug/GUI_Menu_Debug.h
@@ -35,6 +35,7 @@ class GUI_Menu_Debug {
 private:
 
     ERS_STRUCT_SystemUtils* SystemUtils_; /**<SystemUtils Instance, Used To Get Systems From Other Classes*/
+    std::unique_ptr<ERS_CLASS_OpenGLDebug> ERS_CLASS_OpenGLDebug_; /**<Class used to draw the opengl debug menu into the main debug menu*/
     std::unique_ptr<Window_TestEditor> TestEditor_; /**<GUI Window Test Editor Instance*/
 
     bool ShowImGuiDemoWindow_ = false; /**<Show/hide ImGui DemoWindow*/
