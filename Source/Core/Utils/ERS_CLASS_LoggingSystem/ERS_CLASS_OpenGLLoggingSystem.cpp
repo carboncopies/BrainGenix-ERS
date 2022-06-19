54bcf6b6d967d4091824718481a2c319db97a787
datacrystals <tliao@carboncopies.org>
2022-06-19 07:08:38 +0000

    Implement Debug Callbacks (#222)

--- Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_OpenGLLoggingSystem.cpp
+++ Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_OpenGLLoggingSystem.cpp
@@ -3,3 +3,14 @@
 //======================================================================//
 
 #include <ERS_CLASS_OpenGLLoggingSystem.h>
+
+
+ERS_CLASS_OpenGLLoggingSystem::ERS_CLASS_OpenGLLoggingSystem(ERS_CLASS_LoggingSystem* Logger) {
+
+    Logger_ = Logger;
+
+    Logger_->Log("Initializing OpenGL Log Collection Subsystem", 5);
+    glDisable(GL_DEBUG_OUTPUT);
+
+}
+
