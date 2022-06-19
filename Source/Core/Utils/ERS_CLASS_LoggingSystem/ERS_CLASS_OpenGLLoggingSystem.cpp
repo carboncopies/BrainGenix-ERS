ca2b4ded8ff51ff11eb09c2b9091eb949e6a3ae0
datacrystals <tliao@carboncopies.org>
2022-06-19 18:50:12 +0000

    Implement Debug Callbacks (#222)

--- Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_OpenGLLoggingSystem.cpp
+++ Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_OpenGLLoggingSystem.cpp
@@ -128,7 +128,7 @@ void ERS_CLASS_OpenGLLoggingSystem::SetCollectionStatus(bool Status) {
         Logger_->Log("Enabling OpenGL Debug Output", 5);
         glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
         std::cout<<glGetString(GL_VERSION)<<std::endl;
-        glDebugMessageCallback(ERS_MessageCallback, NULL);
+        // glDebugMessageCallback(ERS_MessageCallback, NULL);
 
         std::string DebugMessage = "Debug Messages Are Now Enabled And Working Properly";
         glDebugMessageInsert(GL_DEBUG_SOURCE_APPLICATION, GL_DEBUG_TYPE_OTHER, 0, GL_DEBUG_SEVERITY_NOTIFICATION, DebugMessage.size(), DebugMessage.c_str());
