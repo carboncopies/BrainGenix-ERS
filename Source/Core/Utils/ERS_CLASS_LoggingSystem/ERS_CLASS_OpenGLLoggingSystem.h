c9820e79373b1dc9ad7b8d20abd773bf6453e590
datacrystals <tliao@carboncopies.org>
2022-06-20 00:47:41 +0000

    Implement Debug Callbacks (#222)

--- Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_OpenGLLoggingSystem.h
+++ Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_CLASS_OpenGLLoggingSystem.h
@@ -21,6 +21,9 @@
 
 //void APIENTRY ERS_MessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *msg, const void *data);
 
+struct ERS_STRUCT_MessageCallbackParam {
+
+};
 
 /**
  * @brief This Class Provides A System To Get OpenGL Context Issues as well as debug other things related to opengl.
