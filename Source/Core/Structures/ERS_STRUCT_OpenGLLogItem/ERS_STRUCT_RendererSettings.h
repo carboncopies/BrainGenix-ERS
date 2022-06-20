9cd24b435a70341212c61a898993acb701090ed0
datacrystals <tliao@carboncopies.org>
2022-06-20 00:30:11 +0000

    Implement Debug Callbacks (#222)

---
+++ Source/Core/Structures/ERS_STRUCT_OpenGLLogItem/ERS_STRUCT_RendererSettings.h
@@ -0,0 +1,33 @@
+// ToDO: then add to project struct, then update project loader/writer with this info. Then check trello board for other related tasks like live ediitng.
+
+
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <string>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
+
+/**
+ * @brief Struct containing renderer settings.
+ * 
+ */
+struct ERS_STRUCT_RendererSettings {
+
+    // Shadow Map Settings
+    int ShadowMapX_ = 2048;
+    int ShadowMapY_ = 2048;
+
+
+    // Indicate If Settings Have Changed
+    bool HaveSettingsChanged_ = false;
+
+};
