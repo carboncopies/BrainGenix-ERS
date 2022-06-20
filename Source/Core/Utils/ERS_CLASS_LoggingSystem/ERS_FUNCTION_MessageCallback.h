7b0c075418c2cddccc6dfdd2c1a3a2d17e89626b
datacrystals <tliao@carboncopies.org>
2022-06-20 01:28:23 +0000

    Implement Debug Callbacks (#222)

---
+++ Source/Core/Utils/ERS_CLASS_LoggingSystem/ERS_FUNCTION_MessageCallback.h
@@ -0,0 +1,18 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <iostream>
+#include <vector>
+#include <string>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <glad/glad.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_CLASS_LoggingSystem.h>
+#include <ERS_STRUCT_OpenGLLogItem.h>
+#include <ERS_STRUCT_MessageCallbackUserParam.h>
