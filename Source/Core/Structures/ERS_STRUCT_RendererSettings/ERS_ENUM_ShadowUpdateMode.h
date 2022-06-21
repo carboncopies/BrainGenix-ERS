031da0ad6ccf12891a3e3a03a8fc489b7c2de4b0
datacrystals <tliao@carboncopies.org>
2022-06-21 22:26:47 +0000

    Implement Settings To Control Rendering (#220)

---
+++ Source/Core/Structures/ERS_STRUCT_RendererSettings/ERS_ENUM_ShadowUpdateMode.h
@@ -0,0 +1,30 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <string>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
+namespace ERS {
+
+    namespace Renderer {
+
+        enum ShadowUpdateMode {
+
+            ERS_NO_FILTERING,
+            ERS_PCF_FILTERING,
+            ERS_POISSON_SAMPLING,
+            ERS_STRATIFIED_POISSON_SAMPLING
+
+        };
+
+    }
+
+}
