6b2e575339a1254e810291ed3ccdf39e1fc3c8d9
datacrystals <tliao@carboncopies.org>
2022-06-21 22:16:54 +0000

    Implement Settings To Control Rendering (#220)

---
+++ Source/Core/Structures/ERS_STRUCT_RendererSettings/ERS_STRUCT_ShadowFilteringType.h
@@ -0,0 +1,16 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <string>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
+
+/
