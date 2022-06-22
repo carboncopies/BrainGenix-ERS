c50e16bbd2b513cd33a8681b2cfb992ea15078cf
datacrystals <tliao@carboncopies.org>
2022-06-22 06:57:29 +0000

    Implement Settings To Control Rendering (#220)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ShadowMaps.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ShadowMaps.cpp
@@ -98,6 +98,20 @@ void ERS_CLASS_ShadowMaps::UpdateShadowMaps(ERS_STRUCT_Shader* DepthMapShader, E
                 LightDistances[i] = Distance;
             }
 
+            // Sort Based On Value In Acending Order
+            typedef std::pair<std::string, int> pair;
+            std::sort(LightDistances.begin(), LightDistances.end(),
+            [](const pair &l, const pair &r)
+            {
+                if (l.second != r.second) {
+                    return l.second < r.second;
+                }
+ 
+                return l.first < r.first;
+            });
+
+            
+
             for (unsigned int i = 0; i <LightDistances.size();i++) {
                 std::cout<<i<<" | "<<LightDistances[i]<<std::endl;
             }
