24a1c3638253ffbadf5e695926436a6a62d3eede
datacrystals <tliao@carboncopies.org>
2022-06-22 21:54:45 +0000

    Implement Settings To Control Rendering (#220)

---
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_MeshTransparencySortNoTextures.cpp
@@ -0,0 +1,32 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#include <ERS_FUNCTION_MeshTransparencySort.h>
+
+
+void ERS_FUNCTION_MeshTransparencySort(std::vector<ERS_STRUCT_Mesh*>* OpaqueMeshes, std::vector<ERS_STRUCT_Mesh*>* TransparentMeshes, ERS_STRUCT_Scene* Scene) {
+
+
+    for (unsigned long i = 0; i < Scene->Models.size(); i++) {
+
+        ERS_STRUCT_Model *Model = Scene->Models[i].get();
+        glm::mat4 ModelMatrix = Model->GetMat4();
+        if (Model->FullyReady && Model->Enabled) {
+            for (unsigned int i = 0; i < Model->Meshes.size(); i++) {
+
+                ERS_STRUCT_Mesh* Mesh = &Model->Meshes[i];
+                Mesh->ModelMatrix = ModelMatrix;
+                if (Mesh->HasTransparency_) {
+                    TransparentMeshes->push_back(Mesh);
+                } else {
+                    OpaqueMeshes->push_back(Mesh);
+                }
+
+            }
+        }
+
+    }
+
+
+}
