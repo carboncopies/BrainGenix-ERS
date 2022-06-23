742ef7ca4a026fad445b1865ab9e291195a94ea9
datacrystals <tliao@carboncopies.org>
2022-06-23 03:38:01 +0000

    Implement Settings To Control Rendering (#220)

--- Source/Core/Structures/ERS_STRUCT_Light/ERS_STRUCT_DirectionalLight.h
+++ Source/Core/Structures/ERS_STRUCT_Light/ERS_STRUCT_DirectionalLight.h
@@ -24,8 +24,8 @@ struct ERS_STRUCT_DirectionalLight {
     glm::vec3 Color; /**<Color of the light*/
     float Intensity; /**<Intensity of the light*/
     float MaxDistance; /**<Distance After Which This Light No Longer Affects The Scene*/
-    
-    bool CastsShadows_; /*<Enable or disable shadows being cast from this light*/
+
+    bool CastsShadows_ = true; /*<Enable or disable shadows being cast from this light*/
     
     glm::vec3 Pos; /**<Position*/
     glm::vec3 Rot; /**<Rotation or Direction Of Light*/
