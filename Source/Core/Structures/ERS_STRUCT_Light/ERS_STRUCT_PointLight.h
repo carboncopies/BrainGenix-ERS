a9a02baf5b5dae6ea4ff118befd71cd211f9f75b
datacrystals <tliao@carboncopies.org>
2022-06-23 03:37:52 +0000

    Implement Settings To Control Rendering (#220)

--- Source/Core/Structures/ERS_STRUCT_Light/ERS_STRUCT_PointLight.h
+++ Source/Core/Structures/ERS_STRUCT_Light/ERS_STRUCT_PointLight.h
@@ -31,6 +31,6 @@ struct ERS_STRUCT_PointLight {
 
     ERS_STRUCT_DepthMap DepthMap; /**<Contains OpenGL IDS Used To Calculate Shadows*/
 
-    bool CastsShadows_; /*<Enable or disable shadows being cast from this light*/
+    bool CastsShadows_ = true; /*<Enable or disable shadows being cast from this light*/
 
 };
