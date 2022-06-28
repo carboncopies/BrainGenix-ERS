a01dea87548bf722987fce2d4afe89c2dc5a3679
datacrystals <tliao@carboncopies.org>
2022-06-28 06:06:34 +0000

    Convert Widgets To Window Classes (#238)

--- Source/Core/Editor/MenuEntries/GUI_Menu_Window/GUI_Menu_Window.h
+++ Source/Core/Editor/MenuEntries/GUI_Menu_Window/GUI_Menu_Window.h
@@ -27,7 +27,7 @@
 #include <GUI_Widget_FramerateGraph.h>
 #include <GUI_Widget_FrameratePlot.h>
 #include <GUI_Widget_FrameLatencyGraph.h>
-#include <GUI_GUI_Window_ObjectProperties.h>
+#include <GUI_Window_ObjectProperties.h>
 #include <GUI_Window_RAMGraph.h>
 
 #include <GUI_Window_SceneTree.h>
