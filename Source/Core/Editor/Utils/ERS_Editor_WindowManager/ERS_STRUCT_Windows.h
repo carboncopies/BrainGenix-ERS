a10ce668abe946a21738ea1774d2e8953a5a2c36
datacrystals <tliao@carboncopies.org>
2022-06-30 22:23:24 +0000

    Add Window Manager (#242)

---
+++ Source/Core/Editor/Utils/ERS_Editor_WindowManager/ERS_STRUCT_Windows.h
@@ -0,0 +1,24 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <memory.h>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
+/**
+ * @brief This structure holds unique pointers to all windows that are instantiated by the GUI.
+ * When adding a new window, add a unique_ptr here and update the WindowManager as well so that it's updated every frame.
+ * 
+ */
+struct ERS_STRUCT_Windows {
+
+
+
+};
