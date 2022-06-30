031436cd0195e1b966b8c53b2d54244acd43a360
datacrystals <tliao@carboncopies.org>
2022-06-30 22:26:31 +0000

    Add Window Manager (#242)

--- Source/Core/Editor/Utils/ERS_Editor_WindowManager/ERS_STRUCT_Windows.h
+++ Source/Core/Editor/Utils/ERS_Editor_WindowManager/ERS_STRUCT_Windows.h
@@ -10,7 +10,25 @@
 // Third-Party Libraries (BG convention: use <> instead of "")
 
 // Internal Libraries (BG convention: use <> instead of "")
-
+#include <GUI_Window_About.h>
+#include <GUI_Window_AssetExplorer.h>
+#include <GUI_Window_ControllerSettings.h>
+#include <GUI_Window_FrameLatencyGraph.h>
+#include <GUI_Window_FramerateCounter.h>
+#include <GUI_Window_FramerateGraph.h>
+#include <GUI_Window_FramerateHistogram.h>
+#include <GUI_Window_FrameratePlot.h>
+#include <GUI_Window_ImportProgressBar.h>
+#include <GUI_Window_ObjectProperties.h>
+#include <GUI_Window_ProjectSettings.h>
+#include <GUI_Window_RAMGraph.h>
+#include <GUI_Window_RenderingSettings.h>
+#include <GUI_Window_SceneTree.h>
+#include <GUI_Window_ScriptEditor.h>
+#include <GUI_Window_ShaderEditor.h>
+#include <GUI_Window_SystemLog.h>
+#include <GUI_Window_TestEditor.h>
+#include <GUI_Window_ThemeSelector.h>
 
 /**
  * @brief This structure holds unique pointers to all windows that are instantiated by the GUI.
