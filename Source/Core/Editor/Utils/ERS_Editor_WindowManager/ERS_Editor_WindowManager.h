a707bba2e3bc9e7e859907815c9bb66215ad9386
datacrystals <tliao@carboncopies.org>
2022-06-30 22:10:32 +0000

    Add Window Manager (#242)

--- Source/Core/Editor/Utils/ERS_Editor_WindowManager/ERS_Editor_WindowManager.h
+++ Source/Core/Editor/Utils/ERS_Editor_WindowManager/ERS_Editor_WindowManager.h
@@ -7,22 +7,11 @@
 // Standard Libraries (BG convention: use <> instead of "")
 #include <string>
 
-
 // Third-Party Libraries (BG convention: use <> instead of "")
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtx/string_cast.hpp>
-#include <glm/gtx/matrix_decompose.hpp>
-
-#include <imgui.h>
-
-#include <ImGuizmo.h>
 
 // Internal Libraries (BG convention: use <> instead of "")
-#include <ERS_CLASS_LoggingSystem.h>
-
-#include <ERS_STRUCT_Camera.h>
-#include <ERS_STRUCT_LocRotScale.h>
+#include <ERS_STRUCT_SystemUtils.h>
 
 
+
+class ERS_
