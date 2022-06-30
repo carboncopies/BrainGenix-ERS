3687405943ea1ef8a5506519923c57a454f9cd7d
datacrystals <tliao@carboncopies.org>
2022-06-30 22:18:03 +0000

    Add Window Manager (#242)

--- Source/Core/Editor/Utils/ERS_Editor_WindowManager/ERS_Editor_WindowManager.h
+++ Source/Core/Editor/Utils/ERS_Editor_WindowManager/ERS_Editor_WindowManager.h
@@ -26,8 +26,51 @@ private:
     ERS_STRUCT_SystemUtils* SystemUtils_; /**<Pointer to System utillities struct, used to get things like logger, etc.*/
 
 public:
+
+    /**
+     * @brief Construct a new ers class windowmanager object
+     * 
+     */
     ERS_CLASS_WindowManager();
+
+    /**
+     * @brief Destroy the ers class windowmanager object
+     * 
+     */
     ~ERS_CLASS_WindowManager();
 
+
+    /**
+     * @brief Call this once per frame at the end of the GUI update process - will call the draw function on all windows in the system.
+     * 
+     */
+    void UpdateAllWindows();
+
+    /**
+     * @brief Returns a list containing the name of all windows in the system. 
+     * These names can be used to query the state of a given window, or set the state of that window.
+     * 
+     * @return std::vector<std::string> 
+     */
+    std::vector<std::string> GetWindowNames();
+
+    /**
+     * @brief Use this function to get the state of the window in question.
+     * Will return true for enabled, or false for disabled.
+     * 
+     * @param WindowName 
+     * @return true 
+     * @return false 
+     */
+    bool GetWindowStatus(std::string WindowName);
+
+    /**
+     * @brief Enable or disable a window with this function.
+     * Pass in the window's name and the state to set it to.
+     * 
+     * @param WindowName 
+     * @param WindowStatus 
+     */
+    void SetWindowStatus(std::string WindowName, bool WindowStatus);
 
 };
