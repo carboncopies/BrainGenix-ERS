dee251a436900e216e3d4c79c5598259c4570caf
datacrystals <tliao@carboncopies.org>
2022-07-02 02:58:03 +0000

    Add Window Manager (#242)

--- Source/Core/Editor/Utils/ERS_Editor_WindowManager/ERS_Editor_WindowManager.cpp
+++ Source/Core/Editor/Utils/ERS_Editor_WindowManager/ERS_Editor_WindowManager.cpp
@@ -30,7 +30,22 @@ ERS_CLASS_ThemeManager* ThemeManager, Cursors3D* Gizmo) {
     SystemUtils_->Logger_->Log("WindowManager Subsystem Setting Up Window Struct", 4);
 
     // Construct All Widnows
-    Windows_->GUI_Window_About_ = std::make_unique<GUI_Window_About>(SystemUtils_);
-    Windows_->GUI_Window_AssetExplorer_ = std::make_unique<GUI_Window_AssetExplorer>(SystemUtils_, ProjectUtils);
+    Windows_->GUI_Window_About_                = std::make_unique<GUI_Window_About>(SystemUtils_);
+    Windows_->GUI_Window_AssetExplorer_        = std::make_unique<GUI_Window_AssetExplorer>(SystemUtils_, ProjectUtils);
+    Windows_->GUI_Window_ControllerSettings_   = 
+    Windows_->GUI_Window_FrameLatencyGraph_    = 
+    Windows_->GUI_Window_FramerateCounter_     =
+    Windows_->GUI_Window_FramerateHistogram_   =
+    Windows_->GUI_Window_FrameratePlot_        =
+    Windows_->GUI_Window_ImportProgressBar_    =
+    Windows_->GUI_Window_ObjectProperties_     =
+    Windows_->GUI_Window_RAMGraph_             =
+    Windows_->GUI_Window_RenderingSettings_    =
+    Windows_->GUI_Window_SceneTree_            =
+    Windows_->GUI_Window_ScriptEditor_         =
+    Windows_->GUI_Window_ShaderEditor_         =
+    Windows_->GUI_Window_SystemLog_            =
+    Windows_->GUI_Window_TestEditor_           =
+    Windows_->GUI_Window_ThemeSelector_        =
 
 }
