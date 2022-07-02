0aa7d0f82f7b6eb0a3936f5efbf9ed533b2e810a
datacrystals <tliao@carboncopies.org>
2022-07-02 12:07:34 +0000

    Add Window Manager (#242)

--- Source/Core/Editor/Utils/ERS_Editor_WindowManager/ERS_Editor_WindowManager.h
+++ Source/Core/Editor/Utils/ERS_Editor_WindowManager/ERS_Editor_WindowManager.h
@@ -80,7 +80,7 @@ public:
      * 
      */
     void GenerateWindowStruct(ERS_STRUCT_ProjectUtils* ProjectUtils, ERS_STRUCT_HumanInputDeviceUtils* HIDUtils, ERS_CLASS_VisualRenderer* VisualRenderer,
-    ERS_CLASS_ThemeManager* ThemeManager, Cursors3D* Gizmo, ERS_CLASS_SceneManager* SceneManager);
+    Cursors3D* Gizmo, ERS_CLASS_SceneManager* SceneManager);
 
 
     /**
