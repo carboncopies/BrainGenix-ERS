17dc8e72d8267ddd3ab8b6358f58cc92c1195145
datacrystals <tliao@carboncopies.org>
2022-07-04 15:23:44 +0000

    Move View Menu To Settings Menu (#263)

--- Source/Core/Editor/Menus/GUI_Menu_File/GUI_Menu_File.cpp
+++ Source/Core/Editor/Menus/GUI_Menu_File/GUI_Menu_File.cpp
@@ -52,22 +52,6 @@ void GUI_Menu_File::Draw() {
         }
         ImGui::Separator();
 
-        // Scene Options
-        // if (ImGui::MenuItem("Save Active Scene")) {
-        //     SceneWriter_->ProcessScene(
-        //         SceneManager_->Scenes_[SceneManager_->ActiveScene_].get(),
-        //         SceneManager_->Scenes_[SceneManager_->ActiveScene_]->ScenePath
-        //         );
-        // }
-        // if (ImGui::MenuItem("Save All Scenes")) {
-        //     for (int i = 0; (long)i < (long)SceneManager_->Scenes_.size(); i++) {
-        //         SceneWriter_->ProcessScene(
-        //             SceneManager_->Scenes_[i].get(),
-        //             SceneManager_->Scenes_[i]->ScenePath
-        //             );
-        //     }
-        // }
-
         // Save All
         if (ImGui::MenuItem("Save")) {
 
