ecadad05e6299f08679349dfaa2721d645ec2e4e
datacrystals <tliao@carboncopies.org>
2022-07-04 13:04:34 +0000

    Implement Per-Model Shader Control (#91)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_SetShaderUniformData.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_SetShaderUniformData.cpp
@@ -17,12 +17,12 @@ void ERS_FUNCTION_SetShaderUniformData(ERS_STRUCT_Shader* Shader, ERS_STRUCT_Sha
 
 
     // Set Camera Data
-    Shaders_[ShaderIndex]->SetMat4  ("projection",               Data.Projection_);
-    Shaders_[ShaderIndex]->SetMat4  ("view",                     Data.View_);
-    Shaders_[ShaderIndex]->SetFloat ("Exposure_",                Data.Exposure_);
-    Shaders_[ShaderIndex]->SetFloat ("Gamma_",                   Data.Gamma_);
-    Shaders_[ShaderIndex]->SetBool  ("GammaCorrectionEnabled_",  Data.GammaCorrectionEnabled_);
-    Shaders_[ShaderIndex]->SetBool  ("HDREnabled_",              Data.HDREnabled_);
+    Shader->SetMat4  ("projection",               Data.Projection_);
+    Shader->SetMat4  ("view",                     Data.View_);
+    Shader->SetFloat ("Exposure_",                Data.Exposure_);
+    Shader->SetFloat ("Gamma_",                   Data.Gamma_);
+    Shader->SetBool  ("GammaCorrectionEnabled_",  Data.GammaCorrectionEnabled_);
+    Shader->SetBool  ("HDREnabled_",              Data.HDREnabled_);
 
 
     // Set Shadow Info
