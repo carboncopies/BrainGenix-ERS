570c941c78771d64104ebce7459cf79fd1f43fa8
datacrystals <tliao@carboncopies.org>
2022-07-05 05:57:10 +0000

    Add File Open Option To ERS (#264)

--- Source/Core/Utils/ERS_CLASS_ArgumentParser/ERS_CLASS_ArgumentParser.h
+++ Source/Core/Utils/ERS_CLASS_ArgumentParser/ERS_CLASS_ArgumentParser.h
@@ -9,17 +9,8 @@
 #include <chrono>
 
 // Third-Party Libraries (BG convention: use <> instead of "")
-#include <yaml-cpp/yaml.h>
-
-#include <infoware/cpu.hpp>
-#include <infoware/version.hpp>
-#include <infoware/system.hpp>
-#include <infoware/pci.hpp>
-#include <infoware/gpu.hpp>
 
 // Internal Libraries (BG convention: use <> instead of "")
-#include <ERS_CLASS_LoggingSystem.h>
-#include <ERS_STRUCT_HardwareInfo.h>
 
 
 /**
@@ -30,40 +21,14 @@ class ERS_CLASS_ArgumentParser {
 
     private:
 
-        // Class Instances
-        ERS_CLASS_LoggingSystem* Logger_; /**<Instance Of Logging System*/
-        ERS_STRUCT_HardwareInfo HardwareInfo_; /**<Internal Hardware Information Struct*/
 
-        // Config
-        YAML::Node SystemConfiguration_; /**MSystem Configuration*/
-
-        // Functions from https://github.com/ThePhD/infoware/blob/main/examples
-        const char* gpu_vendor_name(iware::gpu::vendor_t vendor) noexcept;
-        const char* kernel_variant_name(iware::system::kernel_t variant) noexcept;
-        const char* endianness_name(iware::cpu::endianness_t endianness) noexcept;
-        const char* architecture_name(iware::cpu::architecture_t architecture) noexcept;
-        const char* cache_type_name(iware::cpu::cache_type_t cache_type) noexcept;
-
-        // Internal Functions
-        void GetDynamicInformation(); /**<Updates Dynamic Information*/
-        void DynamicInformationThread(); /**Function To Update Info In Background*/
-        std::thread SpawnThread(); /**<Create New Thread*/
-
-        // Control Vars
-        float DynamicInfoRefreshRate_; /*<Set Number Of ms To wait until next dynamic info refresh*/
-        bool ShouldDynamicInfoThreadRun_ = true; /**<Control Variable For Dynamic Info Thread*/
-
-        std::thread DynamicUpdateThread_; /**<Dynamic Update Thread*/
 
     public:
 
         /**
          * @brief Construct a new Hardware Information object
-         * 
-         * @param Logger 
-         * @param SystemConfig 
          */
-        ERS_CLASS_ArgumentParser(ERS_CLASS_LoggingSystem* Logger, YAML::Node SystemConfig);
+        ERS_CLASS_ArgumentParser();
 
         /**
          * @brief Destroy the Hardware Information object
