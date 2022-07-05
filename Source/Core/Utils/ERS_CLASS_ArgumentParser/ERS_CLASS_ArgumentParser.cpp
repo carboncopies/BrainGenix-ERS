0665a8431852bf3eafa8ac156b5b0e50fce2f5a9
datacrystals <tliao@carboncopies.org>
2022-07-05 06:11:36 +0000

    Add File Open Option To ERS (#264)

--- Source/Core/Utils/ERS_CLASS_ArgumentParser/ERS_CLASS_ArgumentParser.cpp
+++ Source/Core/Utils/ERS_CLASS_ArgumentParser/ERS_CLASS_ArgumentParser.cpp
@@ -4,3 +4,23 @@
 
 #include <ERS_CLASS_ArgumentParser.h>
 
+
+ERS_CLASS_ArgumentParser::ERS_CLASS_ArgumentParser(ERS_CLASS_LoggingSystem* Logger) {
+
+    Logger_ = Logger;
+    Logger_->Log("Initialiizng Argument Parser Subsystem", 4);
+
+    ArgumentPairs_.clear();
+    Arguments_.clear();
+    ArgumentString_ = "";
+
+}
+
+ERS_CLASS_ArgumentParser::~ERS_CLASS_ArgumentParser() {
+
+    Logger_->Log("Argument Parser Destructor Called", 6);
+
+}
+
+
+
