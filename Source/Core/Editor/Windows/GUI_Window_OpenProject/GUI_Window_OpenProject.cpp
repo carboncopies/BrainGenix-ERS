06f43e1470eeeedd889c6b7d0fac437963233f89
datacrystals <tliao@carboncopies.org>
2022-07-05 07:04:54 +0000

    Add File Open Option To ERS (#264)

--- Source/Core/Editor/Windows/GUI_Window_OpenProject/GUI_Window_OpenProject.cpp
+++ Source/Core/Editor/Windows/GUI_Window_OpenProject/GUI_Window_OpenProject.cpp
@@ -2,28 +2,28 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-#include <GUI_ImportAsset.h>
+#include <GUI_Window_OpenProject.h>
 
 
-GUI_ImportAsset::GUI_ImportAsset(ERS_STRUCT_SystemUtils* SystemUtils) {
+GUI_Window_OpenProject::GUI_Window_OpenProject(ERS_STRUCT_SystemUtils* SystemUtils) {
 
     SystemUtils_ = SystemUtils;
     SystemUtils_->Logger_->Log("Initializing Asset Importer GUI", 5);
 
     GUI_Window_ImportProgressBar_ = std::make_unique<GUI_Window_ImportProgressBar>(SystemUtils_);
-    AssetImportBackend_ = std::make_unique<ERS_CLASS_ImportAsset>(SystemUtils_);
+    AssetImportBackend_ = std::make_unique<ERS_CLASS_Window_OpenProject>(SystemUtils_);
 
 }
 
 
-GUI_ImportAsset::~GUI_ImportAsset() {
+GUI_Window_OpenProject::~GUI_Window_OpenProject() {
 
     SystemUtils_->Logger_->Log("Asset Importer GUI Destructor Called", 6);
 
 }
 
 
-void GUI_ImportAsset::Draw() {
+void GUI_Window_OpenProject::Draw() {
 
     // Draw File Dialog
     if (ImGuiFileDialog::Instance()->Display("Import Model", ImGuiWindowFlags_None, ImVec2(400, 200))) {
@@ -59,7 +59,7 @@ void GUI_ImportAsset::Draw() {
 
 }
 
-void GUI_ImportAsset::OpenFileDialog() {
+void GUI_Window_OpenProject::OpenFileDialog() {
 
     ImGuiFileDialog::Instance()->OpenDialog("Import Model", "Import Model", ".*", ".", "", 0);
 
