a6c99df5491f34d274c39e4bd2677b2a48efd00c
datacrystals <tliao@carboncopies.org>
2022-07-06 05:30:59 +0000

    Implement Option To Create New Project (#271)

--- Source/Core/Editor/Windows/GUI_Window_OpenProject/GUI_Window_OpenProject.cpp
+++ Source/Core/Editor/Windows/GUI_Window_OpenProject/GUI_Window_OpenProject.cpp
@@ -29,7 +29,7 @@ void GUI_Window_OpenProject::Draw() {
     if (Enabled_) {
 
         // Draw File Dialog
-        if (ImGuiFileDialog::Instance()->Display("Open Project", ImGuiWindowFlags_None, ImVec2(400, 200))) {
+        if (ImGuiFileDialog::Instance()->Display("Open Project", ImGuiWindowFlags_None, ImVec2(600, 300))) {
 
 
             if (ImGuiFileDialog::Instance()->IsOk())
