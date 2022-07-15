efa0fc8cf2cd5c13d21e9b1a6b181230f58e6a97
datacrystals <tliao@carboncopies.org>
2022-07-15 23:24:15 +0000

    Debug OpenGL Errors

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ShadowMaps.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_ShadowMaps.cpp
@@ -210,13 +210,13 @@ void ERS_CLASS_ShadowMaps::UpdateShadowMaps(ERS_STRUCT_Shader* DepthMapShader, E
     // Handle Updating Depth Maps
     std::vector<ERS_STRUCT_DepthMap*> DepthMaps;
     std::vector<glm::vec3> LightPositions;
-    std::cout<<"============================================"<<glGetError()<<std::endl;
+    //std::cout<<"============================================"<<glGetError()<<std::endl;
 
     GetDepthMaps(&DepthMaps, &LightPositions);
-    std::cout<<"============================================"<<glGetError()<<std::endl;
+    //std::cout<<"============================================"<<glGetError()<<std::endl;
 
     PrioritizeDepthMaps(DepthMaps, LightPositions, CameraPosition);
-    std::cout<<"============================================"<<glGetError()<<std::endl;
+    //std::cout<<"============================================"<<glGetError()<<std::endl;
 
     // Update All Depth Maps
     ERS_CLASS_DepthMaps_->UpdateDepthMaps(DepthMapShader, CubemapDepthShader);
