281f0b02d038d6aabfa24ad8bc57f3409c4dde9c
datacrystals <tliao@carboncopies.org>
2022-07-18 18:05:03 +0000

    Outline Classes (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingManager.h
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingManager.h
@@ -49,3 +49,15 @@ class ERS_CLASS_AssetStreamingManager {
 class ERS_CLASS_AsyncTextureUpdater {
 
 };
+
+
+/**
+ * @brief This class gets information about how the system is performing to influence the streaming manager.
+ * Essentially, it just informs the manager when we're about to run out of memory to ensure that we can unload things as needed.
+ * Additionally, it does the opposite - informing us when we have more memory so that we know when we can load more textures into CPU RAM.
+ * This class gets the CPU/System RAM info from the Hardware Information Class, and processes that info to make 'reccomendataions' to the manager.
+ * 
+ */
+class ERS_CLASS_AssetStreamingSystemResourceMonitor {
+
+};
