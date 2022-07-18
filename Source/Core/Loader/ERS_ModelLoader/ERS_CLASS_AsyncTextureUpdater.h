adb1561048f95b880f5be10b326816f9b466a28a
datacrystals <tliao@carboncopies.org>
2022-07-18 18:50:45 +0000

    Outline Class Methods (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.h
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.h
@@ -29,5 +29,59 @@
  */
 class ERS_CLASS_AsyncTextureUpdater {
 
+private:
+
+    ERS_STRUCT_SystemUtils* SystemUtils_; /**<Struct containing essential services such as logging and Asset IO*/
+
+public:
+
+    /**
+     * @brief Create instance of the texture updater. The optional threads parameters sets the limit on the number of threads we can use.
+     * 
+     * @param SystemUtils 
+     * @param Threads Setting this to 0 will autodetect the number of threads we should use. Otherwise, we'll use whatever is passed in.
+     */
+    ERS_CLASS_AsyncTextureUpdater(ERS_STRUCT_SystemUtils* SystemUtils, unsigned int Threads = 0);
+
+    /**
+     * @brief Destroy the ers class asynctextureupdater object
+     * 
+     */
+    ~ERS_CLASS_AsyncTextureUpdater();
+
+
+    /**
+     * @brief Adds a work item to the queue to load the minimum size texture so we have something to display - this is kept in memory constantly.
+     * 
+     * @param Model 
+     */
+    void LoadInitialTexture(ERS_STRUCT_Model* Model);
+
+    /**
+     * @brief Adds a work item to the queue to load the specified texture (resolution) level.
+     * If the level does not exist, no work will be done.
+     * 
+     * Texture levels are the inverse of mip maps, the higher the number, the higher the resolution.
+     * We do this to simplify what goes on inside this class, apologies for the confusion.
+     * 
+     * @param Model 
+     * @param TextureLevel 
+     */
+    void LoadTextureLevel(ERS_STRUCT_Model* Model, int TextureLevel);
+
+    /**
+     * @brief Add work item(s) to load all texture levels up to the specified range.
+     * If you put int for example 4 as the tex level, we'll load 0,1,2,3,4 (unless one of those is already loaded in which case we skip it)
+     * 
+     * 
+     * Texture levels are the inverse of mip maps, the higher the number, the higher the resolution.
+     * We do this to simplify what goes on inside this class, apologies for the confusion.
+     * 
+     * @param Model 
+     * @param TextureLevel 
+     */
+    void LoadTextureLevelRange(ERS_STRUCT_Model* Model, int TextureLevel);
+
+
 };
 
