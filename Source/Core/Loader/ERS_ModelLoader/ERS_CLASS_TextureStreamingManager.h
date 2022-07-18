07d2b3c4bb5d590d97efe4a282e8d57b06144522
datacrystals <tliao@carboncopies.org>
2022-07-18 17:48:31 +0000

    Cleanup Model Loader (#252)

---
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_TextureStreamingManager.h
@@ -0,0 +1,42 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <future>
+#include <thread>
+#include <chrono>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <FreeImage.h>
+
+//#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_STRUCT_Mesh.h>
+#include <ERS_STRUCT_Texture.h>
+#include <ERS_STRUCT_Vertex.h>
+#include <ERS_STRUCT_Model.h>
+
+#include <ERS_STRUCT_SystemUtils.h>
