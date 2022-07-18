5b836b176317fca0bf53b07d4ee9f7cfde7866bd
datacrystals <tliao@carboncopies.org>
2022-07-18 18:01:22 +0000

    Outline Classes (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_TextureStreamingManager.h
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_TextureStreamingManager.h
@@ -34,3 +34,18 @@ class ERS_CLASS_AssetStreamingManager {
 
 
 };
+
+
+/**
+ * @brief This class is responsible for managing the different textures that the model needs. 
+ * As the Asset Streaming Manager Tells us what to do, here we use lots of threads to load the different textures and move them in and out of GPU memory and regular system RAM.
+ * 
+ * Texture Caching:
+ * We have two different levels of caching here, firstly there's GPU ram for textures actively in use or frequently in use.
+ * When a texture is deemed not importent enough to be moved out of that level, we store it in CPU memory until that's full. 
+ * This lets us keep our textures as close to the GPU as possble which lets us reduce pop-in issues.
+ * 
+ */
+class ERS_CLASS_AsyncTextureUpdater {
+
+};
