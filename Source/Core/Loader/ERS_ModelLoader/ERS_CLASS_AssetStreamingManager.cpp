6cf3a239272a23d928b77466c4504de9dc212436
datacrystals <tliao@carboncopies.org>
2022-07-18 22:36:01 +0000

    Implement System Methods (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingManager.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingManager.cpp
@@ -35,3 +35,4 @@ void ERS_CLASS_AssetStreamingManager::SetCameraStructs(std::vector<ERS_STRUCT_Ca
 void ERS_CLASS_AssetStreamingManager::PreloadScene(ERS_STRUCT_Scene* Scene) {
     std::cout<<"WARNING: NOT YET IMPLEMENTED!\n";
 }
+
