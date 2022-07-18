42b42c89e14c6a4af5953560a8ad39a900632e1e
datacrystals <tliao@carboncopies.org>
2022-07-18 19:48:51 +0000

    Implement System Methods (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingSystemResourceMonitor.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingSystemResourceMonitor.cpp
@@ -11,6 +11,8 @@ ERS_CLASS_AssetStreamingSystemResourceMonitor::ERS_CLASS_AssetStreamingSystemRes
     SystemUtils_->Logger_->Log("Starting Asset Streaming Subsystem", 5);
 
     // Get System Resource Info
+    SystemUtils_->Logger_->Log("Detecting Hardware Memory Information", 4);
+    ERS_STRUCT_HardwareInfo HWInfo = SystemUtils_->ERS_CLASS_HardwareInformation_->GetHWInfo();
 
 
 
