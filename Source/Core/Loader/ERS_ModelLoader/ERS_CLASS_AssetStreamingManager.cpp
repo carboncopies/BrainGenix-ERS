c428b5634c1ed42858c6ff37dd0a232d51cf4616
datacrystals <tliao@carboncopies.org>
2022-07-18 20:46:24 +0000

    Implement System Methods (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingManager.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingManager.cpp
@@ -3,3 +3,14 @@
 //======================================================================//
 
 #include <ERS_CLASS_AssetStreamingManager.h>
+
+
+ERS_CLASS_AssetStreamingManager::ERS_CLASS_AssetStreamingManager(ERS_STRUCT_SystemUtils* SystemUtils) {
+
+    SystemUtils_ = SystemUtils;
+    SystemUtils_->Logger_->Log("Initializing Asset Streaming Subsystem", 5);
+
+    // Setup Subsystems
+    ResourceMonitor_ = std::make_shared<ERS_CLASS_AssetStreamingSystemResourceMonitor>(SystemUtils_);
+
+}
