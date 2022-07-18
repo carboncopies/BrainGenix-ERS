3ce9a88cb5105632681e5b86a1d7c48e0674d24a
datacrystals <tliao@carboncopies.org>
2022-07-18 18:39:34 +0000

    Outline Class Methods (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingSystemResourceMonitor.h
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingSystemResourceMonitor.h
@@ -25,6 +25,66 @@
  */
 class ERS_CLASS_AssetStreamingSystemResourceMonitor {
 
+private:
+
+    ERS_STRUCT_SystemUtils* SystemUtils_; /**<Struct containing essential services such as logging and Asset IO*/
+
+public:
+
+    /**
+     * @brief Sets up the resource monitor for the asset streaming service.
+     * 
+     * @param SystemUtils 
+     */
+    ERS_CLASS_AssetStreamingSystemResourceMonitor(ERS_STRUCT_SystemUtils* SystemUtils);
+
+    /**
+     * @brief Destroy the ers class assetstreamingsystemresourcemonitor object
+     * 
+     */
+    ~ERS_CLASS_AssetStreamingSystemResourceMonitor();
+
+    /**
+     * @brief Determines if we can both fit the given texture in VRAM and within our self-imposed texture budget.
+     * The actual vram limit is pretty self-explanitory. We just want to make sure that there's still space for the given texture.
+     * The self imposed limit regarding texture budget allows us to ensure we don't eat all the system's resources - leading to a crash.
+     * 
+     * The texture VRAM budget can be configured via the SetTextureVRAMBudget method, or by adjusting the rendering settings in the systemutils struct.
+     * 
+     * @param NumberBytes 
+     * @return true 
+     * @return false 
+     */
+    bool TextureFitsInVRAMBudget(unsigned long NumberBytes);
+
+    /**
+     * @brief This allows us to update the current texture VRAM budget.
+     * If a number is larger than the systems memory, that limit will apply first so setting it too high is okay.
+     * 
+     * @param NewTextureVRAMBudget 
+     */
+    void SetTextureVRAMBudget(unsigned long NewTextureVRAMBudget);
+
+    /**
+     * @brief Determines if we can fit the given texture into regular system memory and within our self-imposed budget.
+     * This is basically the same as 'TextureFitsInVRAMBudget' but for system memory.
+     * 
+     * Again, the budget can be adjusted with the associated SetTextureRAMBudget call or via the rendering settings strut in SystemUtils.
+     * 
+     * @param NumberBytes 
+     * @return true 
+     * @return false 
+     */
+    bool TextureFitsInRAMBudget(unsigned long NumberBytes);
+
+    /**
+     * @brief This does the same thing that set vram budget does but for the system's RAM budget.
+     * We require that this be larger than the VRAM budget due to some optimizations we've made internally.
+     * Ideally this RAM budget should be around 4X larger than the VRAM budget.
+     * 
+     * @param NewTextureRAMBudget 
+     */
+    void SetTextureRAMBudget(unsigned long NewTextureRAMBudget);
 
 
 };
