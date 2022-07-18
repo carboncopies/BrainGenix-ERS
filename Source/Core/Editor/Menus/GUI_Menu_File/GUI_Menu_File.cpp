d616b5a35d5041e07cc8f67562ef3cfbfffbb335
datacrystals <tliao@carboncopies.org>
2022-07-18 02:41:18 +0000

    Cleanup Lang Test

--- Source/Core/Editor/Menus/GUI_Menu_File/GUI_Menu_File.cpp
+++ Source/Core/Editor/Menus/GUI_Menu_File/GUI_Menu_File.cpp
@@ -31,8 +31,6 @@ void GUI_Menu_File::Draw() {
     // File Menu
     if (ImGui::BeginMenu("File")) {
 
-        ImGui::MenuItem("Ceci est un test d'ajout de français");
-
         ImGui::MenuItem("Open", "", &Windows_->GUI_Window_OpenProject_->Enabled_);
         ImGui::Separator();
 
