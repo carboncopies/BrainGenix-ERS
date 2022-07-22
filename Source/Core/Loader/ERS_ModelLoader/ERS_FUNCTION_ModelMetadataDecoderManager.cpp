1864502f30f241022477f4655e22bf68bd5143c0
datacrystals <tliao@carboncopies.org>
2022-07-22 23:35:11 +0000

    Implement Image Importing System (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_FUNCTION_ModelMetadataDecoderManager.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_FUNCTION_ModelMetadataDecoderManager.cpp
@@ -7,7 +7,18 @@
 
 bool ERS_FUNCTION_DecodeModelMetadata(YAML::Node Metadata, ERS_STRUCT_Model* Model, ERS_STRUCT_SystemUtils* SystemUtils, long AssetID) {
 
+    // Detect The Current Version
+    std::string FormatVersion = "0.0.0";
+    if (Metadata["FormatVersion"]) {
+        FormatVersion = Metadata["FormatVersion"].as<std::string>();
+    }
 
+    // Decode Based On Version
+    if (FormatVersion == "0.0.0") {
+
+    } else if (FormatVersion == "0.0.1") {
+        
+    }
 
 
 }
