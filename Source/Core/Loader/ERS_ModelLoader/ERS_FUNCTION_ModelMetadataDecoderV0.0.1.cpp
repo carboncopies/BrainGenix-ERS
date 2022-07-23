4c3dcebc0ba20cab4bbbfe586fb61632369b8daf
datacrystals <tliao@carboncopies.org>
2022-07-23 00:55:48 +0000

    Implement Image Importing System (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_FUNCTION_ModelMetadataDecoderV0.0.1.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_FUNCTION_ModelMetadataDecoderV0.0.1.cpp
@@ -50,6 +50,8 @@ bool ERS_FUNCTION_DecodeModelMetadataV001(YAML::Node Metadata, ERS_STRUCT_Model*
                     Texture.LevelTextureIDs.push_back(LevelInfo["TextureLevelAssetID"].as<long>());
                     Texture.LevelMemorySizeBytes.push_back(LevelInfo["TextureLevelMemorySizeBytes"].as<int>());
                     Texture.LevelResolutions.push_back(std::make_pair(LevelInfo["TextureLevelResolutionX"].as<int>(), LevelInfo["TextureLevelResolutionY"].as<int>()));
+                    Texture.LevelLoadedInRAM.push_back(false);
+                    Texture.LevelLoadedInVRAM.push_back(false);
 
                     SystemUtils->Logger_->Log(std::string("Detected Texture Level '") + std::to_string(LevelIterator->first.as<int>())
                     + "', Resolution '" + std::to_string(LevelInfo["TextureLevelResolutionX"].as<int>())
