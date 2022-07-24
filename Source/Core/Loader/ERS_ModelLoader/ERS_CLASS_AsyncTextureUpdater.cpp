6da6b7b71186017c4325321899fb9a752d2689b2
datacrystals <tliao@carboncopies.org>
2022-07-24 04:35:47 +0000

    Implement Image Importing System (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
@@ -3,3 +3,6 @@
 //======================================================================//
 
 #include <ERS_CLASS_AsyncTextureUpdater.h>
+
+// todo, implement system that searches over all models in the scene and loads the requested texture level
+// also for now, we disable reference loading acceleration due to simplicity.
