cf6338f9639fdf272b60db7f491c3399ef7da16b
datacrystals <tliao@carboncopies.org>
2022-07-24 03:43:56 +0000

    Implement Image Importing System (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_ModelLoader.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_ModelLoader.cpp
@@ -470,8 +470,7 @@ void ERS_CLASS_ModelLoader::LoadModel(long AssetID, std::shared_ptr<ERS_STRUCT_M
     }
 
     // Decode Mesh, Create Texture Pointers
-    std::vector<std::pair<std::string, std::string>>* ModelRequestedTextures;
-    ProcessNode(&(*Model), Scene->mRootNode, Scene, ModelRequestedTextures);
+    ProcessNode(&(*Model), Scene->mRootNode, Scene);
 
     // // Get Texture Images From Loader, Push Into Vector
     // for (unsigned long i = 0; i < DecodedTextures.size(); i++) {
