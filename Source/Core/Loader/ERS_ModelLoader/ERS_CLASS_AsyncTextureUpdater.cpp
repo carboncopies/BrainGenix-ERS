0d02029f297fa28d3fb023cf574c3129945ecc70
datacrystals <tliao@carboncopies.org>
2022-07-26 05:43:15 +0000

    Implement Image Importing System (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
@@ -77,13 +77,16 @@ ERS_CLASS_AsyncTextureUpdater::~ERS_CLASS_AsyncTextureUpdater() {
 }
 
 
-void ERS_CLASS_AsyncTextureUpdater::UploadTextureData(FIBITMAP* ImageData, int Width, int Height, int Channels, unsigned int TextureID, int MipMapLevel, int RowsPerUpload = -1) {
+void ERS_CLASS_AsyncTextureUpdater::UploadTextureData(FIBITMAP* ImageData, int Width, int Height, int Channels, unsigned int TextureID, int MipMapLevel) {
 
-    // Generate PBO
+    // Generate And Initialize PBO (We use this as an intermediary stage to transfer image data from ram to GPU Texture)
     unsigned int PixelBufferObjectID;
     glGenBuffers(1, &PixelBufferObjectID);
     glBindBuffer(GL_PIXEL_UNPACK_BUFFER, PixelBufferObjectID);
-    glBufferData(GL_PIXEL_UNPACK_BUFFER, Width*Height)
+    glBufferData(GL_PIXEL_UNPACK_BUFFER, Width*Height*Channels, NULL, GL_STREAM_DRAW);
+
+    // Get Image Data
+    unsigned char* ImageBytes = (unsigned char*)ImageData->data;
 
 
 }
