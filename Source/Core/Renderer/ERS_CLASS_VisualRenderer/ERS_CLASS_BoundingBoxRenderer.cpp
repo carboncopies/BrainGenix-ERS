b41227d563e544519b021eaf5a2b0b6c6d54954c
datacrystals <tliao@carboncopies.org>
2022-07-27 07:24:27 +0000

    Implement Image Importing System (#252)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_BoundingBoxRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_BoundingBoxRenderer.cpp
@@ -145,7 +145,7 @@ void ERS_CLASS_BoundingBoxRenderer::DrawAll(ERS_STRUCT_Camera* Camera, ERS_STRUC
             ModelOutlineColor = glm::vec3(1.0f - LevelPercentage, LevelPercentage, 0.0f);
         }
 
-        std::cout<<Scene->Models[i]->BoxScale_<<std::endl;
+        std::cout<<glm::to_string(Scene->Models[i]->BoxScale_)<<std::endl;
 
         // Set Shader Uniforms
         BoundingBoxRendererShader_->SetMat4("Model", ModelMatrix);
