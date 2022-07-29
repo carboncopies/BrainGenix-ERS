cadc6716a57d7469a80681987013e0d51027bcf8
datacrystals <tliao@carboncopies.org>
2022-07-29 23:12:35 +0000

    Implement Image Importing System (#252)

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_DrawMesh.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_FUNCTION_DrawMesh.cpp
@@ -90,7 +90,7 @@ void ERS_FUNCTION_DrawMesh(ERS_STRUCT_Mesh* Mesh, ERS_STRUCT_OpenGLDefaults* Ope
         if (Mesh->Textures_[i]->HasAnyLevelReady) {
             glActiveTexture(GL_TEXTURE0 + TypeID);
             glBindTexture(GL_TEXTURE_2D, Mesh->Textures_[i]->BestAvailableOpenGLID);
-
+            std::cout<<Type<<"  |  "<<Mesh->Textures_[i]->BestAvailableOpenGLID<<std::endl;
         } else {
             glActiveTexture(GL_TEXTURE0 + TypeID);
             glBindTexture(GL_TEXTURE_2D, OpenGLDefaults->Loadingtexture_);
