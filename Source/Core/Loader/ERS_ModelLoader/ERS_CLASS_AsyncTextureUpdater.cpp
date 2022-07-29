a23ee8b1ee319bf7c2d4ebb17247554d5d08a93a
datacrystals <tliao@carboncopies.org>
2022-07-29 00:25:05 +0000

    Implement Image Importing System (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
@@ -347,20 +347,29 @@ void ERS_CLASS_AsyncTextureUpdater::SortModels(ERS_STRUCT_Scene* Scene) {
         // If There's Anything To Update, Add To Queue
         if (VRAMUpdate || RAMUpdate) {
             BlockThreads_.lock();
+
             bool CanAdd = true;
+
+            // Check If Queue Full
             if (WorkItems_.size() >= WorkQueueLimit_) {
-                BlockThreads_.unlock();
-                return;
+                CanAdd = false;
             }
-            for (unsigned int x = 0; x < WorkItems_.size(); x++) {
-                if (WorkItems_[x] == Scene->Models[i]) {
-                    CanAdd = false;
-                    break;
+
+            // Skip Checking If Already In Queue If We Can't Add
+            if (CanAdd) {
+                for (unsigned int x = 0; x < WorkItems_.size(); x++) {
+                    if (WorkItems_[x] == Scene->Models[i]) {
+                        CanAdd = false;
+                        break;
+                    }
                 }
             }
+
+            // If We Can Actually Add It, Do SO
             if (CanAdd) {
                 WorkItems_.push_back(Scene->Models[i]);
             }
+            
             BlockThreads_.unlock();
         }
 
