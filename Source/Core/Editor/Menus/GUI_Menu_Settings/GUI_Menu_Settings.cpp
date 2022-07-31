51955ce46debeb6d7c326bff78c6bea856433b4a
datacrystals <tliao@carboncopies.org>
2022-07-31 19:04:16 +0000

    Add Layout Manager to GUI.h
    Co-authored-by: allenyangcc <allenyangcc@users.noreply.github.com>

--- Source/Core/Editor/Menus/GUI_Menu_Settings/GUI_Menu_Settings.cpp
+++ Source/Core/Editor/Menus/GUI_Menu_Settings/GUI_Menu_Settings.cpp
@@ -36,27 +36,31 @@ void GUI_Menu_Settings::Draw() {
             // Create Submenu with layouts which the user can select from
             if (ImGui::BeginMenu("Select Layouts")) {
 
-
+                std::string ActiveLayoutName = LayoutManager_->GetActiveLayoutName();
+                std::vector<std::string> LayoutNames = LayoutManager_->GetLayoutNames();
+                for (unsigned int i = 0; i < LayoutNames.size(); i++) {
+                    if (ImGui::Selectable(LayoutNames[i].c_str(), (LayoutNames[i] == ActiveLayoutName))) {
+                        LayoutManager_->ApplyLayout(LayoutNames[i]);
+                    }
+                }
 
             ImGui::EndMenu();
             }
 
             // Normal buttons that the user can select
             if (ImGui::MenuItem("Save Active Layout")) {
-
+                std::string ActiveLayoutName = LayoutManager_->GetActiveLayoutName();
+                LayoutManager_->SaveLayout(ActiveLayoutName);
             }
             
             if (ImGui::MenuItem("Create New Layout")) {
-
+                LayoutManager_->CreateLayout(std::string("Untitled Layout"));
             }
 
             if (ImGui::MenuItem("Rename Active Layout")) {
-
+                // Todo later - not part of layout manager
             }
 
-            if (ImGui::MenuItem("Re-Detect Layouts")) {
-
-            }
 
         ImGui::EndMenu();
         }
