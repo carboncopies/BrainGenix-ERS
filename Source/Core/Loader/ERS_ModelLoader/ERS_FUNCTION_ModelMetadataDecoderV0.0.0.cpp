645f1fea663c3598aacc455c0e408dba09ece89a
datacrystals <tliao@carboncopies.org>
2022-08-01 07:38:03 +0000

    Implement Image Importing System (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_FUNCTION_ModelMetadataDecoderV0.0.0.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_FUNCTION_ModelMetadataDecoderV0.0.0.cpp
@@ -10,6 +10,8 @@ bool ERS_FUNCTION_DecodeModelMetadataV000(YAML::Node Metadata, ERS_STRUCT_Model*
     // Setup Processing Variables
     bool DecodeStatus = true;
 
+    SystemUtils->Logger->Log("Loading Model Metadata Version '0.0.0'", 3, LogEnable);
+
     // Attempt To Decode, Handle Errors
     try {
 
