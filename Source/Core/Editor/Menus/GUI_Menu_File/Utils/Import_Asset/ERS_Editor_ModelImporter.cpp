35451d54bfb5c8e9372d99be880fe69c83a8e2ef
datacrystals <tliao@carboncopies.org>
2022-08-01 06:09:17 +0000

    Implement Image Importing System (#252)

--- Source/Core/Editor/Menus/GUI_Menu_File/Utils/Import_Asset/ERS_Editor_ModelImporter.cpp
+++ Source/Core/Editor/Menus/GUI_Menu_File/Utils/Import_Asset/ERS_Editor_ModelImporter.cpp
@@ -451,11 +451,11 @@ void ERS_CLASS_ModelImporter::WriteTextures(ERS_STRUCT_Model* Model, std::vector
             FIBITMAP* Red = FreeImage_GetChannel(NewImage, FICC_RED);
             FIBITMAP* Green = FreeImage_GetChannel(NewImage, FICC_GREEN);
             FIBITMAP* Blue = FreeImage_GetChannel(NewImage, FICC_BLUE);
-            //FIBITMAP* Alpha = FreeImage_GetChannel(NewImage, FICC_ALPHA);
+            FIBITMAP* Alpha = FreeImage_GetChannel(NewImage, FICC_ALPHA);
             FreeImage_SetChannel(NewImage, Red, FICC_BLUE);
             FreeImage_SetChannel(NewImage, Green, FICC_GREEN);
             FreeImage_SetChannel(NewImage, Blue, FICC_RED);
-            //FreeImage_SetChannel(NewImage, Alpha, FICC_ALPHA);
+            FreeImage_SetChannel(NewImage, Alpha, FICC_ALPHA);
             FreeImage_Unload(Red);
             FreeImage_Unload(Green);
             FreeImage_Unload(Blue);
