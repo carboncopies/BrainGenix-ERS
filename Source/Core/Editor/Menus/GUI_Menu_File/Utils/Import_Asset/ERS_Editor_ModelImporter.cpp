7e8468a1b20ad7f2b10623eb89fd2366e9c576fa
datacrystals <tliao@carboncopies.org>
2022-08-05 20:55:42 +0000

    Implement Image Importing System (#252)

--- Source/Core/Editor/Menus/GUI_Menu_File/Utils/Import_Asset/ERS_Editor_ModelImporter.cpp
+++ Source/Core/Editor/Menus/GUI_Menu_File/Utils/Import_Asset/ERS_Editor_ModelImporter.cpp
@@ -508,11 +508,9 @@ void ERS_CLASS_ModelImporter::WriteTextures(ERS_STRUCT_Model* Model, std::vector
             SystemUtils_->ERS_IOSubsystem_->ReadAsset(ImageAssetID, Data.get());
             FIMEMORY* FIImageData = FreeImage_OpenMemory(Data->Data.get(), Data->Size_B);
             FREE_IMAGE_FORMAT Format = FreeImage_GetFileTypeFromMemory(FIImageData);
-            FIBITMAP* RawImage = FreeImage_LoadFromMemory(Format, FIImageData);
+            FIBITMAP* TestImage = FreeImage_LoadFromMemory(Format, FIImageData);
             FreeImage_CloseMemory(FIImageData);
-            FreeImage_FlipVertical(RawImage);
-            FIBITMAP* TestImage = FreeImage_ConvertTo32Bits(RawImage);
-            FreeImage_Unload(RawImage);
+            FreeImage_FlipVertical(TestImage);
 
             // Detect Channels
             int Line = FreeImage_GetLine(TestImage);
