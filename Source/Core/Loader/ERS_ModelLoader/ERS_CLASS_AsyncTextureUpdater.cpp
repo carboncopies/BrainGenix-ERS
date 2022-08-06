93862ed1d2620512a79d62ed26c99c4448534e08
datacrystals <tliao@carboncopies.org>
2022-08-06 02:04:32 +0000

    Implement Image Importing System (#252)

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
@@ -211,9 +211,9 @@ bool ERS_CLASS_AsyncTextureUpdater::LoadImageDataVRAM(ERS_STRUCT_Texture* Textur
 
     // Get Texture Information
     int MaxLevel = Texture->LevelResolutions.size() - 1;
-    int MaxWidth = Texture->LevelResolutions[MaxLevel - (Level+1)].first;
-    int MaxHeight = Texture->LevelResolutions[MaxLevel - (Level+1)].second;
-    int ImageSize = FreeImage_GetMemorySize(Texture->LevelBitmaps[MaxLevel - (Level+1)]);//Texture->LevelMemorySizeBytes[MaxLevel - Level];
+    int MaxWidth = Texture->LevelResolutions[MaxLevel - Level].first;
+    int MaxHeight = Texture->LevelResolutions[MaxLevel - Level].second;
+    int ImageSize = FreeImage_GetMemorySize(Texture->LevelBitmaps[MaxLevel - Level]);//Texture->LevelMemorySizeBytes[MaxLevel - Level];
 
 
 
@@ -282,7 +282,7 @@ bool ERS_CLASS_AsyncTextureUpdater::LoadImageDataVRAM(ERS_STRUCT_Texture* Textur
     // glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, MaxWidth, MaxHeight, TextureExternFormat, GL_UNSIGNED_BYTE, 0);
 
     // Load MipMaps Into Texture
-    for (int i = 0; i < (Level + 1); i++) {
+    for (int i = 0; i < Level; i++) {
         int Width = Texture->LevelResolutions[i].first;
         int Height = Texture->LevelResolutions[i].second;
         unsigned char* LevelImageBytes = (unsigned char*)FreeImage_GetBits(Texture->LevelBitmaps[i]);
