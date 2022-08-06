cfca9252f350bd2c519562f2d5a5fceb9da6bfc7
datacrystals <tliao@carboncopies.org>
2022-08-06 03:18:59 +0000

    Implement Image Importing System (#252)

--- Source/Core/Structures/ERS_STRUCT_TextureLevel/ERS_STRUCT_TextureLevel.h
+++ Source/Core/Structures/ERS_STRUCT_TextureLevel/ERS_STRUCT_TextureLevel.h
@@ -19,13 +19,13 @@
 // Structure To Hold 2D ERS Texture
 struct ERS_STRUCT_TextureLevel {
 
-    unsigned int LevelTextureOpenGLIDs; /**<List of opengl ids for every texture level*/
-    unsigned int LevelTextureAssetIDs; /**<List of asset ids for every texture level*/
+    unsigned int LevelTextureOpenGLID; /**<List of opengl ids for every texture level*/
+    unsigned int LevelTextureAssetID; /**<List of asset ids for every texture level*/
     bool LevelLoadedInRAM; /**<True/false for every level showing if that level is loaded in RAM*/
     bool LevelLoadedInVRAM; /**<True/false for every level showing f that level is loaded in VRAM*/  
-    std::pair<int, int> LevelResolutions; /**<List of all texture level's resolutions*/
+    std::pair<int, int> LevelResolution; /**<List of all texture level's resolutions*/
     int LevelMemorySizeBytes; /**<Tells the system how many bytes every level will take*/
-    FIBITMAP* LevelBitmaps; /**<List of bitmaps for each level - contains the texture's RAM data.*/
-    int LevelChannels; /**<List containing number of channels for every texture layer*/
+    FIBITMAP* LevelBitmap; /**<List of bitmaps for each level - contains the texture's RAM data.*/
+    int LevelChannel; /**<List containing number of channels for every texture layer*/
 
 };
