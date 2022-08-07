83172ad11deb63152354992a7db81f81711a3f8a
datacrystals <tliao@carboncopies.org>
2022-08-07 04:18:27 +0000

    Fix Windows GPU Usage Issue (#252)

---
+++ Source/GPURequest.h
@@ -0,0 +1,14 @@
+
+// Requests using the high power gpu on windows platforms
+#ifdef _WIN32
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__declspec(dllexport) DWORD NvOptimusEnablement = 1;
+__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -56,19 +56,6 @@
 //-----------------------------------------------------------------------------------------------------
 //
 
-#ifdef _WIN32
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-__declspec(dllexport) DWORD NvOptimusEnablement = 1;
-__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
-
-#ifdef __cplusplus
-}
-#endif
-#endif
-
 
 
 
