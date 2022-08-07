78c2c4d7abcdca5aefd353152531261640391dac
datacrystals <tliao@carboncopies.org>
2022-08-07 04:15:20 +0000

    Fix Windows GPU Usage Issue (#252)

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -56,6 +56,18 @@
 //-----------------------------------------------------------------------------------------------------
 //
 
+#ifdef _WIN32_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__declspec(dllexport) DWORD NvOptimusEnablement = 1;
+__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
 
 
 
