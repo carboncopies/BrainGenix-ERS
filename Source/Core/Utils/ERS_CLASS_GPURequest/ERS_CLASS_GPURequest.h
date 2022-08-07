690322c07ccc8a3e8f9d99f46a45a652a7bdc618
datacrystals <tliao@carboncopies.org>
2022-08-07 04:22:52 +0000

    Fix Windows GPU Usage Issue (#252)

---
+++ Source/Core/Utils/ERS_CLASS_GPURequest/ERS_CLASS_GPURequest.h
@@ -0,0 +1,14 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
