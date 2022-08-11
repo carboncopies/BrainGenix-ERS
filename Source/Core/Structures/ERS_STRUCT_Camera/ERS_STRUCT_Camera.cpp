3415e2f8d1cfa07837f84fc165d9d2767af43dd5
datacrystals <tliao@carboncopies.org>
2022-08-11 04:31:43 +0000

    Implement Scene Camera Integrations

--- Source/Core/Structures/ERS_STRUCT_Camera/ERS_STRUCT_Camera.cpp
+++ Source/Core/Structures/ERS_STRUCT_Camera/ERS_STRUCT_Camera.cpp
@@ -17,9 +17,9 @@ void ERS_STRUCT_Camera::Rotate(glm::vec3 Rotation) {
 
 
     glm::mat4 RotMatrix;
-    RotMatrix = glm::rotate(RotMatrix, glm::radians(Rotation[0]), glm::vec3(1, 0, 0));
-    RotMatrix = glm::rotate(RotMatrix, glm::radians(Rotation[1]), glm::vec3(0, 1, 0));
     RotMatrix = glm::rotate(RotMatrix, glm::radians(Rotation[2]), glm::vec3(0, 0, 1));
+    RotMatrix = glm::rotate(RotMatrix, glm::radians(Rotation[1]), glm::vec3(0, 1, 0));
+    RotMatrix = glm::rotate(RotMatrix, glm::radians(Rotation[0]), glm::vec3(1, 0, 0));
 
 
     Front_ = glm::normalize(glm::vec3(RotMatrix[2]));
