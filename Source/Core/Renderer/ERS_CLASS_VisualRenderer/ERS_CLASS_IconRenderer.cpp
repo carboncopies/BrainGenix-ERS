f74f0362ee2bb2f560dac922958de2f288d143b2
datacrystals <tliao@carboncopies.org>
2022-08-11 01:04:49 +0000

    Implement Scene Camera Integrations

--- Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_IconRenderer.cpp
+++ Source/Core/Renderer/ERS_CLASS_VisualRenderer/ERS_CLASS_IconRenderer.cpp
@@ -6,31 +6,31 @@
 
 
 
-ERS_CLASS_LightIconRenderer::ERS_CLASS_LightIconRenderer(ERS_STRUCT_OpenGLDefaults* Defaults, ERS_STRUCT_SystemUtils* SystemUtils, ERS_STRUCT_Shader* LightIconRendererShader) {
+ERS_CLASS_IconRenderer::ERS_CLASS_IconRenderer(ERS_STRUCT_OpenGLDefaults* Defaults, ERS_STRUCT_SystemUtils* SystemUtils, ERS_STRUCT_Shader* IconRendererShader) {
 
 
     SystemUtils_ = SystemUtils;
-    LightIconRendererShader_ = LightIconRendererShader;
+    IconRendererShader_ = IconRendererShader;
     OpenGLDefaults_ = Defaults;
 
 
-    SystemUtils_->Logger_->Log("Setting Up VisualRenderer LightIconRenderer System", 5);
+    SystemUtils_->Logger_->Log("Setting Up VisualRenderer IconRenderer System", 5);
 
 
 
-    const float LightIconRendererVertices[] = {
+    const float IconRendererVertices[] = {
         -1.0,  1.0, 0.0, 0.0, 1.0,  // Top Left
         -1.0, -1.0, 0.0, 1.0, 1.0,  // Bottom Left
         1.0, -1.0,  0.0, 1.0, 0.0,  // Bottom Right
         1.0,  1.0,  0.0, 0.0, 0.0   // Top Right
     };
 
-    glGenBuffers(1, &LightIconRendererVBO_);
-    glBindBuffer(GL_ARRAY_BUFFER, LightIconRendererVBO_);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(LightIconRendererVertices), LightIconRendererVertices, GL_STATIC_DRAW);
+    glGenBuffers(1, &IconRendererVBO_);
+    glBindBuffer(GL_ARRAY_BUFFER, IconRendererVBO_);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(IconRendererVertices), IconRendererVertices, GL_STATIC_DRAW);
     
-    glGenVertexArrays(1, &LightIconRendererVAO_);
-    glBindVertexArray(LightIconRendererVAO_);
+    glGenVertexArrays(1, &IconRendererVAO_);
+    glBindVertexArray(IconRendererVAO_);
 
     int PositionIndex = 0;
     glVertexAttribPointer(PositionIndex, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
@@ -42,20 +42,20 @@ ERS_CLASS_LightIconRenderer::ERS_CLASS_LightIconRenderer(ERS_STRUCT_OpenGLDefaul
 
 
     // Initialize Model Array
-    LightIconRendererModelArray_ = glm::mat4(1.0f);
+    IconRendererModelArray_ = glm::mat4(1.0f);
 
 }
 
-ERS_CLASS_LightIconRenderer::~ERS_CLASS_LightIconRenderer() {
+ERS_CLASS_IconRenderer::~ERS_CLASS_IconRenderer() {
 
-    SystemUtils_->Logger_->Log("LightIconRenderer Destructor Called", 6);
+    SystemUtils_->Logger_->Log("IconRenderer Destructor Called", 6);
 
 }
 
-void ERS_CLASS_LightIconRenderer::Draw(ERS_STRUCT_Camera* Camera, ERS_CLASS_SceneManager* SceneManager) {
+void ERS_CLASS_IconRenderer::Draw(ERS_STRUCT_Camera* Camera, ERS_CLASS_SceneManager* SceneManager) {
 
     glDisable(GL_DEPTH_TEST);
-    LightIconRendererShader_->MakeActive();
+    IconRendererShader_->MakeActive();
     glm::mat4 View = Camera->GetViewMatrix();
     glm::mat4 Projection = Camera->GetProjectionMatrix();
     glm::vec3 CameraPosition = Camera->Position_;
@@ -66,7 +66,7 @@ void ERS_CLASS_LightIconRenderer::Draw(ERS_STRUCT_Camera* Camera, ERS_CLASS_Scen
     for (int i = 0; (long)i < (long)SceneManager->Scenes_[SceneManager->ActiveScene_]->PointLights.size(); i++) {
 
         glm::vec3 LightPosition = SceneManager->Scenes_[SceneManager->ActiveScene_]->PointLights[i]->Pos;
-        glm::mat4 NewModelMatrix = glm::translate(LightIconRendererModelArray_, LightPosition);
+        glm::mat4 NewModelMatrix = glm::translate(IconRendererModelArray_, LightPosition);
 
 
         // FIXME: Make Lights a "Billboard" So they Rotate Towards The Camera
@@ -75,26 +75,26 @@ void ERS_CLASS_LightIconRenderer::Draw(ERS_STRUCT_Camera* Camera, ERS_CLASS_Scen
         // NewModelMatrix = glm::rotate(NewModelMatrix, ModelRotation.x, glm::vec3(1, 0, 0));
         // NewModelMatrix = glm::rotate(NewModelMatrix, ModelRotation.y, glm::vec3(0, 1, 0));
         // NewModelMatrix = glm::rotate(NewModelMatrix, ModelRotation.z, glm::vec3(0, 0, 1));
-        NewModelMatrix = glm::scale(NewModelMatrix, glm::vec3(LightIconRendererScale_));
+        NewModelMatrix = glm::scale(NewModelMatrix, glm::vec3(IconRendererScale_));
 
-        LightIconRendererShader_->SetMat4("model", NewModelMatrix);
-        LightIconRendererShader_->SetMat4("view", View);
-        LightIconRendererShader_->SetMat4("projection", Projection);
+        IconRendererShader_->SetMat4("model", NewModelMatrix);
+        IconRendererShader_->SetMat4("view", View);
+        IconRendererShader_->SetMat4("projection", Projection);
 
-        LightIconRendererShader_->SetVec3("CameraPosition", CameraPosition);
-        LightIconRendererShader_->SetVec3("CameraRight", CameraRight);
-        LightIconRendererShader_->SetVec3("CameraUp", CameraUp);
+        IconRendererShader_->SetVec3("CameraPosition", CameraPosition);
+        IconRendererShader_->SetVec3("CameraRight", CameraRight);
+        IconRendererShader_->SetVec3("CameraUp", CameraUp);
 
-        LightIconRendererShader_->SetFloat("BillboardSize", LightIconRendererScale_);
-        LightIconRendererShader_->SetVec3("BillboardPosition", LightPosition);
+        IconRendererShader_->SetFloat("BillboardSize", IconRendererScale_);
+        IconRendererShader_->SetVec3("BillboardPosition", LightPosition);
         
-        //LightIconRendererShader_->SetVec3("BillboardRotation", SceneManager->Scenes_[SceneManager->ActiveScene_]->PointLights[i]->Pos);
+        //IconRendererShader_->SetVec3("BillboardRotation", SceneManager->Scenes_[SceneManager->ActiveScene_]->PointLights[i]->Pos);
         
-        glUniform1i(glGetUniformLocation(LightIconRendererShader_->ShaderProgram_, "IconTexture"), 0);
+        glUniform1i(glGetUniformLocation(IconRendererShader_->ShaderProgram_, "IconTexture"), 0);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, OpenGLDefaults_->PointLightTexture_);
 
-        glBindVertexArray(LightIconRendererVAO_);
+        glBindVertexArray(IconRendererVAO_);
         glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
     }
@@ -104,7 +104,7 @@ void ERS_CLASS_LightIconRenderer::Draw(ERS_STRUCT_Camera* Camera, ERS_CLASS_Scen
     for (int i = 0; (long)i < (long)SceneManager->Scenes_[SceneManager->ActiveScene_]->DirectionalLights.size(); i++) {
 
         glm::vec3 LightPosition = SceneManager->Scenes_[SceneManager->ActiveScene_]->DirectionalLights[i]->Pos;
-        glm::mat4 NewModelMatrix = glm::translate(LightIconRendererModelArray_, LightPosition);
+        glm::mat4 NewModelMatrix = glm::translate(IconRendererModelArray_, LightPosition);
 
 
         // FIXME: Make Lights a "Billboard" So they Rotate Towards The Camera
@@ -114,25 +114,25 @@ void ERS_CLASS_LightIconRenderer::Draw(ERS_STRUCT_Camera* Camera, ERS_CLASS_Scen
         NewModelMatrix = glm::rotate(NewModelMatrix, LampRotation.x, glm::vec3(1, 0, 0));
         NewModelMatrix = glm::rotate(NewModelMatrix, LampRotation.y, glm::vec3(0, 1, 0));
         NewModelMatrix = glm::rotate(NewModelMatrix, LampRotation.z, glm::vec3(0, 0, 1));
-        NewModelMatrix = glm::scale(NewModelMatrix, glm::vec3(LightIconRendererScale_));
+        NewModelMatrix = glm::scale(NewModelMatrix, glm::vec3(IconRendererScale_));
 
-        LightIconRendererShader_->SetMat4("model", NewModelMatrix);
-        LightIconRendererShader_->SetMat4("view", View);
-        LightIconRendererShader_->SetMat4("projection", Projection);
+        IconRendererShader_->SetMat4("model", NewModelMatrix);
+        IconRendererShader_->SetMat4("view", View);
+        IconRendererShader_->SetMat4("projection", Projection);
 
-        LightIconRendererShader_->SetVec3("CameraPosition", CameraPosition);
-        LightIconRendererShader_->SetVec3("CameraRight", CameraRight);
-        LightIconRendererShader_->SetVec3("CameraUp", CameraUp);
+        IconRendererShader_->SetVec3("CameraPosition", CameraPosition);
+        IconRendererShader_->SetVec3("CameraRight", CameraRight);
+        IconRendererShader_->SetVec3("CameraUp", CameraUp);
 
-        LightIconRendererShader_->SetFloat("BillboardSize", LightIconRendererScale_);
-        LightIconRendererShader_->SetVec3("BillboardPosition", LightPosition);
-        LightIconRendererShader_->SetVec3("BillboardRotation", LampRotation);
+        IconRendererShader_->SetFloat("BillboardSize", IconRendererScale_);
+        IconRendererShader_->SetVec3("BillboardPosition", LightPosition);
+        IconRendererShader_->SetVec3("BillboardRotation", LampRotation);
         
-        glUniform1i(glGetUniformLocation(LightIconRendererShader_->ShaderProgram_, "IconTexture"), 0);
+        glUniform1i(glGetUniformLocation(IconRendererShader_->ShaderProgram_, "IconTexture"), 0);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, OpenGLDefaults_->DirectionalLightTexture_);
 
-        glBindVertexArray(LightIconRendererVAO_);
+        glBindVertexArray(IconRendererVAO_);
         glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
     }
@@ -142,7 +142,7 @@ void ERS_CLASS_LightIconRenderer::Draw(ERS_STRUCT_Camera* Camera, ERS_CLASS_Scen
     for (int i = 0; (long)i < (long)SceneManager->Scenes_[SceneManager->ActiveScene_]->SpotLights.size(); i++) {
 
         glm::vec3 LightPosition = SceneManager->Scenes_[SceneManager->ActiveScene_]->SpotLights[i]->Pos;
-        glm::mat4 NewModelMatrix = glm::translate(LightIconRendererModelArray_, LightPosition);
+        glm::mat4 NewModelMatrix = glm::translate(IconRendererModelArray_, LightPosition);
 
 
         // FIXME: Make Lights a "Billboard" So they Rotate Towards The Camera
@@ -152,25 +152,25 @@ void ERS_CLASS_LightIconRenderer::Draw(ERS_STRUCT_Camera* Camera, ERS_CLASS_Scen
         NewModelMatrix = glm::rotate(NewModelMatrix, LampRotation.x, glm::vec3(1, 0, 0));
         NewModelMatrix = glm::rotate(NewModelMatrix, LampRotation.y, glm::vec3(0, 1, 0));
         NewModelMatrix = glm::rotate(NewModelMatrix, LampRotation.z, glm::vec3(0, 0, 1));
-        NewModelMatrix = glm::scale(NewModelMatrix, glm::vec3(LightIconRendererScale_));
+        NewModelMatrix = glm::scale(NewModelMatrix, glm::vec3(IconRendererScale_));
 
-        LightIconRendererShader_->SetMat4("model", NewModelMatrix);
-        LightIconRendererShader_->SetMat4("view", View);
-        LightIconRendererShader_->SetMat4("projection", Projection);
+        IconRendererShader_->SetMat4("model", NewModelMatrix);
+        IconRendererShader_->SetMat4("view", View);
+        IconRendererShader_->SetMat4("projection", Projection);
 
-        LightIconRendererShader_->SetVec3("CameraPosition", CameraPosition);
-        LightIconRendererShader_->SetVec3("CameraRight", CameraRight);
-        LightIconRendererShader_->SetVec3("CameraUp", CameraUp);
+        IconRendererShader_->SetVec3("CameraPosition", CameraPosition);
+        IconRendererShader_->SetVec3("CameraRight", CameraRight);
+        IconRendererShader_->SetVec3("CameraUp", CameraUp);
 
-        LightIconRendererShader_->SetFloat("BillboardSize", LightIconRendererScale_);
-        LightIconRendererShader_->SetVec3("BillboardPosition", LightPosition);
-        LightIconRendererShader_->SetVec3("BillboardRotation", LampRotation);
+        IconRendererShader_->SetFloat("BillboardSize", IconRendererScale_);
+        IconRendererShader_->SetVec3("BillboardPosition", LightPosition);
+        IconRendererShader_->SetVec3("BillboardRotation", LampRotation);
         
-        glUniform1i(glGetUniformLocation(LightIconRendererShader_->ShaderProgram_, "IconTexture"), 0);
+        glUniform1i(glGetUniformLocation(IconRendererShader_->ShaderProgram_, "IconTexture"), 0);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, OpenGLDefaults_->SpotLightTexture_);
 
-        glBindVertexArray(LightIconRendererVAO_);
+        glBindVertexArray(IconRendererVAO_);
         glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
     }
