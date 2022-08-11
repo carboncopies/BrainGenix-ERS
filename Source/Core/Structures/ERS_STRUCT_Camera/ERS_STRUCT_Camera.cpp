13e0355afdd269b9d8bda59deb85b364f3cad06e
datacrystals <tliao@carboncopies.org>
2022-08-11 07:46:37 +0000

    Implement Scene Camera Integrations

--- Source/Core/Structures/ERS_STRUCT_Camera/ERS_STRUCT_Camera.cpp
+++ Source/Core/Structures/ERS_STRUCT_Camera/ERS_STRUCT_Camera.cpp
@@ -18,18 +18,22 @@ glm::mat4 ERS_STRUCT_Camera::GetProjectionMatrix() {
 
 void ERS_STRUCT_Camera::Rotate(glm::vec3 Rotation) {
 
+    Yaw_ = Rotation.x;
+    Pitch_ = Rotation.y;
+    Roll_ = Rotation.z;
 
     glm::mat4 RotMatrix;
     RotMatrix = glm::rotate(RotMatrix, glm::radians(Rotation[2]), glm::vec3(0, 0, 1));
     RotMatrix = glm::rotate(RotMatrix, glm::radians(Rotation[1]), glm::vec3(0, 1, 0));
     RotMatrix = glm::rotate(RotMatrix, glm::radians(Rotation[0]), glm::vec3(1, 0, 0));
-
-
     Front_ = glm::normalize(glm::vec3(RotMatrix[2]));
 
     // Calculate Right, Up Vector
     Right_ = glm::normalize(glm::cross(Front_, WorldUp_));
     Up_ = glm::normalize(glm::cross(Right_, Front_));
+    
+    glm::mat4 roll_mat = glm::rotate(glm::mat4(1.0f), glm::radians(Roll_), Front_);
+    Up_ = glm::mat3(roll_mat) * Up_;
 }
 
 
