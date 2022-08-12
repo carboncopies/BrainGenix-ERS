a7cbc393f812d6ef55abb89dbc13920e40bb2cb1
datacrystals <tliao@carboncopies.org>
2022-08-12 21:45:25 +0000

    Update Scene Loader

---
+++ Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_SceneDecoderV3.h
@@ -0,0 +1,16 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <string>
+#include <vector>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <yaml-cpp/yaml.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ERS_STRUCT_SystemUtils.h>
+#include <ERS_STRUCT_Scene.h>
