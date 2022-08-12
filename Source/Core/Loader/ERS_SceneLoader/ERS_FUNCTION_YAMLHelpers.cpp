9d6462b85b9d22f620031d0dd05b34e171e15638
datacrystals <tliao@carboncopies.org>
2022-08-12 22:41:05 +0000

    Update Scene Loader

--- Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_YAMLHelpers.cpp
+++ Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_YAMLHelpers.cpp
@@ -3,3 +3,35 @@
 //======================================================================//
 
 #include <ERS_FUNCTION_YAMLHelpers.h>
+
+
+bool ERS_FUNCTION_GetInt(YAML::Node Data, std::string Name, int &Target) {
+    try {
+        Target = Data[Name].as<int>();
+        return true;
+    } catch (YAML::TypedBadConversion<int>) {
+        return false;
+    } catch (YAML::KeyNotFound) {
+        return false;
+    }
+}
+bool ERS_FUNCTION_GetFloat(YAML::Node Data, std::string Name, float &Target) {
+    try {
+        Target = Data[Name].as<float>();
+        return true;
+    } catch (YAML::TypedBadConversion<float>) {
+        return false;
+    } catch (YAML::KeyNotFound) {
+        return false;
+    }
+}
+bool ERS_FUNCTION_GetBool(YAML::Node Data, std::string Name, bool &Target) {
+    try {
+        Target = Data[Name].as<bool>();
+        return true;
+    } catch (YAML::TypedBadConversion<bool>) {
+        return false;
+    } catch (YAML::KeyNotFound) {
+        return false;
+    }
+}
