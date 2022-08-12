ab9102102b4986f0a65c66714feba657ef01bd66
datacrystals <tliao@carboncopies.org>
2022-08-12 21:52:33 +0000

    Update Scene Loader

--- Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_SceneDecoderV3.h
+++ Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_SceneDecoderV3.h
@@ -14,3 +14,16 @@
 // Internal Libraries (BG convention: use <> instead of "")
 #include <ERS_STRUCT_SystemUtils.h>
 #include <ERS_STRUCT_Scene.h>
+
+
+/**
+ * @brief Decodes the specified version of the scene format.
+ * 
+ * @param SceneData YAML::Node containing the scene data.
+ * @param Scene Pointer to the scene to be updated.
+ * @param SystemUtils Pointer to the systemutils struct.
+ * @param LogEnable Enable or disable information log messages.
+ * @return true Loading completed without errors.
+ * @return false Loading failed.
+ */
+bool ERS_FUNCTION_DecodeSceneV3(YAML::Node SceneData, ERS_STRUCT_Scene *Scene, ERS_STRUCT_SystemUtils *SystemUtils, bool LogEnable = true);
