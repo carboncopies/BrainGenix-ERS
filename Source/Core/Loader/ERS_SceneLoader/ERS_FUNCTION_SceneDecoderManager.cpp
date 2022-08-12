31a744171207211387c502c05fecae902c7e869a
datacrystals <tliao@carboncopies.org>
2022-08-12 21:48:48 +0000

    Update Scene Loader

--- Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_SceneDecoderManager.cpp
+++ Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_SceneDecoderManager.cpp
@@ -5,8 +5,15 @@
 #include <ERS_FUNCTION_SceneDecoderManager.h>
 
 
+
+
 bool ERS_FUNCTION_DecodeScene(YAML::Node SceneData, ERS_STRUCT_Scene *Scene, ERS_STRUCT_SystemUtils *SystemUtils, bool LogEnable) {
 
+    SystemUtils->Logger_->Log("Decoding ERS Scene", 5, LogEnable);
 
+    // Extract Current Version
+    int Version;
+    SystemUtils->Logger_->Log("Identifying Scene Format Version", 4, LogEnable);
+    if (SceneData["SceneFormatVersion"])
 
 }
