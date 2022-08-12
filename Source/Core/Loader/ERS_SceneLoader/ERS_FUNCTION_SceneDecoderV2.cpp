36500c66956ddf4cc7290475fab41471be4dcf44
datacrystals <tliao@carboncopies.org>
2022-08-12 22:16:20 +0000

    Update Scene Loader

--- Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_SceneDecoderV2.cpp
+++ Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_SceneDecoderV2.cpp
@@ -13,7 +13,7 @@
 
 
 
-bool ERS_FUNCTION_DecodeSceneV1(YAML::Node SceneData, ERS_STRUCT_Scene *Scene, ERS_STRUCT_SystemUtils *SystemUtils, ERS_CLASS_ModelLoader* ModelLoader, bool LogEnable = true) {
+bool ERS_FUNCTION_DecodeSceneV2(YAML::Node SceneData, ERS_STRUCT_Scene *Scene, ERS_STRUCT_SystemUtils *SystemUtils, ERS_CLASS_ModelLoader* ModelLoader, bool LogEnable = true) {
 
 
     // Grab Metadata
