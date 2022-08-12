39ba7bc5784fd8f8e63722f666336de3e9c3a101
datacrystals <tliao@carboncopies.org>
2022-08-12 22:29:57 +0000

    Update Scene Loader

--- Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_YAMLHelpers.cpp
+++ Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_YAMLHelpers.cpp
@@ -2,4 +2,4 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-#include <ERS_FUNCTION_SceneDecoderV3.h>
+#include <ERS_FUNCTION_YAMLHelpers.h>
