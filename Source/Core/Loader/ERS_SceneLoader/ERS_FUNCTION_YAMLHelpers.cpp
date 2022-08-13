cba24d79f2ead6618285b9cf9577f1ac3792feec
datacrystals <tliao@carboncopies.org>
2022-08-13 01:21:40 +0000

    Update YAML Helpers

--- Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_YAMLHelpers.cpp
+++ Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_YAMLHelpers.cpp
@@ -5,7 +5,7 @@
 #include <ERS_FUNCTION_YAMLHelpers.h>
 
 // Standart Type Helper Functions
-bool ERS_FUNCTION_GetInt(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, int &Target) {
+bool ERS_FUNCTION_GetInt(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, int &Target) {
     try {
         Target = Data[Name].as<int>();
         return true;
@@ -15,7 +15,7 @@ bool ERS_FUNCTION_GetInt(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std:
         return false;
     }
 }
-bool ERS_FUNCTION_GetFloat(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, float &Target) {
+bool ERS_FUNCTION_GetFloat(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, float &Target) {
     try {
         Target = Data[Name].as<float>();
         return true;
@@ -25,7 +25,7 @@ bool ERS_FUNCTION_GetFloat(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, st
         return false;
     }
 }
-bool ERS_FUNCTION_GetBool(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, bool &Target) {
+bool ERS_FUNCTION_GetBool(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, bool &Target) {
     try {
         Target = Data[Name].as<bool>();
         return true;
@@ -35,7 +35,7 @@ bool ERS_FUNCTION_GetBool(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std
         return false;
     }
 }
-bool ERS_FUNCTION_GetLong(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, long &Target) {
+bool ERS_FUNCTION_GetLong(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, long &Target) {
     try {
         Target = Data[Name].as<long>();
         return true;
@@ -45,7 +45,7 @@ bool ERS_FUNCTION_GetLong(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std
         return false;
     }
 }
-bool ERS_FUNCTION_GetDouble(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, double &Target) {
+bool ERS_FUNCTION_GetDouble(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, double &Target) {
     try {
         Target = Data[Name].as<double>();
         return true;
@@ -55,7 +55,7 @@ bool ERS_FUNCTION_GetDouble(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, s
         return false;
     }
 }
-bool ERS_FUNCTION_GetUnsignedInt(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, unsigned int &Target) {
+bool ERS_FUNCTION_GetUnsignedInt(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, unsigned int &Target) {
     try {
         Target = Data[Name].as<unsigned int>();
         return true;
@@ -65,7 +65,7 @@ bool ERS_FUNCTION_GetUnsignedInt(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Da
         return false;
     }
 }
-bool ERS_FUNCTION_GetBool(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, unsigned long &Target) {
+bool ERS_FUNCTION_GetBool(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, unsigned long &Target) {
     try {
         Target = Data[Name].as<unsigned long>();
         return true;
@@ -75,7 +75,7 @@ bool ERS_FUNCTION_GetBool(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std
         return false;
     }
 }
-bool ERS_FUNCTION_GetLongLong(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, long long &Target) {
+bool ERS_FUNCTION_GetLongLong(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, long long &Target) {
     try {
         Target = Data[Name].as<long long>();
         return true;
@@ -85,7 +85,7 @@ bool ERS_FUNCTION_GetLongLong(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data,
         return false;
     }
 }
-bool ERS_FUNCTION_GetUnsignedLongLong(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, unsigned long long &Target) {
+bool ERS_FUNCTION_GetUnsignedLongLong(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, unsigned long long &Target) {
     try {
         Target = Data[Name].as<unsigned long long>();
         return true;
@@ -95,7 +95,7 @@ bool ERS_FUNCTION_GetUnsignedLongLong(ERS_CLASS_LoggingSystem* Logger,  YAML::No
         return false;
     }
 }
-bool ERS_FUNCTION_GetString(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, std::string &Target) {
+bool ERS_FUNCTION_GetString(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, std::string &Target) {
     try {
         Target = Data[Name].as<std::string>();
         return true;
@@ -105,7 +105,7 @@ bool ERS_FUNCTION_GetString(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, s
         return false;
     }
 }
-bool ERS_FUNCTION_GetNode(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, YAML::Node &Target) {
+bool ERS_FUNCTION_GetNode(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, YAML::Node &Target) {
     try {
         Target = Data[Name];
         return true;
@@ -116,7 +116,7 @@ bool ERS_FUNCTION_GetNode(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std
 
 
 // Vector Helpers
-bool ERS_FUNCTION_GetStringVector(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, std::vector<std::string> &Target) {
+bool ERS_FUNCTION_GetStringVector(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, std::vector<std::string> &Target) {
     try {
         YAML::Node TargetNode = Data[Name];
         for (YAML::const_iterator it=TargetNode.begin(); it!=TargetNode.end(); ++it) {
@@ -129,7 +129,7 @@ bool ERS_FUNCTION_GetStringVector(ERS_CLASS_LoggingSystem* Logger,  YAML::Node D
         return false;
     }
 }
-bool ERS_FUNCTION_GetIntVector(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, std::vector<int> &Target) {
+bool ERS_FUNCTION_GetIntVector(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, std::vector<int> &Target) {
     try {
         YAML::Node TargetNode = Data[Name];
         for (YAML::const_iterator it=TargetNode.begin(); it!=TargetNode.end(); ++it) {
@@ -142,7 +142,7 @@ bool ERS_FUNCTION_GetIntVector(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data
         return false;
     }
 }
-bool ERS_FUNCTION_GetLongVector(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, std::vector<long> &Target) {
+bool ERS_FUNCTION_GetLongVector(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, std::vector<long> &Target) {
     try {
         YAML::Node TargetNode = Data[Name];
         for (YAML::const_iterator it=TargetNode.begin(); it!=TargetNode.end(); ++it) {
@@ -155,7 +155,7 @@ bool ERS_FUNCTION_GetLongVector(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Dat
         return false;
     }
 }
-bool ERS_FUNCTION_GetNodeVector(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string Name, std::vector<YAML::Node> &Target) {
+bool ERS_FUNCTION_GetNodeVector(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string Name, std::vector<YAML::Node> &Target) {
     try {
         YAML::Node TargetNode = Data[Name];
         for (YAML::const_iterator it=TargetNode.begin(); it!=TargetNode.end(); ++it) {
@@ -169,7 +169,7 @@ bool ERS_FUNCTION_GetNodeVector(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Dat
 
 
 // GLM Helper Functions
-bool ERS_FUNCTION_GetVec3(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string NameBase, glm::vec3 &Target) {
+bool ERS_FUNCTION_GetVec3(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string NameBase, glm::vec3 &Target) {
     try {
         float X = Data[NameBase + "X"].as<float>();
         float Y = Data[NameBase + "Y"].as<float>();
@@ -182,7 +182,7 @@ bool ERS_FUNCTION_GetVec3(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std
         return false;
     }
 }
-bool ERS_FUNCTION_GetVec3Color(ERS_CLASS_LoggingSystem* Logger,  YAML::Node Data, std::string NameBase, glm::vec3 &Target) {
+bool ERS_FUNCTION_GetVec3Color(ERS_CLASS_LoggingSystem* Logger, YAML::Node Data, std::string NameBase, glm::vec3 &Target) {
     try {
         float X = Data[NameBase + "Red"].as<float>();
         float Y = Data[NameBase + "Green"].as<float>();
