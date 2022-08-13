965147c50dd6bd3be28e59361746098c4eafd3b6
datacrystals <tliao@carboncopies.org>
2022-08-13 01:31:25 +0000

    Update YAML Helpers

--- Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_SceneDecoderV2.cpp
+++ Source/Core/Loader/ERS_SceneLoader/ERS_FUNCTION_SceneDecoderV2.cpp
@@ -33,23 +33,23 @@ bool ERS_FUNCTION_DecodeSceneV2(YAML::Node SceneData, ERS_STRUCT_Scene *Scene, E
         // Get Asset Information
         YAML::Node Item = SceneItems[i];
         std::string AssetName, AssetType;
-        Success &= ERS_FUNCTION_GetString(Item, "AssetName", AssetName);
-        Success &= ERS_FUNCTION_GetString(Item, "AssetType", AssetType);
+        Success &= ERS_FUNCTION_GetString(Logger, Item, "AssetName", AssetName);
+        Success &= ERS_FUNCTION_GetString(Logger, Item, "AssetType", AssetType);
 
 
         if (AssetType == "Model") {
 
             ERS_STRUCT_Model Model;
-            Success &= ERS_FUNCTION_GetLong       (Item, "AssetID",              Model.AssetID                 );
-            Success &= ERS_FUNCTION_GetVec3       (Item, "AssetPosition",        Model.ModelPosition           );
-            Success &= ERS_FUNCTION_GetVec3       (Item, "AssetRotation",        Model.ModelRotation           );
-            Success &= ERS_FUNCTION_GetVec3       (Item, "AssetScale",           Model.ModelScale              );
-            Success &= ERS_FUNCTION_GetBool       (Item, "CastDynamicShadows",   Model.CastDynamicShadows_     );
-            Success &= ERS_FUNCTION_GetBool       (Item, "CastStaticShadows",    Model.CastStaticShadows_      );
-            Success &= ERS_FUNCTION_GetBool       (Item, "ReceiveShadows",       Model.ReceiveShadows_         );
-            Success &= ERS_FUNCTION_GetLong       (Item, "ShaderOverrideIndex",  Model.ShaderOverrideIndex_    );
-            Success &= ERS_FUNCTION_GetString     (Item, "AssetName",            Model.Name                    );
-            Success &= ERS_FUNCTION_GetLongVector (Item, "AttachedScripts",      Model.AttachedScriptIndexes_  );
+            Success &= ERS_FUNCTION_GetLong       (Logger, Item, "AssetID",              Model.AssetID                 );
+            Success &= ERS_FUNCTION_GetVec3       (Logger, Item, "AssetPosition",        Model.ModelPosition           );
+            Success &= ERS_FUNCTION_GetVec3       (Logger, Item, "AssetRotation",        Model.ModelRotation           );
+            Success &= ERS_FUNCTION_GetVec3       (Logger, Item, "AssetScale",           Model.ModelScale              );
+            Success &= ERS_FUNCTION_GetBool       (Logger, Item, "CastDynamicShadows",   Model.CastDynamicShadows_     );
+            Success &= ERS_FUNCTION_GetBool       (Logger, Item, "CastStaticShadows",    Model.CastStaticShadows_      );
+            Success &= ERS_FUNCTION_GetBool       (Logger, Item, "ReceiveShadows",       Model.ReceiveShadows_         );
+            Success &= ERS_FUNCTION_GetLong       (Logger, Item, "ShaderOverrideIndex",  Model.ShaderOverrideIndex_    );
+            Success &= ERS_FUNCTION_GetString     (Logger, Item, "AssetName",            Model.Name                    );
+            Success &= ERS_FUNCTION_GetLongVector (Logger, Item, "AttachedScripts",      Model.AttachedScriptIndexes_  );
 
             Scene->Models.push_back(std::make_shared<ERS_STRUCT_Model>(Model));
             ModelLoader->AddModelToLoadingQueue(Scene->Models[Scene->Models.size()-1]);
@@ -57,56 +57,56 @@ bool ERS_FUNCTION_DecodeSceneV2(YAML::Node SceneData, ERS_STRUCT_Scene *Scene, E
         } else if (AssetType == std::string("DirectionalLight")) {
 
             ERS_STRUCT_DirectionalLight Light;
-            Success &= ERS_FUNCTION_GetString     (Item, "AssetName",            Light.UserDefinedName         );
-            Success &= ERS_FUNCTION_GetVec3Color  (Item, "Color",                Light.Color                   );
-            Success &= ERS_FUNCTION_GetVec3       (Item, "Pos",                  Light.Pos                     );
-            Success &= ERS_FUNCTION_GetVec3       (Item, "Rot",                  Light.Rot                     );
-            Success &= ERS_FUNCTION_GetFloat      (Item, "Intensity",            Light.Intensity               );
-            Success &= ERS_FUNCTION_GetFloat      (Item, "MaxDistance",          Light.MaxDistance             );
-            Success &= ERS_FUNCTION_GetBool       (Item, "CastShadows",          Light.CastsShadows_           );
-            Success &= ERS_FUNCTION_GetLongVector (Item, "AttachedScripts",      Light.AttachedScriptIndexes_  );
+            Success &= ERS_FUNCTION_GetString     (Logger, Item, "AssetName",            Light.UserDefinedName         );
+            Success &= ERS_FUNCTION_GetVec3Color  (Logger, Item, "Color",                Light.Color                   );
+            Success &= ERS_FUNCTION_GetVec3       (Logger, Item, "Pos",                  Light.Pos                     );
+            Success &= ERS_FUNCTION_GetVec3       (Logger, Item, "Rot",                  Light.Rot                     );
+            Success &= ERS_FUNCTION_GetFloat      (Logger, Item, "Intensity",            Light.Intensity               );
+            Success &= ERS_FUNCTION_GetFloat      (Logger, Item, "MaxDistance",          Light.MaxDistance             );
+            Success &= ERS_FUNCTION_GetBool       (Logger, Item, "CastShadows",          Light.CastsShadows_           );
+            Success &= ERS_FUNCTION_GetLongVector (Logger, Item, "AttachedScripts",      Light.AttachedScriptIndexes_  );
             Scene->DirectionalLights.push_back(std::make_shared<ERS_STRUCT_DirectionalLight>(Light));
 
         } else if (AssetType == std::string("PointLight")) {
 
             ERS_STRUCT_PointLight Light;
-            Success &= ERS_FUNCTION_GetString     (Item, "AssetName",            Light.UserDefinedName         );
-            Success &= ERS_FUNCTION_GetVec3Color  (Item, "Color",                Light.Color                   );
-            Success &= ERS_FUNCTION_GetVec3       (Item, "Pos",                  Light.Pos                     );
-            Success &= ERS_FUNCTION_GetFloat      (Item, "Intensity",            Light.Intensity               );
-            Success &= ERS_FUNCTION_GetFloat      (Item, "MaxDistance",          Light.MaxDistance             );
-            Success &= ERS_FUNCTION_GetBool       (Item, "CastShadows",          Light.CastsShadows_           );
-            Success &= ERS_FUNCTION_GetLongVector (Item, "AttachedScripts",      Light.AttachedScriptIndexes_  );
+            Success &= ERS_FUNCTION_GetString     (Logger, Item, "AssetName",            Light.UserDefinedName         );
+            Success &= ERS_FUNCTION_GetVec3Color  (Logger, Item, "Color",                Light.Color                   );
+            Success &= ERS_FUNCTION_GetVec3       (Logger, Item, "Pos",                  Light.Pos                     );
+            Success &= ERS_FUNCTION_GetFloat      (Logger, Item, "Intensity",            Light.Intensity               );
+            Success &= ERS_FUNCTION_GetFloat      (Logger, Item, "MaxDistance",          Light.MaxDistance             );
+            Success &= ERS_FUNCTION_GetBool       (Logger, Item, "CastShadows",          Light.CastsShadows_           );
+            Success &= ERS_FUNCTION_GetLongVector (Logger, Item, "AttachedScripts",      Light.AttachedScriptIndexes_  );
             Scene->PointLights.push_back(std::make_shared<ERS_STRUCT_PointLight>(Light));
 
         } else if (AssetType == std::string("SpotLight")) {
 
             ERS_STRUCT_SpotLight Light;
-            Success &= ERS_FUNCTION_GetString     (Item, "AssetName",            Light.UserDefinedName         );
-            Success &= ERS_FUNCTION_GetVec3Color  (Item, "Color",                Light.Color                   );
-            Success &= ERS_FUNCTION_GetVec3       (Item, "Pos",                  Light.Pos                     );
-            Success &= ERS_FUNCTION_GetVec3       (Item, "Rot",                  Light.Rot                     );
-            Success &= ERS_FUNCTION_GetFloat      (Item, "Intensity",            Light.Intensity               );
-            Success &= ERS_FUNCTION_GetFloat      (Item, "MaxDistance",          Light.MaxDistance             );
-            Success &= ERS_FUNCTION_GetFloat      (Item, "CutOff",               Light.CutOff                  );
-            Success &= ERS_FUNCTION_GetFloat      (Item, "RollOff",              Light.Rolloff                 );
-            Success &= ERS_FUNCTION_GetBool       (Item, "CastShadows",          Light.CastsShadows_           );
-            Success &= ERS_FUNCTION_GetLongVector (Item, "AttachedScripts",      Light.AttachedScriptIndexes_  );
+            Success &= ERS_FUNCTION_GetString     (Logger, Item, "AssetName",            Light.UserDefinedName         );
+            Success &= ERS_FUNCTION_GetVec3Color  (Logger, Item, "Color",                Light.Color                   );
+            Success &= ERS_FUNCTION_GetVec3       (Logger, Item, "Pos",                  Light.Pos                     );
+            Success &= ERS_FUNCTION_GetVec3       (Logger, Item, "Rot",                  Light.Rot                     );
+            Success &= ERS_FUNCTION_GetFloat      (Logger, Item, "Intensity",            Light.Intensity               );
+            Success &= ERS_FUNCTION_GetFloat      (Logger, Item, "MaxDistance",          Light.MaxDistance             );
+            Success &= ERS_FUNCTION_GetFloat      (Logger, Item, "CutOff",               Light.CutOff                  );
+            Success &= ERS_FUNCTION_GetFloat      (Logger, Item, "RollOff",              Light.Rolloff                 );
+            Success &= ERS_FUNCTION_GetBool       (Logger, Item, "CastShadows",          Light.CastsShadows_           );
+            Success &= ERS_FUNCTION_GetLongVector (Logger, Item, "AttachedScripts",      Light.AttachedScriptIndexes_  );
             Scene->SpotLights.push_back(std::make_shared<ERS_STRUCT_SpotLight>(Light));
 
         } else if (AssetType == std::string("SceneCamera")) {
 
             ERS_STRUCT_SceneCamera Camera;
-            Success &= ERS_FUNCTION_GetString     (Item, "AssetName",            Camera.UserDefinedName_       );
-            Success &= ERS_FUNCTION_GetVec3       (Item, "Pos",                  Camera.Pos_                   );
-            Success &= ERS_FUNCTION_GetVec3       (Item, "Rot",                  Camera.Rot_                   );
-            Success &= ERS_FUNCTION_GetFloat      (Item, "NearClip",             Camera.NearClip_              );
-            Success &= ERS_FUNCTION_GetFloat      (Item, "FarClip",              Camera.FarClip_               );
-            Success &= ERS_FUNCTION_GetFloat      (Item, "FOV",                  Camera.FOV_                   );
-            Success &= ERS_FUNCTION_GetBool       (Item, "EnforceAspectRatio",   Camera.EnforceAspectRatio_    );
-            Success &= ERS_FUNCTION_GetFloat      (Item, "AspectRatio",          Camera.AspectRatio_           );
-            Success &= ERS_FUNCTION_GetInt        (Item, "StreamingPriority",    Camera.StreamingPriority_     );
-            Success &= ERS_FUNCTION_GetLongVector (Item, "AttachedScripts",      Camera.AttachedScriptIndexes_ );
+            Success &= ERS_FUNCTION_GetString     (Logger, Item, "AssetName",            Camera.UserDefinedName_       );
+            Success &= ERS_FUNCTION_GetVec3       (Logger, Item, "Pos",                  Camera.Pos_                   );
+            Success &= ERS_FUNCTION_GetVec3       (Logger, Item, "Rot",                  Camera.Rot_                   );
+            Success &= ERS_FUNCTION_GetFloat      (Logger, Item, "NearClip",             Camera.NearClip_              );
+            Success &= ERS_FUNCTION_GetFloat      (Logger, Item, "FarClip",              Camera.FarClip_               );
+            Success &= ERS_FUNCTION_GetFloat      (Logger, Item, "FOV",                  Camera.FOV_                   );
+            Success &= ERS_FUNCTION_GetBool       (Logger, Item, "EnforceAspectRatio",   Camera.EnforceAspectRatio_    );
+            Success &= ERS_FUNCTION_GetFloat      (Logger, Item, "AspectRatio",          Camera.AspectRatio_           );
+            Success &= ERS_FUNCTION_GetInt        (Logger, Item, "StreamingPriority",    Camera.StreamingPriority_     );
+            Success &= ERS_FUNCTION_GetLongVector (Logger, Item, "AttachedScripts",      Camera.AttachedScriptIndexes_ );
             Scene->SceneCameras.push_back(std::make_shared<ERS_STRUCT_SceneCamera>(Camera));
 
         } else {
