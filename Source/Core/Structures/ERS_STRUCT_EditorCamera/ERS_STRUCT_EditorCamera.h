7cb4cb93a8f5e095d0c4a191190b8153b0126447
datacrystals <tliao@carboncopies.org>
2022-08-13 23:03:51 +0000

    Create Editor Camera Struct

--- Source/Core/Structures/ERS_STRUCT_EditorCamera/ERS_STRUCT_EditorCamera.h
+++ Source/Core/Structures/ERS_STRUCT_EditorCamera/ERS_STRUCT_EditorCamera.h
@@ -36,13 +36,13 @@ public:
 
 
     /**
-     * @brief Create the camera object.
+     * @brief Create the editor camera object.
      * 
      */
     ERS_STRUCT_EditorCamera();
 
     /**
-     * @brief Destroy the camera object.
+     * @brief Destroy the editor camera object.
      * 
      */
     ~ERS_STRUCT_EditorCamera();
@@ -50,7 +50,7 @@ public:
 
 
     /**
-     * @brief Processes keyboard input for the camera.
+     * @brief Processes keyboard input for the editor camera.
      * 
      * @param Direction Direction currently being pressed.
      * @param DeltaTime Frame time used to ensure speed isn't tied to framerate
@@ -58,7 +58,7 @@ public:
     void ProcessKeyboard(CameraMovement Direction, float DeltaTime);
 
     /**
-     * @brief Processed mouse movement across the camera's viewport.
+     * @brief Processed mouse movement across the editor camera's viewport.
      * 
      * @param XOffset Pixels moved in the x direction
      * @param Yoffset Pixels moved in the y direction
@@ -67,7 +67,7 @@ public:
     void ProcessMouseMovement(float XOffset, float Yoffset, GLboolean ConstrainPitch = true);
 
     /**
-     * @brief Handles the mouse scrolling inputs
+     * @brief Handles the mouse scrolling inputs on the editor camera.
      * 
      * @param YOffset mouse scroll amount
      */
@@ -76,148 +76,16 @@ public:
 
     
     /**
-     * @brief Updates the position and other information about the editor camera
+     * @brief Updates the position and other information about the editor editor camera
      * 
      */
     void Update();
 
 
 
-    /**
-     * @brief Helper function, gets the projection/perspective matrix and the view matrix.
-     * Note that if called before the update function, the values returned will be from the last frame.
-     * 
-     * @param Perspective 
-     * @param View 
-     */
-    void GetMatrices(glm::mat4& Perspective, glm::mat4& View);
-
-    /**
-     * @brief Helper function, sets the camera's clipping distance.
-     * 
-     * @param NearClip Minimum distance in units for things to be rendered in.
-     * @param FarClip Maximum distance in units for things to be rendered in.
-     */
-    void SetClipBoundries(float NearClip, float FarClip);
-
-    /**
-     * @brief Helper Function, gets the current camera clip distance.
-     * 
-     * @param NearClip Minimum distance in units for things to be rendered in.
-     * @param FarClip Maximum distance in units for things to be rendered in.
-     */
-    void GetClipBoundires(float &NearClip, float &FarClip);
-
-    /**
-     * @brief Helper function, sets the camera's maximum and minimum freeflying movement speeds.
-     * 
-     * @param MinSpeed Slowest speed that the user can set the camera's keys to move in units per second.
-     * @param MaxSpeed Fastest speed that the user can set the camera's keys to move in units per second.
-     */
-    void SetMovementSpeedBoundries(float MinSpeed, float MaxSpeed);
-
-    /**
-     * @brief Helper Function, gets the camera's maximum and minimum freeflying movement speeds.
-     * 
-     * @param MinSpeed Slowest speed that the user can set the camera's keys to move in units per second.
-     * @param MaxSpeed Fastest speed that the user can set the camera's keys to move in units per second.
-     */
-    void GetMovementSpeedBoundries(float &MinSpeed, float &MaxSpeed);
-
-    /**
-     * @brief Helper function, Sets the speed of the camera.
-     * 
-     * @param Speed Desired speed in units per second.
-     * @param EnforceSpeedBoundries Enable/disable hard boundry enforcement.
-     */
-    void SetMovementSpeed(float Speed, bool EnforceSpeedBoundries = true);
-    
-    /**
-     * @brief Gets the mouse sensitivity multiplier.
-     * 
-     * @param Sensitivity Mouse sensitivity multiplier. 
-     */
-    void GetMouseSensitivity(float &Sensitivity);
-
-    /**
-     * @brief Sets the mouse sensitivity multiplier.
-     * 
-     * @param Sensitivity Mouse sensitivity multiplier.
-     */
-    void SetMouseSensitivity(float Sensitivity);
-
-    /**
-     * @brief Gets the current field of view angle.
-     * 
-     * @param FOV Field of view in degrees.
-     */
-    void GetFOV(float &FOV);
-    float GetFOV();
-
-    /**
-     * @brief Sets the field of view angle.
-     * 
-     * @param FOV Field of view in degrees.
-     */
-    void SetFOV(float FOV);
-
-    /**
-     * @brief Set the camera's aspect ratio (width/height)
-     * 
-     * @param AspectRatio Aspect ratio (width/height)
-     */
-    void SetAspectRatio(float AspectRatio);
-
-    /**
-     * @brief Sets the rotation of the camera.
-     * 
-     * @param Rotation Rotation in degrees (x,y,z) or (pitch, yaw, roll)
-     */
-    void SetRotation(glm::vec3 Rotation);
-
-    /**
-     * @brief Get the rotation 
-     * 
-     * @param Rotation Rotation in degrees (x,y,z) or (pitch, yaw, roll)
-     */
-    void GetRotation(glm::vec3 &Rotation);
-    glm::vec3 GetRotation();
-
-    /**
-     * @brief Set the position of the camera.
-     * 
-     * @param Position Position (x,y,z) in units relative to world origin.
-     */
-    void SetPosition(glm::vec3 Position);
-    
-    /**
-     * @brief Get the position of the camera.
-     * 
-     * @param Position Position (x,y,z) in units relative to world origin.
-     */
-    void GetPosition(glm::vec3 &Position);
-    glm::vec3 GetPosition();
-
-    /**
-     * @brief Get the camera's streaming priority.
-     * 
-     * @param StreamingPriority Int between 1-10 setting the priority. 
-     */
-    void GetStreamingPriority(int &StreamingPriority);
-    int GetStreamingPriority();
-
-    /**
-     * @brief Set the Streaming Priority int.
-     * 
-     * @param StreamingPriority Int between 1-10 setting the priority. 
-     */
-    void SetStreamingPriority(int StreamingPriority);
-
 private:
 
-    // Camea Configuration Information
-    float NearClip_          = 0.01f;  /**<Closest distance before geometry is culled.*/
-    float FarClip_           = 100.0f; /**<Farthest distance before geometry is called*/
+    // Editor Camera Configuration Information
     float MinMovementSpeed_  = 0.01f;  /**<Slowest movement speed allowed in units per second*/
     float MaxMovementSpeed_  = 50.0f;  /**<Fastest movement speed allowed in units per second*/
 
@@ -225,23 +93,9 @@ private:
     // Internal Camera State Information
     float MovementSpeed_     = 0.2f;   /**<Current Movement Speed*/
     float MouseSensitivity_  = 0.05f;  /**<Mouse sensitivity multiplier*/
-    float FOV_               = 50.0f;  /**<Field of view in degrees*/
-    float AspectRatio_       = 1.25f;  /**<Internal variable used to setup the projection matrix*/
 
-    glm::vec3 Orientation_;            /**<Rotation of the camera in degrees. */
-    glm::vec3 Position_;               /**<Position of the camera in units */
+    glm::vec3 Orientation_;            /**<Rotation of the editor camera in degrees. */
+    glm::vec3 Position_;               /**<Position of the editor camera in units */
     
-    glm::vec3 Front_;                  /**<Unit vector facing out the front of the camera. (in local space)*/
-    glm::vec3 Up_;                     /**<Unit vector facing out the top of the camera. (in local space)*/
-    glm::vec3 Right_;                  /**<Unit vector facing to the right of the camera. (in local space)*/
-
-    glm::mat4 PerspectiveMatrix_;      /**<Perspective/projection matrix, updated every time the Update function is called*/
-    glm::mat4 ViewMatrix_;             /**<View matrix, updated every time the Update function is called*/
-
-
-    // Asset Streaming Settings
-    int StreamingPriority_ = 1;        /**< Higher this is, the more the system will try and load assets for this camera. Should be in range (1-10)*/
-
-
 };
 
