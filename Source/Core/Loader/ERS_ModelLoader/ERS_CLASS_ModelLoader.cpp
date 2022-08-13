f5acf9d8369a3192e1df1aef35a851a8e611892b
datacrystals <tliao@carboncopies.org>
2022-08-13 09:12:31 +0000

    Add System To Convert Scene Geometry

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_ModelLoader.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_ModelLoader.cpp
@@ -609,8 +609,8 @@ void ERS_CLASS_ModelLoader::IdentifyMeshTextures(aiMaterial* Mat, ERS_STRUCT_Mes
             Mat->GetTexture(Type, i, &TextureString);
             std::string TextureIdentifier = std::string(std::string(TextureString.C_Str()));
 
-            //std::string Message = std::string("Model Requesting Texture Of Type '") + TypeName + std::string("' With Identifier '") + TextureIdentifier + std::string("'");
-            //SystemUtils_->Logger_->Log(Message, 3);
+            std::string Message = std::string("Model Requesting Texture Of Type '") + TypeName + std::string("' With Identifier '") + TextureIdentifier + std::string("'");
+            SystemUtils_->Logger_->Log(Message, 3);
 
             // Search Texture List For Index Of Same Match, Add To List Of Unique Textures If Not Found
             bool AlreadyHasTexture = false;
