d59bbac3a63f4b54d49a7751924449b20f53e5fd
datacrystals <tliao@carboncopies.org>
2022-08-14 02:08:07 +0000

    Implement Editor Camera Window

--- Source/Core/Editor/Windows/GUI_Window_EditorCameraSettings/GUI_Window_EditorCameraSettings.cpp
+++ Source/Core/Editor/Windows/GUI_Window_EditorCameraSettings/GUI_Window_EditorCameraSettings.cpp
@@ -22,48 +22,47 @@ void GUI_Window_EditorCameraSettings::Draw() {
     if (Enabled_) {
         bool Visible = ImGui::Begin("Editor Camera Settings", &Enabled_);
 
-            // Set Initial Window Size
-            ImGui::SetWindowSize(ImVec2(400,250), ImGuiCond_FirstUseEver);
+        // Set Initial Window Size
+        ImGui::SetWindowSize(ImVec2(400,250), ImGuiCond_FirstUseEver);
 
-            // Limit Index
-            SelectedViewportIndex_ = std::max((int)VisualRenderer_->Viewports_.size() - 1, SelectedViewportIndex_);
-
-
-            if (Visible) {
-
-                // Show Information About Every Viewport
-                ImGuiTabBarFlags Flags = ImGuiTabBarFlags_FittingPolicyScroll;
-                if (ImGui::BeginTabBar("Viewports", Flags)) {
-
-                    for (unsigned int i = 0; i < VisualRenderer_->Viewports_.size(); i++) {
-
-                        std::string Name = VisualRenderer_->Viewports_[i]->Name;
-                        bool TabSelected = SelectedViewportIndex_ == (int)i;
-                        bool TabStatus = ImGui::BeginTabItem(Name.c_str(), &TabSelected);
-                        if (TabStatus) {
-                            SelectedViewportIndex_ = i;
-                        }
-
-                        if (TabSelected) {
-                            ImGui::Text(VisualRenderer_->Viewports_[i]->Name.c_str());
+        // Limit Index
+        SelectedViewportIndex_ = std::max((int)VisualRenderer_->Viewports_.size() - 1, SelectedViewportIndex_);
 
 
-                        }
-                        ImGui::EndTabItem();
-                        
-                    
+        if (Visible) {
 
+            // Show Information About Every Viewport
+            ImGuiTabBarFlags Flags = ImGuiTabBarFlags_FittingPolicyScroll;
+            if (ImGui::BeginTabBar("Viewports", Flags)) {
+
+                for (unsigned int i = 0; i < VisualRenderer_->Viewports_.size(); i++) {
+
+                    std::string Name = VisualRenderer_->Viewports_[i]->Name;
+                    bool TabSelected = SelectedViewportIndex_ == (int)i;
+                    bool TabStatus = ImGui::BeginTabItem(Name.c_str(), &TabSelected);
+                    if (TabStatus) {
+                        SelectedViewportIndex_ = i;
                     }
 
-                ImGui::EndTabBar();
+                    if (TabSelected) {
+                        ImGui::Text(VisualRenderer_->Viewports_[i]->Name.c_str());
+
+                        ImGui::EndTabItem();
+                    }
+                    
+                
+
                 }
 
+            ImGui::EndTabBar();
             }
 
+        }
 
 
-        // End System Controls Window
-        ImGui::End();
+
+    // End System Controls Window
+    ImGui::End();
 
     }
 
