798de4ac714b24d7bf0271c5237b7fa7830edd40
datacrystals <tliao@carboncopies.org>
2022-08-14 00:27:48 +0000

    Setup Editor Camera Settings Window

--- Source/Core/Editor/Windows/GUI_Window_EditorCameraSettings/GUI_Window_EditorCameraSettings.cpp
+++ Source/Core/Editor/Windows/GUI_Window_EditorCameraSettings/GUI_Window_EditorCameraSettings.cpp
@@ -7,37 +7,7 @@
 GUI_Window_EditorCameraSettings::GUI_Window_EditorCameraSettings(ERS_STRUCT_SystemUtils* SystemUtils) {
 
     SystemUtils_ = SystemUtils;
-    SystemUtils_->Logger_->Log("Initializing EditorCamera Settings GUI_Window", 5);
-
-    // Setup Default Values For Input Fields
-    SystemUtils_->Logger_->Log("Setting Up Default Renderer Setting Input Fields", 3);
-
-    SystemUtils_->Logger_->Log("Copying Shadow Map Resolution", 2);
-    ERS_STRUCT_RendererSettings* Settings = SystemUtils_->RendererSettings_.get();
-    DepthMapResolution_ = Settings->ShadowMapX_;
-
-
-    if (Settings->ShadowFilteringType_ == ERS::Renderer::ERS_SHADOW_FILTERING_DISABLED) {
-        SelectedShadowFiltering_ = 0;
-    } else if (Settings->ShadowFilteringType_ == ERS::Renderer::ERS_SHADOW_FILTERING_PCF) {
-        SelectedShadowFiltering_ = 1;
-    } else if (Settings->ShadowFilteringType_ == ERS::Renderer::ERS_SHADOW_FILTERING_POISSON_SAMPLING) {
-        SelectedShadowFiltering_ = 2;
-    } else if (Settings->ShadowFilteringType_ == ERS::Renderer::ERS_SHADOW_FILTERING_STRATIFIED_POISSON_SAMPLING) {
-        SelectedShadowFiltering_ = 3;
-    }
-
-    if (Settings->ShadowUpdateMode_ == ERS::Renderer::ERS_SHADOW_UPDATE_MODE_DISABLED) {
-        SelectedShadowUpdates_ = 0;
-    } else if (Settings->ShadowUpdateMode_ == ERS::Renderer::ERS_SHADOW_UPDATE_MODE_RANDOM) {
-        SelectedShadowUpdates_ = 1;
-    } else if (Settings->ShadowUpdateMode_ == ERS::Renderer::ERS_SHADOW_UPDATE_MODE_CONSECUTIVE) {
-        SelectedShadowUpdates_ = 2;
-    } else if (Settings->ShadowUpdateMode_ == ERS::Renderer::ERS_SHADOW_UPDATE_MODE_DISTANCE_PRIORITIZED) {
-        SelectedShadowUpdates_ = 3;
-    } else if (Settings->ShadowUpdateMode_ == ERS::Renderer::ERS_SHADOW_UPDATE_MODE_ALL) {
-        SelectedShadowUpdates_ = 4;
-    }
+    SystemUtils_->Logger_->Log("Initializing Editor Camera Settings GUI_Window", 5);
 
 
 }
@@ -49,7 +19,7 @@ GUI_Window_EditorCameraSettings::~GUI_Window_EditorCameraSettings() {
 void GUI_Window_EditorCameraSettings::Draw() {
 
     if (Enabled_) {
-        bool Visible = ImGui::Begin("EditorCamera Settings", &Enabled_);
+        bool Visible = ImGui::Begin("Editor Camera Settings", &Enabled_);
 
             // Set Initial Window Size
             ImGui::SetWindowSize(ImVec2(400,250), ImGuiCond_FirstUseEver);
