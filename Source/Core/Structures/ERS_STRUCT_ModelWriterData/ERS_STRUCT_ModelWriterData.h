972040c7804cf7b1498dc9406c060268e7dbbc10
datacrystals <tliao@carboncopies.org>
2022-08-15 23:47:59 +0000

    Move ImportAsset To Window Class

--- Source/Core/Structures/ERS_STRUCT_ModelWriterData/ERS_STRUCT_ModelWriterData.h
+++ Source/Core/Structures/ERS_STRUCT_ModelWriterData/ERS_STRUCT_ModelWriterData.h
@@ -22,9 +22,13 @@ struct ERS_STRUCT_ModelWriterData {
 
 
     aiScene*           ModelScene;               /**<Pointer to assimp scene decoded by the importer*/
+    ERS_STRUCT_Model*  Model;                    /**<Pointer to the model to be saved by the writer*/
     long               ModelAssetID;             /**<Asset ID of model geometry*/
     std::string        ModelOriginDirectoryPath; /**<Path to the model's original file*/
-    ERS_STRUCT_Model*  Model;                    /**<Pointer to the model to be saved by the writer*/
+
+    std::vector<std::string> TextureList; /**<Texture List Vector*/
+    std::vector<std::string> TextureNames; /**<List of names of the textures*/
+    std::vector<std::string> TextureTypes; /**<Types of the textures in the texture list*/
 
 
 };
