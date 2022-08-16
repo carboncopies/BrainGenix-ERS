8fcde1aa18512cbd03a280c39e15a1a5f67bbbbe
datacrystals <tliao@carboncopies.org>
2022-08-16 04:55:55 +0000

    Refactor Import System

--- Source/Core/Editor/Windows/GUI_Window_ImportModel/GUI_Window_ImportAsset.h
+++ Source/Core/Editor/Windows/GUI_Window_ImportModel/GUI_Window_ImportAsset.h
@@ -15,7 +15,6 @@
 
 // Internal Libraries (BG convention: use <> instead of "")
 #include <ERS_STRUCT_SystemUtils.h>
-#include <ERS_Editor_ImportAsset.h>
 #include <GUI_Window_ImportProgressBar.h>
 
 
