d021f0139389133e5012e04d5a5848ec09b4d5f0
datacrystals <tliao@carboncopies.org>
2022-08-16 07:08:37 +0000

    Refactor Import System

--- Source/Core/Editor/Windows/GUI_Window_ImportModelDirectory/GUI_Window_ImportModelDirectory.cpp
+++ Source/Core/Editor/Windows/GUI_Window_ImportModelDirectory/GUI_Window_ImportModelDirectory.cpp
@@ -45,7 +45,6 @@ void GUI_Window_ImportModelDirectory::Draw() {
             }
 
             // Add To Queue, Launch Import
-            //AssetImportBackend_->AddToImportQueue(FilePaths);
             ProjectUtils_->ModelImporter_->AddToImportQueue(FilePaths);
             GUI_Window_ImportProgressBar_->Enabled_ = true;
 
