0f0fcfa011febe2427fc01d1fde0f8e9b8f1b3da
datacrystals <tliao@carboncopies.org>
2022-08-27 17:13:56 +0000

    Update Lucifer

--- CMake/BuildUtils/Lucifer.cmake
+++ CMake/BuildUtils/Lucifer.cmake
@@ -3,6 +3,7 @@
 ##########################
 
 ERSBuildLogger(${Green} "Configuring Lucifer Library")
+cmake_policy(SET CMP0063 NEW)
 add_subdirectory(${LIB_DIR}/LuciferIL/LuciferIL)
 disable_target_warnings(Lucifer)
 ERSBuildLogger(${BoldGreen} "Finished Configuring Lucifer Library")
