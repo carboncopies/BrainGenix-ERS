cca9f4e0fb0876f7d75d2c4ea4a6caef8ba166bc
datacrystals <tliao@carboncopies.org>
2022-08-27 10:35:45 +0000

    Add Lucifer

--- CMake/BuildUtils/Lucifer.cmake
+++ CMake/BuildUtils/Lucifer.cmake
@@ -3,6 +3,6 @@
 ##########################
 
 ERSBuildLogger(${Green} "Configuring Lucifer Library")
-add_subdirectory(${LIB_DIR}/LuciferIL)
+add_subdirectory(${LIB_DIR}/LuciferIL/LuciferIL)
 disable_target_warnings(Lucifer)
 ERSBuildLogger(${BoldGreen} "Finished Configuring Lucifer Library")
