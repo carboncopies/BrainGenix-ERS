257da51755f10fc6720c28908c676126070b0060
datacrystals <tliao@carboncopies.org>
2022-08-29 05:41:07 +0000

    Fix Windows Stupidity

--- Source/Core/Utils/ERS_CLASS_ExternalModelLoader/ERS_CLASS_ExternalModelLoader.h
+++ Source/Core/Utils/ERS_CLASS_ExternalModelLoader/ERS_CLASS_ExternalModelLoader.h
@@ -29,7 +29,6 @@
 
 #include <LuciferIL/Lucifer.h>
 #include <LuciferIL/Lucifer_ImageUtils.h>
-#include <LuciferIL/Lucifer_LoadingStatusEnum.h>
 
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
