33aab326e6190f56688427ff78b013052cc79bdb
datacrystals <tliao@carboncopies.org>
2022-08-31 08:26:41 +0000

    Implement Texture Res Cap

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingManager.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingManager.cpp
@@ -107,6 +107,16 @@ void ERS_CLASS_AssetStreamingManager::SortSceneModels(std::map<unsigned int, int
                 TargetTextureLevelRAM = std::min(MaxLOD_, TargetTextureLevelRAM);
                 TargetTextureLevelVRAM = std::min(MaxLOD_, TargetTextureLevelVRAM);
             }
+            if (TargetTextureLevelVRAM > NumberTextureLevels - 1) {
+                TargetTextureLevelVRAM = NumberTextureLevels - 1;
+            } else if (TargetTextureLevelVRAM < 0) {
+                TargetTextureLevelVRAM = 0;
+            }
+            if (TargetTextureLevelRAM > NumberTextureLevels - 1) {
+                TargetTextureLevelRAM = NumberTextureLevels - 1;
+            } else if (TargetTextureLevelRAM < 0) {
+                TargetTextureLevelRAM = 0;
+            }
             if (MaxTextureResolution_ > 0 && Model->Textures_.size() > 0) {
                 while (Model->Textures_[0].TextureLevels[TargetTextureLevelRAM].LevelResolution.first > MaxTextureResolution_) {
                     TargetTextureLevelRAM -=1;
@@ -118,17 +128,6 @@ void ERS_CLASS_AssetStreamingManager::SortSceneModels(std::map<unsigned int, int
             } else if (MaxTextureResolution_ < 0) {
                 MaxTextureResolution_ = 0;
             }
-            if (TargetTextureLevelVRAM > NumberTextureLevels - 1) {
-                TargetTextureLevelVRAM = NumberTextureLevels - 1;
-            } else if (TargetTextureLevelVRAM < 0) {
-                TargetTextureLevelVRAM = 0;
-            }
-            if (TargetTextureLevelRAM > NumberTextureLevels - 1) {
-                TargetTextureLevelRAM = NumberTextureLevels - 1;
-            } else if (TargetTextureLevelRAM < 0) {
-                TargetTextureLevelRAM = 0;
-            }
-
   
 
 
