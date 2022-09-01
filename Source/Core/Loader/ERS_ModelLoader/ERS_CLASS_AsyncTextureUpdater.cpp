b9d2bbd6207d4ef9976e1a32c01f302a3dbff23d
datacrystals <tliao@carboncopies.org>
2022-09-01 02:00:07 +0000

    Stop Texture Pusher Thread Getting Blocked

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
@@ -637,9 +637,9 @@ void ERS_CLASS_AsyncTextureUpdater::TexturePusherThread(int Index) {
             // We Need All Texture Levels In RAM To Push To VRAM, Check This Is True!
             // If It's Not, We're Going To Wait Until They Are By Moving This Item To The End Of The Queue
             if (WorkItem->TextureLevelInRAM_ <= WorkItem->TargetTextureLevelVRAM) {
-                // if (Model->TargetTextureLevelRAM < Model->TargetTextureLevelVRAM) {
-                //     Model->TargetTextureLevelRAM = Model->TargetTextureLevelVRAM;
-                // }
+                if (Model->TargetTextureLevelRAM < Model->TargetTextureLevelVRAM) {
+                    Model->TargetTextureLevelRAM = Model->TargetTextureLevelVRAM;
+                }
                 // SetLevelRAM(Model, LogEnable);
                 BlockPusherThreads_.lock();
                 PushWorkItems_.push_back(WorkItem);
