1926e8284be66baaa7690f4f89d3803f6087b01c
datacrystals <tliao@carboncopies.org>
2022-09-01 02:56:07 +0000

    Debug Texture Level Target System

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingManager.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AssetStreamingManager.cpp
@@ -315,6 +315,9 @@ std::map<float, unsigned int> ERS_CLASS_AssetStreamingManager::SortModelsByDista
 
         float TotalDistance = glm::distance(Camera->GetPosition(), Scene->Models[i]->ModelPosition);
         
+        if (TotalDistance <= 0.05f) {
+            std::cout<<i<<std::endl;
+        }
         // glm::vec3 UnscaledAngle = Camera->GetPosition() - Scene->Models[i]->ModelPosition;
         // float MaxSide = UnscaledAngle.x;
         // if (UnscaledAngle.y > MaxSide) {
