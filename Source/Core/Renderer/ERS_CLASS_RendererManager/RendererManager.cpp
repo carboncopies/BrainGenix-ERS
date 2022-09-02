0ed48e3f6bcb53cca23e5e21aa6ee4d06d91c384
datacrystals <tliao@carboncopies.org>
2022-09-02 08:46:45 +0000

    Debug Texture LOD Issues

--- Source/Core/Renderer/ERS_CLASS_RendererManager/RendererManager.cpp
+++ Source/Core/Renderer/ERS_CLASS_RendererManager/RendererManager.cpp
@@ -196,11 +196,6 @@ void RendererManager::UpdateLoop(float DeltaTime) {
 
 
     ERS_STRUCT_Scene* TargetScene = ProjectUtils_->SceneManager_->Scenes_[ProjectUtils_->SceneManager_->ActiveScene_].get();
-    
-    for (unsigned int i =0; i < TargetScene->Models.size(); i++) {
-        TargetScene->Models[i]->TargetTextureLevelVRAM = -1;
-    }
-    
     ProjectUtils_->ModelLoader_->AssetStreamingManager_->AsyncTextureUpdater_->SortModels(TargetScene);
 
     std::vector<ERS_STRUCT_Camera*> Cameras;
