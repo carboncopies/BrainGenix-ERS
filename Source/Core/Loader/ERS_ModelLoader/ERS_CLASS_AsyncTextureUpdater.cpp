8357beb9f363e1d06e2660c76d3fb1eaa607ec79
datacrystals <tliao@carboncopies.org>
2022-09-02 07:03:05 +0000

    Debug Texture LOD Issues

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
@@ -395,6 +395,10 @@ void ERS_CLASS_AsyncTextureUpdater::SetLevelRAM(ERS_STRUCT_Model* Model, bool Lo
 }
 void ERS_CLASS_AsyncTextureUpdater::SetLevelVRAM(ERS_STRUCT_Model* Model, bool LogEnable) {
 
+        if (Model->TextureLevelInVRAM_ == -1) {
+            std::cout<<"FDsafds\n";
+        }
+
         // Abort If Level Not Loaded Yet
         if (Model->TextureLevelInVRAM_ < Model->TargetTextureLevelVRAM) {
 
