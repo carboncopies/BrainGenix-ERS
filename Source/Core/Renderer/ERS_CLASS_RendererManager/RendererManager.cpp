9a22682e04096f2f9086ec5815ae32bf99b5231f
datacrystals <tliao@carboncopies.org>
2022-09-02 07:32:06 +0000

    Debug Texture LOD Issues

--- Source/Core/Renderer/ERS_CLASS_RendererManager/RendererManager.cpp
+++ Source/Core/Renderer/ERS_CLASS_RendererManager/RendererManager.cpp
@@ -196,6 +196,10 @@ void RendererManager::UpdateLoop(float DeltaTime) {
     ERS_STRUCT_Scene* TargetScene = ProjectUtils_->SceneManager_->Scenes_[ProjectUtils_->SceneManager_->ActiveScene_].get();
     ProjectUtils_->ModelLoader_->AssetStreamingManager_->AsyncTextureUpdater_->SortModels(TargetScene);
 
+    for (unsigned int i = 0; i < TargetScene->Models.size(); i++) {
+        TargetScene->Models[i]->TargetTextureLevelVRAM = -1;
+    }
+
     // Update Window Title
     std::string SceneTitle = ProjectUtils_->ProjectManager_->Project_.ProjectName + std::string(" - BrainGenix-ERS");
     glfwSetWindowTitle(Window_, SceneTitle.c_str());
