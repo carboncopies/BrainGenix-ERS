ecc6524d15bbea0cade507e6315c03cda6ecb769
datacrystals <tliao@carboncopies.org>
2022-09-17 21:49:28 +0000

    Fix Texture Budgeting Subsystem

--- Source/Core/Structures/ERS_STRUCT_TextureLevel/ERS_STRUCT_TextureLevel.h
+++ Source/Core/Structures/ERS_STRUCT_TextureLevel/ERS_STRUCT_TextureLevel.h
@@ -21,11 +21,19 @@ struct ERS_STRUCT_TextureLevel {
 
     unsigned int LevelTextureOpenGLID; /**<List of opengl ids for every texture level*/
     unsigned int LevelTextureAssetID; /**<List of asset ids for every texture level*/
+
     bool LevelLoadedInRAM; /**<True/false for every level showing if that level is loaded in RAM*/
-    bool LevelLoadedInVRAM; /**<True/false for every level showing f that level is loaded in VRAM*/  
+    bool LevelLoadedInVRAM; /**<True/false for every level showing f that level is loaded in VRAM*/
+
+    bool AllocatedRAMBudget; /**<True/False indicating if the level has been allocated from the RAM budget*/
+    bool AllocatedVRAMBudget; /**<True/False indicating if the level has been allocated from the VRAM budget*/
+
     std::pair<int, int> LevelResolution; /**<List of all texture level's resolutions*/
+    
     int LevelMemorySizeBytes; /**<Tells the system how many bytes every level will take*/
+    
     FIBITMAP* LevelBitmap; /**<List of bitmaps for each level - contains the texture's RAM data.*/
+    
     int LevelChannel; /**<List containing number of channels for every texture layer*/
     int Level; /**<What level is this (used for sorting internally)*/
 
