40a7c4dcf5bd6c59804f3f7247d8a468a13b807b
datacrystals <tliao@carboncopies.org>
2022-09-17 21:42:19 +0000

    Fix Texture Budgeting Subsystem

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
@@ -167,8 +167,6 @@ bool ERS_CLASS_AsyncTextureUpdater::LoadImageDataRAM(ERS_STRUCT_Texture* Texture
     // add error textures so that we can display the error on the model to make it easier to understand what the issue is - for example, display "Loading Error: Invalid Num Image Channels" for the above issue and add the same for the other issues.
 
     // Finally After Passing Sanity Checks, Populate Info.
-    long MemorySize = FreeImage_GetMemorySize(Image);
-    ResourceMonitor_->AllocateTextureRAMFromBudget(MemorySize);
     Texture->TextureLevels[Level].LevelBitmap = Image;
     Texture->TextureLevels[Level].LevelLoadedInRAM = true;
 
