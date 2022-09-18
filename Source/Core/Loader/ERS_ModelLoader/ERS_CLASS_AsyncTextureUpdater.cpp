163eae63c7ef4f901b8011547c610edcd1184f89
datacrystals <tliao@carboncopies.org>
2022-09-18 03:23:27 +0000

    Fix Texture Budgeting Subsystem

--- Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ERS_CLASS_AsyncTextureUpdater.cpp
@@ -60,6 +60,9 @@ bool ERS_CLASS_AsyncTextureUpdater::LoadImageDataRAM(ERS_STRUCT_Texture* Texture
     } else if (Level > (int)Texture->TextureLevels.size()) {
         SystemUtils_->Logger_->Log("Texture Updater Tried To Load Nonexistant Texture Level", 8, LogEnable);
         return false;
+    } else if (Texture->TextureLevels[Level].LevelMemorySizeBytes > SystemUtils_->ERS_CLASS_HardwareInformation_->GetHWInfo().Dynamic_.PhysicalMemoryFree) {
+        SystemUtils_->Logger_->Log("Texture Updater Cannot Load Texture, Out Of Memory", 8, LogEnable);
+        return false;
     }
 
     // Check If Level Already Loaded
