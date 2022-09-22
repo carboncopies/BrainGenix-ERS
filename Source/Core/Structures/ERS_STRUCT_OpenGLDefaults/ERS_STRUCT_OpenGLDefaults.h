cb9217385db207f011327088abd1e70399f5a105
datacrystals <tliao@carboncopies.org>
2022-09-22 19:12:56 +0000

    Update Transparent Texture Rendering System

--- Source/Core/Structures/ERS_STRUCT_OpenGLDefaults/ERS_STRUCT_OpenGLDefaults.h
+++ Source/Core/Structures/ERS_STRUCT_OpenGLDefaults/ERS_STRUCT_OpenGLDefaults.h
@@ -24,11 +24,13 @@ struct ERS_STRUCT_OpenGLDefaults {
     unsigned int AllBlackTexture_; /**<Default All-Black Texture*/
     unsigned int AllWhiteTexture_; /**<Completely white texture*/
     unsigned int Loadingtexture_; /**<Texture used to fill in for textures not yet loaded*/
+    unsigned int TransparentTexture_; /**<Transparent Texture Used To Replace Missing Textures If Option Enabled*/
 
     unsigned int PointLightTexture_; /**<Texture ID For Point Light*/
     unsigned int DirectionalLightTexture_; /**<Texture ID For Directional Light*/
     unsigned int SpotLightTexture_; /**<Texture ID For Spot Light*/
     
     unsigned int SceneCameraTexture_; /**<Texture ID For Scene Camera Icon*/
+
 
 };
