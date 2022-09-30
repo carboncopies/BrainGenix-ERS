aa2cbee4b301eb063b6967b4ee97b4710aca502c
datacrystals <tliao@carboncopies.org>
2022-09-30 19:07:45 +0000

    Add CompileTimeInfo

--- CMake/Scripts/CompileTimeStamp/CompileTimeStamp.cmake
+++ CMake/Scripts/CompileTimeStamp/CompileTimeStamp.cmake
@@ -9,7 +9,7 @@ set(COMPILE_TIME_STAMP "")
 string(COMPILE_TIME_STAMP TODAY "%Y%m%d")
 
 message(" -- Configuring File ERS_CompileTimeStamp.h.in")
-configure_File(${CMAKE_SCRIPTS_DIR}/CompileTimestamp/ERS_CompileTimeStamp.h.in ${SRC_DIR}/Interface/Configuration/CompileTimestamp/ERS_CompileTimeStamp.h)
+configure_File(${CMAKE_SCRIPTS_DIR}/CompileTimeStamp/ERS_CompileTimeStamp.h.in ${SRC_DIR}/Interface/Configuration/CompileTimeStamp/ERS_CompileTimeStamp.h)
 
 message(" -- Adding Timestamp Header Target")
 add_subdirectory(${SRC_DIR}/Interface/Configuration/CompileTimeStamp)
