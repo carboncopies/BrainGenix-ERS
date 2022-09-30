89fd8f91c1614260b98e2fb6288cafa52aa4e7a2
datacrystals <tliao@carboncopies.org>
2022-09-30 19:12:34 +0000

    Add CompileTimeInfo

--- Source/Interface/Configuration/CompileTimeStamp/ERS_CompileTimeStamp.h
+++
@@ -1,8 +0,0 @@
-//======================================================================//
-// This file is part of the BrainGenix-ERS Environment Rendering System //
-//======================================================================//
-
-#pragma once
-
-
-#define ERS_COMPILE_TIME_STAMP "20220930"  // Timestamp when build was made
