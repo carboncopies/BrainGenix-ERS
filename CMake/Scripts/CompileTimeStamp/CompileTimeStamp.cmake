56b55e5973a654141591f497b5cfe2990a12f401
datacrystals <tliao@carboncopies.org>
2022-09-30 19:22:49 +0000

    Update Date Format

--- CMake/Scripts/CompileTimeStamp/CompileTimeStamp.cmake
+++ CMake/Scripts/CompileTimeStamp/CompileTimeStamp.cmake
@@ -6,7 +6,7 @@ ERSBuildLogger(${Green} "Configuring Timestamp Info")
 
 message(" -- Detecting Timestamp Information")
 
-string(TIMESTAMP COMPILE_TIME_STAMP "%Y-%m-%d %Hh%m:%s")
+string(TIMESTAMP COMPILE_TIME_STAMP "%Y-%m-%d %H:%m:%s")
 
 message(" -- Configuring File ERS_CompileTimeStamp.h.in")
 configure_File(${CMAKE_SCRIPTS_DIR}/CompileTimeStamp/ERS_CompileTimeStamp.h.in ${SRC_DIR}/Interface/Configuration/CompileTimeStamp/ERS_CompileTimeStamp.h)
