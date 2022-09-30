42362a0a6018d6746b1e16ec44b3ea8efe6b7d25
datacrystals <tliao@carboncopies.org>
2022-09-30 19:33:57 +0000

    Add System Info Log Function

---
+++ Source/Internal/LoggingSystem/ERS_LogSystemInfo.h
@@ -0,0 +1,20 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <memory.h>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
+
+/**
+ * @brief Logs the system information as defined during compilation (version, target os, etc.)
+ * 
+ */
+void LogSystemInfo();
