ec2c2709001cb0317097cedc192b559861b8fc40
datacrystals <tliao@carboncopies.org>
2022-09-30 16:19:29 +0000

    Update Versioning System

--- CMake/Scripts/VersioningSystem/VersioningSystem.cmake
+++ CMake/Scripts/VersioningSystem/VersioningSystem.cmake
@@ -3,5 +3,8 @@
 ###############################
 
 ERSBuildLogger(${Green} "Configuring Version Header File")
+
+set(TEST, "An example value")
+
 configure_File(${CMAKE_SCRIPTS_DIR}/VersioningSystem/ERS_Version.h.in ${SOURCE_DIR}/Interface/Configuration/VersioningSystem/ERS_Version.h)
 ERSBuildLogger(${BoldGreen} "Finished Configuring Assimp Library")
