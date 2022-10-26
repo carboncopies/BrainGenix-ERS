81445517db8d4cfe2c5d0272829454e90d7a15f7
datacrystals <tliao@carboncopies.org>
2022-10-26 22:27:17 +0000

    Implement Scene Camera Scripting

--- Source/Core/Script/ERS_CLASS_PythonInterpreterIntegration/PyBind11PointLightClass.cpp
+++ Source/Core/Script/ERS_CLASS_PythonInterpreterIntegration/PyBind11PointLightClass.cpp
@@ -5,6 +5,10 @@
 #include <PyBind11PointLightClass.h>
 
 
+PYBIND11_EMBEDDED_MODULE(Camera, m) {
+    m.doc() = "Allows the script to manipulate the Scene Camera that it's attached to.";
+}
+
 
 PYBIND11_EMBEDDED_MODULE(PointLight, m) {
     m.doc() = "Allows the script to manipulate the point light that it's attached to.";
