42d89b73b754e56bdf034a513a0974e78abacdc9
datacrystals <tliao@carboncopies.org>
2022-11-08 10:13:40 -0800

    Add Boost Library

--- CMake/BuildUtils/Boost.cmake
+++ CMake/BuildUtils/Boost.cmake
@@ -92,15 +92,37 @@ include(ProcessorCount)
 ProcessorCount(N)
 
 ExternalProject_Add(boost
+
+    # Specify Directory Configuration Options
     SOURCE_DIR ${LIB_DIR}/boost
-    BIN_DIR ${BIN_DIR}/boost
+    BINARY_DIR ${BIN_DIR}/boost
+    
+    # Show Output To Terminal
+    LOG_DOWNLOAD TRUE
+    LOG_UPDATE TRUE
+    LOG_PATCH TRUE
+    LOG_CONFIGURE TRUE
+    LOG_BUILD TRUE
+    LOG_INSTALL TRUE
+    LOG_TEST TRUE
+    LOG_MERGED_STDOUTERR TRUE
+
+    USES_TERMINAL_DOWNLOAD TRUE
+    USES_TERMINAL_UPDATE TRUE
+    USES_TERMINAL_PATCH TRUE
+    USES_TERMINAL_CONFIGURE TRUE
+    USES_TERMINAL_BUILD TRUE
+    USES_TERMINAL_INSTALL TRUE
+    USES_TERMINAL_TEST TRUE
+
+    # Set Configuation Commands
 	UPDATE_COMMAND "${BOOST_UPDATE_COMMAND}"
 	CONFIGURE_COMMAND ""
 	BUILD_COMMAND ./b2 ${BOOST_LIBS_TO_BUILD_CMD} -j ${N} ${NUM_PROCESSORS} toolset=${BOOST_TOOLSET} link=static stage ${BOOST_CONFIG_OPTIONS}
 	INSTALL_COMMAND ""
 )
 ExternalProject_Get_Property( boost binary_dir )
-# set(boost_BINARY_DIR ${BIN_DIR} CACHE PATH "" FORCE)
+set(boost_BINARY_DIR ${binary_dir} CACHE PATH "" FORCE)
 
 message(STATUS ${Boost_LIBRARIES})
 include_directories(${Boost_INCLUDE_DIRS})
