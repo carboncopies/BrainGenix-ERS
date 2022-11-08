6d117fcf58fe0b5050a527990ec747e6fa193566
datacrystals <tliao@carboncopies.org>
2022-11-08 09:26:29 -0800

    Add CPPFS Library

--- CMake/BuildUtils/CPPFS.cmake
+++ CMake/BuildUtils/CPPFS.cmake
@@ -2,7 +2,14 @@
 # Builds Library For ERS #
 ##########################
 
+include(ExternalProject)
+
+
 ERSBuildLogger(${Green} "Configuring CPPFS Library")
-add_subdirectory(${LIB_DIR}/cppfs)
-disable_target_warnings(cppfs)
+ExternalProject_Add("CPPFS" 
+    SOURCE_DIR ${LIB_DIR}/cppfs
+    BINARY_DIR ${BIN_DIR}
+    )
+# add_subdirectory(${LIB_DIR}/cppfs)
+# disable_target_warnings(cppfs)
 ERSBuildLogger(${BoldGreen} "Finished Configuring CPPFS Library")
