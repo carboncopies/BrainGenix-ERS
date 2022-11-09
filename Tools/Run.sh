c38ea65ea995019c8e0b52ab937afefe2d2d91ba
datacrystals <tliao@carboncopies.org>
2022-11-09 02:51:34 +0000

    Update Build Script To Run On Mac

--- Tools/Run.sh
+++ Tools/Run.sh
@@ -1,7 +1,30 @@
 
+# Enter Artifact/Binary Dir
 echo "[ERS BUILD SYSTEM] - Entering Artifacts Directory"
 cd ..
 cd Binaries
 
+# Detect Platform, Execute Program From That
+echo "[ERS BUILD SYSTEM] - Detecting Platform"
+MachineName="$(uname -s)"
+case "${MachineName}" in
+    Linux*)     Machine="Linux";;
+    Darwin*)    Machine="Mac";;
+    CYGWIN*)    Machine="Cygwin";;
+    MINGW*)     Machine="MinGw";;
+    *)          Machine="UNKNOWN:${MachineName}"
+esac
+echo "[ERS BUILD SYSTEM] - Detected Platform To Be '${Machine}'"
+
+
+# Depending On Platform, Run
 echo "[ERS BUILD SYSTEM] - Running Binary"
-./BrainGenix-ERS
+if [ ${Machine} == "Linux" ]
+then
+    ./BrainGenix-ERS
+elif ${Machine} == "Mac"
+then
+    bash BrainGenix-ERS
+else
+    echo "Unknown Platform, Cannot Run Executable"
+fi
