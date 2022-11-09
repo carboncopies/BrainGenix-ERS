87b08e6b37ee64b25d89859e17d91b46cdd7184e
datacrystals <tliao@carboncopies.org>
2022-11-08 16:42:02 -0800

    Add Mac Test 5

--- Sandbox/DarwinGLTestV5/Source/Main.cpp
+++ Sandbox/DarwinGLTestV5/Source/Main.cpp
@@ -9,8 +9,8 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "learnopengl/shader_m.h"
-#include "learnopengl/camera.h"
+#include "shader_m.h"
+#include "camera.h"
 
 #include <iostream>
 
