de5380f8ddcea421983af4952cfc00314781664f
datacrystals <tliao@carboncopies.org>
2022-11-08 16:03:51 -0800

    Debug Broken Window For MacOS

--- Source/Core/Renderer/ERS_CLASS_RendererManager/RendererManager.cpp
+++ Source/Core/Renderer/ERS_CLASS_RendererManager/RendererManager.cpp
@@ -141,6 +141,11 @@ void RendererManager::InitializeGLFW() {
 
     // Initialize GLFW
     glfwInit();
+
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
