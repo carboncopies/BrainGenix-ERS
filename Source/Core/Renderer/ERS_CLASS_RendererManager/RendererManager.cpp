3bd3ac437342a98aec858540f5e6ae18b22d8ebb
datacrystals <tliao@carboncopies.org>
2022-11-08 16:14:04 -0800

    Debug OpenGL Issues On Mac

--- Source/Core/Renderer/ERS_CLASS_RendererManager/RendererManager.cpp
+++ Source/Core/Renderer/ERS_CLASS_RendererManager/RendererManager.cpp
@@ -142,14 +142,13 @@ void RendererManager::InitializeGLFW() {
     // Initialize GLFW
     glfwInit();
 
-    #ifdef __APPLE__
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    #endif
-
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
     //glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 
