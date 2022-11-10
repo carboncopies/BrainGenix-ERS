31642a46c4929be6563b3a6e16caa245490ac269
datacrystals <tliao@carboncopies.org>
2022-11-09 17:26:48 -0800

    Update Error System

--- Source/Internal/GetExecutablePath/ERS_GetExecutablePath.cpp
+++ Source/Internal/GetExecutablePath/ERS_GetExecutablePath.cpp
@@ -9,6 +9,21 @@ namespace BrainGenix {
 namespace ERS {
 namespace Module {
 
+#ifdef __APPLE__
+    #include <mach-o/dyld.h>
+    #include <limits.h>
+
+    int main(int argc, char **argv)
+    {
+    char buf [PATH_MAX];
+    uint32_t bufsize = PATH_MAX;
+    if(!_NSGetExecutablePath(buf, &bufsize))
+        puts(buf);
+    return 0;
+    }
+#endif
+
+
 
 
 std::string GetExecutablePath() {
