ff3d950caf0e8e2d2f48c64a9e175c5a0434213a
datacrystals <tliao@carboncopies.org>
2022-11-10 00:08:51 +0000

    Fix Test Projects

--- Source/Internal/GetExecutablePath/ERS_GetExecutablePath.cpp
+++ Source/Internal/GetExecutablePath/ERS_GetExecutablePath.cpp
@@ -2,7 +2,7 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
-#include <ERS_LocalConfigLoader.h>
+#include <ERS_GetExecutablePath.h>
 
 
 namespace BrainGenix {
