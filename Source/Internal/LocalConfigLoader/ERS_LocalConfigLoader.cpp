790948ea38b5deccec945511cb6f696273a64918
datacrystals <tliao@carboncopies.org>
2022-11-09 18:23:42 -0800

    Add PlatformFolders Lib To Get Current Path

--- Source/Internal/LocalConfigLoader/ERS_LocalConfigLoader.cpp
+++ Source/Internal/LocalConfigLoader/ERS_LocalConfigLoader.cpp
@@ -10,60 +10,78 @@ namespace ERS {
 namespace Module {
 
 
+bool TryWorkingPath(std::string Path) {
 
-bool LoadLocalConfiguration(std::string Path, YAML::Node& Configuration) {
+    // Enter Folder
+    chdir(Path.c_str());
 
-    // Attempt Load
+    // Check If Good
     try {
-        Configuration = YAML::LoadFile("Config.yaml");
+        YAML::LoadFile("Config.yaml")
         return true;
-    } catch (YAML::BadFile&) {
+    } catch {YAML::BadFile&} {
+        return false;
+    }
 
-        // Try And Change Into Executable Path And See There - Otherwise Give Up
-        try {
-            std::string ValidExecuatblePath = GetExecutableDirectory();
-            if (ValidExecuatblePath == "" || ValidExecuatblePath == "Unable To Get Binary Path") {
-                throw YAML::BadFile("Unable to change path");
-            }
-            chdir(ValidExecuatblePath.c_str());
-            Configuration = YAML::LoadFile("Config.yaml");
-        } catch (YAML::BadFile&) {
+}
 
-            // Build Error Message
-            std::string Output;
-            Output += "Failed to Load File 'Config.yaml' Is ERS Being Run In The Right Working Directory?\n";
-            Output += "\n";
-            Output += "Current Executable Path Is: '" + GetExecutablePath() + "'\n";
-            Output += "Current Executable Directory Is: '" + GetExecutableDirectory() + "'\n";
-            Output += "Current Directory Is: '" + std::string(ghc::filesystem::current_path()) + "'\n";
-            Output += "\n";
-            Output += "-- Current Working Directory's Files --\n";
-            for (const auto & entry : ghc::filesystem::directory_iterator(".")) {
-                Output += std::string(entry.path()) + " ";
-            }
-            Output += "\n-- End Of File List --\n";
-            Output += "Fatal Error - Exiting!\n";
+void ChuckErrorAndGiveUp() {
 
-            // Dump Error To Console
-            std::cout<<Output;
+    // Build Error Message
+    std::string Output;
+    Output += "Failed to Load File 'Config.yaml' Is ERS Being Run In The Right Working Directory?\n";
+    Output += "\n";
+    Output += "Current Executable Path Is: '" + GetExecutablePath() + "'\n";
+    Output += "Current Executable Directory Is: '" + GetExecutableDirectory() + "'\n";
+    Output += "Current Directory Is: '" + std::string(ghc::filesystem::current_path()) + "'\n";
+    Output += "\n";
+    Output += "-- Current Working Directory's Files --\n";
+    for (const auto & entry : ghc::filesystem::directory_iterator(".")) {
+        Output += std::string(entry.path()) + " ";
+    }
+    Output += "\n-- End Of File List --\n";
+    Output += "Fatal Error - Exiting!\n";
 
-            // If OSX - Change Dir To Log Dump Folder
+    // Dump Error To Console
+    std::cout<<Output;
+
+    // If OSX - Change Dir To Log Dump Folder
 #if __APPLE__
-            chdir(std::string(sago::getConfigHome()).c_str());
-            mkdir("com.Carboncopies.BrainGenix.ERS", 0777);
-            chdir("com.Carboncopies.BrainGenix.ERS");
+    chdir(std::string(sago::getDesktopFolder()).c_str());
+    mkdir("com.Carboncopies.BrainGenix.ERS", 0777);
+    chdir("com.Carboncopies.BrainGenix.ERS");
 #endif
 
 
-            // Dump Error To File For Reading Even If No Console Is Present
-            std::ofstream FileStream("Log.txt");
-            FileStream << Output;
-            FileStream.close();
+    // Dump Error To File For Reading Even If No Console Is Present
+    std::ofstream FileStream("Log.txt");
+    FileStream << Output;
+    FileStream.close();
 
-            return false;
-    
-        }
+}
+
+
+bool LoadLocalConfiguration(std::string Path, YAML::Node& Configuration) {
+
+    // Try a bunch of differet paths, chuck an error at the end and give up
+    if (TryWorkingPath(".")) {
+        Configuration = YAML::LoadFile("Config.yaml");
+        return true;
+    } else if (TryWorkingPath("../Resources")) {
+        Configuration = YAML::LoadFile("Config.yaml");
+        return true;
+    } else if (TryWorkingPath(GetExecutableDirectory())) {
+        Configuration = YAML::LoadFile("Config.yaml");
+        return true;
+    } else if (TryWorkingPath("../Resources")) {
+        Configuration = YAML::LoadFile("Config.yaml");
+        return true;
+    } else {
+        ChuckErrorAndGiveUp();
+        return false;
     }
+
+
 
 }
 
