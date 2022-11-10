379cbf1b4549e2bb20e8748c8ee8fd168510a09a
datacrystals <tliao@carboncopies.org>
2022-11-09 17:03:58 -0800

    Update Terminal Output

--- CMake/Scripts/DarwinBundle/DarwinBundle.cmake
+++ CMake/Scripts/DarwinBundle/DarwinBundle.cmake
@@ -14,35 +14,35 @@ set(MACOSX_DEPLOYMENT_TARGET "macos")
 
 # Setup Bundle
 add_executable(${PROJECT_NAME} MACOSX_BUNDLE ${MAIN_SOURCES})            
-# set_target_properties(${PROJECT_NAME} PROPERTIES
-#     BUNDLE True
-#     MACOSX_BUNDLE_GUI_IDENTIFIER com.Carboncopies.BrainGenix.ERS
-#     MACOSX_BUNDLE_BUNDLE_NAME ${PROJECT_NAME}
-#     MACOSX_BUNDLE_BUNDLE_VERSION "0.1"      
-#     MACOSX_BUNDLE_SHORT_VERSION_STRING "0.1"
-#     MACOSX_BUNDLE_INFO_PLIST ${CMAKE_SCRIPTS_DIR}/DarwinBundle/Info.plist.in
-# )
+set_target_properties(${PROJECT_NAME} PROPERTIES
+    BUNDLE True
+    MACOSX_BUNDLE_GUI_IDENTIFIER com.Carboncopies.BrainGenix.ERS
+    MACOSX_BUNDLE_BUNDLE_NAME ${PROJECT_NAME}
+    MACOSX_BUNDLE_BUNDLE_VERSION "0.1"      
+    MACOSX_BUNDLE_SHORT_VERSION_STRING "0.1"
+    MACOSX_BUNDLE_INFO_PLIST ${CMAKE_SCRIPTS_DIR}/DarwinBundle/Info.plist.in
+)
 
-# set(EXECUTABLE_TYPE "OSX_Bundle")
+set(EXECUTABLE_TYPE "OSX_Bundle")
 
 set(APP_NAME ${PROJECT_NAME})
 
-# Destination paths below are relative to ${CMAKE_INSTALL_PREFIX}
-install(TARGETS ${APP_NAME}
-    BUNDLE DESTINATION . COMPONENT Runtime
-    RUNTIME DESTINATION bin COMPONENT Runtime
-    )
+# # Destination paths below are relative to ${CMAKE_INSTALL_PREFIX}
+# install(TARGETS ${APP_NAME}
+#     BUNDLE DESTINATION . COMPONENT Runtime
+#     RUNTIME DESTINATION bin COMPONENT Runtime
+#     )
 
-# Note Mac specific extension .app
-set(APPS "\${CMAKE_INSTALL_PREFIX}/${APP_NAME}.app")
+# # Note Mac specific extension .app
+# set(APPS "\${CMAKE_INSTALL_PREFIX}/${APP_NAME}.app")
 
-# Directories to look for dependencies
-set(DIRS ${CMAKE_BINARY_DIR})
+# # Directories to look for dependencies
+# set(DIRS ${CMAKE_BINARY_DIR})
 
-install(CODE "include(BundleUtilities)
-    fixup_bundle(\"${APPS}\" \"\" \"${DIRS}\")")
+# install(CODE "include(BundleUtilities)
+#     fixup_bundle(\"${APPS}\" \"\" \"${DIRS}\")")
 
-set(CPACK_GENERATOR "DRAGNDROP")
-include(CPack)
+# set(CPACK_GENERATOR "DRAGNDROP")
+# include(CPack)
 
 ERSBuildLogger(${BoldCyan} "Finished Configuring Darwin Bundle System")
