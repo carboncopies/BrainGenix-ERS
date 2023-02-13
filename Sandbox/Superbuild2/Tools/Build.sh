8d8c0e63eb43d8c0d8f12af9948ec86a9a4cd498
datacrystals <tliao@carboncopies.org>
2023-02-13 01:38:13 +0000

    Create Superbuild Sandbox2

--- Sandbox/Superbuild2/Tools/Build.sh
+++ Sandbox/Superbuild2/Tools/Build.sh
@@ -1,27 +1,53 @@
-# Exit Tools Dir
-echo "Exiting Tools Dir"
+# Get Into Root Repo Dir
+echo "[ERS BUILD HELPER] - Entering Root Repo Directory"
 cd ..
 
-# Create Build Dir
-#echo "Creating Build Directory"
-#mkdir -p Build
 
-# Enter Build Dir
-echo "Entering Build Directory"
-cd Build
+# Delete Binary If Exists 
+echo "[ERS BUILD HELPER] - Checking If Binary Exists"
+if [ -f "Binaries/BrainGenix-ERS" ]
+then
+    echo "[ERS BUILD HELPER] - Removing Last Executable"
+    rm Binaries/BrainGenix-ERS
+else
+    echo "[ERS BUILD HELPER] - No Binaries To Clean"
+fi
 
-# Make Only BrainGenix-ERS
-#echo "Running Cmake"
-#cmake ..
 
-# Build
-echo "Building BrainGenix-ERS"
-cmake --build . --target BGImageTest -- -j 24
+# Check If Configuration Needs To Be Run
+echo "[ERS BUILD HELPER] - Checking If Build Directory Already Exists"
+if [ -d "Build" ]
+then 
+    echo "[ERS BUILD HELPER] - Build Directory Already Exists, Skipping Generation"
+    cd Build
+else 
+    # Create Build Dir
+    echo "[ERS BUILD HELPER] - Creating Build Directory"
+    mkdir -p Build
 
+    # Enter Build Dir
+    echo "[ERS BUILD HELPER] - Entering Build Directory"
+    cd Build
+
+    # Make Only BrainGenix-ERS
+    echo "[ERS BUILD HELPER] - Configuring Build Files"
+    cmake ..
+fi
+
+
+# Build Files
+echo "[ERS BUILD HELPER] - Building, Please Wait. This may take some time"
+cmake --build . -j $1
 
 
 # Run Program
-echo "Running Program"
 cd ../Binaries
-./BGImageTest
+echo "[ERS BUILD HELPER] - Checking If Binary Exists"
+if [ -f "BrainGenix-ERS" ]
+then
+    echo "[ERS BUILD HELPER] - Running Program"
+    ./BrainGenix-ERS
+else
+    echo "[ERS BUILD HELPER] - Build Failed! Exiting"
+fi
 cd ..
