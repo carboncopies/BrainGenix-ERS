c0b03805c52ac2b769cd57fba5d9ce4c5af39a30
datacrystals <tliao@carboncopies.org>
2023-02-13 04:51:52 +0000

    Migrate Assimp To Superbuild

--- CMake/SuperBuilds/Assimp.cmake
+++ CMake/SuperBuilds/Assimp.cmake
@@ -26,12 +26,13 @@ ExternalProject_Add (ThirdParty_${TARGET_NAME}
     INSTALL_DIR = ${CMAKE_CURRENT_BINARY_DIR}/ThirdParty/${TARGET_NAME}/Build
     LOG_DIR     = ${CMAKE_CURRENT_BINARY_DIR}/ThirdParty/${TARGET_NAME}/Logs
 
-    INSTALL_COMMAND ""
+    # INSTALL_COMMAND ""
 
     CMAKE_ARGS -DASSIMP_BUILD_ASSIMP_TOOLS:BOOL=OFF
                -DASSIMP_BUILD_ZLIB:BOOL=ON
                -DASSIMP_BUILD_SAMPLES:BOOL=OFF
                -DASSIMP_BUILD_TESTS:BOOL=OFF
+               -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/ThirdParty/${TARGET_NAME}/Install
 
 
 )
@@ -43,5 +44,5 @@ ExternalProject_Add (ThirdParty_${TARGET_NAME}
 
 
 list (APPEND EXTRA_CMAKE_ARGS
-  -Dassimp_DIR=${CMAKE_CURRENT_BINARY_DIR}/ThirdParty/${TARGET_NAME}/Build/generated
+  -Dassimp_DIR=${CMAKE_CURRENT_BINARY_DIR}/ThirdParty/${TARGET_NAME}/Install/lib/cmake/assimp-5.2/
 )
