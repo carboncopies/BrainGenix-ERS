e0c3dc2596b68abbf9281bdf00261e2a3c81e857
datacrystals <tliao@carboncopies.org>
2023-02-13 00:46:06 +0000

    Update CMake For Superbuild

--- CMake/SuperBuildMaster.cmake
+++ CMake/SuperBuildMaster.cmake
@@ -1,9 +1,13 @@
+# Superbuild Main Runner
+
+# Setup System
 include (ExternalProject)
 
 set_property (DIRECTORY PROPERTY EP_BASE Dependencies)
 
 set (DEPENDENCIES)
 set (EXTRA_CMAKE_ARGS)
+
 
 # Use static linking to avoid issues with system-wide installations of Boost.
 list (APPEND DEPENDENCIES ep_boost)
