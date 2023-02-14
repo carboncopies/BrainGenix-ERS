f06800e715d8b3f3b908b81b92af2a86b79eb725
datacrystals <tliao@carboncopies.org>
2023-02-14 04:42:39 +0000

    Refactor ThirdParty Directory

--- CMake/SuperBuilds/Assimp.cmake
+++ CMake/SuperBuilds/Assimp.cmake
@@ -1,39 +1,38 @@
 # ERS Superbuild
 
 
-# Configuration Parameters
-set(TARGET_NAME Assimp)
-set(LIB_SOURCE_DIR ${LIB_DIR}/SuperBuild/assimp)
+# First part of two, build the library if we're currently doing so
+if (USE_SUPERBUILD)
 
-# Add To Dependencies
-list (APPEND DEPENDENCIES ThirdParty_${TARGET_NAME})
+    # Configuration Parameters
+    set(TARGET_NAME Assimp)
+    set(LIB_SOURCE_DIR ${LIB_DIR}/SuperBuild/assimp)
 
-# Create External Project
-message(STATUS "Configuring Library ${TARGET_NAME}")
-ExternalProject_Add (ThirdParty_${TARGET_NAME}
+    # Add To Dependencies
+    list (APPEND DEPENDENCIES ThirdParty_${TARGET_NAME})
 
-    SOURCE_DIR ${LIB_SOURCE_DIR}
-    # PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ThirdParty/${TARGET_NAME}
-    
-    # STAMP_DIR   = ${CMAKE_CURRENT_BINARY_DIR}/ThirdParty/${TARGET_NAME}/Stamp
-    # BINARY_DIR  = ${CMAKE_CURRENT_BINARY_DIR}/ThirdParty/${TARGET_NAME}/Build
-    # INSTALL_DIR = ${CMAKE_CURRENT_BINARY_DIR}/ThirdParty/${TARGET_NAME}/Install
-    # LOG_DIR     = ${CMAKE_CURRENT_BINARY_DIR}/ThirdParty/${TARGET_NAME}/Logs
+    # Create External Project
+    message(STATUS "Configuring Library ${TARGET_NAME}")
+    ExternalProject_Add (ThirdParty_${TARGET_NAME}
+        SOURCE_DIR ${LIB_SOURCE_DIR}
+        CMAKE_ARGS -DASSIMP_BUILD_ASSIMP_TOOLS:BOOL=OFF
+                  -DASSIMP_BUILD_ZLIB:BOOL=ON
+                  -DASSIMP_BUILD_SAMPLES:BOOL=OFF
+                  -DASSIMP_BUILD_TESTS:BOOL=OFF
+                  -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/Dependencies/Install/ThirdParty_${TARGET_NAME}/
+    )
+    message(STATUS "Finished Configuring Library ${TARGET_NAME}")
 
-    # INSTALL_COMMAND ""
+    message(STATUS "Adding CMake Path Argument")
+    list (APPEND EXTRA_CMAKE_ARGS
+        -Dassimp_DIR=${CMAKE_CURRENT_BINARY_DIR}/Dependencies/Install/ThirdParty_${TARGET_NAME}/lib/cmake/assimp-5.2/
+    )
+    message(STATUS "Added CMake Path Argument")
 
-    CMAKE_ARGS -DASSIMP_BUILD_ASSIMP_TOOLS:BOOL=OFF
-               -DASSIMP_BUILD_ZLIB:BOOL=ON
-               -DASSIMP_BUILD_SAMPLES:BOOL=OFF
-               -DASSIMP_BUILD_TESTS:BOOL=OFF
-               -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/Dependencies/Install/ThirdParty_${TARGET_NAME}/
+# Run second part of build, create target/find library we built in the above superbuild target
+else()
 
+    message(STATUS "Assimp Configuration Install Directory: ${assimp_DIR}")
+    find_package(assimp REQUIRED)
 
-)
-message(STATUS "Finished Configuring Library ${TARGET_NAME}")
-
-message(STATUS "Adding CMake Path Argument")
-list (APPEND EXTRA_CMAKE_ARGS
-  -Dassimp_DIR=${CMAKE_CURRENT_BINARY_DIR}/Dependencies/Install/ThirdParty_${TARGET_NAME}/lib/cmake/assimp-5.2/
-)
-message(STATUS "Added CMake Path Argument")
+endif()
