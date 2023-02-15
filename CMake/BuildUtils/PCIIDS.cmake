06866c5b85ed85673e0ca8e9a9a55800b1d8f959
datacrystals <tliao@carboncopies.org>
2023-02-15 21:44:26 +0000

    Migrate Non-Superbuild Deps To Dir

--- CMake/BuildUtils/PCIIDS.cmake
+++ CMake/BuildUtils/PCIIDS.cmake
@@ -3,8 +3,8 @@
 ##########################
 
 ERSBuildLogger(${Green} "Configuring PCIIDS Library")
-set(INFOWARE_PCI_IDS_PATH ${LIB_DIR}/pciids/pci.ids)
-include_directories(${LIB_DIR}/infoware/include)
-add_subdirectory(${LIB_DIR}/infoware)
+set(INFOWARE_PCI_IDS_PATH ${LIB_DIR}/NonSuperBuild/pciids/pci.ids)
+include_directories(${LIB_DIR}/NonSuperBuild/infoware/include)
+add_subdirectory(${LIB_DIR}/NonSuperBuild/infoware)
 disable_target_warnings(infoware)
 ERSBuildLogger(${BoldGreen} "Finished Configuring PCIIDS Library")
