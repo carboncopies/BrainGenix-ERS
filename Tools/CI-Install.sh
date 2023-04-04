1770561d96ea8ba90213ad87de3e7ea3909760ed
Thomas Liao <tliao@carboncopies.org>
2023-04-04 00:14:54 +0000

    Update Tools/CI-Install.sh

--- Tools/CI-Install.sh
+++ Tools/CI-Install.sh
@@ -1,19 +1,6 @@
 # Apt Update
 apt update
 
-# Install Compiler Tools
-apt install git wget cmake ninja-build g++ python3 python3-pip -y
+apt install git wget cmake ninja-build g++ python3 python3-pip binutils-dev libunwind-dev libdwarf-dev libdw-dev libx11-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libssl-dev libusb-1.0-0-dev glslang-dev glslang-tools libeigen3-dev libudev-dev libgl1-mesa-dev -y
 
-# Backward Deps
-apt install binutils-dev libunwind-dev libdwarf-dev libdw-dev -y
-
-# Install GLFW Deps
-apt install libx11-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev -y
-
-# Install MySQL Connector Deps
-apt install libssl-dev -y
-
-# Install Monado Deps
 pip3 install numpy
-apt install libusb-1.0-0-dev glslang-dev glslang-tools libeigen3-dev libudev-dev libgl1-mesa-dev -y
-
