1ac46e98131593d52fae7ee0f29e8349131d4f59
tliao <tliao@carboncopies.org>
2023-04-05 13:01:41 -0700

    Update CI names

---
+++ CI/Images/BuildLinuxAlpine3.16/PushImage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker tag ers_build_linux_alpine_316 10.1.3.11:5000/ers_build_linux_alpine_316
+docker push 10.1.3.11:5000/ers_build_linux_alpine_316
---
+++ CI/Images/BuildLinuxAlpine3.17/BuildImage.sh
@@ -0,0 +1,2 @@
+docker pull alpine:3.17
+docker build -t ers_build_linux_alpine_317 .
