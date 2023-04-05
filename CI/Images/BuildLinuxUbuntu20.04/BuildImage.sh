6757a361583c74659e208a670a0a508bc1dfa9ec
tliao <tliao@carboncopies.org>
2023-04-04 20:30:17 -0700

    Update CI

---
+++ CI/Images/BuildLinuxUbuntu20.04/BuildImage.sh
@@ -0,0 +1,2 @@
+docker pull ubuntu:20.04
+docker build -t ers_build_linux_ubuntu_2004 .
---
+++ CI/Images/BuildLinuxUbuntu20.04/PushImage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker tag ers_build_linux_ubuntu_2004 10.1.3.11:5000/ers_build_linux_ubuntu_2004
+docker push 10.1.3.11:5000/ers_build_linux_ubuntu_2004
