95c294413e08e2871e6101923f062c881b3ca8fb
Thomas Liao <tliao@carboncopies.org>
2023-04-13 21:18:27 +0000

    Fix OSX M1 Build

--- Tools/Darwin-Build.sh
+++ Tools/Darwin-Build.sh
@@ -81,7 +81,7 @@ fi
 
 # Build Files
 echo "[ERS BUILD HELPER] - Building, Please Wait. This may take some time"
-cmake --build . --target BrainGenix-ERS --parallel $1
+cmake --build . --target ERS --parallel $1
 
 
 # Run Program
