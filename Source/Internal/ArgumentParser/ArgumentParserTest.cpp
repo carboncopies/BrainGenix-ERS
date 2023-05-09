b33a197862a97987dc983e897cf54502959fc452
Thomas Liao <tliao@carboncopies.org>
2023-05-09 11:21:38 -0700

    Get Tests Project Structure Setup

---
+++ Source/Internal/ArgumentParser/ArgumentParserTest.cpp
@@ -0,0 +1,7 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#include <ArgumentParserTest.h>
+
+// Your gtest code here...
---
+++ Source/Internal/ArgumentParser/ArgumentParserTest.h
@@ -0,0 +1,14 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <gtest/gtest.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <ArgumentParser.h>
+
