ed37410c46b218f538f41d775fbd969490c9c926
yasufumi <52214705+yasufumi-nakata@users.noreply.github.com>
2026-05-16 02:46:36 +0900

    Improve cooperative shutdown for worker threads

--- Source/Internal/ModelImporter/ModelImporter.cpp
+++ Source/Internal/ModelImporter/ModelImporter.cpp
@@ -15,6 +15,7 @@ ERS_ModelImporter::ERS_ModelImporter(ERS_STRUCT_SystemUtils* SystemUtils) {
     ModelLoader_ = std::make_unique<BrainGenix::ERS::Module::ExternalModelLoader>(SystemUtils_);
 
     SystemUtils_->Logger_->Log("Starting Asset Import Thread", 4);
+    ImportThreadActive_ = true;
     ImportThread_ = std::thread(&ERS_ModelImporter::ImportThread, this);
     SystemUtils_->Logger_->Log("Started Asset Import Thread", 3);
 
@@ -32,37 +33,26 @@ ERS_ModelImporter::~ERS_ModelImporter() {
     SystemUtils_->Logger_->Log("Asset Importer Backend Destructor Called", 6);
 
     SystemUtils_->Logger_->Log("Sending Halt Signal To Asset Import Thread", 4);
-    BlockThread_.lock();
-    StopThread_ = true;
-    BlockThread_.unlock();
+    {
+        std::lock_guard<std::mutex> Lock(LockAssetImportQueue_);
+        StopThread_ = true;
+    }
+    WorkAvailableCondition_.notify_all();
+
+    {
+        std::unique_lock<std::mutex> Lock(LockAssetImportQueue_);
+        bool ImporterStopped = ImportThreadShutdownCondition_.wait_for(Lock, std::chrono::seconds(2), [this]() {
+            return !ImportThreadActive_;
+        });
+        if (!ImporterStopped) {
+            SystemUtils_->Logger_->Log("The asset import thread did not stop within 2 seconds. ERS will keep waiting for a clean shutdown instead of forcing termination.", 9);
+        }
+    }
 
     SystemUtils_->Logger_->Log("Joining Asset Import Thread", 3);
-	//if (ImportThread_.joinable()) {
-		//TerminateThread tt(std::thread(ImportThread_));
-	//}
-	
-	if (ImportThread_.joinable()) {
-		
-		// Wait for the thread to finish for up to 10 seconds
-		for (int i = 0; i < 100 && ImportThread_.joinable(); ++i) {
-			if (ImportThread_.joinable()) 
-				ImportThread_.join();
-			else
-				break;
-			
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		}
-
-		//If the thread is still running, terminate it
-		if (ImportThread_.joinable()) {
-			ImportThread_.detach();
-			
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-			
-		if (ImportThread_.joinable()) 
-			std::terminate();
-		}
-	}	
+    if (ImportThread_.joinable()) {
+        ImportThread_.join();
+    }
 
 }
 
@@ -73,50 +63,50 @@ void ERS_ModelImporter::ImportThread() {
     SetThreadName("ImportManager");
 
     while (true) {
+        std::string AssetPath;
+        bool FlipTextures = false;
 
-        // Check Control Variables
-        BlockThread_.lock();
-        if (StopThread_) {
-            BlockThread_.unlock();
-            break;
-        }
-        BlockThread_.unlock();
+        {
+            std::unique_lock<std::mutex> Lock(LockAssetImportQueue_);
+            WorkAvailableCondition_.wait(Lock, [this]() {
+                return StopThread_ || !AssetImportQueue_.empty();
+            });
 
-        // Check Queue, Import As Needed, Empty Processing Items That Are Done
-        LockAssetImportQueue_.lock();
-        if (AssetImportQueue_.size() > 0) {
+            if (StopThread_) {
+                break;
+            }
 
             HasJobFinished_ = false;
-            std::string AssetPath = AssetImportQueue_[0];
-            bool FlipTextures = AssetQueueFlipTextures_[0];
+            AssetPath = AssetImportQueue_[0];
+            FlipTextures = AssetQueueFlipTextures_[0];
             AssetImportQueue_.erase(AssetImportQueue_.begin());
             AssetQueueFlipTextures_.erase(AssetQueueFlipTextures_.begin());
+        }
 
-            LockAssetImportQueue_.unlock();
+        ERS_STRUCT_Model Model;
+        ERS_STRUCT_ModelWriterData ModelData;
+        ModelData.Model = &Model;
+        ModelLoader_->LoadModel(AssetPath, ModelData);
+        ModelWriter_->WriteModel(ModelData, FlipTextures);
 
-
-                ERS_STRUCT_Model Model;
-                ERS_STRUCT_ModelWriterData ModelData;
-                ModelData.Model = &Model;
-                ModelLoader_->LoadModel(AssetPath, ModelData);
-                ModelWriter_->WriteModel(ModelData, FlipTextures);
-
-
-
-            LockAssetImportQueue_.lock();
+        {
+            std::lock_guard<std::mutex> Lock(LockAssetImportQueue_);
             TotalItemsProcessed_++;
-            LockAssetImportQueue_.unlock();
-        } else {
-            
-            HasJobFinished_ = true;
-            TotalItemsToImport_ = 0;
-            TotalItemsProcessed_ = 0;
-            LockAssetImportQueue_.unlock();
-
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            if (AssetImportQueue_.empty()) {
+                HasJobFinished_ = true;
+                TotalItemsToImport_ = 0;
+                TotalItemsProcessed_ = 0;
+            }
         }
 
     }
+
+    {
+        std::lock_guard<std::mutex> Lock(LockAssetImportQueue_);
+        HasJobFinished_ = true;
+        ImportThreadActive_ = false;
+    }
+    ImportThreadShutdownCondition_.notify_all();
 
 }
 
@@ -124,19 +114,20 @@ void ERS_ModelImporter::ImportThread() {
 void ERS_ModelImporter::AddToImportQueue(std::vector<std::string> AssetPaths, std::vector<bool> FlipTextures) {
 
     SystemUtils_->Logger_->Log("Appending Assets To Asset Import Queue", 5);
-    LockAssetImportQueue_.lock();
-    HasJobFinished_ = false;
-    for (int i = 0; (long)i < (long)AssetPaths.size(); i++) {
+    {
+        std::lock_guard<std::mutex> Lock(LockAssetImportQueue_);
+        HasJobFinished_ = false;
+        for (int i = 0; (long)i < (long)AssetPaths.size(); i++) {
 
-        std::string LogStr = std::string("Appending Asset: '") + AssetPaths[i] + std::string("' To Import Queue");
-        SystemUtils_->Logger_->Log(LogStr.c_str(), 4);
-        AssetImportQueue_.push_back(AssetPaths[i]);
-        AssetQueueFlipTextures_.push_back(FlipTextures[i]);
-        TotalItemsToImport_ += 1;
+            std::string LogStr = std::string("Appending Asset: '") + AssetPaths[i] + std::string("' To Import Queue");
+            SystemUtils_->Logger_->Log(LogStr.c_str(), 4);
+            AssetImportQueue_.push_back(AssetPaths[i]);
+            AssetQueueFlipTextures_.push_back(FlipTextures[i]);
+            TotalItemsToImport_ += 1;
 
+        }
     }
-
-    LockAssetImportQueue_.unlock();
+    WorkAvailableCondition_.notify_one();
 
 }
 long ERS_ModelImporter::GetTotalItemsToImport() {
@@ -146,8 +137,6 @@ long ERS_ModelImporter::GetTotalItemsImported() {
     return TotalItemsProcessed_;
 }
 bool ERS_ModelImporter::HasJobFinished() {
-    LockAssetImportQueue_.lock();
-    bool Out = HasJobFinished_;
-    LockAssetImportQueue_.unlock();
-    return Out;
+    std::lock_guard<std::mutex> Lock(LockAssetImportQueue_);
+    return HasJobFinished_;
 }
