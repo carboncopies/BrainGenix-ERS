303127ce2d787f5426ef715da22a3781265c173d
yasufumi <52214705+yasufumi-nakata@users.noreply.github.com>
2026-05-16 06:03:46 +0900

    Document audio source include analyzer exception

--- Source/Core/Structures/ERS_STRUCT_AudioSource/AudioSource.cpp
+++ Source/Core/Structures/ERS_STRUCT_AudioSource/AudioSource.cpp
@@ -2,4 +2,5 @@
 // This file is part of the BrainGenix-ERS Environment Rendering System //
 //======================================================================//
 
+// cppcheck-suppress missingIncludeSystem
 #include <AudioSource.h>
