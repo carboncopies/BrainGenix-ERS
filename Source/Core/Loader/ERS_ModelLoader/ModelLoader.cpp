547af851eeafb78c27dc414f7a1f14ea35391e53
yasufumi <52214705+yasufumi-nakata@users.noreply.github.com>
2026-05-16 09:11:03 +0900

    Ensure at least one model loader worker

--- Source/Core/Loader/ERS_ModelLoader/ModelLoader.cpp
+++ Source/Core/Loader/ERS_ModelLoader/ModelLoader.cpp
@@ -23,8 +23,12 @@ ERS_CLASS_ModelLoader::ERS_CLASS_ModelLoader(ERS_STRUCT_SystemUtils* SystemUtils
 
     if (MaxModelLoadingThreads == -1) {
         SystemUtils_->Logger_->Log("Identifying Number Of CPU Cores", 4);
-        MaxModelLoadingThreads = std::thread::hardware_concurrency();
+        unsigned int DetectedHardwareThreads = std::thread::hardware_concurrency();
+        MaxModelLoadingThreads = DetectedHardwareThreads == 0 ? 1 : (int)DetectedHardwareThreads;
         SystemUtils_->Logger_->Log(std::string(std::string("Identified ") + std::to_string(MaxModelLoadingThreads) + std::string(" Cores")).c_str(), 4);
+    }
+    if (MaxModelLoadingThreads < 1) {
+        MaxModelLoadingThreads = 1;
     }
     SystemUtils_->Logger_->Log(std::string(std::string("Creating ") + std::to_string(MaxModelLoadingThreads) + std::string("Model Loading Threads")).c_str(), 4);
     for (int i = 0; i < MaxModelLoadingThreads; i++) {
@@ -644,4 +648,3 @@ void ERS_CLASS_ModelLoader::IdentifyMeshTextures(aiMaterial* Mat, ERS_STRUCT_Mes
     }
 
 }
-
